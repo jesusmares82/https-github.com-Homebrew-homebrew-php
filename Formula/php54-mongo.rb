7de74d2a7d26c9f28eea5945c52aad68db71a92f
Eashwar Ranganathan <crystal.dome@gmail.com>
2013-01-31 15:01:51 -0800

    php-mongo 1.3.4

--- Formula/php54-mongo.rb
+++ Formula/php54-mongo.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Mongo < AbstractPhp54Extension
   init
   homepage 'http://pecl.php.net/package/mongo'
-  url 'http://pecl.php.net/get/mongo-1.3.2.tgz'
-  sha1 '0c9b780271c47eba6c04303383bdf4918320a835'
+  url 'http://pecl.php.net/get/mongo-1.3.4.tgz'
+  sha1 'f9f92d17318c2aabb2e36f0e61677e3a46a182c4'
   head 'https://github.com/mongodb/mongo-php-driver.git'
 
   def install
