cef3be8f433676067720eba998b8234c91f2dc7a
Eashwar Ranganathan <crystal.dome@gmail.com>
2013-01-31 15:10:13 -0800

    php-twig 1.12.1

--- Formula/php54-twig.rb
+++ Formula/php54-twig.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Twig < AbstractPhp54Extension
   init
   homepage 'http://twig.sensiolabs.org/'
-  url 'https://github.com/fabpot/Twig/tarball/v1.11.1'
-  sha1 'dfac84344d7d70141e5bd3296a8c9572c1bb4086'
+  url 'https://github.com/fabpot/Twig.git', :using => :git, :tag => 'v1.12.1'
+  version '1.12.1'
   head 'https://github.com/fabpot/Twig.git', :using => :git
 
   def install
