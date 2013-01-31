d19d2fe1ee34083f21cc1f5172c08cceb46241ee
Eashwar Ranganathan <crystal.dome@gmail.com>
2013-01-31 14:49:56 -0800

    php-memcache 2.2.7

--- Formula/php53-memcache.rb
+++ Formula/php53-memcache.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php53Memcache < AbstractPhp53Extension
   init
   homepage 'http://pecl.php.net/package/memcache'
-  url 'http://pecl.php.net/get/memcache-2.2.6.tgz'
-  sha1 'be0b12fa09ed127dc35c0da29a576a9112be1bde'
+  url 'http://pecl.php.net/get/memcache-2.2.7.tgz'
+  sha1 '15235e0f85b54c429bdbf8f6b529243580e504a7'
   head 'https://svn.php.net/repository/pecl/memcache/trunk/', :using => :svn
 
   def install
