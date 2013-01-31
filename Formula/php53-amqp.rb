9ed54f5f621f59f7d61213ccd5830d22aea51f21
Eashwar Ranganathan <crystal.dome@gmail.com>
2013-01-31 14:36:11 -0800

    php-amqp 1.0.9

--- Formula/php53-amqp.rb
+++ Formula/php53-amqp.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php53Amqp < AbstractPhp53Extension
   init
   homepage 'http://pecl.php.net/package/amqp'
-  url 'http://pecl.php.net/get/amqp-1.0.3.tgz'
-  sha1 '90a92f9752fde0cb8e4c6525aabfa6e678bbea23'
+  url 'http://pecl.php.net/get/amqp-1.0.9.tgz'
+  sha1 '020ae71214fa2f57aeb70f0ec139ffe43a477ded'
   head 'http://svn.php.net/repository/pecl/amqp/trunk/', :using => :svn
 
   depends_on 'rabbitmq-c'
