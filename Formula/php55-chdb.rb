75be7125a5f78dc32029c9e132a841573f5448d1
Stig Bakken <stig@stigbakken.com>
2013-02-12 01:49:15 +0100

    add chdb extension

---
+++ Formula/php55-chdb.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php55Chdb < AbstractPhp55Extension
+  init
+  homepage 'http://pecl.php.net/package/chdb'
+  url 'http://pecl.php.net/get/chdb-1.0.2.tgz'
+  sha1 '476a6e84dd97c1ec24538edef9e3ba145256346e'
+  head 'https://github.com/lcastelli/chdb', :using => :git
+
+  depends_on 'libcmph'
+
+  def install
+    Dir.chdir "chdb-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/chdb.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
