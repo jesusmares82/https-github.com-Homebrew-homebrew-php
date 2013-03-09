737bb89d71f0517f3c48254ed1b7f95767a331e5
Shane O'Grady <shane.ogrady@gmail.com>
2013-03-09 12:34:38 +0000

    Add formula for PhpDocumentor 2

---
+++ Formula/phpdocumentor.rb
@@ -0,0 +1,31 @@
+require 'formula'
+require File.expand_path("../../Requirements/php-meta-requirement", Pathname.new(__FILE__).realpath)
+require File.expand_path("../../Requirements/phar-requirement", Pathname.new(__FILE__).realpath)
+
+class Phpdocumentor < Formula
+  homepage 'http://www.phpdoc.org'
+  url 'http://www.phpdoc.org/phpDocumentor.phar'
+  sha1 'ca66cc4e95aaefff7bc4341bd9d2d1d25c47eea3'
+  version '2.0.0a12'
+
+  depends_on PhpMetaRequirement
+  depends_on PharRequirement
+
+  def install
+    libexec.install "phpDocumentor.phar"
+    sh = libexec + "phpdoc"
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpDocumentor.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  def caveats; <<-EOS.undent
+    Verify your installation by running:
+      "phpdoc --version".
+
+    You can read more about phpdocumentor by running:
+      "brew home phpdocumentor".
+    EOS
+  end
+
+end
