362d71b9920c5f1d81c08aaec9d723ae6a320efc
Robson Peixoto <robsonpeixoto@gmail.com>
2013-04-10 10:51:20 -0300

    Timezonedb formula

---
+++ Formula/php54-timezonedb.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Timezonedb < AbstractPhp54Extension
+  init
+  homepage 'http://pecl.php.net/package/timezonedb'
+  url 'http://pecl.php.net/get/timezonedb-2013.2.tgz'
+  version '2013.2'
+  sha1 '8bade3e0c5427ddaa0ba7148cef0e2569bce31ec'
+
+  head 'https://svn.php.net/repository/pecl/timezonedb/trunk/', :using => :svn
+
+  def install
+    Dir.chdir "timezonedb-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/timezonedb.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+
+end
