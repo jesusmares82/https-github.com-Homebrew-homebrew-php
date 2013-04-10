51666584b95c7d255de29cc66d4aa17b353cca92
Robson Peixoto <robsonpeixoto@gmail.com>
2013-04-10 10:21:36 -0300

    Upgrade php54-apc to 3.1.13
    The beta state is recommended for php5.4

--- Formula/php54-apc.rb
+++ Formula/php54-apc.rb
@@ -3,23 +3,11 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Apc < AbstractPhp54Extension
   init
   homepage 'http://pecl.php.net/package/apc'
-  url 'http://pecl.php.net/get/APC-3.1.10.tgz'
-  sha1 '02c8a2f60c5b1789e7cccd9c1b421d822ca77734'
+  url 'http://pecl.php.net/get/APC-3.1.13.tgz'
+  sha1 'cafd6ba92ac1c9f500a6c1e300bbe8819daddfae'
   head 'https://svn.php.net/repository/pecl/apc/trunk/', :using => :svn
 
-  devel do
-    url 'http://pecl.php.net/get/APC-3.1.13.tgz'
-    version '3.1.13'
-    sha1 'cafd6ba92ac1c9f500a6c1e300bbe8819daddfae'
-  end
-
   depends_on 'pcre'
-
-  def patches
-    # fixes "Incorrect version tag: APC 3.1.10 shows 3.1.9"
-    # https://bugs.php.net/bug.php?id=61695
-    DATA if version == '3.1.10'
-  end
 
   def install
     Dir.chdir "APC-#{version}" unless build.head?
@@ -48,18 +36,3 @@ class Php54Apc < AbstractPhp54Extension
     EOS
   end
 end
-
-__END__
-diff --git a/APC-3.1.10/php_apc.h b/APC-3.1.10/php_apc.h
-index 77f66d5..aafa3b7 100644
---- a/APC-3.1.10/php_apc.h
-+++ b/APC-3.1.10/php_apc.h
-@@ -35,7 +35,7 @@
-  #include "apc_php.h"
-  #include "apc_globals.h"
-
--#define PHP_APC_VERSION "3.1.9"
-+#define PHP_APC_VERSION "3.1.10"
-
-  extern zend_module_entry apc_module_entry;
-  #define apc_module_ptr &apc_module_entry
