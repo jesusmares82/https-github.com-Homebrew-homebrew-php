fd647cabcae0901ac65311463e1141c0b61d14ad
Eashwar Ranganathan <crystal.dome@gmail.com>
2013-04-24 11:40:27 -0700

    Update URLs for 5.4.14 modules Fixes #540

--- Formula/php54-pcntl.rb
+++ Formula/php54-pcntl.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Pcntl < AbstractPhp54Extension
   init
   homepage 'http://php.net/manual/en/book.pcntl.php'
-  url 'http://www.php.net/get/php-5.4.13.tar.bz2/from/this/mirror'
+  url 'http://www.php.net/get/php-5.4.14.tar.bz2/from/this/mirror'
   sha1 '68e90795071f769b8fda22af7d71092d09f42dea'
   version '5.4.14'
 
