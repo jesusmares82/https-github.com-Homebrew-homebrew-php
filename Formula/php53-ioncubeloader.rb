95a3cb629b78c238a5b63985dfb4069652f82151
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-05-12 16:10:46 -0400

    make the version common for ioncubeloader

--- Formula/php53-ioncubeloader.rb
+++ Formula/php53-ioncubeloader.rb
@@ -3,14 +3,13 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php53Ioncubeloader < AbstractPhp53Extension
   init
   homepage 'http://www.ioncube.com/loaders.php'
+  version '4.2.2'
   if Hardware.is_64_bit?
     url 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz'
     sha1 '067c47387725db434ab0e7d486a2d6eff6fab37c'
-    version '4.2.2'
   else
     url 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86.tar.gz'
     sha1 'a370d6cab86fe32be8df7ac4c2a1e16fa55c3742'
-    version '4.2.2'
   end
 
   skip_clean :all
