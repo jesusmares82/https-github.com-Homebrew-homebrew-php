a7f0d6e59f6ffe16aad370134f4e3e562a32e84b
David Guyon <d.guyon@playmedia.fr>
2013-06-04 15:36:14 +0200

    Update mongo to v1.4.1

--- Formula/php54-mongo.rb
+++ Formula/php54-mongo.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Mongo < AbstractPhp54Extension
   init
   homepage 'http://pecl.php.net/package/mongo'
-  url 'http://pecl.php.net/get/mongo-1.4.0.tgz'
-  sha1 'f92e5d5becc48c508fe80ac54700099517066c8e'
+  url 'http://pecl.php.net/get/mongo-1.4.1.tgz'
+  sha1 '8cdb575497609ba3bade4055dad58c98779e3f49'
   head 'https://github.com/mongodb/mongo-php-driver.git'
 
   def install
