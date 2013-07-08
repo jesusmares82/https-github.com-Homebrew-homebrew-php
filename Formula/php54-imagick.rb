773a87470b0d8adcb97051e29f622e98ce0057cf
Tolga Ozudogru <tolga@hetge.com>
2013-07-08 17:33:24 -0400

    imagick: Added patch for the header file MagickWand.h could not be located error during ./configure

--- Formula/php54-imagick.rb
+++ Formula/php54-imagick.rb
@@ -10,6 +10,16 @@ class Php54Imagick < AbstractPhp54Extension
   depends_on 'pkg-config' => :build
   depends_on 'imagemagick'
 
+  def patches
+    # Rationale: Fix for the header file MagickWand.h
+    #     could not be located error during ./configure
+    #     
+    # Original error message:
+    #     checking for MagickWand.h header file...
+    #     configure: error: Cannot locate header file MagickWand.h
+    DATA
+  end
+
   def install
     Dir.chdir "imagick-#{version}" unless build.head?
 
@@ -23,3 +33,18 @@ class Php54Imagick < AbstractPhp54Extension
     write_config_file unless build.include? "without-config-file"
   end
 end
+
+__END__
+diff --git a/imagick-3.1.0RC2/config.m4 b/imagick-3.1.0RC2/config.m4
+index 6caa29a..1c6cdfb 100644
+--- a/imagick-3.1.0RC2/config.m4
++++ b/imagick-3.1.0RC2/config.m4
+@@ -54,6 +54,8 @@ if test $PHP_IMAGICK != "no"; then
+ 
+     if test -r $WAND_DIR/include/ImageMagick/wand/MagickWand.h; then
+       AC_MSG_RESULT(found in $WAND_DIR/include/ImageMagick/wand/MagickWand.h)
++    elif test -r $WAND_DIR/include/ImageMagick-6/wand/MagickWand.h; then
++      AC_MSG_RESULT(found in $WAND_DIR/include/ImageMagick-6/wand/MagickWand.h)
+     else
+       AC_MSG_ERROR(Cannot locate header file MagickWand.h)
+     fi
