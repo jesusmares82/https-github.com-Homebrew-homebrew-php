7d2414ae7db755c7d44437f8aa91f58a3ebea572
denji <denji@localhost>
2013-07-10 00:17:14 +0300

    APCu v4.0.1

--- Formula/php55-apcu.rb
+++ Formula/php55-apcu.rb
@@ -3,10 +3,12 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Apcu < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/apcu'
-  url 'http://pecl.php.net/get/apcu-4.0.0.tgz'
-  version '4.0.0'
-  sha1 'acb373b28cffefad6c19634ee0d504f209c23808'
+  url 'http://pecl.php.net/get/apcu-4.0.1.tgz'
+  version '4.0.1'
+  sha1 'f7569e959e7ff09c90de3636f4638be766bab5fd'
+  head 'https://github.com/krakjoe/apcu.git'
 
+  option 'with-apc-bc', "Enable APC full compatibility support"
   depends_on 'pcre'
 
   def install
@@ -14,10 +16,15 @@ class Php55Apcu < AbstractPhp55Extension
 
     ENV.universal_binary if build.universal?
 
+    args = []
+    args << "--prefix=#{prefix}"
+    args << phpconfig
+    args << "--enable-apcu"
+    args << "--enable-apc-bc" if build.include? 'with-apc-bc'
+
     safe_phpize
-    system "./configure", "--prefix=#{prefix}",
-                          "--enable-apc",
-                          phpconfig
+
+    system "./configure", *args
     system "make"
     prefix.install %w(modules/apcu.so)
     write_config_file unless build.include? "without-config-file"
@@ -26,13 +33,10 @@ class Php55Apcu < AbstractPhp55Extension
   def config_file
     super + <<-EOS.undent
       apc.enabled=1
-      apc.shm_segments=1
       apc.shm_size=64M
       apc.ttl=7200
-      apc.user_ttl=7200
-      apc.num_files_hint=1024
       apc.mmap_file_mask=/tmp/apc.XXXXXX
-      apc.enable_cli=0
+      apc.enable_cli=1
     EOS
   end
 end
