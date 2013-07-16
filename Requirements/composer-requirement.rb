a81ef879ef662b719ad0bba0e54a0d16ba03a568
Tolga Y. Ozudogru <tolga@hetge.com>
2013-07-15 23:44:31 -0400

    Fixed composer requirements for PHP55

--- Requirements/composer-requirement.rb
+++ Requirements/composer-requirement.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
 
 class ComposerRequirement < HomebrewPhpRequirement
-  COMMAND = 'curl -s http://getcomposer.org/installer | /usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off -d date.timezone=UTC -- --check'
+  COMMAND = 'curl -s http://getcomposer.org/installer | /usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off -d date.timezone=UTC -d apc.enable_cli=Off -- --check'
 
   def satisfied?
     @result = `#{COMMAND}`
--- Requirements/php-meta-requirement.rb
+++ Requirements/php-meta-requirement.rb
@@ -2,10 +2,10 @@ require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
 
 class PhpMetaRequirement < HomebrewPhpRequirement
   def satisfied?
-    %w{php53 php54}.any? { |php| Formula.factory(php).installed? }
+    %w{php53 php54 php55}.any? { |php| Formula.factory(php).installed? }
   end
 
   def message
-    "Missing PHP53 or PHP54 from homebrew-php. Please install one or the other before continuing"
+    "Missing PHP53, PHP54 or PHP55 from homebrew-php. Please install one of them before continuing"
   end
 end
