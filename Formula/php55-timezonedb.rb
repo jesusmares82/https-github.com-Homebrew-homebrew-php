2a14974107a6d8316d6936246581e39a26535eec
Lucas <lucas@semalead.com>
2013-07-27 11:15:27 +0200

    Update php-timezonedb to version 2013.4
    Also fixed redundant version and redundant :using

--- Formula/php55-timezonedb.rb
+++ Formula/php55-timezonedb.rb
@@ -3,10 +3,9 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Timezonedb < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/timezonedb'
-  url 'http://pecl.php.net/get/timezonedb-2013.2.tgz'
-  sha1 '8bade3e0c5427ddaa0ba7148cef0e2569bce31ec'
-  head 'https://svn.php.net/repository/pecl/timezonedb/trunk/', :using => :svn
-  version '2013.2'
+  url 'http://pecl.php.net/get/timezonedb-2013.4.tgz'
+  sha1 '9910eec433d07fc64d908280dc9743200bb89bbd'
+  head 'https://svn.php.net/repository/pecl/timezonedb/trunk/'
 
   def install
     Dir.chdir "timezonedb-#{version}" unless build.head?
