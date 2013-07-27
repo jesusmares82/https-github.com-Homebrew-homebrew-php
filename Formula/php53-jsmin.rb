436adffb70f5ccc18638bbc74abb0638926765c9
jwhitcraft <jwhitcraft@sugarcrm.com>
2013-07-27 08:58:35 -0400

    JSmin Pecl Extension

---
+++ Formula/php53-jsmin.rb
@@ -0,0 +1,22 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Jsmin < AbstractPhp53Extension
+  init
+  homepage 'http://pecl.php.net/package/jsmin'
+  url 'http://pecl.php.net/get/jsmin-0.1.0.tgz'
+  sha1 '0cc709e7ef6728535905a3a7492bf2347244a30f'
+  head 'https://github.com/sqmk/pecl-jsmin.git'
+
+  def install
+    Dir.chdir "jsmin-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/jsmin.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
