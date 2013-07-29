88d8fad7d50930a2758df9ed8f5da39e637381b5
Lucas <lucas@semalead.com>
2013-07-29 23:01:43 +0200

    Update php*-mailparse.rb
    Fix brew audit warning message : "redundant :using specification in url or head"

--- Formula/php55-mailparse.rb
+++ Formula/php55-mailparse.rb
@@ -5,7 +5,7 @@ class Php55Mailparse < AbstractPhp55Extension
   homepage 'http://pecl.php.net/package/mailparse'
   url 'http://pecl.php.net/get/mailparse-2.1.6.tgz'
   sha1 'f0deaa433a1f7833e80f80dabc1bbbdbe0071b3c'
-  head 'https://svn.php.net/repository/pecl/mailparse/trunk', :using => :svn
+  head 'https://svn.php.net/repository/pecl/mailparse/trunk'
 
   depends_on 'pcre'
 
