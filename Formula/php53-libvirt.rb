c5a9e9918a17eef7a1253fc7c1c5d26e280e7b36
Andy Blyler <andy@blyler.cc>
2013-07-30 19:00:03 -0700

    update libvirt extension to 0.4.8 - move from git branch to downloaded tar - fixes issue where new autoconf was preventing automake from running

--- Formula/php53-libvirt.rb
+++ Formula/php53-libvirt.rb
@@ -3,23 +3,17 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php53Libvirt < AbstractPhp53Extension
   init
   homepage 'http://libvirt.org/php'
-  url 'git://libvirt.org/libvirt-php', :using => :git, :tag => 'libvirt-php-0.4.7'
-  version '0.4.7'
+  url 'http://libvirt.org/sources/php/libvirt-php-0.4.8.tar.gz'
+  sha1 '933c20e70f83107d056528b8c665a024da9bc7a4'
+  version '0.4.8'
 
-  depends_on 'automake' => :build
-  depends_on 'autoconf' => :build
+  depends_on 'pkg-config' => :build
   depends_on 'libvirt'
 
   def install
     ENV.universal_binary if build.universal?
 
-    # autogen
-    system "PATH=$PATH:/usr/local/bin ./autogen.sh"
-
-    # safe_phpize
-    system "./configure", "--prefix=#{prefix}",
-                          phpconfig,
-                          "--with-libvirt=#{Formula.factory('libvirt').opt_prefix}"
+    system "./configure", "--prefix=#{prefix}", phpconfig
     system "make"
     prefix.install 'src/libvirt-php.so' => 'libvirt.so'
     write_config_file unless build.include? "without-config-file"
