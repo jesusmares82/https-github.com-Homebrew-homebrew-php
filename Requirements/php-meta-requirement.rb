2ba04affa07116a6c1ecfa2e447d5e31b6df048e
Hisateru Tanaka <tanakahisateru@gmail.com>
2013-08-05 19:34:28 +0900

    Allow non newest version PHP to tools.
    Installation of PHP tools was blocked if installed PHP does not match to the newest formulas.

--- Requirements/php-meta-requirement.rb
+++ Requirements/php-meta-requirement.rb
@@ -2,7 +2,10 @@ require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
 
 class PhpMetaRequirement < HomebrewPhpRequirement
   def satisfied?
-    %w{php53 php54 php55}.any? { |php| Formula.factory(php).installed? }
+    %w{php53 php54 php55}.any? do |php|
+        f = Formula.factory(php)
+        f.rack.directory? && f.rack.children.length > 0
+    end
   end
 
   def message
