2ed248c5f6be56734807f62e4d7e2383ca294da4
David Guyon <dguyon@gmail.com>
2013-08-23 16:13:08 +0200

    Bump PHP 5.5.3

--- Formula/php55-pcntl.rb
+++ Formula/php55-pcntl.rb
@@ -3,9 +3,9 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Pcntl < AbstractPhp55Extension
   init
   homepage 'http://php.net/manual/en/book.pcntl.php'
-  url 'http://www.php.net/get/php-5.5.1.tar.bz2/from/this/mirror'
-  sha1 'b31b6922cb2796a52be9dc6696f31fcbb20ac916'
-  version '5.5.1'
+  url 'http://www.php.net/get/php-5.5.3.tar.bz2/from/this/mirror'
+  sha1 '3952e6d678164a700296dbdb8e506b8bb80727fb'
+  version '5.5.3'
 
   def install
     Dir.chdir "ext/pcntl"
