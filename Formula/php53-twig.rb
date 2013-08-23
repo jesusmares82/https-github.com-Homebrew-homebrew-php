036164d2a75717b5ab59f0d667f5293b0ff1de16
Lucas <lucas@semalead.com>
2013-08-23 18:47:36 +0200

    Update php*-twig from 1.13.0 to 1.13.2
    Also added sha1 check on archive

--- Formula/php53-twig.rb
+++ Formula/php53-twig.rb
@@ -3,7 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php53Twig < AbstractPhp53Extension
   init
   homepage 'http://twig.sensiolabs.org/'
-  url 'https://github.com/fabpot/Twig.git', :tag => 'v1.13.0'
+  url 'https://github.com/fabpot/Twig/archive/v1.13.2.tar.gz'
+  sha1 '231ee99dc313b38cfde60ca903777f98d40daf2f'
   head 'https://github.com/fabpot/Twig.git'
 
   def install
