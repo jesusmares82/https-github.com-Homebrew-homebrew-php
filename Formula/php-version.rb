ce105478f9e690021ad7c18989b00ec92cb083e2
Wil Moore III <wil.moore@wilmoore.com>
2013-08-26 02:03:15 -0600

    Update php-version to 0.9.10

--- Formula/php-version.rb
+++ Formula/php-version.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class PhpVersion < Formula
   homepage  'http://wilmoore.github.com/php-version'
-  url       'https://github.com/wilmoore/php-version/archive/0.9.8.tar.gz'
-  sha1      '461471fde3907c2a356250de18e081916214176f'
+  url       'https://github.com/wilmoore/php-version/archive/0.9.10.tar.gz'
+  sha1      '65092d0f1774294accbbac93245141d2cfb0e81b'
   head      'https://github.com/wilmoore/php-version.git'
 
   def install
@@ -12,19 +12,13 @@ class PhpVersion < Formula
 
   def caveats;
     <<-EOS.undent
-      Add the following script block to `$HOME/.bashrc`, `$HOME/.zshrc`, or your shell's equivalent configuration file.
+      Add the following to $HOME/.bashrc, $HOME/.zshrc, or your shell's equivalent configuration file:
 
-      Change `5.x.x` to the version of PHP you'd like your shell to default to or remove `php-version 5.x.x >/dev/null`
-      if you do not wish to have a default version of PHP loaded into your `$PATH`. The large comment block is optional:
-
-      ########################################################################################
-      # php-version (activate default PHP version and autocompletion)
-      # PHP_HOME                      => should reflect location of compiled PHP versions
-      # PHPVERSION_DISABLE_COMPLETE=1 => to disable shell completion
-      ########################################################################################
-      export PHP_VERSIONS=$(dirname $(brew --prefix php))
-      [ -f $(brew --prefix php-version)/php-version.sh ] &&
-        source $(brew --prefix php-version)/php-version.sh && php-version 5.x.x >/dev/null
+      test -d $(brew --prefix php-version)
+        && source $(brew --prefix php-version)/php-version.sh
+        && php-version 5 >/dev/null
+        
+      Type `php-version --help` for more configuration options such as how to add extra PHP installation paths.
     EOS
   end
 end
