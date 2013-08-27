0e821af9bee3309da02fec100a5d024b3ef19d09
bmtKIA6 <bmtKIA6@users.noreply.github.com>
2013-08-27 17:40:42 -0400

    update xcache extention to be a normal (non-zend) extention
    As of XCache 3.0, it should be loaded with "extension" and not "zend_extension." See http://xcache.lighttpd.net/wiki/Release-3.0.1

--- Formula/php54-xcache.rb
+++ Formula/php54-xcache.rb
@@ -6,7 +6,7 @@ class Php54Xcache < AbstractPhp54Extension
   url 'http://xcache.lighttpd.net/pub/Releases/3.0.1/xcache-3.0.1.tar.bz2'
   sha1 'abd5c1cdff3e2f2207e1d39eb99470ffd48efc0f'
 
-  def extension_type; "zend_extension"; end
+  def extension_type; "extension"; end
 
   def install
     ENV.universal_binary if build.universal?
