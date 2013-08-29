cf1d54b8a408dbdd0c126ac11ed8f7de631f31ee
Lucas <lucas@semalead.com>
2013-08-29 11:26:38 +0200

    php*-scrypt refactor
    - use archive
    - added sha1 check
    - removed export for ENV
    -removed redundant version

--- Formula/php55-scrypt.rb
+++ Formula/php55-scrypt.rb
@@ -2,20 +2,21 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 
 class Php55Scrypt < AbstractPhp55Extension
   init
-  homepage 'http://github.com/DomBlack/php-scrypt'
-  url 'https://github.com/DomBlack/php-scrypt.git', :tag => 'v1.2'
+  homepage 'https://github.com/DomBlack/php-scrypt'
+  url 'https://github.com/DomBlack/php-scrypt/archive/v1.2.tar.gz'
+  sha1 '56d1d987b06cc54c2a360ca5a93b84e9f3f0c5c9'
   head 'https://github.com/DomBlack/php-scrypt.git'
-  version '1.2'
 
   def install
     ENV.universal_binary if build.universal?
+    ENV['CFLAGS'] = '-arch i386 -arch x86_64'
 
     safe_phpize
-    system "export CFLAGS='-arch i386 -arch x86_64'"
-    system "./configure", "--prefix=#{prefix}", "--enable-scrypt",
-                          phpconfig
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--enable-scrypt"
     system "make"
-    prefix.install %w(modules/scrypt.so)
+    prefix.install "modules/scrypt.so"
 
     write_config_file unless build.include? "without-config-file"
   end
