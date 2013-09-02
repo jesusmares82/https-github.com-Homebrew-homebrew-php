d5702019b3192e0f8969645bd2fede8f52842bd5
Lucas <lucas@semalead.com>
2013-09-02 20:41:05 +0200

    upgrade php*-redis from 2.2.3 to 2.2.4

--- Formula/php54-redis.rb
+++ Formula/php54-redis.rb
@@ -3,31 +3,30 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Redis < AbstractPhp54Extension
   init
   homepage 'https://github.com/nicolasff/phpredis'
-  url 'https://github.com/nicolasff/phpredis/archive/2.2.3.tar.gz'
-  sha1 '99074be0285cd7b1140a971bf5340ef5cbea142a'
+  url 'https://github.com/nicolasff/phpredis/archive/2.2.4.tar.gz'
+  sha1 'e3b66ee2fc64549403f94c687e24ff121d564909'
   head 'https://github.com/nicolasff/phpredis.git'
 
   option 'with-igbinary', "Build with igbinary support"
-  depends_on 'php54-igbinary' if build.include?('with-igbinary')
 
-  fails_with :clang do
-    build 318
-    cause <<-EOS.undent
-      argument to 'va_arg' is of incomplete type 'void'
-      This is fixed in HEAD, and can be removed for the next release.
-      EOS
-  end unless build.head?
+  depends_on 'php54-igbinary' if build.include?('with-igbinary')
 
   def install
     ENV.universal_binary if build.universal?
 
     args = []
-    args << "--prefix=#{prefix}"
-    args << phpconfig
     args << "--enable-redis-igbinary" if build.include? 'with-igbinary'
 
     safe_phpize
-    system "./configure", *args
+
+    if build.include? 'with-igbinary'
+      system "mkdir -p ext/igbinary"
+      cp "#{Formula.factory('php54-igbinary').opt_prefix}/include/igbinary.h", "ext/igbinary/igbinary.h"
+    end
+
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          *args
     system "make"
     prefix.install "modules/redis.so"
     write_config_file unless build.include? "without-config-file"
