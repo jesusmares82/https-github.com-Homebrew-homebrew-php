a9e0d0da26ea46226b07883da0255384a14684cd
Andy Blyler <andy@blyler.cc>
2013-09-06 22:25:20 -0400

    add phpmd

---
+++ Formula/phpmd.rb
@@ -0,0 +1,31 @@
+require 'formula'
+require File.expand_path("../../Requirements/php-meta-requirement", Pathname.new(__FILE__).realpath)
+require File.expand_path("../../Requirements/phar-requirement", Pathname.new(__FILE__).realpath)
+
+class Phpmd < Formula
+  homepage 'http://phpmd.org'
+  url 'http://static.phpmd.org/php/1.5.0/phpmd.phar'
+  sha1 '4e64bd506afad7e9aa1b0c1f4216660395e347ac'
+  version '1.5.0'
+
+  depends_on PhpMetaRequirement
+  depends_on PharRequirement
+
+  def install
+    libexec.install "phpmd.phar"
+    sh = libexec + "phpmd"
+    sh.write("#!/bin/sh\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpmd.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  def test
+    system 'phpmd'
+  end
+
+  def caveats; <<-EOS.undent
+    You can read more about phpmd by running:
+      "brew home phpmd".
+    EOS
+  end
+end
