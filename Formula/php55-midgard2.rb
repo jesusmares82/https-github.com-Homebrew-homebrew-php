9684f4308c310ab53c009112f18b4e82ada4c21e
Lucas <lucas@semalead.com>
2013-09-07 11:46:09 +0200

    upgrade php*-midgard2
    - added link to archive
    - added sha1 check

--- Formula/php55-midgard2.rb
+++ Formula/php55-midgard2.rb
@@ -3,7 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Midgard2 < AbstractPhp55Extension
   init
   homepage 'http://www.midgard-project.org'
-  url 'https://github.com/midgardproject/midgard-php5.git', :tag => '12.09.1'
+  url 'https://github.com/midgardproject/midgard-php5/archive/12.09.1.tar.gz'
+  sha1 'd83519a9d513a47e9e6a02e90f6fb957f036b082'
   head 'https://github.com/midgardproject/midgard-php5.git', :branch => 'ratatoskr'
 
   depends_on 'pkg-config' => :build
