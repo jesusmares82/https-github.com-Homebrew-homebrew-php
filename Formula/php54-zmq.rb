84306699c919ec3d9cebefc6cacf2b5c1ff44322
Lucas <lucas@semalead.com>
2013-09-07 12:16:51 +0200

    upgrade php*-zmq from 1.0.5 to 1.0.6
    - added zeromq dependency

--- Formula/php54-zmq.rb
+++ Formula/php54-zmq.rb
@@ -3,11 +3,12 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Zmq < AbstractPhp54Extension
   init
   homepage 'http://php.zero.mq/'
-  url 'https://github.com/mkoppanen/php-zmq/archive/1.0.5.tar.gz'
-  sha1 '9be2a968985d1af87bc8549a5c0944fba9a80e6d'
+  url 'https://github.com/mkoppanen/php-zmq/archive/1.0.6.tar.gz'
+  sha1 '7754db7e69d881aa76c8493f35e84513f1b465f0'
   head 'https://github.com/mkoppanen/php-zmq.git'
 
   depends_on 'pkg-config' => :build
+  depends_on 'zeromq'
 
   def install
     ENV.universal_binary if build.universal?
