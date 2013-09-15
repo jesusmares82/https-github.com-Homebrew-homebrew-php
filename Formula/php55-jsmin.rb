cd0bf2cda7eff935985276197eec191bf60722fd
Lucas <lucas@semalead.com>
2013-09-15 11:52:21 +0200

    php*-jsmin
    Update jsmin extension

--- Formula/php55-jsmin.rb
+++ Formula/php55-jsmin.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Jsmin < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/jsmin'
-  url 'http://pecl.php.net/get/jsmin-0.1.0.tgz'
-  sha1 '0cc709e7ef6728535905a3a7492bf2347244a30f'
+  url 'http://pecl.php.net/get/jsmin-0.1.1.tgz'
+  sha1 'b1abfb443ce5df80866fc9350a792f3be814f568'
   head 'https://github.com/sqmk/pecl-jsmin.git'
 
   def install
