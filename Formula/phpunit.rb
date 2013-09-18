5f0ae92cee7f81fe17ecb9c944abc89095b44a59
Lucas <lucas@semalead.com>
2013-09-19 00:56:22 +0200

    Upgrade PHPUnit from 3.7.24 to 3.7.27

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -2,10 +2,12 @@ require 'formula'
 
 class Phpunit < Formula
   homepage 'http://phpunit.de/manual/current/en/'
-  url 'http://pear.phpunit.de/get/phpunit-3.7.24.phar'
-  sha1 'de677ba60b2ccd135002d02501cde16a471503f5'
+  url 'https://github.com/sebastianbergmann/phpunit/releases/download/3.7.27/phpunit.phar'
+  sha1 'dc693b34a62644f61cfc01a22d9654ff4b8764af'
+  version '3.7.27'
 
   def install
+    mv "phpunit.phar", "phpunit-#{version}.phar"
     libexec.install "phpunit-#{version}.phar"
     sh = libexec + "phpunit"
     sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpunit-#{version}.phar $*")
