730f14d701bd3850c643102d444e1f06ec8d6ab6
Wil Moore III <wil.moore@wilmoore.com>
2013-09-23 11:59:04 -0600

    php-version: corrected configuration line

--- Formula/php-version.rb
+++ Formula/php-version.rb
@@ -14,10 +14,9 @@ class PhpVersion < Formula
     <<-EOS.undent
       Add the following to $HOME/.bashrc, $HOME/.zshrc, or your shell's equivalent configuration file:
 
-      test -d $(brew --prefix php-version)
-        && source $(brew --prefix php-version)/php-version.sh
-        && php-version 5 >/dev/null
-        
+        source $(brew --prefix php-version)/php-version.sh \
+            && php-version 5 2>/dev/null
+
       Type `php-version --help` for more configuration options such as how to add extra PHP installation paths.
     EOS
   end
