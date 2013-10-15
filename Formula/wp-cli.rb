dc9acb5c698ad6c0c084196260a7897b8dff427f
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2013-10-15 18:35:16 +0200

    added option 'without-package-index'
    http://wp-cli.org/package-index/
    https://github.com/wp-cli/wp-cli/wiki/Community-Packages

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -7,6 +7,7 @@ class WpCli < Formula
   sha1 'b334f1728fec439f8db083f4ee335c707cd36649'
 
   option 'without-bash-completion', "Don't install bash completion"
+  option 'without-package-index', "Don't add package index repository (http://wp-cli.org/package-index)"
 
   depends_on 'composer'
 
@@ -15,8 +16,10 @@ class WpCli < Formula
     prefix.install Dir['*']
 
     unless build.without? 'bash-completion'
-      (prefix + 'etc/bash_completion.d').install
-        "#{prefix}/utils/wp-completion.bash"
+      (prefix + 'etc/bash_completion.d').install "#{prefix}/utils/wp-completion.bash"
+    end
+    unless build.without? 'package-index'
+      system "#{HOMEBREW_PREFIX}/bin/composer config --file='#{prefix}/composer.json' repositories.wp-cli composer http://wp-cli.org/package-index/"
     end
   end
 
