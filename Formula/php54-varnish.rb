0e1b2b954e99fe165c45e35a3afb6f881e2140c2
Lucas <lucas@semalead.com>
2013-10-22 01:12:57 +0200

    Varnish extension
    Add Varnish extension for PHP 5.3, 5.4 & 5.5
    Closes #795

---
+++ Formula/php54-varnish.rb
@@ -0,0 +1,21 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Varnish < AbstractPhp54Extension
+  init
+  homepage 'http://pecl.php.net/package/varnish'
+  url 'http://pecl.php.net/get/varnish-1.1.1.tgz'
+  sha1 '9cc3b308c6dccb4bbf376fce4bcc2268aa7a7c1d'
+
+  def install
+    Dir.chdir "varnish-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/varnish.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
