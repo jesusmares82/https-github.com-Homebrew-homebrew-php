ae6588cc3e289d35aacb0e734cfd099ca2d94e7b
Lucas <lucas@semalead.com>
2013-10-31 14:48:45 +0100

    Upgrade php*-timezonedb

--- Formula/php55-timezonedb.rb
+++ Formula/php55-timezonedb.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Timezonedb < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/timezonedb'
-  url 'http://pecl.php.net/get/timezonedb-2013.7.tgz'
-  sha1 '01c41e0cf9e7c62474029878ea0e580c8aecc29f'
+  url 'http://pecl.php.net/get/timezonedb-2013.8.tgz'
+  sha1 '93f6027001c2e0c48bd42669f7dfd3d2fa67ee5b'
   head 'https://svn.php.net/repository/pecl/timezonedb/trunk/'
 
   def install
