7e1ded3b1b6fd0599c76b28f9dde820d65fb4bc0
Lucas <lucas@semalead.com>
2013-11-03 11:04:20 +0100

    Add php*-xmldiff PECL extension

---
+++ Formula/php53-xmldiff.rb
@@ -0,0 +1,21 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Xmldiff < AbstractPhp53Extension
+  init
+  homepage 'http://pecl.php.net/package/xmldiff'
+  url 'http://pecl.php.net/get/xmldiff-0.9.1.tgz'
+  sha1 '93f790182d017c307da3d56d3fb3d1c1a89a83e1'
+
+  def install
+    Dir.chdir "xmldiff-#{version}"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/xmldiff.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
