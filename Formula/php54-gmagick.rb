1e52df49cdcd5a1f7de1f2bd5e8b9070e84d0855
Lucas <lucas@semalead.com>
2013-11-08 19:09:37 +0100

    Add php*-gmagick PECL extension

---
+++ Formula/php54-gmagick.rb
@@ -0,0 +1,23 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Gmagick < AbstractPhp54Extension
+  init
+  homepage 'http://pecl.php.net/package/gmagick'
+  url 'http://pecl.php.net/get/gmagick-1.1.5RC1.tgz'
+  sha1 '95d9a7c8f7ae9b88de236be2cfd71df77fbfd968'
+
+  depends_on "graphicsmagick"
+
+  def install
+    Dir.chdir "gmagick-#{version}"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/gmagick.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
