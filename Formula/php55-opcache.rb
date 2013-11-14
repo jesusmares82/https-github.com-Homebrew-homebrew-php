c620e357b065a2ed1f62f05525ae2a08f597a5f7
Ben Davies <ben.davies@boxuk.com>
2013-11-14 16:48:18 +0000

    correct the setting prefix for opcache

--- Formula/php55-opcache.rb
+++ Formula/php55-opcache.rb
@@ -24,13 +24,13 @@ class Php55Opcache < AbstractPhp55Extension
 
   def config_file
     super + <<-EOS.undent
-      zend_optimizerplus.memory_consumption=128
-      zend_optimizerplus.interned_strings_buffer=8
-      zend_optimizerplus.max_accelerated_files=4000
-      zend_optimizerplus.revalidate_freq=60
-      zend_optimizerplus.fast_shutdown=1
-      zend_optimizerplus.enable_cli=1
-      zend_optimizerplus.enable_file_override=1
+      opcache.memory_consumption=128
+      opcache.interned_strings_buffer=8
+      opcache.max_accelerated_files=4000
+      opcache.revalidate_freq=60
+      opcache.fast_shutdown=1
+      opcache.enable_cli=1
+      opcache.enable_file_override=1
       apc.cache_by_default = false
     EOS
   end
