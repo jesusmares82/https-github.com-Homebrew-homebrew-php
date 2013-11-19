a4595a5e6f532de769f5173a468bea73c70cf5ee
Maurus Cuelenaere <mcuelenaere@gmail.com>
2013-11-19 20:11:35 +0100

    Add php5{3,4,5}-sphinx (+1 squashed commit) Squashed commits: [f9a2073] Add libsphinxclient (needed by php-5{3,4,5}-sphinx)

---
+++ Formula/php54-sphinx.rb
@@ -0,0 +1,26 @@
+require 'formula'
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Sphinx < AbstractPhp54Extension
+  init
+  homepage 'http://pecl.php.net/package/sphinx'
+  url 'http://pecl.php.net/get/sphinx-1.3.0.tgz'
+  sha1 '87a25a6dc8fecaade453ca55ff5d1822bbef0b1a'
+
+  depends_on 'libsphinxclient'
+
+  def install
+    Dir.chdir "sphinx-#{version}" unless build.head?
+
+    args = []
+    args << "--prefix=#{prefix}"
+    args << phpconfig
+
+    safe_phpize
+
+    system "./configure", *args
+    system "make"
+    prefix.install "modules/sphinx.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
