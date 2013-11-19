cf3de54d429f3b9f6a6ba4e4b42e9d69ae63d8c9
Lucas <lucas@semalead.com>
2013-11-19 10:48:36 +0100

    Upgrade php*-yaml to 1.1.1

--- Formula/php53-yaml.rb
+++ Formula/php53-yaml.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php53Yaml < AbstractPhp53Extension
   init
   homepage 'http://pecl.php.net/package/yaml'
-  url 'http://pecl.php.net/get/yaml-1.1.0.tgz'
-  sha1 '8ab77d3e49eed6944bbb3130c054288402f484d7'
+  url 'http://pecl.php.net/get/yaml-1.1.1.tgz'
+  sha1 '7b095f76fe0e4b3b9072c7f0099a203265686f3a'
   head 'https://svn.php.net/repository/pecl/yaml/trunk'
 
   depends_on 'libyaml'
