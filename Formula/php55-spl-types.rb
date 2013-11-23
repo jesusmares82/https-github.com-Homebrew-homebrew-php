4d8b92699ab3abdd130cee7e16addc06bd50f06d
Lucas <lucas@semalead.com>
2013-11-23 13:31:43 +0100

    Add php*-spl-types PECL extension

---
+++ Formula/php55-spl-types.rb
@@ -0,0 +1,20 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php55SplTypes < AbstractPhp55Extension
+  init
+  homepage 'http://pecl.php.net/package/SPL_Types'
+  url 'http://pecl.php.net/get/SPL_Types-0.4.0.tgz'
+  sha1 '14c2ccd34c5f4fc98b5af4a62267bc7fd71c9546'
+
+  def install
+    Dir.chdir "SPL_Types-#{version}"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/spl_types.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
