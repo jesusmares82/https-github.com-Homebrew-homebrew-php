bb0d65fd76cedf09d49aa97acd86e9ad9feaf93b
Lucas <lucas@semalead.com>
2013-11-23 13:17:02 +0100

    Add php*-propro PECL extension

---
+++ Formula/php55-propro.rb
@@ -0,0 +1,20 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php55Propro < AbstractPhp55Extension
+  init
+  homepage 'http://pecl.php.net/package/propro'
+  url 'http://pecl.php.net/get/propro-1.0.0.tgz'
+  sha1 'f2cbcbc322e88fda55b2fc8908426106e2f18db4'
+
+  def install
+    Dir.chdir "propro-#{version}"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/propro.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
