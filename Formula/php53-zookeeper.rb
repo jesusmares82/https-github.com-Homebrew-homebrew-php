519691bc62b2aba738f3d7c3737f1fe562ec7d63
Stig Bakken <stig@zedge.net>
2013-11-27 11:48:53 +0100

    Add zookeeper extension

---
+++ Formula/php53-zookeeper.rb
@@ -0,0 +1,45 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Zookeeper < AbstractPhp53Extension
+  homepage 'http://pecl.php.net/package/zookeeper'
+  url 'http://pecl.php.net/get/zookeeper-0.2.2.tgz'
+  sha1 '029c0d9c989c56c5afead8bb4621b7f2236a7b6f'
+  head 'https://github.com/andreiz/php-zookeeper.git'
+
+  def self.init
+    super
+    option 'disable-session', 'Disable zookeeper session handler support'
+    depends_on 'zookeeper' => '--c'
+  end
+
+  init
+
+  def install
+    Dir.chdir "zookeeper-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    args = []
+    args << "--prefix=#{prefix}"
+    args << phpconfig
+    args << "--with-libzookeeper-dir=#{Formula.factory('zookeeper').opt_prefix}"
+    args << "--disable-zookeeper-session" if build.include? 'disable-session'
+
+    safe_phpize
+
+    system "./configure", *args
+    system "make"
+    prefix.install "modules/zookeeper.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+
+  def caveats
+    super + <<-EOS.undent
+
+      Note that zookeeper must be installed with the --c option for the
+      PHP extension to build.
+
+    EOS
+  end
+
+end
