7b3c3677f3b2bd78d811a09cb223b5ae1250ca3e
Lucas Michot <lucas@semalead.com>
2013-12-09 01:23:43 +0100

    Add Sundown extension 0.3.9 Close https://github.com/josegonzalez/homebrew-php/pull/884.

---
+++ Formula/php53-sundown.rb
@@ -0,0 +1,20 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Sundown < AbstractPhp53Extension
+  init
+  homepage 'http://pecl.php.net/package/sundown'
+  url 'http://pecl.php.net/get/sundown-0.3.9.tgz'
+  sha1 '27a5f0215e73b9270acc4c27bcfde187b68ea6d4'
+
+  def install
+    Dir.chdir "sundown-#{version}"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/sundown.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
