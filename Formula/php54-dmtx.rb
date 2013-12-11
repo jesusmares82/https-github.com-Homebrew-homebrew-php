990b0f6d79bd1a4b484aea131ee0135fc269d5e1
Andrey Red <mattue@mail.ru>
2013-12-11 22:40:17 +0400

    added dmtx formula for php53, php55 (+1 squashed commit) Squashed commits: [db7da8d] added dmtx formula for php54

---
+++ Formula/php54-dmtx.rb
@@ -0,0 +1,25 @@
+require 'formula'
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Dmtx < AbstractPhp54Extension
+  init
+  homepage 'http://www.libdmtx.org'
+  url 'https://github.com/maZahaca/php-dmtx/archive/0.0.2-dev.tar.gz'
+  sha1 'b5fc8cc244d29177a35995afd968b8d2ddf22e31'
+  head 'https://github.com/maZahaca/php-dmtx.git'
+
+  depends_on 'libdmtx'
+  depends_on 'php54-imagick'
+  depends_on 'pkg-config' => :build
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install %w(modules/dmtx.so)
+    write_config_file unless build.include? "without-config-file"
+  end
+end
