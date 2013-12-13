872958066707708778f516f80ca2ffc508ca7f11
Michael Barrett <mike182uk@gmail.com>
2013-12-13 09:05:03 +0000

    add behat formula

---
+++ Formula/behat.rb
@@ -0,0 +1,35 @@
+require 'formula'
+require File.expand_path("../../Requirements/php-meta-requirement", Pathname.new(__FILE__).realpath)
+require File.expand_path("../../Requirements/phar-requirement", Pathname.new(__FILE__).realpath)
+
+class Behat < Formula
+  homepage 'http://behat.org/'
+  url 'http://behat.org/downloads/behat-2.4.5.phar'
+  sha1 '0c9b1950c106d0ad52a96010cf68484d2cfcd837'
+  version '2.4.5'
+
+  depends_on PhpMetaRequirement
+  depends_on PharRequirement
+
+  def install
+    libexec.install "behat-2.4.5.phar"
+    sh = libexec + "behat"
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/behat-2.4.5.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  def test
+    system 'behat --version'
+  end
+
+  def caveats; <<-EOS.undent
+    Verify your installation by running:
+      "behat --version".
+
+    You can read more about behat by running:
+      "brew home behat".
+    EOS
+  end
+
+end
