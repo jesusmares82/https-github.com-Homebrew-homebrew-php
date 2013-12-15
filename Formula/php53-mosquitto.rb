3760006fecbed2c98932a4f68dc5dcc7223deee8
Lucas Michot <lucas@semalead.com>
2013-12-15 19:36:11 +0100

    Add php*-mosquitto extension

---
+++ Formula/php53-mosquitto.rb
@@ -0,0 +1,23 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Mosquitto < AbstractPhp53Extension
+  init
+  homepage 'https://github.com/mgdm/Mosquitto-PHP/'
+  url 'http://pecl.php.net/get/Mosquitto-0.2.1.tgz'
+  sha1 '0fefee84da5f84dcf78ad0c5deea35f5cce451e4'
+  head 'https://github.com/mgdm/Mosquitto-PHP.git'
+
+  depends_on 'mosquitto'
+
+  def install
+    Dir.chdir "mosquitto-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/mosquitto.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
