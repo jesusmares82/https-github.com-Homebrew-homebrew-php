c917354007320f6f1da3d82703dbe9b1e874c4c8
Lucas Michot <lucas@semalead.com>
2013-12-15 19:20:28 +0100

    Update php*-gmagick to 1.1.6RC2

--- Formula/php54-gmagick.rb
+++ Formula/php54-gmagick.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Gmagick < AbstractPhp54Extension
   init
   homepage 'http://pecl.php.net/package/gmagick'
-  url 'http://pecl.php.net/get/gmagick-1.1.5RC1.tgz'
-  sha1 '95d9a7c8f7ae9b88de236be2cfd71df77fbfd968'
+  url 'http://pecl.php.net/get/gmagick-1.1.6RC2.tgz'
+  sha1 '8779b2d8d1b836f2f69bc93744a905b277ab2690'
 
   depends_on "graphicsmagick"
 
