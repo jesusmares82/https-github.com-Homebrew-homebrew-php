b9e2e51d5b2f53d89c80fa57da3922acd10d5463
Maurus Cuelenaere <mcuelenaere@gmail.com>
2013-12-26 13:31:08 +0100

    Sigh, fix typo (+2 squashed commits) Squashed commits: [17df993] Address remarks [333ff67] Add Blitz templater extension for PHP 5.{3,4,5}

---
+++ Formula/php53-blitz.rb
@@ -0,0 +1,43 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Blitz < AbstractPhp53Extension
+  init
+  homepage 'http://alexeyrybak.com/blitz/blitz_en.html'
+  url 'http://alexeyrybak.com/blitz/blitz-0.8.6.tar.gz'
+  sha1 '688ceb3579d9da6cd0b84122445ebe7c847c4525'
+
+  def install
+    safe_phpize
+
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/blitz.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+      blitz.auto_escape=0
+      blitz.check_recursion=1
+      blitz.comment_close="*/"
+      blitz.comment_open="/*"
+      blitz.enable_alternative_tags=1
+      blitz.enable_callbacks=1
+      blitz.enable_comments=0
+      blitz.enable_include=1
+      blitz.enable_php_callbacks=1
+      blitz.lower_case_method_names=1
+      blitz.path=""
+      blitz.php_callbacks_first=1
+      blitz.remove_spaces_around_context_tags=1
+      blitz.scope_lookup_limit=0
+      blitz.tag_close="}}"
+      blitz.tag_close_alt="-->"
+      blitz.tag_open="{{"
+      blitz.tag_open_alt="<!--"
+      blitz.throw_exceptions=0
+      blitz.var_prefix="$"
+      blitz.warn_context_duplicates=0
+    EOS
+  end
+end
