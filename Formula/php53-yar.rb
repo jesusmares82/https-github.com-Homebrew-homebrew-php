5133dded7d7a09127dd7d5a9291ccba19661be78
Lucas Michot <lucas@semalead.com>
2014-01-01 17:01:50 +0100

    Add php*-yar extension 1.2.2

---
+++ Formula/php53-yar.rb
@@ -0,0 +1,20 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Yar < AbstractPhp53Extension
+  init
+  homepage 'http://pecl.php.net/package/yar'
+  url 'http://pecl.php.net/get/yar-1.2.2.tgz'
+  sha1 '882937c7892a4252ec03714f783317c4a1df66f3'
+
+  def install
+    Dir.chdir "yar-#{version}"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/yar.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
