c56c0b075ea5f884977e2e6e541228d251290bc8
Lucas Michot <lucas@semalead.com>
2014-01-01 16:51:30 +0100

    Add php*-swoole 1.6.8

---
+++ Formula/php53-swoole.rb
@@ -0,0 +1,21 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Swoole < AbstractPhp53Extension
+  init
+  homepage 'https://github.com/matyhtf/swoole'
+  url 'http://pecl.php.net/get/swoole-1.6.8.tgz'
+  sha1 'fd45cc0e34ccbcf1060b2d5e8a816b241b8b3ef9'
+  head 'https://github.com/matyhtf/swoole.git'
+
+  def install
+    Dir.chdir "swoole-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/swoole.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
