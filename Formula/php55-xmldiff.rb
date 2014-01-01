85f6e3971bb7e4052cad8037c734e1ca49e1a73e
Lucas Michot <lucas@semalead.com>
2014-01-01 17:05:05 +0100

    Update php*-xmldiff to 0.9.2

--- Formula/php55-xmldiff.rb
+++ Formula/php55-xmldiff.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Xmldiff < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/xmldiff'
-  url 'http://pecl.php.net/get/xmldiff-0.9.1.tgz'
-  sha1 '93f790182d017c307da3d56d3fb3d1c1a89a83e1'
+  url 'http://pecl.php.net/get/xmldiff-0.9.2.tgz'
+  sha1 '7cc2b31a34ea9bbce23d163154f4a86bb819bc17'
 
   def install
     Dir.chdir "xmldiff-#{version}"
@@ -12,8 +12,7 @@ class Php55Xmldiff < AbstractPhp55Extension
     ENV.universal_binary if build.universal?
 
     safe_phpize
-    system "./configure", "--prefix=#{prefix}",
-                          phpconfig
+    system "./configure", "--prefix=#{prefix}", phpconfig
     system "make"
     prefix.install "modules/xmldiff.so"
     write_config_file unless build.include? "without-config-file"
