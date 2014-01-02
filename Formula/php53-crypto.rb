842ccdef0a606f5c5d2074ebf513beae27e78de4
Lucas Michot <lucas@semalead.com>
2014-01-02 08:54:43 +0100

    Add php*-crypto 0.1.0

---
+++ Formula/php53-crypto.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Crypto < AbstractPhp53Extension
+  init
+  homepage 'http://pecl.php.net/package/crypto'
+  url 'http://pecl.php.net/get/crypto-0.1.0.tgz'
+  sha1 '91294bbbc672782899a980001519a25b58041e09'
+  head 'https://github.com/bukka/php-crypto.git'
+
+  def install
+    Dir.chdir "crypto-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    args = []
+    args << "--with-crypto"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/crypto.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
