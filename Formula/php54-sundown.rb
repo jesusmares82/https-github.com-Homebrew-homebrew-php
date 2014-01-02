4e5918c015cb7126995b2068be1ab0fc64e726e6
Lucas Michot <lucas@semalead.com>
2014-01-02 20:39:18 +0100

    Upgrade php*-sundown to 0.3.11

--- Formula/php54-sundown.rb
+++ Formula/php54-sundown.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Sundown < AbstractPhp54Extension
   init
   homepage 'http://pecl.php.net/package/sundown'
-  url 'http://pecl.php.net/get/sundown-0.3.9.tgz'
-  sha1 '27a5f0215e73b9270acc4c27bcfde187b68ea6d4'
+  url 'http://pecl.php.net/get/sundown-0.3.11.tgz'
+  sha1 'e905cf2cd3540480af598e971a3c37c769775351'
 
   def install
     Dir.chdir "sundown-#{version}"
