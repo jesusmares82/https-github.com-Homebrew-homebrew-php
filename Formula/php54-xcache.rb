b1e99d83f8bea16a810c23b8bd407004b765df47
ando <lifeandcoding@gmail.com>
2014-01-26 00:51:22 +0300

    XCache is no longer a zend extension.

--- Formula/php54-xcache.rb
+++ Formula/php54-xcache.rb
@@ -6,8 +6,6 @@ class Php54Xcache < AbstractPhp54Extension
   url 'http://xcache.lighttpd.net/pub/Releases/3.1.0/xcache-3.1.0.tar.bz2'
   sha1 'db30f642de9bc97538fe4f85940e8a69a1a365c8'
 
-  def extension_type; "extension"; end
-
   def install
     ENV.universal_binary if build.universal?
 
