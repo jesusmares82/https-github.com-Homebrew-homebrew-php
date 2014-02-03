7582c0537a28b8d24d79ffb37a6eca8cfa75978e
Maurus Cuelenaere <mcuelenaere@gmail.com>
2014-02-03 10:20:09 +0100

    php5{3,4,5}-protobuf: use 0.10 release

--- Formula/php54-protobuf.rb
+++ Formula/php54-protobuf.rb
@@ -4,9 +4,8 @@ class Php54Protobuf < AbstractPhp54Extension
   init
   homepage 'https://github.com/allegro/php-protobuf'
   head 'https://github.com/allegro/php-protobuf.git'
-  url 'https://github.com/allegro/php-protobuf/archive/5839c25f1472736637e8c0484e542135f5636146.zip'
-  sha1 'f99207901fa79c157009e7f9498f119115fc9dfe'
-  version '5839c25'
+  url 'https://github.com/allegro/php-protobuf/archive/0.10.tar.gz'
+  sha1 'a8553d4090f4da4882a16f51c50319eba5704be2'
 
   def install
     safe_phpize
