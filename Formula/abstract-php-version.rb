00ec70cbfd5660f72ee0ca00df8795cfccc34a84
Andy Blyler <andy@blyler.cc>
2014-02-13 21:29:22 -0500

    Bump php56 to 5.6.0alpha2

--- Formula/abstract-php-version.rb
+++ Formula/abstract-php-version.rb
@@ -59,15 +59,15 @@ class AbstractPhpVersion < Formula
   end
 
   module Php56Defs
-    PHP_SRC_TARBALL = 'http://downloads.php.net/tyrael/php-5.6.0alpha1.tar.bz2'
+    PHP_SRC_TARBALL = 'http://downloads.php.net/tyrael/php-5.6.0alpha2.tar.bz2'
     PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
-    PHP_VERSION     = '5.6.0-alpha.1'
+    PHP_VERSION     = '5.6.0-alpha.2'
     PHP_BRANCH      = 'PHP-5.6'
 
     PHP_CHECKSUM    = {
-                        :md5    => 'd1ac1df6ff701546a005e2d9799d2002',
-                        :sha1   => '08dec1cd5ef3acccaafd7ca79f5a8b22643e7d65',
-                        :sha256 => '17123fd1b07aa1cd16eedc9ae653dfbd560e2b7da95961f546334fda14bba804',
+                        :md5    => '5a28f05cae2b0077f4126bdc19179dd6',
+                        :sha1   => 'f5d53a293805c723d0e4aeb8f3db8b08fdac99d5',
+                        :sha256 => '7ad8990bbe6ce2cbb384ad22ecc14385098d6e3c9d4ee1ddd386bc6df3dea504',
                       }
   end
 end
