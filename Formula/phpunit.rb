d1566baff11d9bd8a206adb6047102a3fb031645
timothykang <timkang@ucla.edu>
2014-02-15 18:56:56 -0800

    Update PHPUnit to v3.7.30

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Phpunit < Formula
   homepage 'http://phpunit.de/manual/current/en/'
-  url 'https://github.com/sebastianbergmann/phpunit/releases/download/3.7.28/phpunit.phar'
-  sha1 '4acc07c1730d85e2016a1ed613a1355a9d271636'
-  version '3.7.28'
+  url 'https://github.com/sebastianbergmann/phpunit/releases/download/3.7.30/phpunit.phar'
+  sha1 'b3215880ddcd888f2117554bd101818139b4bd96'
+  version '3.7.30'
 
   def install
     mv "phpunit.phar", "phpunit-#{version}.phar"
