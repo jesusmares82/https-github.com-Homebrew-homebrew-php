6605021d372b52a0a5894c68a1ecd2e9b43ab126
Gabriel Bretschner <software@kanedo.net>
2014-02-28 12:58:19 +0100

    Relies on an alternative versioned URL (+1 squashed commit) Squashed commits: [46ac292] PHPUnit-Skelgen (v1.2.1) new Formula Added a formula to install phpunit-skelgen

---
+++ Formula/phpunit-skeleton-generator.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class PhpunitSkeletonGenerator < Formula
+  homepage 'http://phpunit.de/manual/current/en/'
+  url 'https://phar.phpunit.de/phpunit-skelgen-1.2.1.phar'
+  sha1 '104c4f0a88ad5cbcc1ab18c449fffc22a56344f6'
+
+  def install
+    libexec.install "phpunit-skelgen-#{version}.phar"
+    sh = libexec + "phpunit-skelgen"
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpunit-skelgen-#{version}.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+end
