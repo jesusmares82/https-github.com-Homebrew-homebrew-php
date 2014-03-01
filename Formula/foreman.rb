e0287232d129d9dbc15bcd620f18f77483c75820
Lucas Michot <lucas@semalead.com>
2014-03-01 09:46:44 +0100

    Foreman 1.0.0

---
+++ Formula/foreman.rb
@@ -0,0 +1,35 @@
+require 'formula'
+require File.expand_path("../../Requirements/php-meta-requirement", Pathname.new(__FILE__).realpath)
+require File.expand_path("../../Requirements/phar-requirement", Pathname.new(__FILE__).realpath)
+
+class Foreman < Formula
+  homepage 'https://github.com/Indatus/foreman'
+  url 'https://github.com/Indatus/foreman/raw/v1.0.0/foreman.phar'
+  sha1 '787d7674f47faad1df9484bd106673c23827cf16'
+  version '1.0.0'
+
+  depends_on PhpMetaRequirement
+  depends_on PharRequirement
+
+  def install
+    libexec.install "foreman.phar"
+    sh = libexec + "foreman"
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/foreman.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  def test
+    system 'foreman'
+  end
+
+  def caveats; <<-EOS.undent
+    Verify your installation by running:
+      "foreman".
+
+    You can read more about foreman by running:
+      "brew home foreman".
+    EOS
+  end
+
+end
