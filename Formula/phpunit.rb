d2d83fa1d045c705f8b59977ea39d60182c477a5
Ruud Kamphuis <ruudk@mphuis.com>
2014-03-07 11:59:06 +0100

    removed redundant version (+1 squashed commit) Squashed commits: [82d812b] PHPUnit 4.0.1

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -2,17 +2,14 @@ require 'formula'
 
 class Phpunit < Formula
   homepage 'http://phpunit.de/manual/current/en/'
-  url 'https://github.com/sebastianbergmann/phpunit/releases/download/3.7.30/phpunit.phar'
-  sha1 'b3215880ddcd888f2117554bd101818139b4bd96'
-  version '3.7.30'
+  url 'https://phar.phpunit.de/phpunit-4.0.1.phar'
+  sha1 '9a18b119232a13eb80f29d769cb85df6e790fcb3'
 
   def install
-    mv "phpunit.phar", "phpunit-#{version}.phar"
     libexec.install "phpunit-#{version}.phar"
     sh = libexec + "phpunit"
     sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpunit-#{version}.phar $*")
     chmod 0755, sh
     bin.install_symlink sh
   end
-
 end
