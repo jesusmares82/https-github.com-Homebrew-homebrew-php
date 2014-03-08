e8feb59138b39cf41763d21dd26f7349f3df53c9
Jesus A. Domingo <jesus.domingo@gmail.com>
2014-03-08 22:39:41 +0800

    added virtphp formula
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/virtphp.rb
@@ -0,0 +1,31 @@
+require 'formula'
+require File.expand_path("../../Requirements/php-meta-requirement", Pathname.new(__FILE__).realpath)
+require File.expand_path("../../Requirements/phar-requirement", Pathname.new(__FILE__).realpath)
+
+class Virtphp < Formula
+  homepage 'http://virtphp.org'
+  url 'https://github.com/virtphp/virtphp/releases/download/v0.2.0-alpha/virtphp.phar'
+  sha1 '332c2c366d18bf13cc26847340e818f1dc5634f4'
+  version '0.2.0-alpha'
+
+  depends_on PhpMetaRequirement
+  depends_on PharRequirement
+
+  def install
+    libexec.install "virtphp.phar"
+    sh = libexec + "virtphp"
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/virtphp.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  def caveats; <<-EOS.undent
+    Verify your installation by running:
+      "virtphp --version".
+
+    You can read more about virtphp by running:
+      "brew home virtphp".
+    EOS
+  end
+
+end
