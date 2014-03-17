cadb2c3f68d2daf0afc999cd19cf41b166e36cb6
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2014-03-17 20:12:29 -0300

    twig: upgrade to 1.15.1

--- Formula/php54-twig.rb
+++ Formula/php54-twig.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Twig < AbstractPhp54Extension
   init
   homepage 'http://twig.sensiolabs.org/'
-  url 'https://github.com/fabpot/Twig/archive/v1.13.2.tar.gz'
-  sha1 '231ee99dc313b38cfde60ca903777f98d40daf2f'
+  url 'https://github.com/fabpot/Twig/archive/v1.15.1.tar.gz'
+  sha1 '6431a8544f3a95f01d3e36d723daec037131629a'
   head 'https://github.com/fabpot/Twig.git'
 
   def install
