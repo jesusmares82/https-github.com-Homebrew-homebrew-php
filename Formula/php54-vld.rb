46dbcd0a3ce27e800b6c4281d56cd9f1c82fb6ba
denji <denji0k@gmail.com>
2014-03-18 02:45:06 +0200

    php*-vld in github --HEAD (+1 squashed commit) Squashed commits: [a261a19] php*-vld: Logic Dumper hooks into the Zend Engine and dumps all the opcodes
    The Vulcan Logic Dumper hooks into the Zend Engine and dumps all the
    opcodes (execution units) of a script. It can be used to see what is
    going on in the Zend Engine.
    
    This is an extension for advanced users and can not be used to
    un-encode PHP code that has been encoded with any encoder. If you can't
    make it work then this tool is not for you. If you are certain there is
    a bug, then please provide a patch (or a good bug report).

---
+++ Formula/php54-vld.rb
@@ -0,0 +1,22 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Vld < AbstractPhp54Extension
+  init
+  homepage 'http://pecl.php.net/package/vld'
+  url 'http://pecl.php.net/get/vld-0.12.0.tgz'
+  sha1 'c4006aeed9fd8190501f6b5d6c20315259f2795a'
+  head 'https://github.com/derickr/vld.git'
+
+  def install
+    Dir.chdir "vld-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/vld.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
