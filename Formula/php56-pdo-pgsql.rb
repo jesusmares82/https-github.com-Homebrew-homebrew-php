faf0a4ad591623e493bb881d4c7450049e531991
vicchi <gary@vicchi.org>
2014-03-26 07:18:18 -0700

    Add PDO PostgreSQL support

---
+++ Formula/php56-pdo-pgsql.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php56PdoPgsql < AbstractPhp56Extension
+  init
+  homepage 'https://github.com/php/php-src/tree/master/ext/pdo_pgsql'
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on 'postgresql'
+
+  def extension
+    "pdo_pgsql"
+  end
+
+  def install
+    Dir.chdir "ext/pdo_pgsql"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", "--with-pdo-pgsql=#{Formula['postgresql'].prefix}", phpconfig
+    system "make"
+    prefix.install "modules/pdo_pgsql.so"
+    write_config_file if build.with? "config-file"
+  end
+end
