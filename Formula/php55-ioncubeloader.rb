d7da8f05ceec9cdbda8ff5bd64d3e74fc1d62b0c
Lucas Michot <lucas@semalead.com>
2014-04-05 11:00:19 +0200

    Upgrade iconcube loader to 4.6.0

--- Formula/php55-ioncubeloader.rb
+++ Formula/php55-ioncubeloader.rb
@@ -4,13 +4,13 @@ class Php55Ioncubeloader < AbstractPhp55Extension
   init
   homepage 'http://www.ioncube.com/loaders.php'
   if Hardware.is_64_bit?
-    version '4.5.3'
+    version '4.6.0'
     url 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz'
-    sha1 '8db9d893b8be25519fc5cbebe76895fdfb510992'
+    sha1 '39cdebc7bd77b0fd21bf2a9ed8df5e620a9d6ed5'
   else
-    version '4.5.3'
+    version '4.6.0'
     url 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86.tar.gz'
-    sha1 'e42a3fa4869449771c245891ed5f2fab2ed44e64'
+    sha1 '5c36e22544c2e39c3c5a4ff8490d2bfb9b6cf4b8'
   end
 
   def extension_type; "zend_extension"; end
