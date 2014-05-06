ec60bf9d9f71da23b9f343a3b27c45cbb4b410fa
Lucas Michot <lucas@semalead.com>
2014-05-06 19:58:55 +0200

    Upgrade php*-sphinx to 1.3.1

--- Formula/php55-sphinx.rb
+++ Formula/php55-sphinx.rb
@@ -4,8 +4,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Sphinx < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/sphinx'
-  url 'http://pecl.php.net/get/sphinx-1.3.0.tgz'
-  sha1 '87a25a6dc8fecaade453ca55ff5d1822bbef0b1a'
+  url 'http://pecl.php.net/get/sphinx-1.3.1.tgz'
+  sha1 '969ecd6dbff65e7af67b5c448af8a0015fd494d0'
 
   depends_on 'libsphinxclient'
 
