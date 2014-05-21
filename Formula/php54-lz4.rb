c1a92421fb126f21a57524c058ae665d1561121b
Lucas Michot <lucas@semalead.com>
2014-05-21 13:13:51 +0200

    Add lz4 extension

---
+++ Formula/php54-lz4.rb
@@ -0,0 +1,18 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Lz4 < AbstractPhp54Extension
+  init
+  homepage 'https://code.google.com/p/lz4/'
+  url 'https://github.com/kjdev/php-ext-lz4/archive/0.2.0.tar.gz'
+  sha1 '5622f65c1f357b7cf5cd7542fad811f582151883'
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/lz4.so"
+    write_config_file if build.with? "config-file"
+  end
+end
