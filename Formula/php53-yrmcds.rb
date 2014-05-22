8238e03df97fd890bf3e332b88729481a46fd280
Konstantin Kuklin <konstantin.kuklin@gmail.com>
2014-05-22 21:58:05 +0400

    refactored with @lucasmichot advices (+1 squashed commit) Squashed commits: [27776ae] added formulas for installing yrmcds extension to php by home brew

---
+++ Formula/php53-yrmcds.rb
@@ -0,0 +1,26 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php53Yrmcds < AbstractPhp53Extension
+  init
+  homepage 'https://github.com/cybozu/php-yrmcds'
+  url 'https://github.com/cybozu/php-yrmcds/archive/v1.0.2.tar.gz'
+  sha1 'eeb6689822071aa7d7705a5e838a341af3976484'
+  head 'https://github.com/cybozu/php-yrmcds.git'
+
+  def patches
+    return [
+        "https://gist.githubusercontent.com/KonstantinKuklin/4cebe58997e2152cdc35/raw/09a5c4cd1a2739d2d9e0e38338d4e9d01058f914/patch_php-yrmcds_mac.patch"
+    ]
+  end
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+
+    system "make"
+    prefix.install "modules/yrmcds.so"
+    write_config_file if build.with? "config-file"
+  end
+end
