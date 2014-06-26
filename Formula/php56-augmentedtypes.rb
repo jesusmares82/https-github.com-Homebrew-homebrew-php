ddf424292c4c83404db16146e7a6e4f11ac33c47
Lucas Michot <lucas@semalead.com>
2014-06-26 21:09:19 +0200

    Added augmentedtypes 0.6.2 for PHP 5.6

---
+++ Formula/php56-augmentedtypes.rb
@@ -0,0 +1,31 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php56Augmentedtypes < AbstractPhp56Extension
+  init
+  homepage 'https://github.com/box/augmented_types'
+  url 'https://github.com/box/augmented_types/archive/v0.6.2.tar.gz'
+  sha1 'fcb4223b94f4af09d9a8fa67917e945f89faafe5'
+  head 'https://github.com/box/augmented_types.git'
+
+  def extension_type; "zend_extension"; end
+
+  def extension
+    "augmented_types"
+  end
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-augmented_types"
+    system "make"
+    prefix.install "modules/augmented_types.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+      augmented_types.enforce_by_default = 1
+    EOS
+  end
+end
