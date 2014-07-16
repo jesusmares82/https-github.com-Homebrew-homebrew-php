f947b09da7eee2ed27921c4bd00fdbc380ccfa5c
Lucas Michot <lucas@semalead.com>
2014-07-16 20:43:25 +0200

    Enable PHP 5.6 meta requirements

--- Requirements/php-meta-requirement.rb
+++ Requirements/php-meta-requirement.rb
@@ -2,13 +2,13 @@ require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
 
 class PhpMetaRequirement < HomebrewPhpRequirement
   def satisfied?
-    %w{php53 php54 php55}.any? do |php|
+    %w{php53 php54 php55 php56}.any? do |php|
         f = Formula[php]
         f.rack.directory? && f.rack.children.length > 0
     end
   end
 
   def message
-    "Missing PHP53, PHP54 or PHP55 from homebrew-php. Please install one of them before continuing"
+    "Missing PHP53, PHP54, PHP55 or PHP56 from homebrew-php. Please install one of them before continuing"
   end
 end
