9c1614a5eca696ab1bb4ef0a4e0bcb1de1e94719
Andy Blyler <andy@blyler.cc>
2014-08-10 11:32:57 -0400

    add pecl event to php5[4-6] (php53 not supported by this ext)

---
+++ Formula/php56-event.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php56Event < AbstractPhp56Extension
+  init
+  homepage 'http://pecl.php.net/package/event'
+  url 'http://pecl.php.net/get/event-1.10.3.tgz'
+  sha1 '0d8881b0b157493d399fed9211bc242f1b416245'
+  head 'https://bitbucket.org/osmanov/pecl-event.git'
+
+  depends_on 'libevent' => :build
+
+  def install
+    Dir.chdir "event-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    args = []
+    args << "--with-event"
+    args << "--with-event-libevent-dir=#{Formula['libevent'].opt_prefix}"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/event.so"
+    write_config_file if build.with? "config-file"
+  end
+end
