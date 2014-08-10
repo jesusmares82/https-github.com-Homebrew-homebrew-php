fa3d802304616a522d3f5ed269690d49d0f97c06
Andy Blyler <andy@blyler.cc>
2014-08-10 13:22:11 -0400

    fpm: change listen.mode to 0666 during install (refs issue #1153)

--- Formula/abstract-php.rb
+++ Formula/abstract-php.rb
@@ -391,6 +391,7 @@ INFO
         inreplace config_path+"php-fpm.conf" do |s|
           s.sub!(/^;?daemonize\s*=.+$/,'daemonize = no')
           s.sub!(/^;include\s*=.+$/,";include=#{config_path}/fpm.d/*.conf")
+          s.sub!(/^;?listen\.mode\s*=.+$/,'listen.mode = 0666')
           s.sub!(/^;?pm\.max_children\s*=.+$/,'pm.max_children = 10')
           s.sub!(/^;?pm\.start_servers\s*=.+$/,'pm.start_servers = 3')
           s.sub!(/^;?pm\.min_spare_servers\s*=.+$/,'pm.min_spare_servers = 2')
