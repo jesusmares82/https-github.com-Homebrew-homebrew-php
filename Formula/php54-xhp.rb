b1f15d489257de5bd4036bd1ae440e8019f01ab0
Andy Blyler <andy@blyler.cc>
2014-08-10 14:34:42 -0400

    php-xhp: fix issue installing when clang build <> 500 (refs issue #1240)

--- Formula/php54-xhp.rb
+++ Formula/php54-xhp.rb
@@ -12,7 +12,6 @@ class Php54Xhp < AbstractPhp54Extension
   depends_on 're2c'
 
   fails_with :clang do
-    build 500
     cause <<-EOS.undent
       Clang which does not support the standard rope implementation. Use gcc instead.
       Source: https://github.com/facebook/xhp/commit/8aa2d9fa6bf3a114e113ba5c554181279545fb2f
