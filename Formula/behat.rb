869ee9eed06e75fc453796d18516a399fc475986
Lucas Michot <lucas@semalead.com>
2014-08-15 13:17:44 +0200

    Update Behat to 3.0.12 and download phar from Github

--- Formula/behat.rb
+++ Formula/behat.rb
@@ -4,16 +4,17 @@ require File.expand_path("../../Requirements/phar-requirement", __FILE__)
 
 class Behat < Formula
   homepage 'http://behat.org/'
-  url 'http://behat.org/downloads/behat-2.4.5.phar'
-  sha1 '0c9b1950c106d0ad52a96010cf68484d2cfcd837'
+  url 'https://github.com/Behat/Behat/releases/download/v3.0.12/behat.phar'
+  sha1 'dea2f88668f64e300e2f5b0be6789a14af6092b7'
+  version '3.0.12'
 
   depends_on PhpMetaRequirement
   depends_on PharRequirement
 
   def install
-    libexec.install "behat-#{version}.phar"
+    libexec.install "behat.phar"
     sh = libexec + "behat"
-    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/behat-#{version}.phar $*")
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/behat.phar $*")
     chmod 0755, sh
     bin.install_symlink sh
   end
