fb260760076d3ce7eadf216c9846ad4856574e4a
Jack Nagel <jacknagel@gmail.com>
2014-09-23 23:55:28 -0500

    Simplify formulae that depend on zookeeper
    I fixed the zookeeper formula in core to always build the C libraries,
    so we can simplify some things.

--- Formula/php56-zookeeper.rb
+++ Formula/php56-zookeeper.rb
@@ -1,18 +1,14 @@
 require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Zookeeper < AbstractPhp56Extension
+  init
   homepage 'http://pecl.php.net/package/zookeeper'
   url 'http://pecl.php.net/get/zookeeper-0.2.2.tgz'
   sha1 '029c0d9c989c56c5afead8bb4621b7f2236a7b6f'
   head 'https://github.com/andreiz/php-zookeeper.git'
 
-  def self.init
-    super
-    option 'disable-session', 'Disable zookeeper session handler support'
-    depends_on 'zookeeper' => '--c'
-  end
-
-  init
+  option 'disable-session', 'Disable zookeeper session handler support'
+  depends_on 'zookeeper'
 
   def install
     Dir.chdir "zookeeper-#{version}" unless build.head?
@@ -32,14 +28,4 @@ class Php56Zookeeper < AbstractPhp56Extension
     prefix.install "modules/zookeeper.so"
     write_config_file if build.with? "config-file"
   end
-
-  def caveats
-    super + <<-EOS.undent
-
-      Note that zookeeper must be installed with the --c option for the
-      PHP extension to build.
-
-    EOS
-  end
-
 end
