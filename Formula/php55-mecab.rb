f348c6df4232fa0b0e73e32a0aea3fa74e3bd1d6
n0ts <naoya.n@gmail.com>
2014-11-16 00:27:34 +0900

    Add MeCab binding for PHP

---
+++ Formula/php55-mecab.rb
@@ -0,0 +1,23 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php55Mecab < AbstractPhp55Extension
+  init
+  homepage 'https://github.com/rsky/php-mecab'
+  url 'https://github.com/downloads/rsky/php-mecab/php-mecab-0.5.0.tgz'
+  sha1 '3f451e67f736b8d4a8b494cb0333cfbb5fa4a274'
+  head 'https://github.com/rsky/php-mecab.git'
+
+  depends_on "mecab"
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-mecab=#{HOMEBREW_PREFIX}/opt/mecab",
+                          phpconfig
+    system "make"
+    prefix.install "modules/mecab.so"
+    write_config_file if build.with? "config-file"
+  end
+end
