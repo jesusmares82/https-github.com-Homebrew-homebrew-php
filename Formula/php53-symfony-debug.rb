c50e6d44966c9f4b4c70144b2bffb181a545f4b9
Romain Neutron <imprec@gmail.com>
2014-12-02 13:25:10 +0100

    Add formulae for Symfony Debug extension

---
+++ Formula/php53-symfony-debug.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php53SymfonyDebug < AbstractPhp53Extension
+  init
+  homepage 'http://symfony.com/'
+  url 'https://github.com/symfony/Debug/archive/v2.6.0.tar.gz'
+  sha1 'ea9aa9791acb62dd2e87d90e1bf3499e70483b89'
+  head 'https://github.com/symfony/Debug.git'
+
+  def extension
+    "symfony_debug"
+  end
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    Dir.chdir 'Resources/ext' do
+      safe_phpize
+      system "./configure", "--prefix=#{prefix}",
+             phpconfig
+      system "make"
+      prefix.install %w(modules/symfony_debug.so)
+    end
+    write_config_file if build.with? "config-file"
+  end
+end
