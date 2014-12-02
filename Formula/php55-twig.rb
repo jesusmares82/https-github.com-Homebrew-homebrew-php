1f3a7deabbc95332435d72bec8a245a57ead7695
Romain Neutron <imprec@gmail.com>
2014-12-02 13:55:53 +0100

    Update repository address and bump version for Twig extension

--- Formula/php55-twig.rb
+++ Formula/php55-twig.rb
@@ -3,9 +3,9 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php55Twig < AbstractPhp55Extension
   init
   homepage 'http://twig.sensiolabs.org/'
-  url 'https://github.com/fabpot/Twig/archive/v1.16.0.tar.gz'
-  sha1 '078d346665f30c32d0c33e19c8e8075b7955d156'
-  head 'https://github.com/fabpot/Twig.git'
+  url 'https://github.com/twigphp/Twig/archive/v1.16.2.tar.gz'
+  sha1 '926ff53dad6ccd2560a562942d1dfd1357460cc0'
+  head 'https://github.com/twigphp/Twig.git'
 
   def install
     ENV.universal_binary if build.universal?
