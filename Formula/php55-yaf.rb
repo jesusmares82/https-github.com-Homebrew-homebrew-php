b9293e1bc345a713e2c7341d22f83633ab33c911
Tom Moitie <tomm@carboncreative.net>
2015-01-07 15:34:20 +0000

    Update yaf extension to 2.3.3 to fix #1487
    Fixes #1487.
    Closes #1496.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/php55-yaf.rb
+++ Formula/php55-yaf.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php55Yaf < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/yaf'
-  url 'http://pecl.php.net/get/yaf-2.3.2.tgz'
-  sha1 '03a59027a9b3d7b20b898238cb5322e346a5a907'
+  url 'http://pecl.php.net/get/yaf-2.3.3.tgz'
+  sha1 'dc2bf8d875a659b0159abea317b87e4651dc50d9'
   head 'https://svn.php.net/repository/pecl/yaf/trunk/'
 
   depends_on 'pcre'
