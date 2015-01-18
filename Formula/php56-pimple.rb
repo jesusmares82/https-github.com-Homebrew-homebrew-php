7f9207daca6f4a95062b4509e98f143bcb9251bf
Jesus A. Domingo <jesus.domingo@gmail.com>
2015-01-19 00:26:00 +0800

    added pimple extension formula (53-56)
    Closes #1509.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

---
+++ Formula/php56-pimple.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php56Pimple < AbstractPhp56Extension
+  init
+  homepage "http://pimple.sensiolabs.org/"
+  url "https://github.com/silexphp/Pimple/archive/v3.0.0.tar.gz"
+  sha1 "4ded64814812e3ec2f955b76cdcfab99831d7318"
+  head "https://github.com/silexphp/Pimple.git"
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    Dir.chdir "ext/pimple" do
+      safe_phpize
+      system "./configure", "--prefix=#{prefix}",
+                            phpconfig
+      system "make"
+      prefix.install %w[modules/pimple.so]
+    end
+    write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("pimple")
+  end
+end
