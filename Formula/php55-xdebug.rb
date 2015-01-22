74fff299e3b7e498c46f2461b938647d3999e003
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-22 10:12:37 +0000

    php55-xdebug: add 2.2.7 bottle.

--- Formula/php55-xdebug.rb
+++ Formula/php55-xdebug.rb
@@ -7,6 +7,13 @@ class Php55Xdebug < AbstractPhp55Extension
   sha1 '587d300b8df0d1213910c59dda0c4f5807233744'
   head 'https://github.com/xdebug/xdebug.git'
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/php"
+    sha1 "93fb4659e78d4c98150dbc6241aed7ffed4aab34" => :yosemite
+    sha1 "52b3bca92610cf9b346affed7806311c9a6e1750" => :mavericks
+    sha1 "f54f0f3da1734dee520759c20c55070a8f482a48" => :mountain_lion
+  end
+
   def extension_type; "zend_extension"; end
 
   def install
