d19a854a4f98a38e118611056546ae99a85d6071
Lucas Michot <lucas@semalead.com>
2015-01-30 17:37:00 +0100

    Update php*-mongo to 1.6.0

--- Formula/php53-mongo.rb
+++ Formula/php53-mongo.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php53Mongo < AbstractPhp53Extension
   init
   homepage 'http://pecl.php.net/package/mongo'
-  url 'http://pecl.php.net/get/mongo-1.5.8.tgz'
-  sha1 '768fa06a14d74fdde351f3757230d1f5660ef9c0'
+  url 'http://pecl.php.net/get/mongo-1.6.0.tgz'
+  sha1 '3c5e4f595793a11e1542266f8f71936563a13850'
   head 'https://github.com/mongodb/mongo-php-driver.git'
 
   def install
