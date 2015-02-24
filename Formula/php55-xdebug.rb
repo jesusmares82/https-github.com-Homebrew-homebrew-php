24dd9812d2c68de6b71c1fdd9f76e45b31593e23
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-24 23:47:50 +0000

    php55-xdebug: add 2.3.1 bottle.

--- Formula/php55-xdebug.rb
+++ Formula/php55-xdebug.rb
@@ -7,6 +7,13 @@ class Php55Xdebug < AbstractPhp55Extension
   sha1 'e552cea58e533793d904b62713d7da5546028c76'
   head 'https://github.com/xdebug/xdebug.git'
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha1 "04024df55dfabee6255d4a9e7de19cfacd770bb9" => :yosemite
+    sha1 "06b16bcfb2ee7d2ed4360bd5914fed6e090e29cd" => :mavericks
+    sha1 "fbaeb1691fdd7bf8d4e719eeeb97d23de2d8ba17" => :mountain_lion
+  end
+
   def extension_type; "zend_extension"; end
 
   def install
