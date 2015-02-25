9333ad0f3cbbdeaffa09ab3baba45d7443794989
Alan Ivey <alanivey@gmail.com>
2015-02-25 10:05:07 -0500

    php53-xdebug: add 2.2.7 bottle.

--- Formula/php53-xdebug.rb
+++ Formula/php53-xdebug.rb
@@ -7,6 +7,13 @@ class Php53Xdebug < AbstractPhp53Extension
   sha1 '587d300b8df0d1213910c59dda0c4f5807233744'
   head 'https://github.com/xdebug/xdebug.git'
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha1 "f1f25dcf4bd5a119d8a11eebd82122a47348c670" => :yosemite
+    sha1 "9a35843631103fc0d815a72e480fda7bb52d61b0" => :mavericks
+    sha1 "37fb04e8256a25e022e1f418138872a32c9b54c7" => :mountain_lion
+  end
+
   def extension_type; "zend_extension"; end
 
   def install
