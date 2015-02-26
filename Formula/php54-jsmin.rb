f16e8efd1d2c735f6ad9179064d8de0f881e58e2
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2015-02-26 10:45:32 -0500

    php54-jsmin version 1.1.0
    - Update to the latest from upstream
    
    Closes #1560.
    
    Signed-off-by: Alan Ivey <alan@echo.co>

--- Formula/php54-jsmin.rb
+++ Formula/php54-jsmin.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php54Jsmin < AbstractPhp54Extension
   init
   homepage 'http://pecl.php.net/package/jsmin'
-  url 'http://pecl.php.net/get/jsmin-0.1.1.tgz'
-  sha1 'b1abfb443ce5df80866fc9350a792f3be814f568'
+  url 'http://pecl.php.net/get/jsmin-1.1.0.tgz'
+  sha1 'e081d7c66a9401b9cd8b0ad585f357a4d7e335ef'
   head 'https://github.com/sqmk/pecl-jsmin.git'
 
   def install
