83f0f7e3f018666e6091475542671699a52d3e5f
Alexander Turiak <alex@hexbrain.com>
2015-02-27 12:22:16 +0200

    Update php55-ioncubeloader to 4.7.5
    Closes #1567 and #1570.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/php55-ioncubeloader.rb
+++ Formula/php55-ioncubeloader.rb
@@ -2,21 +2,25 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php55Ioncubeloader < AbstractPhp55Extension
   init
-  homepage 'http://www.ioncube.com/loaders.php'
+  homepage "http://www.ioncube.com/loaders.php"
   if Hardware.is_64_bit?
-    version '4.7.1'
-    url 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz'
-    sha1 '1d255625981c1117195bf526d431852104e57c9c'
+    version "4.7.5"
+    url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz"
   else
-    version '4.7.1'
-    url 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86.tar.gz'
-    sha1 'dcdff3b742a4ff04c71b52f7ad3c8fb74b1e845f'
+    version "4.7.5"
+    url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86.tar.gz"
   end
 
-  def extension_type; "zend_extension"; end
+  def extension_type
+    "zend_extension"
+  end
 
   def install
     prefix.install "ioncube_loader_dar_5.5.so" => "ioncubeloader.so"
     write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("ionCube")
   end
 end
