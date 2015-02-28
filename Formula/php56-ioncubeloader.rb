fc9bf58b001c8aca6cfe051f1e781e3558bfab42
Alexander Turiak <alex@hexbrain.com>
2015-02-28 18:26:54 +0200

    Update php56-ioncubeloader to 4.7.5
    Closes #1568.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

---
+++ Formula/php56-ioncubeloader.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php56Ioncubeloader < AbstractPhp56Extension
+  init
+  homepage "http://www.ioncube.com/loaders.php"
+  if Hardware.is_64_bit?
+    version "4.7.5"
+    url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz"
+  else
+    version "4.7.5"
+    url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86.tar.gz"
+  end
+
+  def extension_type
+    "zend_extension"
+  end
+
+  def install
+    prefix.install "ioncube_loader_dar_5.6.so" => "ioncubeloader.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("ionCube")
+  end
+end
