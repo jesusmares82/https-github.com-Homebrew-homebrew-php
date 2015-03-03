4c7d91b2010e2cda9910f4ae5e01ff9e26130998
Alan Ivey <alan@echo.co>
2015-03-03 10:56:53 -0500

    php56-ioncubeloader: add 4.7.5 bottle.

--- Formula/php56-ioncubeloader.rb
+++ Formula/php56-ioncubeloader.rb
@@ -5,6 +5,13 @@ class Php56Ioncubeloader < AbstractPhp56Extension
   homepage "http://www.ioncube.com/loaders.php"
   if Hardware.is_64_bit?
     version "4.7.5"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha1 "1afa94ee86d9bb533d28d0cc94788dff6f14c15b" => :yosemite
+    sha1 "45b8eb3729394694dbecb94a8866b64c21dded6a" => :mavericks
+    sha1 "da195600da8f73ea5503b35a0e1ec1015e58ef06" => :mountain_lion
+  end
+
     url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz"
   else
     version "4.7.5"
