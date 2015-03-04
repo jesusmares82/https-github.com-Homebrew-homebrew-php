a14fe086e5e974b9d66ce5e60eca966f613e6e01
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-04 09:51:34 +0000

    php53-redland: add 1.0.17.1 bottle.

--- Formula/php53-redland.rb
+++ Formula/php53-redland.rb
@@ -7,6 +7,13 @@ class Php53Redland < AbstractPhp53Extension
   url 'http://download.librdf.org/source/redland-bindings-1.0.17.1.tar.gz'
   sha256 'ff72b587ab55f09daf81799cb3f9d263708fad5df7a5458f0c28566a2563b7f5'
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha1 "bfd3b0035add6db855102234339b1dfd8f0dffa0" => :yosemite
+    sha1 "19f385bebe9d397b7d6816cbeee7300a1a3f6a41" => :mavericks
+    sha1 "20a279b9541218a18e208284c043d7ea05592732" => :mountain_lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'redland'
   depends_on 'raptor'
