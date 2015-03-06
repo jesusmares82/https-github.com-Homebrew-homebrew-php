391d883956a60b86ab88790202319462c449acf9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-06 10:32:48 +0000

    php53-mysqlnd_ms: add 1.5.2 bottle.

--- Formula/php53-mysqlnd_ms.rb
+++ Formula/php53-mysqlnd_ms.rb
@@ -7,6 +7,13 @@ class Php53MysqlndMs < AbstractPhp53Extension
   sha1 "b7b3ddd1f40ffec75a48456edf053c28fc5aad83"
   head "https://svn.php.net/repository/pecl/mysqlnd_ms/trunk/"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "2d952f04416340ba83115854ae0f34b61a5eb6f7eb2de67901fca6d998ae6d0e" => :yosemite
+    sha256 "89d190eaa26aa474c77fe63db63948ddce74fc5815ca95888e9910b5dd4724d1" => :mavericks
+    sha256 "16bdb614b7bbfcb7afe2ec270aa346417887658c8264254df1f6e6dba4aba289" => :mountain_lion
+  end
+
   def extension
     "mysqlnd_ms"
   end
