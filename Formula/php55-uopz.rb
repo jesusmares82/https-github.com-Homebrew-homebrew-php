50f6c7113b1a00c8a6f13ca30b69b52e272ee7c9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-09 15:38:09 +0000

    php55-uopz: add 2.0.7 bottle.

--- Formula/php55-uopz.rb
+++ Formula/php55-uopz.rb
@@ -7,6 +7,13 @@ class Php55Uopz < AbstractPhp55Extension
   sha256 "aca7dac96c00e5bd628625ad7372d733c8a40a48d4b143d7f001feea1c5fb3b1"
   head "https://github.com/krakjoe/uopz.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "c4bf55e189c45a6face4303cef82d701cfc61cdebf3427a6539ad264761a06b8" => :yosemite
+    sha256 "a10d58aea5d5e74684da1130d324e4c3b3aec40340b3be6c92b461d9ab7851b1" => :mavericks
+    sha256 "1c7fe3a31da675efc3436d1ee5f01fdcf36f4df3e77e7c7c02c37c844eeef853" => :mountain_lion
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
