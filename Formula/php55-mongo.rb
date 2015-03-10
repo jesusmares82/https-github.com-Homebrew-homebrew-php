48b6a22072229b22a5e75f689c64b3e1685066e6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-10 18:16:36 +0000

    php55-mongo: add 1.6.4 bottle.

--- Formula/php55-mongo.rb
+++ Formula/php55-mongo.rb
@@ -7,6 +7,13 @@ class Php55Mongo < AbstractPhp55Extension
   sha256 "b6ab5f4ade26ea328081c70bba43f048da3d669e536eebb9eb380f9db90dda6d"
   head "https://github.com/mongodb/mongo-php-driver.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "cdd15fb7a214469ec861529b2fa4792b2a9f313ef66ac44be9873ee1100b807e" => :yosemite
+    sha256 "ec7a0be301ce06dc97c959c62fde417ec417d4ef460d5381a7d41cf46948931f" => :mavericks
+    sha256 "e3ce71a166a00257251ee1ac1ef3610ae1773fa4d1683672fd40eb01c690646f" => :mountain_lion
+  end
+
   def install
     Dir.chdir "mongo-#{version}" unless build.head?
 
