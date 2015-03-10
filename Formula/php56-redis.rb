1243f4bdd7d1d89abc5c126034c8d42dc069b2fc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-10 02:44:20 +0000

    php56-redis: add 2.2.7 bottle.

--- Formula/php56-redis.rb
+++ Formula/php56-redis.rb
@@ -7,6 +7,13 @@ class Php56Redis < AbstractPhp56Extension
   sha256 "a5882dd9b21908e123b3d5c5f72d6dc8cbbbb6a29996e568c4d18ed356c0362b"
   head "https://github.com/phpredis/phpredis.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "fb7a6c68fa796d296a70429042c49b0bb23be210cebcca53f4251aedc3fe1d39" => :yosemite
+    sha256 "cdabbdfc81f769068f25e4843800a1c670f5a25db4ff424c4a407191767de018" => :mavericks
+    sha256 "27ed40fb435528fc05bc6a4fcc499a19e88c6d37da4ed2e6b5051c906e293cf5" => :mountain_lion
+  end
+
   option "with-igbinary", "Build with igbinary support"
 
   depends_on "php56-igbinary" if build.with? "igbinary"
