ceaf8f7e2d843bb7c86e2ba4371e6a282be6379b
Alan Ivey <alanivey@gmail.com>
2015-03-09 20:33:56 -0400

    php56-redis: update to 2.2.7, pass `brew audit`
    Closes #1589.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/php56-redis.rb
+++ Formula/php56-redis.rb
@@ -2,26 +2,26 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Redis < AbstractPhp56Extension
   init
-  homepage 'https://github.com/phpredis/phpredis'
-  url 'https://github.com/phpredis/phpredis/archive/2.2.6.tar.gz'
-  sha1 'df3d6c9c4d48bf5c1dbcfc64947f7cec828b5014'
-  head 'https://github.com/phpredis/phpredis.git'
+  homepage "https://github.com/phpredis/phpredis"
+  url "https://github.com/phpredis/phpredis/archive/2.2.7.tar.gz"
+  sha256 "a5882dd9b21908e123b3d5c5f72d6dc8cbbbb6a29996e568c4d18ed356c0362b"
+  head "https://github.com/phpredis/phpredis.git"
 
-  option 'with-igbinary', "Build with igbinary support"
+  option "with-igbinary", "Build with igbinary support"
 
-  depends_on 'php56-igbinary' if build.with? "igbinary"
+  depends_on "php56-igbinary" if build.with? "igbinary"
 
   def install
     ENV.universal_binary if build.universal?
 
     args = []
-    args << "--enable-redis-igbinary" if build.with? 'igbinary'
+    args << "--enable-redis-igbinary" if build.with? "igbinary"
 
     safe_phpize
 
-    if build.with? 'igbinary'
-      system "mkdir -p ext/igbinary"
-      cp "#{Formula['php56-igbinary'].opt_prefix}/include/igbinary.h", "ext/igbinary/igbinary.h"
+    if build.with? "igbinary"
+      mkdir "ext/igbinary"
+      cp Formula["php56-igbinary"].opt_prefix+"/include/igbinary.h", "ext/igbinary/igbinary.h"
     end
 
     system "./configure", "--prefix=#{prefix}",
@@ -30,5 +30,9 @@ class Php56Redis < AbstractPhp56Extension
     system "make"
     prefix.install "modules/redis.so"
     write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("redis")
   end
 end
