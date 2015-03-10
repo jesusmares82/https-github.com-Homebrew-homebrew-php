1e3e99a31876de5dff37119b4f85ccd0513f80fb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-10 16:32:08 +0000

    php53-mongo: add 1.6.4 bottle.

--- Formula/php53-mongo.rb
+++ Formula/php53-mongo.rb
@@ -7,6 +7,12 @@ class Php53Mongo < AbstractPhp53Extension
   sha256 "b6ab5f4ade26ea328081c70bba43f048da3d669e536eebb9eb380f9db90dda6d"
   head "https://github.com/mongodb/mongo-php-driver.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "9cad72211fff1d7d12437bef08244e05f00f09c671b7acb89b0b00cdd56f210e" => :yosemite
+    sha256 "5d16521d5aa84ed169101410cd42bbfe63c55fd41b101db2b20de0569807b9c1" => :mountain_lion
+  end
+
   def install
     Dir.chdir "mongo-#{version}" unless build.head?
 
