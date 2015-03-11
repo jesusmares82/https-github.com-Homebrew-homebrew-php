4b51c973d22cf6834b2ea243d591ddece5f451a6
Alan Ivey <alanivey@gmail.com>
2015-03-11 11:16:22 -0400

    php54-igbinary: update to 1.2.1, pass `brew audit`, expand config file, add test.
    Closes #1604.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/php54-igbinary.rb
+++ Formula/php54-igbinary.rb
@@ -2,10 +2,10 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php54Igbinary < AbstractPhp54Extension
   init
-  homepage 'http://pecl.php.net/package/igbinary'
-  url 'http://pecl.php.net/get/igbinary-1.1.1.tgz'
-  sha1 'cebe34d18dd167a40a712a6826415e3e5395ab27'
-  head 'https://github.com/igbinary/igbinary.git'
+  homepage "http://pecl.php.net/package/igbinary"
+  url "http://pecl.php.net/get/igbinary-1.2.1.tgz"
+  sha256 "168e51d41a417bbbfe6da0e3cb9b71ef93594f4034f489a951f3b874d03dfdb8"
+  head "https://github.com/igbinary/igbinary.git"
 
   def install
     Dir.chdir "igbinary-#{version}" unless build.head?
@@ -16,8 +16,26 @@ class Php54Igbinary < AbstractPhp54Extension
     system "./configure", "--prefix=#{prefix}",
                           phpconfig
     system "make"
-    include.install %w(apc_serializer.h hash.h hash_function.h igbinary.h php_igbinary.h)
-    prefix.install %w(modules/igbinary.so)
+    include.install %w[apc_serializer.h hash.h igbinary.h php_igbinary.h]
+    prefix.install "modules/igbinary.so"
     write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+      ; Enable or disable compacting of duplicate strings
+      ; The default is On.
+      ;igbinary.compact_strings=On
+
+      ; Use igbinary as session serializer
+      ;session.serialize_handler=igbinary
+
+      ; Use igbinary as APC serializer
+      ;apc.serializer=igbinary
+    EOS
+  end
+
+  test do
+    shell_output("php -m").include?("igbinary")
   end
 end
