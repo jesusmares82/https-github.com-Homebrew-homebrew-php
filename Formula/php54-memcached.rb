48e7cf4383b307d087a4d3ec506e4361250ecf75
Alan Ivey <alanivey@gmail.com>
2015-03-12 11:14:18 -0400

    php54-memcached: update to 2.2.0, remove outdated patch, require igbinary, add upstream ini defaults, add test, pass `brew audit`
    Closes #1612.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/php54-memcached.rb
+++ Formula/php54-memcached.rb
@@ -2,24 +2,19 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php54Memcached < AbstractPhp54Extension
   init
-  homepage 'http://pecl.php.net/package/memcached'
-  url 'http://pecl.php.net/get/memcached-2.1.0.tgz'
-  sha1 '16fac6bfae8ec7e2367fda588b74df88c6f11a8e'
-  head 'https://github.com/php-memcached-dev/php-memcached.git'
+  homepage "http://pecl.php.net/package/memcached"
+  url "http://pecl.php.net/get/memcached-2.2.0.tgz"
+  sha256 "17b9600f6d4c807f23a3f5c45fcd8775ca2e61d6eda70370af2bef4c6e159f58"
+  head "https://github.com/php-memcached-dev/php-memcached.git"
 
-  option 'with-igbinary', "Build with igbinary support"
-  option 'with-sasl', "Build with sasl support"
+  option "with-sasl", "Build with sasl support"
+
+  depends_on "pkg-config" => :build
+  depends_on "php54-igbinary"
   if build.with? "sasl"
     depends_on "libmemcached" => "with-sasl"
   else
     depends_on "libmemcached"
-  end
-  depends_on 'php54-igbinary' if build.with? "igbinary"
-
-  patch do
-    # adapt to libmemcached >= 1.0.9 new instance API (source: paravoid/php-memcached 37069e18ad399a8cc03d5fe9757e1481814ecb44)
-    url "https://gist.githubusercontent.com/ablyler/6331007/raw/409ef282616859b2a7d19ba703c9b736576db16e/libmemcache-new-instance-api.patch"
-    sha1 "bc958b0d50b7a260c13dc71451e6c35c08bc7c35"
   end
 
   def install
@@ -28,22 +23,149 @@ class Php54Memcached < AbstractPhp54Extension
     ENV.universal_binary if build.universal?
 
     args = []
-    args << "--prefix=#{prefix}"
-    args << phpconfig
-    args << "--with-libmemcached-dir=#{Formula['libmemcached'].opt_prefix}"
-    args << "--enable-memcached-igbinary" if build.with? 'igbinary'
-    args << "--enable-memcached-sasl" if build.with? 'sasl'
+    args << "--with-libmemcached-dir=#{Formula["libmemcached"].opt_prefix}"
+    args << "--enable-memcached-igbinary"
+    args << "--enable-memcached-sasl" if build.with? "sasl"
 
     safe_phpize
 
-    if build.with? 'igbinary'
-      system "mkdir -p ext/igbinary"
-      cp "#{Formula['php54-igbinary'].opt_prefix}/include/igbinary.h", "ext/igbinary/igbinary.h"
-    end
+    mkdir_p "ext/igbinary"
+    cp "#{Formula["php54-igbinary"].include}/igbinary.h", "ext/igbinary/igbinary.h"
 
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          *args
     system "make"
     prefix.install "modules/memcached.so"
     write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    # Use upsteam defaults (https://github.com/php-memcached-dev/php-memcached/blob/2.2.0/memcached.ini)
+    # with default values pre-filled
+    super + <<-EOS.undent
+
+      ; Use session locking
+      ; valid values: On, Off
+      ; the default is On
+      memcached.sess_locking = On
+
+      ; Session spin lock retry wait time in microseconds.
+      ; Be carefull when setting this value.
+      ; Valid values are integers, where 0 is interpreted as
+      ; the default value. Negative values result in a reduces
+      ; locking to a try lock.
+      ; the default is 150000
+      memcached.sess_lock_wait = 150000
+
+      ; The maximum time, in seconds, to wait for a session lock
+      ; before timing out.
+      ; Setting to 0 results in default behavior, which is to
+      ; use max_execution_time.
+      memcached.sess_lock_max_wait = 0;
+
+      ; The time, in seconds, before a lock should release itself.
+      ; Setting to 0 results in the default behaviour, which is to
+      ; use the memcached.sess_lock_max_wait setting. If that is
+      ; also 0, max_execution_time will be used.
+      memcached.sess_lock_expire = 0;
+
+      ; memcached session key prefix
+      ; valid values are strings less than 219 bytes long
+      ; the default value is "memc.sess.key."
+      memcached.sess_prefix = "memc.sess.key."
+
+      ; memcached session consistent hash mode
+      ; if set to On, consistent hashing (libketama) is used
+      ; for session handling.
+      ; When consistent hashing is used, one can add or remove cache
+      ; node(s) without messing up too much with existing keys
+      ; default is Off
+      memcached.sess_consistent_hash = Off
+
+      ; Allow failed memcached server to automatically be removed
+      memcached.sess_remove_failed = 1
+
+      ; Write data to a number of additional memcached servers
+      ; This is "poor man's HA" as libmemcached calls it.
+      ; If this value is positive and sess_remove_failed is enabled
+      ; when a memcached server fails the session will continue to be available
+      ; from a replica. However, if the failed memcache server
+      ; becomes available again it will read the session from there
+      ; which could have old data or no data at all
+      memcached.sess_number_of_replicas = 0
+
+      ; memcached session binary mode
+      ; libmemcached replicas only work if binary mode is enabled
+      memcached.sess_binary = Off
+
+      ; memcached session replica read randomize
+      memcached.sess_randomize_replica_read = Off
+
+      ; memcached connect timeout value
+      ; In non-blocking mode this changes the value of the timeout
+      ; during socket connection in milliseconds. Specifying -1 means an infinite timeout.
+      memcached.sess_connect_timeout = 1000
+
+      ; Session SASL username
+      ; Both username and password need to be set for SASL to be enabled
+      ; In addition to this memcached.use_sasl needs to be on
+      memcached.sess_sasl_username = NULL
+
+      ; Session SASL password
+      memcached.sess_sasl_password = NULL
+
+      ; Set the compression type
+      ; valid values are: fastlz, zlib
+      ; the default is fastlz
+      memcached.compression_type = "fastlz"
+
+      ; Compression factor
+      ; Store compressed value only if the compression
+      ; factor (saving) exceeds the set limit.
+      ;
+      ;  store compressed if:
+      ;    plain_len > comp_len * factor
+      ;
+      ; the default value is 1.3 (23% space saving)
+      memcached.compression_factor = "1.3"
+
+      ; The compression threshold
+      ;
+      ; Do not compress serialized values below this threshold.
+      ; the default is 2000 bytes
+      memcached.compression_threshold = 2000
+
+      ; Set the default serializer for new memcached objects.
+      ; valid values are: php, igbinary, json, json_array, msgpack
+      ;
+      ; json - standard php JSON encoding. This serializer
+      ;        is fast and compact but only works on UTF-8
+      ;        encoded data and does not fully implement
+      ;        serializing. See the JSON extension.
+      ; json_array - as json, but decodes into arrays
+      ; php - the standard php serializer
+      ; igbinary - a binary serializer
+      ; msgpack - a cross-language binary serializer
+      ;
+      ; The default is igbinary if available, then msgpack if available, then php otherwise.
+      memcached.serializer = "igbinary"
+
+      ; Use SASL authentication for connections
+      ; valid values: On, Off
+      ; the default is Off
+      memcached.use_sasl = Off
+
+      ; The amount of retries for failed store commands.
+      ; This mechanism allows transparent fail-over to secondary servers when
+      ; set/increment/decrement/setMulti operations fail on the desired server in a multi-server
+      ; environment.
+      ; the default is 2
+      memcached.store_retry_count = 2
+    EOS
+  end
+
+  test do
+    shell_output("php -m").include?("memcached")
   end
 end
