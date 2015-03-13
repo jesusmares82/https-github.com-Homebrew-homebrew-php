97b26f7784d95bc994b1896b4e25d65341e42f93
n0ts <naoya.n@gmail.com>
2015-03-13 13:45:36 +0900

    Upgrade imagick 3.1.2 and Add configure parameter --with-imagick
    Closes #1614.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/php56-imagick.rb
+++ Formula/php56-imagick.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php56Imagick < AbstractPhp56Extension
   init
   homepage 'http://pecl.php.net/package/imagick'
-  url 'http://pecl.php.net/get/imagick-3.1.0RC2.tgz'
-  sha1 '29b6dcd534cde6b37ebe3ee5077b71a9eed685c2'
+  url 'http://pecl.php.net/get/imagick-3.1.2.tgz'
+  sha1 '7cee88bc8f6f178165c9d43e302d99cedfbb3dff'
   head 'https://svn.php.net/repository/pecl/imagick/trunk/'
 
   depends_on 'pkg-config' => :build
@@ -25,7 +25,8 @@ class Php56Imagick < AbstractPhp56Extension
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}",
-                          phpconfig
+                          phpconfig,
+                          "--with-imagick=#{Formula['imagemagick'].opt_prefix}"
     system "make"
     prefix.install "modules/imagick.so"
     write_config_file if build.with? "config-file"
@@ -35,9 +36,9 @@ end
 __END__
 diff --git a/imagick-3.1.0RC2/config.m4 b/imagick-3.1.0RC2/config.m4
 index 6caa29a..1c6cdfb 100644
---- a/imagick-3.1.0RC2/config.m4
-+++ b/imagick-3.1.0RC2/config.m4
-@@ -54,6 +54,8 @@ if test $PHP_IMAGICK != "no"; then
+--- a/imagick-3.1.2/config.m4
++++ b/imagick-3.1.2/config.m4
+@@ -60,6 +60,8 @@ if test $PHP_IMAGICK != "no"; then
  
      if test -r $WAND_DIR/include/ImageMagick/wand/MagickWand.h; then
        AC_MSG_RESULT(found in $WAND_DIR/include/ImageMagick/wand/MagickWand.h)
