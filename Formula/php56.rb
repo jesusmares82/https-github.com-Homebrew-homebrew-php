bf667fcf4fcc4d144b964840b5c15987be24568e
Alan Ivey <alanivey@gmail.com>
2015-03-23 12:41:57 -0400

    php56: update 5.6.7 bottle.

--- Formula/php56.rb
+++ Formula/php56.rb
@@ -8,6 +8,13 @@ class Php56 < AbstractPhp
   sha256  PHP_CHECKSUM[:sha256]
   version PHP_VERSION
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "04f82f00567366231cebdffc4ea0ffa108d29744f783b0b946b87a6c5365ea84" => :yosemite
+    sha256 "5b2f55677cef720b203ec0f8f2a05947b155d6151db7edbbdfc2198029e0766a" => :mavericks
+    sha256 "1aea47c7dc3408fc3fdb5ae08d12a10420480208279b3dc9686e64088f45dbd9" => :mountain_lion
+  end
+
   head    PHP_GITHUB_URL, :branch => PHP_BRANCH
 
   def install_args
