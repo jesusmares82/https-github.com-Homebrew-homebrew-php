678a09ab8baed58511eac7822b2263d264d426eb
Alex Dunn <dunn.alex@gmail.com>
2015-04-17 13:42:52 -0700

    box 2.5.2
    Closes #1667.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,34 +1,25 @@
-require 'formula'
 require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
 require File.expand_path("../../Requirements/phar-requirement", __FILE__)
 
 class Box < Formula
-  homepage 'http://box-project.org/'
-  url 'https://github.com/box-project/box2/releases/download/2.5.0/box-2.5.0.phar'
-  sha1 '5664cb4603917a77ea71fb325845e33d886cccb1'
+  homepage "http://box-project.org/"
+  url "https://github.com/box-project/box2/releases/download/2.5.2/box-2.5.2.phar"
+  sha256 "4df2f37906b5bbb124851d7b8359533b0a3bdd890c47ba14e82c4c07be22ce71"
 
   depends_on PhpMetaRequirement
   depends_on PharRequirement
 
   def install
     libexec.install "box-#{version}.phar"
-    sh = libexec + "box"
-    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/box-#{version}.phar $*")
-    chmod 0755, sh
-    bin.install_symlink sh
+    (libexec/"box").write <<-EOS.undent
+      #!/usr/bin/env bash
+      /usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/box-#{version}.phar $*
+      EOS
+    chmod 0755, (libexec/"box")
+    bin.install_symlink (libexec/"box")
   end
 
   test do
-    system 'box --version'
+    system "box", "--version"
   end
-
-  def caveats; <<-EOS.undent
-    Verify your installation by running:
-      "box --version".
-
-    You can read more about box by running:
-      "brew home box".
-    EOS
-  end
-
 end
