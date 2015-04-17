7f7bb4279fcad8fba24739cb7b3320be43cf00d0
Alexander Turiak <alex@hexbrain.com>
2015-04-17 11:37:56 +0200

    Updated Codeception to v2.0.13
    Closes #1665.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -3,9 +3,10 @@ require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
 require File.expand_path("../../Requirements/phar-requirement", __FILE__)
 
 class Codeception < Formula
-  homepage 'http://codeception.com/quickstart'
-  url 'http://codeception.com/releases/2.0.5/codecept.phar'
-  sha1 '3ab90ea9299d2649e2e612e810f5bbc9444c3c36'
+  homepage "http://codeception.com/quickstart"
+  url "http://codeception.com/releases/2.0.13/codecept.phar"
+  sha256 "9a7ca6750c2bdf83e092304cb158f13d4c676d7f74b4b88adb78132c4a41fa58"
+  version "2.0.13"
 
   def install
     mv "codecept.phar", "codecept-#{version}.phar"
