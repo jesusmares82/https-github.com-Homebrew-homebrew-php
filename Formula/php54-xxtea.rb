d848caf84425d3e130415a249f732f2ee68a713a
andot <mabingyao@gmail.com>
2015-04-22 18:08:13 +0800

    Added php5x-xxtea formulae
    Closes #1671.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

---
+++ Formula/php54-xxtea.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php54Xxtea < AbstractPhp54Extension
+  init
+  homepage "http://pecl.php.net/package/xxtea"
+  url "http://pecl.php.net/get/xxtea-1.0.10.tgz"
+  sha256 "2735d2bc28c3ecdaa12491252f71c357c25c461d0a5802377218489807a801d8"
+  head "https://github.com/xxtea/xxtea-pecl.git"
+
+  def install
+    Dir.chdir "xxtea-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/xxtea.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("xxtea")
+  end
+end
