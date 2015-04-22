019a2153b4d8fd29a42652f4cf9bfc8f34bbffac
andot <mabingyao@gmail.com>
2015-04-22 17:03:47 +0800

    Added php5x-hprose
    Closes #1670.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

---
+++ Formula/php53-hprose.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php53Hprose < AbstractPhp53Extension
+  init
+  homepage "http://pecl.php.net/package/hprose"
+  url "http://pecl.php.net/get/hprose-1.4.2.tgz"
+  sha256 "fcae60918059fc04a840eecd21e56ba0fb59ec0a02c8c0796ee047bf7938b27c"
+  head "https://github.com/hprose/hprose-pecl.git"
+
+  def install
+    Dir.chdir "hprose-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/hprose.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("hprose")
+  end
+end
