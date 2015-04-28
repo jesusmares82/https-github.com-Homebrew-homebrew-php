85a2c4bbcd7de44dc9c4d00e3233bd5dd1978f73
Nate Bedortha <nate@oknoway.com>
2015-04-28 14:21:06 -0700

    wp-cli v0.19.0
    Closes #1687.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>
    
    Also: pass `brew audit --strict`, improve and simplify installation

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,29 +1,30 @@
-require 'formula'
-
 class WpCli < Formula
-  homepage 'https://github.com/wp-cli/wp-cli'
-  head 'https://github.com/wp-cli/wp-cli.git'
-  url 'https://github.com/wp-cli/wp-cli/archive/v0.18.0.tar.gz'
-  sha1 'a4cba7bcc067e5d64f8d0771e7bee734792cbddd'
+  homepage "https://github.com/wp-cli/wp-cli"
+  head "https://github.com/wp-cli/wp-cli.git"
+  url "https://github.com/wp-cli/wp-cli/archive/v0.19.0.tar.gz"
+  sha256 "74a0f04a653f183f542d5711333a860d8639fd925c966bb2f2bd235f3c768fce"
 
-  option 'without-bash-completion', "Don't install bash completion"
-  option 'without-package-index', "Don't add package index repository (http://wp-cli.org/package-index)"
+  option "without-bash-completion", "Don't install bash completion"
+  option "without-package-index", "Don't add package index repository (http://wp-cli.org/package-index)"
 
-  depends_on 'composer'
+  depends_on "composer" => :build
 
   def install
-    system "#{HOMEBREW_PREFIX}/bin/composer install"
-    prefix.install Dir['*']
+    system "composer", "install"
 
-    if build.with? 'bash-completion'
-      (prefix + 'etc/bash_completion.d').install "#{prefix}/utils/wp-completion.bash"
+    rm "bin/wp.bat"
+    prefix.install Dir["*"]
+
+    if build.with? "bash-completion"
+      (prefix + "etc/bash_completion.d").install "#{prefix}/utils/wp-completion.bash"
     end
-    if build.with? 'package-index'
-      system "#{HOMEBREW_PREFIX}/bin/composer config --file='#{prefix}/composer.json' repositories.wp-cli composer http://wp-cli.org/package-index/"
+
+    if build.with? "package-index"
+      system "composer config --file=#{prefix}/composer.json repositories.wp-cli composer http://wp-cli.org/package-index/"
     end
   end
 
   test do
-    system "#{bin}/wp --info"
+    system "#{bin}/wp", "--info"
   end
 end
