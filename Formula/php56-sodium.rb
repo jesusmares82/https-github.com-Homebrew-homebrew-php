d9abb92a2504666eaa831256c3aa557735b07004
Chris McKnight <cmckni3@gmail.com>
2015-04-30 00:36:52 -0500

    Adds php56-sodium
    Closes #1708.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

---
+++ Formula/php56-sodium.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php56Sodium < AbstractPhp56Extension
+  init
+  homepage "https://github.com/alethia7/php-sodium"
+  url "https://github.com/aletheia7/php-sodium/archive/1.0.7.tar.gz"
+  sha256 "07d06b486ab5b687e63109d64bf9fd21c846a315b8c71088639dfa0a7272339e"
+  head "https://github.com/alethia7/php-sodium.git"
+
+  depends_on "libsodium"
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/sodium.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").split("\n").include?("sodium")
+  end
+end
