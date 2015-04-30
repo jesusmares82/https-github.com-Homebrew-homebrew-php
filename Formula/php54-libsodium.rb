d1179f79b4d7f3d620bfbadea927f29863298c4d
Chris McKnight <cmckni3@gmail.com>
2015-04-30 00:12:27 -0500

    Adds php54-libsodium
    Closes #1698.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

---
+++ Formula/php54-libsodium.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php54Libsodium < AbstractPhp54Extension
+  init
+  homepage "https://github.com/jedisct1/libsodium-php"
+  url "https://github.com/jedisct1/libsodium-php/archive/0.1.3.tar.gz"
+  sha256 "cf7314420f270eeef207add6f44c3bdd88b6c6395402d14b9d37ce2171bca734"
+  head "https://github.com/jedisct1/libsodium-php.git"
+
+  depends_on "libsodium"
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/libsodium.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("libsodium")
+  end
+end
