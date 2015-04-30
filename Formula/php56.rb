ba202e651634523e2ed2db63b291fbe3cc235e9f
Alan Ivey <alanivey@gmail.com>
2015-04-30 12:20:57 -0400

    Re-publish php5x bottles; be sure to update your local copy of 'icu4c' before using

--- Formula/php56.rb
+++ Formula/php56.rb
@@ -10,6 +10,13 @@ class Php56 < AbstractPhp
 
   head    PHP_GITHUB_URL, :branch => PHP_BRANCH
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "22f0e4bb4e2add47c485b402373af3fc476e89c645b89312933f1dcc59a13fd1" => :yosemite
+    sha256 "02865b225d88edd736afc7c449a638c0325df36ae001a9e2f2f7cd2e3b76c362" => :mavericks
+    sha256 "442674569deab3e53f1bf0862221168a0b4cf994bf3d7d2067b886e8db17b618" => :mountain_lion
+  end
+
   def install_args
     args = super
 
