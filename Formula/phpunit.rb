1d75770af2d748768cf121e5b04bf51519cf4a80
Guo Xiao <guoxiao08@gmail.com>
2015-05-06 13:52:10 +0800

    phpunit 4.6.6
    Closes #1711.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -2,9 +2,9 @@ require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
 require File.expand_path("../../Requirements/phar-requirement", __FILE__)
 
 class Phpunit < Formula
-  homepage "http://phpunit.de/manual/current/en/"
-  url "https://phar.phpunit.de/phpunit-4.6.4.phar"
-  sha256 "b2e2b274758d8c8b882ec838f9c71e33fafc83c68cf6ec1ce811d11d29e8d501"
+  homepage "http://phpunit.de"
+  url "https://phar.phpunit.de/phpunit-4.6.6.phar"
+  sha256 "67c2225d2a1257aa230bf30431720d452314d3f465c766b82e8de20a24a65676"
 
   def install
     libexec.install "phpunit-#{version}.phar"
@@ -12,5 +12,9 @@ class Phpunit < Formula
     sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpunit-#{version}.phar $*")
     chmod 0755, sh
     bin.install_symlink sh
+  end
+
+  test do
+    shell_output("phpunit --version").include?(version)
   end
 end
