594ba97bd193930cd955062e4faece12fdb89e12
Alex Dunn <dunn.alex@gmail.com>
2015-05-07 12:05:40 -0700

    pdepend 2.0.6
    Closes #1712.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/pdepend.rb
+++ Formula/pdepend.rb
@@ -1,33 +1,23 @@
-require 'formula'
 require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
 require File.expand_path("../../Requirements/phar-requirement", __FILE__)
 
 class Pdepend < Formula
-  homepage 'http://pdepend.org/'
-  url 'http://static.pdepend.org/php/1.1.3/pdepend.phar'
-  sha1 '8f218621ac3ec3137fcef6e95e804787be91253d'
+  homepage "http://pdepend.org/"
+  url "http://static.pdepend.org/php/2.0.6/pdepend.phar"
+  sha256 "71573a4b125a5e1ca42032af52095b2d96a39aafb26704529549c42d6833bf4f"
 
   depends_on PhpMetaRequirement
   depends_on PharRequirement
 
   def install
     libexec.install "pdepend.phar"
-    sh = libexec + "pdepend"
-    sh.write("#!/bin/sh\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/pdepend.phar $*")
-    chmod 0755, sh
-    bin.install_symlink sh
+    (bin/"pdepend").write <<-EOS.undent
+      #!/bin/sh
+      /usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/pdepend.phar $*
+    EOS
   end
 
   test do
-    system 'pdepend --version'
-  end
-
-  def caveats; <<-EOS.undent
-    Verify your installation by running:
-      "pdepend --version".
-
-    You can read more about pdepend by running:
-      "brew home pdepend".
-    EOS
+    system bin/"pdepend", "--version"
   end
 end
