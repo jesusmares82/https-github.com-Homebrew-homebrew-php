3380ad3047e066a288428d4059422877f412d527
Alan Ivey <alanivey@gmail.com>
2015-05-11 11:09:22 -0400

    php55-intl: new formula for intl extension
    See #1701.
    
    Closes #1721.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

---
+++ Formula/php55-intl.rb
@@ -0,0 +1,42 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php55Intl < AbstractPhp55Extension
+  init
+  homepage "http://php.net/manual/en/book.intl.php"
+  url      PHP_SRC_TARBALL
+  sha256   PHP_CHECKSUM[:sha256]
+  version  PHP_VERSION
+
+  depends_on "icu4c"
+
+  def install
+    Dir.chdir "ext/intl"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking",
+                          "--enable-intl",
+                          "--with-icu-dir=#{Formula['icu4c'].prefix}"
+    system "make"
+    prefix.install "modules/intl.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+
+      ;intl.default_locale =
+      ; This directive allows you to produce PHP errors when some error
+      ; happens within intl functions. The value is the level of the error produced.
+      ; Default is 0, which does not produce any errors.
+      ;intl.error_level = E_WARNING
+    EOS
+  end
+
+  test do
+    shell_output("php -m").include?("intl")
+  end
+end
