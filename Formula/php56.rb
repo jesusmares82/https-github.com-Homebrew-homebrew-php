3079683d4cc9573fe20c29743dc573d813dad42b
Alan Ivey <alanivey@gmail.com>
2015-05-11 16:16:50 -0400

    php56: add new 5.6.8 bottle.

--- Formula/php56.rb
+++ Formula/php56.rb
@@ -10,6 +10,13 @@ class Php56 < AbstractPhp
 
   head    PHP_GITHUB_URL, :branch => PHP_BRANCH
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "162cfbabd76435d28a413f18d731719a9ef49594547a8435e03647bdd986c935" => :yosemite
+    sha256 "78919dbfd954718acb10ed1d867c190398bf87cb4fba6eb72f13d0ebab82e696" => :mavericks
+    sha256 "477aef46eec03603c72b9139849fae7539041ea22b5d7581f90971164dba70a7" => :mountain_lion
+  end
+
   def install_args
     args = super
 
