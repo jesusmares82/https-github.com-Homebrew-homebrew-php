f20f82b116d9c07e5677d54ef9ab55b5be390935
Marc Paffen <marc.paffen@hdnet.de>
2015-05-13 22:07:56 +0200

    Fix phpmetrics installation

--- Formula/phpmetrics.rb
+++ Formula/phpmetrics.rb
@@ -4,17 +4,17 @@ require File.expand_path("../../Requirements/phar-requirement", __FILE__)
 
 class Phpmetrics < Formula
   homepage 'http://www.phpmetrics.org/'
-  url 'https://github.com/Halleck45/PhpMetrics/blob/v1.1.1/build/phpmetrics.phar'
-  sha1 '209202b00c81a534198a552b0c056d8a2c0dc587'
+  url 'https://github.com/Halleck45/PhpMetrics/raw/v1.1.1/build/phpmetrics.phar'
+  sha256 'e55f6ea1fe8046f538432b946fca38794ab9e3a88c5251e70c3b08abb7e7cd03'
   version '1.1.1'
 
   depends_on PhpMetaRequirement
   depends_on PharRequirement
 
   def install
-    libexec.install "phpmetrics-#{version}.phar"
+    libexec.install "phpmetrics.phar"
     sh = libexec + "phpmetrics"
-    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpmetrics-#{version}.phar $*")
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpmetrics.phar $*")
     chmod 0755, sh
     bin.install_symlink sh
   end
