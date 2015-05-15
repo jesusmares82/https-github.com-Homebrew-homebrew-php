8a0639257ec1451660ab5758ecc79a62097005b3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 21:23:00 +0100

    php56-pdo-dblib: update 5.6.9 bottle.

--- Formula/php56-pdo-dblib.rb
+++ Formula/php56-pdo-dblib.rb
@@ -8,6 +8,10 @@ class Php56PdoDblib < AbstractPhp56Extension
   version  PHP_VERSION
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "51ca4c02352d9a4e6a8acb0f1a51fbe7e8bb043837af7a95a4c8959d356870ad" => :yosemite
+    sha256 "c58917aa8261730f994814d7e94893d12e58d2505751ffd23621de2522dd81d6" => :mavericks
+    sha256 "48fad268e707b632cddfb22aee7f32f620d99d80363927a1ec05c53cd2e47734" => :mountain_lion
   end
 
   depends_on "freetds"
