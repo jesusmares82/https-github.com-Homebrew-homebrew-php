fdd6e332c034dbce55381ade3b26643622e6f273
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 22:15:12 +0100

    php53-raphf: update 1.0.4 bottle.

--- Formula/php53-raphf.rb
+++ Formula/php53-raphf.rb
@@ -7,6 +7,10 @@ class Php53Raphf < AbstractPhp53Extension
   sha256 "461be283e89d94186a3ed4651b92c7c1a067bad7b6476d0ca7ac8863dc1ed8bf"
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "d85407c95983c971a3d739e723379ce76f15e1079ef29dae87154da025451284" => :yosemite
+    sha256 "6f5e4fac12bb82faa93896c815ceb14305d64096ef57229ec151eb71b2f019ed" => :mavericks
+    sha256 "083d883ccc79f29b185bb4e73f4560f8bde0fc20f0cfa9ad3479779f3db289c4" => :mountain_lion
   end
 
   def install
