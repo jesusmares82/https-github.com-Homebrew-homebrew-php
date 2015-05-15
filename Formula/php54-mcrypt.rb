c40d7634e64796765eba6cdf88d73f35d9e47727
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 19:49:32 +0100

    php54-mcrypt: update 5.4.41 bottle.

--- Formula/php54-mcrypt.rb
+++ Formula/php54-mcrypt.rb
@@ -8,6 +8,10 @@ class Php54Mcrypt < AbstractPhp54Extension
   version  PHP_VERSION
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "da4b4ef9dbc233dd74a8ad33ba6e88becbc33293a41205d1aeeef04aeb7aad16" => :yosemite
+    sha256 "32c1bb241456ba87629c66cded09ef1eead94cc2e518faead6b8fa049bf44546" => :mavericks
+    sha256 "ab373d3c4d7332b84a4bb14004a5b9321ced45c77d940106484e639c292d0528" => :mountain_lion
   end
 
   depends_on "mcrypt"
