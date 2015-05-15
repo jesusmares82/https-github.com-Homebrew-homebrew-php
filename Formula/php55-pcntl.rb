b703a4d2804cbfe6f70a349236f4a3248c6fba97
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 20:07:13 +0100

    php55-pcntl: update 5.5.25 bottle.

--- Formula/php55-pcntl.rb
+++ Formula/php55-pcntl.rb
@@ -8,6 +8,10 @@ class Php55Pcntl < AbstractPhp55Extension
   version  PHP_VERSION
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "105f49c39a763be43bcb5ca31bac317a0c3059058b3e6675f748957d661ef4c7" => :yosemite
+    sha256 "db9a0099c31876706062699783b3383e9c823426340e4db8f549c04275dc6631" => :mavericks
+    sha256 "a9ecdb67b444e363fff48d256fd7a47162c00992ce13ff423c67dd9f3ef6f73c" => :mountain_lion
   end
 
   def install
