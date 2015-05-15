2bd4529535b937060b9959b164dc3192256162f0
Alan Ivey <alanivey@gmail.com>
2015-05-15 16:04:53 -0400

    php5x-http: update to 2.4.3
    Closes #1740.
    
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/php55-http.rb
+++ Formula/php55-http.rb
@@ -2,18 +2,24 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php55Http < AbstractPhp55Extension
   init
-  homepage 'http://pecl.php.net/package/pecl_http'
-  url 'http://pecl.php.net/get/pecl_http-2.2.1.tgz'
-  sha1 'b43f2a6bc9179c4d8a673d466c9b0815ab998795'
-  head 'https://git.php.net/repository/pecl/http/pecl_http.git'
+  homepage "http://pecl.php.net/package/pecl_http"
+  url "http://pecl.php.net/get/pecl_http-2.4.3.tgz"
+  sha256 "2d9f3713331cf35b4a0162bd8b294683a67fecb5223e50978661acccd30c6c39"
 
-  depends_on 'curl' => :build
-  depends_on 'libevent' => :build
-  depends_on 'php55-raphf'
-  depends_on 'php55-propro'
+  head "https://git.php.net/repository/pecl/http/pecl_http.git"
+
+  bottle do
+  end
+
+  depends_on "curl"
+  depends_on "libevent" => :build
+  depends_on "php55-raphf"
+  depends_on "php55-propro"
 
   # overwrite the config file name to ensure extension is loaded after dependencies
-  def config_filename; "zzz_ext-" + extension + ".ini"; end
+  def config_filename
+    "zzz_ext-" + extension + ".ini"
+  end
 
   def install
     Dir.chdir "pecl_http-#{version}" unless build.head?
@@ -23,22 +29,26 @@ class Php55Http < AbstractPhp55Extension
     safe_phpize
 
     # link in the raphf extension header
-    system "mkdir -p ext/raphf"
-    cp "#{Formula['php55-raphf'].opt_prefix}/include/php_raphf.h", "ext/raphf/php_raphf.h"
+    mkdir_p "ext/raphf"
+    cp "#{Formula["php55-raphf"].opt_prefix}/include/php_raphf.h", "ext/raphf/php_raphf.h"
 
     # link in the propro extension header
-    system "mkdir -p ext/propro"
-    cp "#{Formula['php55-propro'].opt_prefix}/include/php_propro.h", "ext/propro/php_propro.h"
+    mkdir_p "ext/propro"
+    cp "#{Formula["php55-propro"].opt_prefix}/include/php_propro.h", "ext/propro/php_propro.h"
 
     system "./configure", "--prefix=#{prefix}",
                           phpconfig,
-                          "--with-libevent-dir=#{Formula['libevent'].opt_prefix}",
-                          "--with-curl-dir=#{Formula['curl'].opt_prefix}"
+                          "--with-libevent-dir=#{Formula["libevent"].opt_prefix}",
+                          "--with-curl-dir=#{Formula["curl"].opt_prefix}"
     system "make"
     prefix.install "modules/http.so"
     write_config_file if build.with? "config-file"
 
     # remove old configuration file
     rm_f config_scandir_path / "ext-http.ini"
+  end
+
+  test do
+    shell_output("php -m").include?("http")
   end
 end
