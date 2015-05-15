bf3665b9d0b6c1992e6bc534999fb77389f723ae
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 21:23:00 +0100

    php56-pdo-pgsql: update 5.6.9 bottle.

--- Formula/php56-pdo-pgsql.rb
+++ Formula/php56-pdo-pgsql.rb
@@ -8,6 +8,10 @@ class Php56PdoPgsql < AbstractPhp56Extension
   version  PHP_VERSION
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "d9abfa0ab01adc4df13b45b16b1f312149f6ceaead42cc6b1bbca855e0a219f0" => :yosemite
+    sha256 "ecb9d5941f5f9e3d51d65490526a177272d50ba01d531d234836bf4927ab0820" => :mavericks
+    sha256 "3c7db2cd076bab7fbb4d7b05045a2162df2cba92fa7e5ec8c5a99b56ca8185ee" => :mountain_lion
   end
 
   depends_on "postgresql"
