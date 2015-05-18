742f2cadc10a3b7d5bda23073859d9873046587a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-18 01:44:16 +0100

    php56-sphinx: add 1.3.2 bottle.

--- Formula/php56-sphinx.rb
+++ Formula/php56-sphinx.rb
@@ -6,6 +6,13 @@ class Php56Sphinx < AbstractPhp56Extension
   url 'http://pecl.php.net/get/sphinx-1.3.2.tgz'
   sha256 '0c3ada36833a44a8147d2dd1b907548010ae53de0a05041a77e68dee036130e6'
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "f2e7a31105f8c6d5af9a547ac13b4ac4f2cb61fbf1a7adf3e2aa6acc32b0e1f1" => :yosemite
+    sha256 "1142c54f1d52a496c4284773f12b2fe9aa73effa59d7d89a2b3649b13e7e3521" => :mavericks
+    sha256 "35d8fd6dfd8eec9e6e558715c21550b747bf85511b9fb5bf55af54d99333488c" => :mountain_lion
+  end
+
   depends_on 'libsphinxclient'
 
   def install
