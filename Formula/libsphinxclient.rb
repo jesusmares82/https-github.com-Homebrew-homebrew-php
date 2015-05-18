a865d160a769c181d459e7122b1ae4c082fc96b6
Denis Denisov <denji0k@gmail.com>
2015-05-18 03:20:18 +0300

    libsphinxclient 2.2.9 stable, --devel 2.3.1
    Signed-off-by: Alan Ivey <alanivey@gmail.com>

--- Formula/libsphinxclient.rb
+++ Formula/libsphinxclient.rb
@@ -1,15 +1,13 @@
-require 'formula'
-
 class Libsphinxclient < Formula
   homepage 'http://www.sphinxsearch.com'
-  url 'http://sphinxsearch.com/files/sphinx-2.1.3-release.tar.gz'
-  sha1 'f558dd2b96dabf26f533f5982bf1784582bf6f32'
+  url 'http://sphinxsearch.com/files/sphinx-2.2.9-release.tar.gz'
+  sha256 '79bcb9fca069ba630fd71fb40fba05bb16e19b475906fb6ae026334d50a6bf3a'
 
   head 'http://sphinxsearch.googlecode.com/svn/trunk/'
 
   devel do
-    url 'http://sphinxsearch.com/files/sphinx-2.2.1-beta.tar.gz'
-    sha1 'dccaa7d14f71cec8fe6dfdb059315856c0712885'
+    url 'http://sphinxsearch.com/files/sphinx-2.3.1-beta.tar.gz'
+    sha256 '0e5ebee66fe5b83dd8cbdebffd236dcd7cd33a7633c2e30b23330c65c61ee0e3'
   end
 
   def install
