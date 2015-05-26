654d010a4316355feb39f275e5a84d058980c81c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-26 22:33:39 +0100

    php54-parsekit: add 1.3.0 bottle.

--- Formula/php54-parsekit.rb
+++ Formula/php54-parsekit.rb
@@ -8,6 +8,14 @@ class Php54Parsekit < AbstractPhp54Extension
   sha256 "447d5ec6412d6c8c6489b03e7333d3872944444610b74eafd608eddcb3bbaf08"
   head "https://github.com/php/pecl-php-parsekit.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    cellar :any
+    sha256 "ae19e135eae58ecf94522e5e711c98f534ecfb531da8371ce7f53f48455c50e8" => :yosemite
+    sha256 "a366b68500abd2dd552a10967bd46f67a0fb65b8b62559d3a3038b9736f32cf6" => :mavericks
+    sha256 "6213e69f2a8108c0bafce5b5cfe04aaa22a88d502830750192b9b0d5a8cded14" => :mountain_lion
+  end
+
   patch do
     # Fix incompatibility issues with parsekit 1.3.0
     # and PHP 5.4 (https://bugs.php.net/61187)
