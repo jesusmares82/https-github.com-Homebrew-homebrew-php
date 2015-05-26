d3d0f6f0aaf34318706f26eb21c2a02c407d135c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-26 22:33:39 +0100

    php53-parsekit: add 1.3.0 bottle.

--- Formula/php53-parsekit.rb
+++ Formula/php53-parsekit.rb
@@ -8,6 +8,14 @@ class Php53Parsekit < AbstractPhp53Extension
   sha256 "447d5ec6412d6c8c6489b03e7333d3872944444610b74eafd608eddcb3bbaf08"
   head "https://github.com/php/pecl-php-parsekit.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    cellar :any
+    sha256 "56a16622ca9276f9b7ad38f4fb827840a5294de36c521079906293ce0f5418a0" => :yosemite
+    sha256 "b0bccbbddc882af1585e4566026013e618265322eb2603d0640737aa4307ac18" => :mavericks
+    sha256 "27d3ec7b1b90e692396e34c5bbca37b09d610c79aca3c92d15d751b5db18bbf1" => :mountain_lion
+  end
+
   def install
     Dir.chdir "parsekit-#{version}" unless build.head?
 
