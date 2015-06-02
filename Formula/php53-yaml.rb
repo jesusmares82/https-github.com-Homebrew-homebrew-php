f0df48fdaf33a7bbbce1c59dda06d9ebf5778e40
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-02 23:17:59 +0100

    php53-yaml: add 1.2.0 bottle.

--- Formula/php53-yaml.rb
+++ Formula/php53-yaml.rb
@@ -8,6 +8,13 @@ class Php53Yaml < AbstractPhp53Extension
   sha256 "fdf0eec6bf16b0c45e58076a2048f96140a778463724fb82ab49ab5fb4c7e19a"
   head "https://github.com/php/pecl-file_formats-yaml.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "70fde3c416ca8b5b58e54dfdf2fde09751190c93b1e6345049b54e51414c7895" => :yosemite
+    sha256 "ca14c18f6c3a83108d44e483dd51af1e6b933e25cf2bc10844747db96fc3704c" => :mavericks
+    sha256 "4711ab1367c883a4e5975db28a92549ecc6b0c88f81d26e3f44c7fc9e143f838" => :mountain_lion
+  end
+
   depends_on "libyaml"
 
   def install
