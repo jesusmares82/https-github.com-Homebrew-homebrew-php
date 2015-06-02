59e9f6993137b904bbc82b91be2e585ea4c86848
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-02 01:26:31 +0100

    php56-swoole: add 1.7.17 bottle.

--- Formula/php56-swoole.rb
+++ Formula/php56-swoole.rb
@@ -7,6 +7,13 @@ class Php56Swoole < AbstractPhp56Extension
   sha256 "e4d3c2466aa1e1c99750ec5497445bde8ef775fd0b304d11016e6df363cc7ec0"
   head "https://github.com/swoole/swoole-src.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "f3560a5d49ca5d0b5b27fab63fd5b16e034a592c47afe1c860be38350fd8d286" => :yosemite
+    sha256 "8cafb220f5ec6123cad36a8523b82c7855c30acf0406018150915b57f77cc4db" => :mavericks
+    sha256 "5107208c3db15bafceaf18d104a91b02539907801e180c3249bc7e23b6e4d9d3" => :mountain_lion
+  end
+
   def install
     Dir.chdir "swoole-#{version}" unless build.head?
 
