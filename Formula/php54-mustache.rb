137a95e3acb3534e8a48dcd15b7736034a47e6f3
Stuart Grimshaw <stuart.grimshaw@wandisco.com>
2015-06-12 21:31:06 +0100

    Added Mustache extension for php5.3 to 5.6
    Closes #1788.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php54-mustache.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php54Mustache < AbstractPhp54Extension
+  desc "Mustache PHP Extension"
+  init
+  homepage "https://github.com/jbboehr/php-mustache#mustache"
+  url "https://github.com/jbboehr/php-mustache/archive/v0.6.0.tar.gz"
+  sha1 "4cd4080572b35425bfdf78b012bcc5f67f354bc7"
+
+  depends_on "libmustache"
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/mustache.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  def test
+    system "php -m | grep mustache"
+  end
+end
