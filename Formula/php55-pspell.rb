86083c4cc4ed9a6d7541428ce910fa1708367fe4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-15 15:36:05 +0100

    php55-pspell: update 5.5.26 bottle.

--- Formula/php55-pspell.rb
+++ Formula/php55-pspell.rb
@@ -8,6 +8,10 @@ class Php55Pspell < AbstractPhp55Extension
   version  PHP_VERSION
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "08569f6050c8037d3e1c571751c3b93491fed0c09b36d06fbb8894c793617c34" => :yosemite
+    sha256 "017cc7f78ae5f426350dc4f94e3a462824b8c4417ffffc9b1de8555d2b829e82" => :mavericks
+    sha256 "b46d04512eebc7059d584d64a3945dd274ac2380d18df6f11b5a37710fd4e916" => :mountain_lion
   end
 
   depends_on "aspell"
