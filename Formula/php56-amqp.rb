15aacd9d33cd555d617bd212236fc11517ca54d4
Guo Xiao <guoxiao08@gmail.com>
2015-06-19 11:07:21 +0800

    php-amqp 1.6.0 beta3

--- Formula/php56-amqp.rb
+++ Formula/php56-amqp.rb
@@ -2,12 +2,13 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Amqp < AbstractPhp56Extension
   init
-  homepage 'http://pecl.php.net/package/amqp'
-  url 'http://pecl.php.net/get/amqp-1.4.0.tgz'
-  sha1 '7a08ff1cf0368f2f61db360b3402ed8c45444e85'
-  head 'http://svn.php.net/repository/pecl/amqp/trunk/'
+  homepage "http://pecl.php.net/package/amqp"
+  url "http://pecl.php.net/get/amqp-1.6.0beta3.tgz"
+  sha256 "4209dbd96355a0610b42a636621b0b2b34169ef64f8067197a1a5a78a5784ac7"
+  version "1.6.0beta3"
+  head "https://github.com/pdezwart/php-amqp.git"
 
-  depends_on 'rabbitmq-c'
+  depends_on "rabbitmq-c"
 
   def install
     Dir.chdir "amqp-#{version}" unless build.head?
