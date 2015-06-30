e7613174bcb8de5323845926a276494e3a61621a
Alex <Alex@Alexs-MacBook-Pro.local>
2015-06-30 10:31:19 +0200

    Updated Codeception to v2.1.0, added caveats

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -2,11 +2,10 @@ require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
 require File.expand_path("../../Requirements/phar-requirement", __FILE__)
 
 class Codeception < Formula
-  homepage "http://codeception.com/quickstart"
   desc "PHP Testing Framework is designed to work just out of the box."
-  url "http://codeception.com/releases/2.0.14/codecept.phar"
-  sha256 "bdef7b350ebacc96ca1aa29beb6fa5f7b1196a267c9bec97e02c3d39e724c9c8"
-  version "2.0.14"
+  homepage "http://codeception.com/quickstart"
+  url "http://codeception.com/releases/2.1.0/codecept.phar"
+  sha256 "640aa82348d84d41e2c5db31696c1b8ef4f622e1bb60ba1b1b393c64ffe3c7c9"
 
   def install
     mv "codecept.phar", "codecept-#{version}.phar"
@@ -15,6 +14,13 @@ class Codeception < Formula
     sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/codecept-#{version}.phar $*")
     chmod 0755, sh
     bin.install_symlink sh
+  end
+
+  def caveats; <<-EOS.undent
+    To update your tests for 2.1 check the "Upgrading" section:
+    http://codeception.com/06-30-2015/codeception-2.1-is-here.html
+
+    EOS
   end
 
   test do
