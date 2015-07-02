28379c5e0b6c72ac72d704c13666d33b6255b92e
wedi <code@dirk-weise.de>
2015-07-02 16:57:48 +0200

    phpmyadmin: fix audit problems
    Fix all audit problems and style complaints reported by `brew audit --strict phpmyadmin`

--- Formula/phpmyadmin.rb
+++ Formula/phpmyadmin.rb
@@ -1,29 +1,28 @@
-require 'formula'
-
 class Phpmyadmin < Formula
-  homepage 'http://www.phpmyadmin.net'
-  url 'https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_4_4_4.tar.gz'
-  sha256 '1fc8f6e9229f22920b06d27fda63ad39c906bd11e76d555d03f8878e9800627f'
-  head 'https://github.com/phpmyadmin/phpmyadmin.git'
+  desc "phpMyAdmin is a tool intended to handle the administration of MySQL over the Web"
+  homepage "http://www.phpmyadmin.net"
+  url "https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_4_4_4.tar.gz"
+  sha256 "1fc8f6e9229f22920b06d27fda63ad39c906bd11e76d555d03f8878e9800627f"
+  head "https://github.com/phpmyadmin/phpmyadmin.git"
 
-  if build.with? 'mcrypt'
-    depends_on "php53-mcrypt" if Formula['php53'].linked_keg.exist?
-    depends_on "php54-mcrypt" if Formula['php54'].linked_keg.exist?
-    depends_on "php55-mcrypt" if Formula['php55'].linked_keg.exist?
-    depends_on "php56-mcrypt" if Formula['php56'].linked_keg.exist?
+  if build.with? "mcrypt"
+    depends_on "php53-mcrypt" if Formula["php53"].linked_keg.exist?
+    depends_on "php54-mcrypt" if Formula["php54"].linked_keg.exist?
+    depends_on "php55-mcrypt" if Formula["php55"].linked_keg.exist?
+    depends_on "php56-mcrypt" if Formula["php56"].linked_keg.exist?
   end
 
   unless MacOS.prefer_64_bit?
-    option 'without-mcrypt', "Exclude the php-mcrypt module"
+    option "without-mcrypt", "Exclude the php-mcrypt module"
   end
 
   def install
-    (share+'phpmyadmin').install Dir['*']
+    (share+"phpmyadmin").install Dir["*"]
 
-    if !(File.exists?(etc+'phpmyadmin.config.inc.php'))
-      cp (share+'phpmyadmin/config.sample.inc.php'), (etc+'phpmyadmin.config.inc.php')
+    unless File.exist?(etc+"phpmyadmin.config.inc.php")
+      cp (share+"phpmyadmin/config.sample.inc.php"), (etc+"phpmyadmin.config.inc.php")
     end
-    ln_s (etc+'phpmyadmin.config.inc.php'), (share+'phpmyadmin/config.inc.php')
+    ln_s (etc+"phpmyadmin.config.inc.php"), (share+"phpmyadmin/config.inc.php")
   end
 
   def caveats; <<-EOS.undent
