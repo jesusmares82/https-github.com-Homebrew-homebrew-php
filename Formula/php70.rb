03335deb5a1e150f4d14734765694c127b56dcbb
Andy Blyler <andy@blyler.cc>
2015-07-09 21:59:41 -0400

    Revert "php70: update 7.0.0-beta.1 bottle."
    This reverts commit a5663b2eee0d423d043bc875e76062bde87da700.

--- Formula/php70.rb
+++ Formula/php70.rb
@@ -12,9 +12,6 @@ class Php70 < AbstractPhp
   head PHP_GITHUB_URL, :branch => PHP_BRANCH
 
   bottle do
-    sha256 "8e8a5e35c2444b2dead17b52dc64e2321f1b0a67a2d306221bea5f5e81ecb9e3" => :yosemite
-    sha256 "d95796a7b501d95c9d83af90ae965baf81b1a3e2e7c3d67544a1223c633a6482" => :mavericks
-    sha256 "0e153f2e8fea709a162d54902b4be90be8c1d2f9a96afbaf403e25266a565735" => :mountain_lion
   end
 
   def install_args
