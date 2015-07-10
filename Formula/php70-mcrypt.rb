957eb79678ba69c011f4ee482f45062132fd32bb
Andy Blyler <andy@blyler.cc>
2015-07-09 21:59:56 -0400

    Revert "update php70 to beta1"
    This reverts commit bbbbd2320e5c259ced161d9aaa012a452b5d07aa.

--- Formula/php70-mcrypt.rb
+++ Formula/php70-mcrypt.rb
@@ -9,6 +9,10 @@ class Php70Mcrypt < AbstractPhp70Extension
   version PHP_VERSION
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-php"
+    sha256 "410775f5c69b65c74a4c1ce2f034063aaaebe7f0e734d56e3eb82920efb9db4a" => :yosemite
+    sha256 "c3ccab8830b60d4794bd9485b30e1556d621e167756c15c8937c25f3f1d3f5d9" => :mavericks
+    sha256 "ac25dc36612e27e1c3490d799512114ae518b2b33d52873f780dd649578ed433" => :mountain_lion
   end
 
   depends_on "mcrypt"
