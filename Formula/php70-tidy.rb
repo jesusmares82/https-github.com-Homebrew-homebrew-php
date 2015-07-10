89ce38326abe984dbd12b112dfdcbf408b963e7e
Andy Blyler <andy@blyler.cc>
2015-07-09 21:59:32 -0400

    Revert "php70-tidy: update 7.0.0-beta.1 bottle."
    This reverts commit 719c7eabc5bcbd893c1843cbf9e647df07a92a3d.

--- Formula/php70-tidy.rb
+++ Formula/php70-tidy.rb
@@ -9,9 +9,6 @@ class Php70Tidy < AbstractPhp70Extension
   version PHP_VERSION
 
   bottle do
-    sha256 "70dadf26501b4ea14443b936bcd69a0cda0a9163dd46c1dba48540cc39793cb3" => :yosemite
-    sha256 "9154e6b0e9bbefa35c0142c9790d989a96582705f8cc613f17f8f9b0d1a38874" => :mavericks
-    sha256 "e57b2a238854b1b8a91085c3d1cf3d8d4b412c7de5df00cf3f0c3d2171518be4" => :mountain_lion
   end
 
   def install
