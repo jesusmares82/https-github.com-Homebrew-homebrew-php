dc5dcf19a4cfdb4bef9be6b5b3fb6810c06caf44
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-10 13:50:35 +0100

    libmustache: add 0.3.2 bottle.

--- Formula/libmustache.rb
+++ Formula/libmustache.rb
@@ -4,6 +4,13 @@ class Libmustache < Formula
   url "https://github.com/jbboehr/libmustache/archive/v0.3.2.tar.gz"
   sha256 "b74e5fed678572a5d8a02fced60bf1c2d9cbe8d432346f0803161280400d93ef"
 
+  bottle do
+    cellar :any
+    sha256 "2bfdc15117dd4ffd1c50a2c7ec32ec06f11e0a44780d5c0b70ce70d49e145f92" => :yosemite
+    sha256 "6e380f2c33b36992b49673caa8108059b3853d4348305f7009355925a9c08772" => :mavericks
+    sha256 "96c194eeda6e194bf7e40560fe1fad5e41162cac0ae73c774d0de9d31294cd96" => :mountain_lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
