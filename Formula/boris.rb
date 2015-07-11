21e79e5c793109dedbbb3f3a14be685b21d9cdd1
Andy Blyler <andy@blyler.cc>
2015-07-11 16:31:08 -0400

    boris: ensure that required php modules are installed

--- Formula/boris.rb
+++ Formula/boris.rb
@@ -13,21 +13,18 @@ class Boris < Formula
   depends_on "php54" if Formula["php54"].linked_keg.exist?
   depends_on "php55" if Formula["php55"].linked_keg.exist?
   depends_on "php56" if Formula["php56"].linked_keg.exist?
+  depends_on "php70" if Formula["php70"].linked_keg.exist?
 
   def install
+    # ensure the required php modules are installed
+    php_modules = Utils.popen_read("php -m")
+    raise "php must be re-compiled, in order to have readline support" if !php_modules.include?("readline")
+    raise "php must be re-compiled with pcntl support" if !php_modules.include?("pcntl")
+    raise "php must be re-compiled with posix support" if !php_modules.include?("posix")
+
     system "composer", "install"
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/boris"
-  end
-
-  def caveats
-    <<-EOS.undent
-    Boris depends on the following PHP features:
-      * PHP >= 5.3
-      * The Readline functions
-      * The PCNTL functions
-      * The POSIX functions
-    EOS
   end
 
   test do
