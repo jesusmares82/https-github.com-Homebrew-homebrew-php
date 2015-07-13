274efbcf260926359d69ca537277e845062fb30e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-14 00:11:59 +0100

    phpunit: update 4.7.6 bottle.

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -7,6 +7,9 @@ class Phpunit < Formula
   sha256 "f97a77f09773dd963e8eb5d36ef541f62039b603cfcb452691d27c423499569d"
 
   bottle do
+    cellar :any
+    sha256 "9c040894f1116e3edc29e279a6040c16d1c6fbd35527e0a88b22d6cfe3f8855a" => :yosemite
+    sha256 "a0115e84ac47de8f70cfe87be14461fd7bbba514800b9139757f98b4f487053f" => :mavericks
   end
 
   def install
