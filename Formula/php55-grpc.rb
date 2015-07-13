6a01499abce021e774c9641c78ace65e4776fb40
Stanley Cheung <stanleycheung@google.com>
2015-07-12 23:31:29 -0700

    add formula for gRPC PHP extension
    Closes #1847.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php55-grpc.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php55Grpc < AbstractPhp55Extension
+  init
+  desc "The PHP extension for the gRPC library"
+  homepage "http://grpc.io"
+  url "https://pecl.php.net/get/grpc-0.5.1.tgz"
+  sha256 "d42fbe6f1e67b21d2c08832fda4ff2baf4ca689324200da8abaefd042500c5a4"
+
+  depends_on "grpc/grpc/grpc"
+
+  def install
+    Dir.chdir "grpc-#{version}"
+    safe_phpize
+    system "./configure", "--enable-grpc=#{HOMEBREW_PREFIX}",
+           "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/grpc.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("grpc")
+  end
+end
