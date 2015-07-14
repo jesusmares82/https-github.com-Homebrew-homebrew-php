1c45518275aba98f012f244f3e64140f5cd83d72
Andy Blyler <andy@blyler.cc>
2015-07-14 18:58:27 -0400

    create bottle for php56 to fix openssl
    Closes #1908.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56.rb
+++ Formula/php56.rb
@@ -7,14 +7,11 @@ class Php56 < AbstractPhp
   url     PHP_SRC_TARBALL
   sha256  PHP_CHECKSUM[:sha256]
   version PHP_VERSION
-  revision 1
+  revision 2
 
   head    PHP_GITHUB_URL, :branch => PHP_BRANCH
 
   bottle do
-    sha256 "e8b7e05d366d775caced2e3d7b847aca4c064922a90738b83158176adec49e70" => :yosemite
-    sha256 "c4f7a8ce7672072285cf31dd1d28864edd5de94b8914c96bd1bbf95decc56105" => :mavericks
-    sha256 "b7690993589505bcea7cb16ccbf4ba5541dcfd2e783ec9636f4a9116237756de" => :mountain_lion
   end
 
   def install_args
