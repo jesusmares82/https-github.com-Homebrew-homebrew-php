dd7ceb71714e40ed4043833ffb77ff20b75ae6fa
Andy Blyler <andy@blyler.cc>
2015-07-14 18:58:51 -0400

    create bottle for php70 to fix openssl
    Closes #1907.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php70.rb
+++ Formula/php70.rb
@@ -8,14 +8,11 @@ class Php70 < AbstractPhp
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
   version PHP_VERSION
-  revision 1
+  revision 2
 
   head PHP_GITHUB_URL, :branch => PHP_BRANCH
 
   bottle do
-    sha256 "ef6660aef71771b0d136c91c449f58617d60523ab8824b358b67462bd5fc78d3" => :yosemite
-    sha256 "7a7ed44a8b14328e3fc7be194f1549791f6899146f193e182e3810665dbd4c8e" => :mavericks
-    sha256 "46b0f3381d7c8cc10f8bd7e5546ef0ace55d2ddbb36596c697bed19402108170" => :mountain_lion
   end
 
   def install_args
