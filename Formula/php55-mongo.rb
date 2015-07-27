ea733ea0057225ae150e68347d890c53f3bd3f1a
Eric Cholis <echolis@dacardworld.com>
2015-07-27 08:06:49 -0400

    Updated MongoDB driver formula to 1.6.10
    Closes #1938.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php55-mongo.rb
+++ Formula/php55-mongo.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php55Mongo < AbstractPhp55Extension
   init
   homepage "http://pecl.php.net/package/mongo"
-  url "http://pecl.php.net/get/mongo-1.6.7.tgz"
-  sha256 "15dce79f28ca8aa30296334eeb111c573f821004dbbf86739ff4176a67708c3d"
+  url "http://pecl.php.net/get/mongo-1.6.10.tgz"
+  sha256 "02ec2002ebef399261b86e317970b3c89058dd44da6e48b71b51880430d34c63"
   head "https://github.com/mongodb/mongo-php-driver.git"
 
   def install
