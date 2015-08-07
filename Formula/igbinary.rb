71df126136286e739aabc63cea3902259f7341d9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-07 21:24:57 +0100

    igbinary: update 1.2.1 bottle.

--- Formula/igbinary.rb
+++ Formula/igbinary.rb
@@ -5,6 +5,10 @@ class Igbinary < Formula
   head "https://github.com/igbinary/igbinary.git"
 
   bottle do
+    cellar :any
+    sha256 "879d7f52a38859cee9abce37e007d5df8e88aa66b5ae3c2bb896ab6a3004f4b6" => :yosemite
+    sha256 "8b43bc396c6223789ddb1f96f95ba2b121308683aab020a718be4342e43e9d6e" => :mavericks
+    sha256 "483d500cbf87ec7e1bd1fdc0be5995f586d1332a8ab38e559f99debe36c75675" => :mountain_lion
   end
 
   def install
