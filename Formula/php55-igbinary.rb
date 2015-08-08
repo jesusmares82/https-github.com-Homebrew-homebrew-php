1d72e8127b12a59f84a46b5a0c084b2df68b817d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-08 17:45:28 +0100

    php55-igbinary: update 1.2.1 bottle.

--- Formula/php55-igbinary.rb
+++ Formula/php55-igbinary.rb
@@ -10,6 +10,10 @@ class Php55Igbinary < AbstractPhp55Extension
   depends_on 'igbinary' => :build
 
   bottle do
+    cellar :any
+    sha256 "2b989ed6c4df57d683e7d2f8f9773072961e212da579fd961871a9a1d73219e0" => :yosemite
+    sha256 "6eb580852234291f131bfd009172be456fdc9dfd322b83dedf4fa612d9a70a34" => :mavericks
+    sha256 "8e8f145596394bf33324c1cd50601cb76f2fdf4bf74608e22a07d987f7c4a422" => :mountain_lion
   end
 
   def install
