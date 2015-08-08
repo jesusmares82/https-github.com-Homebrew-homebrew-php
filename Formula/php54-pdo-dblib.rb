f817fe679d8e4e1bb55762533fe646c62e6c29c9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-08 21:38:39 +0100

    php54-pdo-dblib: update 5.4.44 bottle.

--- Formula/php54-pdo-dblib.rb
+++ Formula/php54-pdo-dblib.rb
@@ -8,6 +8,9 @@ class Php54PdoDblib < AbstractPhp54Extension
   version  PHP_VERSION
 
   bottle do
+    sha256 "214ef3a58a38a312d08b726ada3b922803b483fd3f6f58f8071461cba0803d86" => :yosemite
+    sha256 "b3d0f669a82264b69acf57f21de322a6c1a94123277aef5fd57f2ab4c4db3e2b" => :mavericks
+    sha256 "77c3e8f6e9cbcd3cb0774d2af6bf2e4868aa9e4a9be4c88fcf63eac9ebf8623a" => :mountain_lion
   end
 
   depends_on "freetds"
