754d4de4c53b17631f2cae5a92b8e54494c29caa
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-09 01:42:06 +0100

    php54-tidy: update 5.4.44 bottle.

--- Formula/php54-tidy.rb
+++ Formula/php54-tidy.rb
@@ -8,6 +8,10 @@ class Php54Tidy < AbstractPhp54Extension
   version  PHP_VERSION
 
   bottle do
+    cellar :any
+    sha256 "8a206eb3b0dfb54d83e7db6e379c59638cafc4ac7e137e86ed5fed448822831d" => :yosemite
+    sha256 "8e818066467c5c6c0e4209df9058181d957699911f269c7d0fa48a9bea3dd580" => :mavericks
+    sha256 "3b0f92480a77fd3c0604ecdee61bb2df2639bbb034176561aeb30b459d9628f9" => :mountain_lion
   end
 
   def install
