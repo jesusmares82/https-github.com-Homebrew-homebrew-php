c963310c2c32c1129d4667a54a38af6bfef801a0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-11 16:25:44 +0100

    php53-geoip: add 1.1.0 bottle.

--- Formula/php53-geoip.rb
+++ Formula/php53-geoip.rb
@@ -7,6 +7,12 @@ class Php53Geoip < AbstractPhp53Extension
   sha1 '72475f10cb3549eac7d790ab6ae1869f89c6dae0'
   head 'https://svn.php.net/repository/pecl/geoip/trunk/'
 
+  bottle do
+    sha256 "25f695e659ff6f390f54dc608eed6224346ab56315569452626840d5110c0fb5" => :yosemite
+    sha256 "6c3f7ca3d6901cd1b3a8a25409e3470818b734abadc634f7a74b30c7ddc646e3" => :mavericks
+    sha256 "2df677d40f47d2042dc6ee6de86e709d51cc20071440d3fc1526dd7ec280f4ab" => :mountain_lion
+  end
+
   depends_on 'geoip'
 
   def install
