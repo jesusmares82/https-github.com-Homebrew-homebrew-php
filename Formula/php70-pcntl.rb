1c2589b779e3a1bf3cbe714feda0db66c0360100
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-22 06:04:37 +0100

    php70-pcntl: update 7.0.0-rc.1 bottle.

--- Formula/php70-pcntl.rb
+++ Formula/php70-pcntl.rb
@@ -9,6 +9,10 @@ class Php70Pcntl < AbstractPhp70Extension
   version PHP_VERSION
 
   bottle do
+    cellar :any
+    sha256 "dff7dd2c6058daccebb44d742cf7b32db57b1fb8d0a5b81af00a86d29fce19e5" => :yosemite
+    sha256 "42238564fa85e9720a6d170c6ebe5bc0126fa5aff450e2db7fb93fd799c5d693" => :mavericks
+    sha256 "af071f3b63f34b06acb3f077b3e3be34b4f1ba19edad6d4c7640c1fcc08b189b" => :mountain_lion
   end
 
   def install
