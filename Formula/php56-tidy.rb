0d6c8a7af6fa16b28a2e005eecc3b7fb7dd729f8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-09 18:59:31 +0100

    php56-tidy: update 5.6.13 bottle.

--- Formula/php56-tidy.rb
+++ Formula/php56-tidy.rb
@@ -8,6 +8,11 @@ class Php56Tidy < AbstractPhp56Extension
   version  PHP_VERSION
 
   bottle do
+    cellar :any
+    revision 1
+    sha256 "29e3e8b1649f78c2ca26dad5d9b3a649d6eba2f84ba5dd1e1696a184fd967872" => :yosemite
+    sha256 "fc59ad45faa65ef4186b6ca0e83ff69b67ea99b1a1d618e5608d25d963230f31" => :mavericks
+    sha256 "dcce1bc99316e6237ee4519def4cc2244500f25875572506a0104c773169478e" => :mountain_lion
   end
 
   def install
