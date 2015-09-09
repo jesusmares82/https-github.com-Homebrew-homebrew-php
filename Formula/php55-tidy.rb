1ff2f7dc208c5f39ceb702b07fc998e49552a04c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-10 00:37:07 +0100

    php55-tidy: update 5.5.29 bottle.

--- Formula/php55-tidy.rb
+++ Formula/php55-tidy.rb
@@ -8,6 +8,11 @@ class Php55Tidy < AbstractPhp55Extension
   version  PHP_VERSION
 
   bottle do
+    cellar :any
+    revision 1
+    sha256 "4fba65858958fbcc3a9eb52bee0a2b7eb9018a8e47f213d03df6d2d4af85cea9" => :yosemite
+    sha256 "9b9f247120f78121f278325b5deff6593d6c36ebc381dbdf0c15c697117d9cc6" => :mavericks
+    sha256 "a6fc9305d09fdcb1e61066ebdf71e49d2a857cbe788f0e5b17eb55724dec1041" => :mountain_lion
   end
 
   def install
