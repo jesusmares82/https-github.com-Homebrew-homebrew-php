60678b33eb2220a6d1138ad1e763c8e5dadc8322
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-10 00:37:07 +0100

    php55-mcrypt: update 5.5.29 bottle.

--- Formula/php55-mcrypt.rb
+++ Formula/php55-mcrypt.rb
@@ -8,6 +8,10 @@ class Php55Mcrypt < AbstractPhp55Extension
   version  PHP_VERSION
 
   bottle do
+    revision 1
+    sha256 "98a17cc625aa76af45d1c4255f3c745b544da2cd71913547af97657b4da32a3b" => :yosemite
+    sha256 "b2154349bd6c9b87537c11faf13cd37500b1515f3deda096b479f7c2fe8a4f54" => :mavericks
+    sha256 "ead2e2b4aa9e73405686c5e4e2959ea6aa33e06f3eb058f8a451ecf2a5d6b24a" => :mountain_lion
   end
 
   depends_on "mcrypt"
