767394e152fe9f1a4670f055e9360a0ddd430dac
Andy Blyler <andy@blyler.cc>
2015-09-09 19:08:11 -0400

    fix issue that is preventing php55-pdo-dblib bottles
    Closes #2044.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php55-pdo-dblib.rb
+++ Formula/php55-pdo-dblib.rb
@@ -17,9 +17,6 @@ class Php55PdoDblib < AbstractPhp55Extension
   end
 
   def install
-
-    touch "#{Formula['freetds'].opt_prefix}/include/tds.h"
-    touch "#{Formula['freetds'].opt_prefix}/lib/libtds.a"
     Dir.chdir "ext/pdo_dblib" unless build.head?
 
     ENV.universal_binary if build.universal?
@@ -31,4 +28,3 @@ class Php55PdoDblib < AbstractPhp55Extension
     write_config_file if build.with? "config-file"
   end
 end
-
