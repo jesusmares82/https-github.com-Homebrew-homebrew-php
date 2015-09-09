f933ea6ffe5de24b8809da87db6c87d36a79dce5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-10 00:37:06 +0100

    php55-gmp: update 5.5.29 bottle.

--- Formula/php55-gmp.rb
+++ Formula/php55-gmp.rb
@@ -9,6 +9,10 @@ class Php55Gmp < AbstractPhp55Extension
   version PHP_VERSION
 
   bottle do
+    revision 1
+    sha256 "442c3bfe2527d4eb1f5600006a364297999ccd82884c34386cb18bdf289fd4eb" => :yosemite
+    sha256 "0f2f30075ec253548f4c9ebb8d3d846c5f945d2090422d4ce0b5bae0ac364544" => :mavericks
+    sha256 "b8e1bf49e99ff076d5d3ff6a4a2ffda62c62d6d2775bfd7222eb795977d6cbc1" => :mountain_lion
   end
 
   depends_on "gmp"
