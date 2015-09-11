24bfd4caf11cb805a98c0e7171c043e569f92cef
Andy Blyler <andy@blyler.cc>
2015-09-10 21:46:43 -0400

    Add php5*-maxminddb
    Closes #2050.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php54-maxminddb.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php54Maxminddb < AbstractPhp54Extension
+  init
+  desc "Extension for MaxMind DB file format"
+  homepage "https://github.com/maxmind/libmaxminddb"
+  url "https://github.com/maxmind/MaxMind-DB-Reader-php/archive/v1.0.3.tar.gz"
+  sha256 "c027479441b17ed5928640d27c42bccbb35ca07ac6401423c5bbb7220f720638"
+
+  bottle do
+  end
+
+  depends_on "libmaxminddb"
+
+  def install
+    Dir.chdir "ext"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", "--with-maxminddb=#{Formula["libmaxminddb"].opt_prefix}", phpconfig
+    system "make"
+    prefix.install "modules/maxminddb.so"
+    write_config_file if build.with? "config-file"
+  end
+end
