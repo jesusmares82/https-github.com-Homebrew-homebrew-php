a1efda49a2731a9052604a98e21d509730216538
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-11 22:17:34 +0100

    brew-php-switcher: add 1.5 bottle.

--- Formula/brew-php-switcher.rb
+++ Formula/brew-php-switcher.rb
@@ -4,6 +4,13 @@ class BrewPhpSwitcher < Formula
   sha256 "e5ead8b188a54d270b3a12bdcbae4a97c808e9d2ee72708597abffb8b4e8b591"
   head "https://github.com/philcook/brew-php-switcher.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d1a75a5eb4fd0a1641b520620ad8cf9d661eacbdc780dea906e5a13aeb12e1ff" => :yosemite
+    sha256 "b3f761057a8d9b32530dcadac8c319e353c6d73b13a7a487f073ca629c41d7d4" => :mavericks
+    sha256 "b051cef58b060aa987b4a7f9b8d6f07e0e996aa7b2e3dc47ae7d80e62784c966" => :mountain_lion
+  end
+
   def install
     bin.install "phpswitch.sh"
     sh = libexec + "brew-php-switcher"
