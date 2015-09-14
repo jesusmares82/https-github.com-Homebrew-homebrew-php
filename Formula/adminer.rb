77c68fc5dca04454fcf31bb77c0ae6976400123e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-14 21:20:14 +0100

    adminer: add 4.2.2 bottle.

--- Formula/adminer.rb
+++ Formula/adminer.rb
@@ -5,6 +5,13 @@ class Adminer < Formula
   url "https://downloads.sourceforge.net/project/adminer/Adminer/Adminer%20" + version + "/adminer-" + version + ".php"
   sha256 "4dced0c64dbeb0f6e9a27c4ec5838bbf5f33b82c368a8965b47ae3da6c9bcb2c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "127d8ecb0d2fa755dd44dd63d1c494db734ab48408befb2143fa79e603e3bc04" => :yosemite
+    sha256 "f39cedb75643dfd9638b43b10888190b359d3d472eb973b0c39d2bac208556a9" => :mavericks
+    sha256 "6b3525282d16ad79acf8aa32c9281f43b28ab728af76aad460743becb851df0e" => :mountain_lion
+  end
+
   def install
     (share+"adminer").install "adminer-"+version+".php" => "index.php"
   end
