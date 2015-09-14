6fe068bd1917ed994c5511db14f94aed94eb05c9
Denis Savenko <public@heliosart.info>
2015-09-14 20:06:24 +0300

    Pinba descrition shortened
    Closes #2053.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-pinba.rb
+++ Formula/php56-pinba.rb
@@ -2,7 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Pinba < AbstractPhp56Extension
   init
-  desc "PHP extension for Pinba - realtime monitoring/statistics server"
+  desc "PHP extension for Pinba monitoring server"
   homepage "http://pinba.org/"
   url "https://github.com/tony2001/pinba_extension/archive/6869f30e8c264e7a5742ab55f4f6bd5ee29e6080.tar.gz"
   sha256 "46e3640a47cd9b1951e6edbe637ba44ec5c858688883003c74ebe27a61e14fc5"
