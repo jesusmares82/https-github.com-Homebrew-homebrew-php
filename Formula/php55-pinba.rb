8e62ff5c11eee3337b9b0a6a72e7936392ca98c4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-14 20:57:54 +0100

    php55-pinba: add 6080 bottle.

--- Formula/php55-pinba.rb
+++ Formula/php55-pinba.rb
@@ -8,6 +8,13 @@ class Php55Pinba < AbstractPhp55Extension
   sha256 "46e3640a47cd9b1951e6edbe637ba44ec5c858688883003c74ebe27a61e14fc5"
   head "https://github.com/tony2001/pinba_extension.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1c17646ab09ce26e0d71a8121e84df99b622e1a66b8f8dc54def03c3d6c878ef" => :yosemite
+    sha256 "42435bdc31942a608424faa0adcff39c628c5a431d1658c691c8dddc3fb38596" => :mavericks
+    sha256 "070924963db6e1ee89a1a7c8f83671950ae84803c6b805d85339a905c9a3e83f" => :mountain_lion
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
