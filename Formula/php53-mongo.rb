451a6a5b515f143f326d9f7c3a83611acd17c9c3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-16 18:02:03 +0100

    php53-mongo: add 1.6.11 bottle.

--- Formula/php53-mongo.rb
+++ Formula/php53-mongo.rb
@@ -7,6 +7,13 @@ class Php53Mongo < AbstractPhp53Extension
   sha256 "cd180ae35f537a199d374a3105106ee5a2b0cab618b12f6221e4c985c28955ac"
   head "https://github.com/mongodb/mongo-php-driver.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f86961685e39b5943a867f7212812afcf079086250b6340b88fd4baea1f5a70b" => :el_capitan
+    sha256 "796982addc04cc4f20869f81673bd09d50d3756dd806a5fb4ddc007178cce248" => :yosemite
+    sha256 "fea51e130b0aebc550c4a8e3f130bffe56983cd7683f0dd4d1ef856d59146045" => :mavericks
+  end
+
   def install
     Dir.chdir "mongo-#{version}" unless build.head?
 
