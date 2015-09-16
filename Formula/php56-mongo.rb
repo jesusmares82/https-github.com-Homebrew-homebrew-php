b372327436521ff7359a6693407e2745cf3cd7fb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-16 18:02:03 +0100

    php56-mongo: add 1.6.11 bottle.

--- Formula/php56-mongo.rb
+++ Formula/php56-mongo.rb
@@ -7,6 +7,13 @@ class Php56Mongo < AbstractPhp56Extension
   sha256 "cd180ae35f537a199d374a3105106ee5a2b0cab618b12f6221e4c985c28955ac"
   head "https://github.com/mongodb/mongo-php-driver.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d4857836fff0d1920150d2a6cb6f5aa2cd0eb23f480e4c14ecc891297dea0952" => :el_capitan
+    sha256 "bae04a3a0942aa58900589676f8d0ee0c1b4edb00fb91fb32812295016090ddd" => :yosemite
+    sha256 "fefb27c7c684d8dd642c19eadbae6710aff3511d02f70a87f85c0df313edb87f" => :mavericks
+  end
+
   def install
     Dir.chdir "mongo-#{version}" unless build.head?
 
