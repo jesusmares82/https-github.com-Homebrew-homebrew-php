9afc9d957085e0db23068b760d47c5351c3d4474
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-16 15:40:32 +0100

    php55-httpparser: add 0.1.0 bottle.

--- Formula/php55-httpparser.rb
+++ Formula/php55-httpparser.rb
@@ -8,6 +8,12 @@ class Php55Httpparser < AbstractPhp55Extension
   sha256 "9ba699a116696bb3695b7bff7b5ffd2be4b5cbe6746d1814c628c141eb1ff381"
   head "https://github.com/dhotson/httpparser-php.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "433755a1d55ff38733ac50f529a7754f598f61e626ab27dd55ee05a09e6e800a" => :yosemite
+    sha256 "2eb290d020d64b70fe310c281fccd33baea999f222e13a17b9b04a0a222993fb" => :mavericks
+  end
+
   def extension
     "httpparser"
   end
