f7a8eb67ae93a10e2c1a15d41b68f035a6bd1b1c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-17 07:28:57 +0100

    php-install: add 0.0.1 bottle.

--- Formula/php-install.rb
+++ Formula/php-install.rb
@@ -7,6 +7,13 @@ class PhpInstall < Formula
 
   head 'https://github.com/marcosdsanchez/php-install.git'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dd34d219e395f8b5915f9a576fdc2047faf4905ef75edc42976cf34d801c5d33" => :el_capitan
+    sha256 "f8b4eba89817efeb6a0f8d2392b92900fb383972911ce618a49565cd898d9ab8" => :yosemite
+    sha256 "2607de6bd672f4f264fa74d68ec65b2416c29f6e739664548ed049389783f207" => :mavericks
+  end
+
   def install
     system 'make', 'install', "PREFIX=#{prefix}"
   end
