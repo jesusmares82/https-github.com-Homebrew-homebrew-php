32925f6c45bab02a90696eaeea8bffaecdf0a819
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-17 07:07:05 +0100

    php-build: add 0.10.0 bottle.

--- Formula/php-build.rb
+++ Formula/php-build.rb
@@ -6,6 +6,13 @@ class PhpBuild < Formula
   sha256 '9f3f842608ee7cb3a6a9fcf592a469151fc1e73068d1c2bd6dbd15cac379857c'
   head 'https://github.com/CHH/php-build.git'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5080f0dd4caac6cfd31ddc2264d9e3769d611c34d26866ec86023ddca7f34769" => :el_capitan
+    sha256 "71a56c627cf57bdd36f77645f2f3e50ff1460de1c079c131e746e39b83a56308" => :yosemite
+    sha256 "3f246c48f9163194997128d370ece7a25009da1eb345d2e8a80dc06397ee07d9" => :mavericks
+  end
+
   depends_on 'wget'
   depends_on 'autoconf' => :build
 
