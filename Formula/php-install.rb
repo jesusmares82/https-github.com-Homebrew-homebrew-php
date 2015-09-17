55ef1c24d9713ad210aea321093bcb2d4828efb7
Andy Blyler <andy@blyler.cc>
2015-09-16 20:49:41 -0400

    php-install: convert from sha1 to sha256
    Closes #2069.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php-install.rb
+++ Formula/php-install.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PhpInstall < Formula
   homepage 'https://github.com/marcosdsanchez/php-install#readme'
   url 'https://github.com/marcosdsanchez/php-install/archive/v0.0.1.tar.gz'
-  sha1 '7306700d7963e24bb670b37e1de5613483df24f4'
+  sha256 '97f1124ca0b200234dce4bec307e153768f803c08768a231ed3a3ff2864c36c3'
 
   head 'https://github.com/marcosdsanchez/php-install.git'
 
