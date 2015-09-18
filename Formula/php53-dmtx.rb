887c4596d5d9b4ad53af650c92f68d5337891242
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-18 11:11:49 +0100

    php53-dmtx: add dev bottle.

--- Formula/php53-dmtx.rb
+++ Formula/php53-dmtx.rb
@@ -8,6 +8,12 @@ class Php53Dmtx < AbstractPhp53Extension
   sha256 '864a2c9a39812e89d4aa3379cdf543a10a88b227cbe6d57ac94f2fd388b35e26'
   head 'https://github.com/maZahaca/php-dmtx.git'
 
+  bottle do
+    sha256 "a73ad76996a68239552cd6dd72addfe4559e4c8289c7ce5a670aae5ebf592619" => :el_capitan
+    sha256 "71ffddf16e59d968d4e934f1c1cdb35f230d515803de92680c99970466a6c56c" => :yosemite
+    sha256 "9128b85fed212828fc6d7ec915333daad4567ecd0521e8feda0a8c36d819bdd2" => :mavericks
+  end
+
   depends_on 'libdmtx'
   depends_on 'php53-imagick'
   depends_on 'pkg-config' => :build
