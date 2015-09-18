842eada0217e98ab7cf1a8dfaf7c9831fd4a3051
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-18 07:36:41 +0100

    php70-pspell: update 7.0.0-rc.3 bottle.

--- Formula/php70-pspell.rb
+++ Formula/php70-pspell.rb
@@ -9,6 +9,10 @@ class Php70Pspell < AbstractPhp70Extension
   version PHP_VERSION
 
   bottle do
+    revision 2
+    sha256 "ea91a26a0f9bfd345f32bff4f7c23f2f5dd671f150df83b8afc3de5f34597cb5" => :el_capitan
+    sha256 "eabe53ad903c41189720c570eaa6dd6d805b7d9d6f5b2acebd3d6846e1cc411a" => :yosemite
+    sha256 "f46ea275e7c8aae49a326c3fee7a55569819220425d10c2ed1bfcb33543dbc6d" => :mavericks
   end
 
   depends_on "aspell"
