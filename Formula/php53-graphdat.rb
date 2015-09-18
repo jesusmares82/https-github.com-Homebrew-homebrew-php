5ab8560f88ba681194ee3b65de920b17a20e2ef6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-18 17:09:09 +0100

    php53-graphdat: add 1.0.3 bottle.

--- Formula/php53-graphdat.rb
+++ Formula/php53-graphdat.rb
@@ -7,6 +7,13 @@ class Php53Graphdat < AbstractPhp53Extension
   sha256 '6b436c1f3f37d4d701f970a5d868e91711406b4b55bd78f79de916d21f8fb799'
   head 'https://github.com/alphashack/graphdat-sdk-php.git'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b95b7e8544794edfa3ed766f683f04cc3316300f8b420efb014ff8c54f36b182" => :el_capitan
+    sha256 "8f481c1743ae6646e31b822431b47ea9a34cf665f57a8d9a801db15078bf7d17" => :yosemite
+    sha256 "5b578e4c68ec846075d2bb6b0287f62a51d5e079d20bba78b3f41fe08edd989b" => :mavericks
+  end
+
   def install
     Dir.chdir "graphdat-#{version}" unless build.head?
 
