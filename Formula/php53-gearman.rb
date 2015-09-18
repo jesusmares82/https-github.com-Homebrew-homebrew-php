3aa349ad83005d0157ff9030376df08418843513
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-18 11:32:46 +0100

    php53-gearman: add 1.1.2 bottle.

--- Formula/php53-gearman.rb
+++ Formula/php53-gearman.rb
@@ -7,6 +7,11 @@ class Php53Gearman < AbstractPhp53Extension
   sha256 'c30a68145b4e33f4da929267f7b5296376ca81d76dd801fc77a261696a8a5965'
   head 'https://svn.php.net/repository/pecl/gearman/trunk/'
 
+  bottle do
+    sha256 "6df13554bc809e1229791648a8fc73e1a7d27cd336c16d4aa8942049e82eff3f" => :yosemite
+    sha256 "e18fee51e7d674f32128598528b203e6c95155a5b3a268e34a21a7ef00532e8e" => :mavericks
+  end
+
   depends_on 'gearman'
 
   def install
