fa0fd41ddb116afa3674306dbe654d9042e872d7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 20:58:40 +0100

    php56-midgard2: add 12.09.1 bottle.

--- Formula/php56-midgard2.rb
+++ Formula/php56-midgard2.rb
@@ -7,6 +7,12 @@ class Php56Midgard2 < AbstractPhp56Extension
   sha256 '633ed2dce0c43222c13b2be1d2d044343f37e69cbdf727abc78ac53b6d871fe3'
   head 'https://github.com/midgardproject/midgard-php5.git', :branch => 'ratatoskr'
 
+  bottle do
+    sha256 "b5fdfda206abcddce54e2d719c980d875b7ce049d89f11514d4f2fa3b1901c39" => :el_capitan
+    sha256 "1d2d3d7193e08661f40c7d7b6dc3d84bb622d6ad4ff8728d5ab765f2f771764e" => :yosemite
+    sha256 "0b9c1c7850806c8bf437bc8e5f5ca390395c0eba4e753331929c52441a321c06" => :mavericks
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'midgard2'
 
