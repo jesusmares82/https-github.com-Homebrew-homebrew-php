23388385d3c9888db014ac92fd9a9c47f3357c03
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 17:02:25 +0100

    php56-eio: add 1.2.5 bottle.

--- Formula/php56-eio.rb
+++ Formula/php56-eio.rb
@@ -7,6 +7,11 @@ class Php56Eio < AbstractPhp56Extension
   sha256 'ee7b21aa413cbe39caaef1d2eb893fa3bcb9a278b5665c28d179a83a4a1bdb51'
   head 'https://bitbucket.org/osmanov/pecl-eio.git'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0e453eda7e99b5d309d8af2a510c6eaa2c6e2a1fd4797e017c1a4eec606b4d17" => :mavericks
+  end
+
   depends_on 'libevent' => :build
 
   def install
