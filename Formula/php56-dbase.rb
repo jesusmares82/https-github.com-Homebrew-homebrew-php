27acacd5e534c7d30c36b72497f68e05fb11aff7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 16:46:46 +0100

    php56-dbase: add 5.1.0 bottle.

--- Formula/php56-dbase.rb
+++ Formula/php56-dbase.rb
@@ -7,6 +7,13 @@ class Php56Dbase < AbstractPhp56Extension
   sha256 '20d6a40fb2efe4a06f503ec53512d02d71ceda87eac1f55208d7b5398f287a97'
   head 'https://svn.php.net/repository/pecl/dbase/trunk/'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a95c8f86d6af48b0c1d20dc2cc3d38b24a84c25c19796ceacdf6a291dfa8dfe6" => :el_capitan
+    sha256 "bd92c41d07ff8947903c3e3b05a93fb50e31b75d5d01b574ad3171fab9078db4" => :yosemite
+    sha256 "1c4fe8c4e373b2ca7114828831d31d953376ee8e6f0bf8eff53502e9fc373747" => :mavericks
+  end
+
   def install
     Dir.chdir "dbase-5.1.0"
 
