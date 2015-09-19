3e3fcd3579245a8249bc6a7c35f1bb2fd2fefd3a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 19:27:41 +0100

    php56-leveldb: add 0.1.4 bottle.

--- Formula/php56-leveldb.rb
+++ Formula/php56-leveldb.rb
@@ -7,6 +7,12 @@ class Php56Leveldb < AbstractPhp56Extension
   sha256 'b0d2485e7f1353a794cd58bbaf331154723cda98ae41757f1167e1661bd78eef'
   head 'https://github.com/reeze/php-leveldb.git'
 
+  bottle do
+    cellar :any
+    sha256 "e6e21ed4e95cae2592ba1064e286847e2d26060aaa1a30d273735661a88d9c7c" => :yosemite
+    sha256 "c397595c966c34248ce73086f6398d0bf430fe637ec35f98b926a453a3e7e42f" => :mavericks
+  end
+
   depends_on 'leveldb'
 
   def install
