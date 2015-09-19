2f8e27126b1b9d9b30833464d4421784464b1b4a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 16:52:21 +0100

    php56-dbus: add 0.1.1 bottle.

--- Formula/php56-dbus.rb
+++ Formula/php56-dbus.rb
@@ -7,6 +7,12 @@ class Php56Dbus < AbstractPhp56Extension
   sha256 '018ce17ceb18bd7085f7151596835b09603140865a49c76130b77e00bc7fcdd7'
   head 'http://svn.php.net/repository/pecl/dbus/trunk/'
 
+  bottle do
+    sha256 "7e479464a5e0b05fcc835274149a49d13f055558a604e7bdb29ece3687190dc0" => :el_capitan
+    sha256 "6829a89d6317dae05808dbf07c57f40a527e5bf4011b2a298fcb942094d0a829" => :yosemite
+    sha256 "4ce84f4d7ba78a8e66eec07f3ec5626d8eb2e0fd221d692f621adcd4a175a342" => :mavericks
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'd-bus' => :build
 
