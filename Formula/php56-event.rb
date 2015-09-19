4fc37a4949d19ad62e660bff9e489dcc186e2567
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 17:14:51 +0100

    php56-event: add 1.10.3 bottle.

--- Formula/php56-event.rb
+++ Formula/php56-event.rb
@@ -7,6 +7,11 @@ class Php56Event < AbstractPhp56Extension
   sha256 'f1955c7f470829024e2f816a2a4c5201bbef3c673e49e7d25a24e248011225be'
   head 'https://bitbucket.org/osmanov/pecl-event.git'
 
+  bottle do
+    sha256 "3471bd7b9caeb1abef1a1342b83b64ef684b05e0f25d075cce3e426dafd2c1d0" => :yosemite
+    sha256 "96208c09fa491fb8089bd496d55c7c13dacbf7d77def31a3f56ebbd29040abe2" => :mavericks
+  end
+
   depends_on 'libevent' => :build
 
   def install
