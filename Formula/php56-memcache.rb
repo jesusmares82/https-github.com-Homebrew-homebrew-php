4aab18ab89931d961c6d3cd3e7aeeacdca6a67c2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 21:10:30 +0100

    php56-memcache: add 2.2.7 bottle.

--- Formula/php56-memcache.rb
+++ Formula/php56-memcache.rb
@@ -7,6 +7,13 @@ class Php56Memcache < AbstractPhp56Extension
   sha256 '73006c02194a5a7c196c6488d449e5f8c75573a73568fe1a94b15157c147305d'
   head 'https://svn.php.net/repository/pecl/memcache/trunk/'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a869f9bd49a8d975a7b2f836784f6d6a1a05135562fc2b42e5dff0315902b2ce" => :el_capitan
+    sha256 "e876c59029000634519f46375fbbc37fb6f339bcedc81dc0ee40070577b284c6" => :yosemite
+    sha256 "34445de0a6b7c486ef2cd0289b37ca3519a7530c488c43e82c4ec91fdbc09b7b" => :mavericks
+  end
+
   devel do
     url 'https://pecl.php.net/get/memcache-3.0.8.tgz'
     sha256 '2cae5b423ffbfd33a259829849f6000d4db018debe3e29ecf3056f06642e8311'
