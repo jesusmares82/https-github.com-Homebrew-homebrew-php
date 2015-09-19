81fddbdb79726166f25e5456f2a7ce912a0d9bab
Andy Blyler <andy@blyler.cc>
2015-09-19 19:57:25 -0400

    php5*-ev: update to 0.2.15
    Also fixes issue #2151: "php5*-ev fails to compile on OS X 10.11"
    
    Closes #2168.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php54-ev.rb
+++ Formula/php54-ev.rb
@@ -2,12 +2,19 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php54Ev < AbstractPhp54Extension
   init
-  homepage 'https://pecl.php.net/package/ev'
-  url 'https://pecl.php.net/get/ev-0.2.10.tgz'
-  sha1 'c096e13752cd919a5af8d300f71134492eae8e01'
-  head 'https://bitbucket.org/osmanov/pecl-ev.git'
+  desc "interface to libev library"
+  homepage "https://pecl.php.net/package/ev"
+  url "https://pecl.php.net/get/ev-0.2.15.tgz"
+  sha256 "801b2c39e081263f91d5a1d74e668e6c8ad199a34867c8e13e8fad3e83786fd5"
+  head "https://bitbucket.org/osmanov/pecl-ev.git"
 
-  depends_on 'libev'
+  bottle do
+  end
+
+  depends_on "libev"
+
+  # https://bitbucket.org/osmanov/pecl-ev/pull-requests/4
+  patch :DATA
 
   def install
     Dir.chdir "ev-#{version}" unless build.head?
@@ -15,11 +22,27 @@ class Php54Ev < AbstractPhp54Extension
     ENV.universal_binary if build.universal?
 
     safe_phpize
+    ENV["CFLAGS"] = "-Wno-return-type"
     system "./configure", "--prefix=#{prefix}",
                           phpconfig,
-                          "--with-libev=#{Formula['libev'].opt_prefix}"
+                          "--with-libev=#{Formula["libev"].opt_prefix}"
     system "make"
     prefix.install "modules/ev.so"
     write_config_file if build.with? "config-file"
   end
 end
+
+__END__
+diff --git a/ev-0.2.15/libev/ev.c b/ev-0.2.15/libev/ev.c
+index 03b697a..7a53901 100644
+--- a/ev-0.2.15/libev/ev.c
++++ b/ev-0.2.15/libev/ev.c
+@@ -1010,7 +1010,7 @@ ecb_inline uint64_t ecb_rotr64 (uint64_t x, unsigned int count) { return (x << (
+   #define ecb_unreachable() __builtin_unreachable ()
+ #else
+   /* this seems to work fine, but gcc always emits a warning for it :/ */
+-  ecb_inline void ecb_unreachable (void) ecb_noreturn;
++  ecb_noreturn ecb_inline void ecb_unreachable (void);
+   ecb_inline void ecb_unreachable (void) { }
+ #endif
+ 
