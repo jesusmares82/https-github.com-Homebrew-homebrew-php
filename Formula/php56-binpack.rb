f17001ba33626052119317581cc25316c80388d0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 06:23:08 +0100

    php56-binpack: add 1.0.1 bottle.

--- Formula/php56-binpack.rb
+++ Formula/php56-binpack.rb
@@ -7,6 +7,13 @@ class Php56Binpack < AbstractPhp56Extension
   sha256 '70617a721df4f8c52b5b1b214c11e464f8c36915cc19a850f24fed500c61b297'
   head 'http://binpack.liaohuqiu.net'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6a8b8a820d3b63d84b396735c9a4451d458245658de6f38ccbf7f2720e086c8d" => :el_capitan
+    sha256 "5c64ed1a29b041910713ce984be232099494a0150b86e467d15f9e9765082688" => :yosemite
+    sha256 "516bb468326d2071f813e6a48de02dbcd494bf09dd4c63ea3b919acbc2708442" => :mavericks
+  end
+
   def install
     Dir.chdir "binpack-#{version}" unless build.head?
 
