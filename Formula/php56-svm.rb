41ea3a15fc9f4e269c60448328b42c855b6d8832
Andy Blyler <andy@blyler.cc>
2015-09-19 19:26:59 -0400

    php5*-svm: fix install issue with libsvm is not already installed
    Fixes issue #2154
    
    Closes #2166.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-svm.rb
+++ Formula/php56-svm.rb
@@ -2,18 +2,27 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Svm < AbstractPhp56Extension
   init
-  homepage 'http://php.net/manual/en/book.svm.php'
-  url 'https://github.com/ianbarber/php-svm/archive/0.1.9.tar.gz'
-  sha1 'e59c663f22b7bb3db53bf784e5d85e21f5719a18'
-  head 'https://github.com/ianbarber/php-svm.git'
+  desc "Support Vector Machine Library"
+  homepage "http://php.net/manual/en/book.svm.php"
+  url "https://github.com/ianbarber/php-svm/archive/0.1.9.tar.gz"
+  sha256 "c3dabf7220766193fcb87514559e89c1a9ec0017f510fc58cb98b3ac52819734"
+  head "https://github.com/ianbarber/php-svm.git"
+
+  depends_on "libsvm"
 
   def install
     ENV.universal_binary if build.universal?
 
     safe_phpize
-    ENV["CFLAGS"] = '-Wno-return-type'
-    system "./configure", "--prefix=#{prefix}",
-                          phpconfig
+
+    ENV["CFLAGS"] = "-Wno-return-type"
+
+    args = []
+    args << "--with-svm==#{Formula["libsvm"].opt_prefix}"
+    args << "--prefix=#{prefix}"
+    args << phpconfig
+
+    system "./configure", *args
     system "make"
     prefix.install "modules/svm.so"
     write_config_file if build.with? "config-file"
