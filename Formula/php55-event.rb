09dfaab84d67692594fb2ed625e9ec05fb2d0859
Andy Blyler <andy@blyler.cc>
2015-09-19 19:37:45 -0400

    php5*-event: update to 1.11.1
    Also fixes issue #2152: "php5*-event fails to compile on OS X 10.11"
    
    Closes #2167.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php55-event.rb
+++ Formula/php55-event.rb
@@ -2,12 +2,17 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php55Event < AbstractPhp55Extension
   init
-  homepage 'https://pecl.php.net/package/event'
-  url 'https://pecl.php.net/get/event-1.10.3.tgz'
-  sha1 '0d8881b0b157493d399fed9211bc242f1b416245'
-  head 'https://bitbucket.org/osmanov/pecl-event.git'
+  desc "Provides interface to libevent library"
+  homepage "https://pecl.php.net/package/event"
+  url "https://pecl.php.net/get/event-1.11.1.tgz"
+  sha256 "371e8d559461542058efd57be61fd8316121985dc455f2e7979722e37cedd526"
+  head "https://bitbucket.org/osmanov/pecl-event.git"
 
-  depends_on 'libevent' => :build
+  bottle do
+  end
+
+  depends_on "libevent"
+  depends_on "openssl"
 
   def install
     Dir.chdir "event-#{version}" unless build.head?
@@ -16,7 +21,10 @@ class Php55Event < AbstractPhp55Extension
 
     args = []
     args << "--with-event"
-    args << "--with-event-libevent-dir=#{Formula['libevent'].opt_prefix}"
+    args << "--with-event-extra"
+    args << "--with-event-libevent-dir=#{Formula["libevent"].opt_prefix}"
+    args << "--with-event-openssl"
+    args << "--with-openssl-dir=#{Formula["openssl"].opt_prefix}"
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, *args
--- Formula/php56-event.rb
+++ Formula/php56-event.rb
@@ -2,17 +2,17 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Event < AbstractPhp56Extension
   init
-  homepage 'https://pecl.php.net/package/event'
-  url 'https://pecl.php.net/get/event-1.10.3.tgz'
-  sha256 'f1955c7f470829024e2f816a2a4c5201bbef3c673e49e7d25a24e248011225be'
-  head 'https://bitbucket.org/osmanov/pecl-event.git'
+  desc "Provides interface to libevent library"
+  homepage "https://pecl.php.net/package/event"
+  url "https://pecl.php.net/get/event-1.11.1.tgz"
+  sha256 "371e8d559461542058efd57be61fd8316121985dc455f2e7979722e37cedd526"
+  head "https://bitbucket.org/osmanov/pecl-event.git"
 
   bottle do
-    sha256 "3471bd7b9caeb1abef1a1342b83b64ef684b05e0f25d075cce3e426dafd2c1d0" => :yosemite
-    sha256 "96208c09fa491fb8089bd496d55c7c13dacbf7d77def31a3f56ebbd29040abe2" => :mavericks
   end
 
-  depends_on 'libevent' => :build
+  depends_on "libevent"
+  depends_on "openssl"
 
   def install
     Dir.chdir "event-#{version}" unless build.head?
@@ -21,7 +21,10 @@ class Php56Event < AbstractPhp56Extension
 
     args = []
     args << "--with-event"
-    args << "--with-event-libevent-dir=#{Formula['libevent'].opt_prefix}"
+    args << "--with-event-extra"
+    args << "--with-event-libevent-dir=#{Formula["libevent"].opt_prefix}"
+    args << "--with-event-openssl"
+    args << "--with-openssl-dir=#{Formula["openssl"].opt_prefix}"
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, *args
