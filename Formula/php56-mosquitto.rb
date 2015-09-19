23f099ec4ca6f5e4ed01919452e8e9dd3663daed
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 00:42:52 +0100

    php56-mosquitto: add 0.2.2 bottle.

--- Formula/php56-mosquitto.rb
+++ Formula/php56-mosquitto.rb
@@ -7,6 +7,13 @@ class Php56Mosquitto < AbstractPhp56Extension
   sha256 'e9baa3af1d9a62f8fa1b76ffffbd13fffe7b65e0122130fb389915269543915e'
   head 'https://github.com/mgdm/Mosquitto-PHP.git'
 
+  bottle do
+    cellar :any
+    sha256 "8f49b14d46c1cd808a0f36c2656ac0ee593a0e7860c00e6a15e4820331b3b896" => :el_capitan
+    sha256 "0693897b2f6d29c48880007b94a82561a215204117e71168a39358f7f013923e" => :yosemite
+    sha256 "64b31e0c7b62a8ac5ddba8bb0128837000ed9631acd95074a3823a5c34143f54" => :mavericks
+  end
+
   depends_on 'mosquitto'
 
   def install
