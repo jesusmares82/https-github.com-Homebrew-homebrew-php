dc283d71998b700ac7d848ca7edd3597199efd46
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 20:11:34 +0100

    php56-mecab: add 0.5.0 bottle.

--- Formula/php56-mecab.rb
+++ Formula/php56-mecab.rb
@@ -7,6 +7,12 @@ class Php56Mecab < AbstractPhp56Extension
   sha256 'e5c84a8ffa39a8a0dd60424b879806382ec0f66d50007f6bfad6831074b3bcc5'
   head 'https://github.com/rsky/php-mecab.git'
 
+  bottle do
+    sha256 "451f38aaff878f71881c0e4f03861a2dc6f4082cb3c908e55611057da6cb47fd" => :el_capitan
+    sha256 "ca149643bea494e2791b940cb1637e6019ae5591e44786a850537333b1abf243" => :yosemite
+    sha256 "6eddc9b81c27f6b4c05de5caf9d7121070e519b41d1e65e38b29df58e5ab9326" => :mavericks
+  end
+
   depends_on "mecab"
 
   def install
