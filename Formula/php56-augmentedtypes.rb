8ee773d692d0825145e41736cc0d69c8929ae484
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 05:19:59 +0100

    php56-augmentedtypes: add 0.6.6 bottle.

--- Formula/php56-augmentedtypes.rb
+++ Formula/php56-augmentedtypes.rb
@@ -7,6 +7,14 @@ class Php56Augmentedtypes < AbstractPhp56Extension
   sha256 '54b295f902e56daf1347b1e1f7d633a84c3e03aacac78424e6314adfd922e4db'
   head 'https://github.com/box/augmented_types.git'
 
+  bottle do
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "65ca5e24420e1400fcbb1b5a519463c70218a96938193ea22d2839f0925038aa" => :el_capitan
+    sha256 "2ed0a020a7ad9db49660d5f27629bbc46d377d6dd9603337c7f2461451bbadb7" => :yosemite
+    sha256 "d08293b8aabf4bb3a44bdd2718f35c5e2e6d4fd84107e4f779565807ed5f21f1" => :mavericks
+  end
+
   option 'without-default-enforcement', "Turn off Augmented Types enforcement by default"
 
   def extension_type; "zend_extension"; end
