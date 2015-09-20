10d87ac888f6d4e0ecf2371d9ccc206f75faec81
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 06:30:53 +0100

    php55-svm: add 0.1.9 bottle.

--- Formula/php55-svm.rb
+++ Formula/php55-svm.rb
@@ -8,6 +8,13 @@ class Php55Svm < AbstractPhp55Extension
   sha256 "c3dabf7220766193fcb87514559e89c1a9ec0017f510fc58cb98b3ac52819734"
   head "https://github.com/ianbarber/php-svm.git"
 
+  bottle do
+    cellar :any
+    sha256 "ee7d128ded9166def63057616f7cdb25e2e2fb5af58c66406582c85903783bc9" => :el_capitan
+    sha256 "1e8a5f1c16d27fe4de680c76e9d614f0878038d69c1b1932525ee69365157c22" => :yosemite
+    sha256 "1fca90d63d99120d915031917af646c1d1c17ea6bdc65eb1a225fb6cdb078bb1" => :mavericks
+  end
+
   depends_on "libsvm"
 
   def install
