284f441fc23553d6779502ac08d5a1f342d28713
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 06:55:39 +0100

    php54-ev: update 0.2.15 bottle.

--- Formula/php54-ev.rb
+++ Formula/php54-ev.rb
@@ -9,6 +9,10 @@ class Php54Ev < AbstractPhp54Extension
   head "https://bitbucket.org/osmanov/pecl-ev.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "0f75e6bf9da5e00066d2f952cf006e9dd7b8f9ffea450f4610ac6905e13237c5" => :el_capitan
+    sha256 "473aaf403bd69ff85c9ff1ce4abc23af56bb4539252b6e247a90d76c4cd0fea6" => :yosemite
+    sha256 "bff99dc7bf3cf9a6748b655f43f35e7a5039ca9cda9a24e89875c712880f765e" => :mavericks
   end
 
   depends_on "libev"
