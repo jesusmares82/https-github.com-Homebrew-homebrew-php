8201253bf281141cff008f77f62da29b13e38ba5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 02:44:12 +0100

    php56-proctitle: add 0.1.2 bottle.

--- Formula/php56-proctitle.rb
+++ Formula/php56-proctitle.rb
@@ -7,6 +7,13 @@ class Php56Proctitle < AbstractPhp56Extension
   sha256 'b9f84b1aebbee31cee627356438def1321d1f3bcd480341501315f35f0f9e272'
   head 'https://github.com/MagicalTux/proctitle.git'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f1592a12b1df4939af75338e51f88e7436f9e3bdfd83ac58382c4ee281ce2cd0" => :el_capitan
+    sha256 "9d4087786bbbbff4f5b62792e0a993dc1bf43f98573415962e81197d0b14f9e3" => :yosemite
+    sha256 "ba83be6426dab2ec78bb7cf9ea3c3249a98651448f46c0c82a3afc6f11a9b9d2" => :mavericks
+  end
+
   def install
     Dir.chdir "proctitle-#{version}" unless build.head?
 
