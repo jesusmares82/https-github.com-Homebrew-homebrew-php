21655f99e5fadb31976c8adf9838787b6b720b16
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 15:57:55 +0100

    php53-libsodium: add 0.1.3 bottle.

--- Formula/php53-libsodium.rb
+++ Formula/php53-libsodium.rb
@@ -8,6 +8,14 @@ class Php53Libsodium < AbstractPhp53Extension
   sha256 "cf7314420f270eeef207add6f44c3bdd88b6c6395402d14b9d37ce2171bca734"
   head "https://github.com/jedisct1/libsodium-php.git"
 
+  bottle do
+    cellar :any
+    revision 1
+    sha256 "2b35158e174a9d45dd94f87181f49d625b72d22a1add8dc76f08c32d97f6f3d2" => :el_capitan
+    sha256 "931a3791f235ff5d625e53652aecedf768fba20241a8b58252fa9fbf9e4fee44" => :yosemite
+    sha256 "91cf8045af8f0e41574030925284bd3ad9695d2f4c32ab1d7c54b3bc110a2cbe" => :mavericks
+  end
+
   depends_on "libsodium"
 
   def install
