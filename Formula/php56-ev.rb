754386c399e2345cea460d1d256549eee8e55277
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 06:55:39 +0100

    php56-ev: update 0.2.15 bottle.

--- Formula/php56-ev.rb
+++ Formula/php56-ev.rb
@@ -9,6 +9,10 @@ class Php56Ev < AbstractPhp56Extension
   head "https://bitbucket.org/osmanov/pecl-ev.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "7cb4b47ec84d42d2fff31711cd4811f0f757ac5fb60d449c30915981855399b1" => :el_capitan
+    sha256 "3c3baf440535f5d510465aa44b38205103f3213a4e616db78f5f5c2a70b62d76" => :yosemite
+    sha256 "05286cf12889c5320fc7ce4e25a257872079f89518a189951da0fcafb115a5c0" => :mavericks
   end
 
   depends_on "libev"
