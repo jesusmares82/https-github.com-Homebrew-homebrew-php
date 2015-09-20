21883bd34b10129b1f2436ce4f6a0bb0073c1561
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 06:22:55 +0100

    php55-varnish: add 1.2.1 bottle.

--- Formula/php55-varnish.rb
+++ Formula/php55-varnish.rb
@@ -7,6 +7,13 @@ class Php55Varnish < AbstractPhp55Extension
   url "https://pecl.php.net/get/varnish-1.2.1.tgz"
   sha256 "13d2a4b63197d66854850c5aef50353d87ce3ed95798ba179fb59e289030183a"
 
+  bottle do
+    cellar :any
+    sha256 "6c78c34e5e00eac0d41554b00feed1aad7345129670afc64d62fa661e41c6627" => :el_capitan
+    sha256 "5a7f3297f8421b435ddb80129fab1686d7b0088df66c80f7b05d4d1a97d6142a" => :yosemite
+    sha256 "e716c2fa9bd20bfa3913bf30ba207698c5b15345a29304fc3c8f71cee44d23be" => :mavericks
+  end
+
   depends_on "varnish"
 
   def install
