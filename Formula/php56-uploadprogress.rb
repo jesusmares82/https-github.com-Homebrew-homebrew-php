5f91b3b544abdcc2eb7c06074fffab5724e1de00
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 23:43:26 +0100

    php56-uploadprogress: add 1.0.3.1 bottle.

--- Formula/php56-uploadprogress.rb
+++ Formula/php56-uploadprogress.rb
@@ -7,6 +7,13 @@ class Php56Uploadprogress < AbstractPhp56Extension
   sha256 '30588b4589543bcf6371532546b1a8cee9da1086c206aca9f96ea1bd486bbab2'
   head 'https://svn.php.net/repository/pecl/uploadprogress/trunk/'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c8df07b0a3a5a77a89fe3beac785b8ab20cf2010fc9b6f6d02df7c7b51327b62" => :el_capitan
+    sha256 "149d696e78e1be37bf1d6ec58dc1d5aa22f52927c85c50d435b6baf53fd1f664" => :yosemite
+    sha256 "3bd78efd9a7e512f7f3b0023087c3efca68aee89fd405639a54e63b26dcdeaed" => :mavericks
+  end
+
   depends_on 'pcre'
 
   def install
