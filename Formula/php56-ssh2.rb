7b388ae347c769958c93f6e8bec5bdd4503b6a65
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 21:43:48 +0100

    php56-ssh2: add 0.12 bottle.

--- Formula/php56-ssh2.rb
+++ Formula/php56-ssh2.rb
@@ -7,6 +7,13 @@ class Php56Ssh2 < AbstractPhp56Extension
   sha256 '600c82d2393acf3642f19914f06a7afea57ee05cb8c10e8a5510b32188b97f99'
   head 'https://svn.php.net/repository/pecl/ssh2/trunk/'
 
+  bottle do
+    cellar :any
+    sha256 "dda6e7dccd230ade62ad7255a056cfd89a33fa946ba9fb1a963174512b15d819" => :el_capitan
+    sha256 "c2c29f5d2cacc712fc8c2d88b00f6787ba32d2b466bdb2d408d10525bb66c329" => :yosemite
+    sha256 "c2be20bd46953dbfd2b48e2815d846440af1bd8c2a1a1b1ba173563e41ce06b8" => :mavericks
+  end
+
   depends_on 'libssh2'
 
   def install
