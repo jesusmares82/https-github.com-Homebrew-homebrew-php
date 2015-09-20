105166d1cbfb2608c43473947ca11bd6e25dacc6
Andy Blyler <andy@blyler.cc>
2015-09-20 10:23:20 -0400

    php56-snappy: convert from sha1 to sha256
    Closes #2176.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-snappy.rb
+++ Formula/php56-snappy.rb
@@ -4,7 +4,7 @@ class Php56Snappy < AbstractPhp56Extension
   init
   homepage 'https://github.com/kjdev/php-ext-snappy'
   url 'https://github.com/kjdev/php-ext-snappy/archive/db3abe6ef6b0e74c8deed0f285cdc9d8cff094ef.tar.gz'
-  sha1 'd41dcb066d2d6b495d883d9e0089f6b22f143107'
+  sha256 '4d1acaad08eacaa108618c6c765fe2d8da82c2f09f8996036e8bccb5438ebd61'
   version '0.1.0'
 
   depends_on 'snappy'
