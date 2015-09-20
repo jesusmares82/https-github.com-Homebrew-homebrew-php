fd1854d85efee43a333a03204a17a60ea22c22b3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 20:16:38 +0100

    php-version: add 0.12.0 bottle.

--- Formula/php-version.rb
+++ Formula/php-version.rb
@@ -5,6 +5,13 @@ class PhpVersion < Formula
   sha256 "6f432d836394fecfedcf1db926fa540a4be542feea1ddf56e78d38b1a8ab8798"
   head "https://github.com/wilmoore/php-version.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "34b62e9c82ac7a67a67ff77e39d2f05dc496b595879cea5cf78a222ceb924735" => :el_capitan
+    sha256 "59fbb783d2792408701488d75c98ed42905ec4659614d5519234cc836e9fa497" => :yosemite
+    sha256 "c73168717e6c1b46c8bbd6b9db20a10b65cc420f2e2872ca609d480bb53b0bdf" => :mavericks
+  end
+
   def install
     prefix.install Dir["*"]
   end
