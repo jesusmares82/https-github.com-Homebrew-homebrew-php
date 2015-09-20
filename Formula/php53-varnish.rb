6e93abd1f051b48c7299d99e0ec974800c5aea9f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 06:22:54 +0100

    php53-varnish: add 1.2.1 bottle.

--- Formula/php53-varnish.rb
+++ Formula/php53-varnish.rb
@@ -7,6 +7,13 @@ class Php53Varnish < AbstractPhp53Extension
   url "https://pecl.php.net/get/varnish-1.2.1.tgz"
   sha256 "13d2a4b63197d66854850c5aef50353d87ce3ed95798ba179fb59e289030183a"
 
+  bottle do
+    cellar :any
+    sha256 "b25128913c68a4047043700a47bb8f12c3cd2442ddb7d7560289f69280927248" => :el_capitan
+    sha256 "856f3de1e5992e7152fe6c3f0296ede47f6c27a39593b14cc875cb4b0e0f498b" => :yosemite
+    sha256 "557479b3c1a98d55eb18b25760a927aac7dddf2b8c829d793ff211ffab580557" => :mavericks
+  end
+
   depends_on "varnish"
 
   def install
