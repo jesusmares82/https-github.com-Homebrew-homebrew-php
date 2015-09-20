911401cd6153deb4280bc52f6eba7624fe8f1af8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 22:12:05 +0100

    php56-symfony-debug: add 2.6.0 bottle.

--- Formula/php56-symfony-debug.rb
+++ Formula/php56-symfony-debug.rb
@@ -7,6 +7,13 @@ class Php56SymfonyDebug < AbstractPhp56Extension
   sha256 '00bfc3c279ee1fd10eb8f771e4a3cca988cf4eb0f1bda88fcd595e5149f32a28'
   head 'https://github.com/symfony/Debug.git'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2545b865f036ca8f8be7130507b6cebb66c2f777810e19dfc72e037e68cfb510" => :el_capitan
+    sha256 "223be6709ab629fafff644862099b5791b4f5f255805385468f2391883d73992" => :yosemite
+    sha256 "3756f60e3e8a62f73f27f323e21ad1bd088bbfc22995f33bb3dcb5218c56918d" => :mavericks
+  end
+
   def extension
     "symfony_debug"
   end
