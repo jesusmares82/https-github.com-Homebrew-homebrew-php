b57b2f20c708dbfc89070e1aaf1c039737632a12
Andy Blyler <andy@blyler.cc>
2015-09-20 16:58:31 -0400

    php56-symfony-debug: convert from sha1 to sha256
    Closes #2184.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-symfony-debug.rb
+++ Formula/php56-symfony-debug.rb
@@ -4,7 +4,7 @@ class Php56SymfonyDebug < AbstractPhp56Extension
   init
   homepage 'http://symfony.com/'
   url 'https://github.com/symfony/Debug/archive/v2.6.0.tar.gz'
-  sha1 'ea9aa9791acb62dd2e87d90e1bf3499e70483b89'
+  sha256 '00bfc3c279ee1fd10eb8f771e4a3cca988cf4eb0f1bda88fcd595e5149f32a28'
   head 'https://github.com/symfony/Debug.git'
 
   def extension
