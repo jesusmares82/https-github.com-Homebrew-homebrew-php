46868b143311eb7392842f01d43e9145df7a71eb
Ruud Kamphuis <ruudk@mphuis.com>
2015-09-20 16:53:42 +0200

    PHP 7 extension for Redis

---
+++ Formula/php70-redis.rb
@@ -0,0 +1,29 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Redis < AbstractPhp70Extension
+  init
+  desc "PHP extension for Redis"
+  homepage "https://github.com/phpredis/phpredis"
+  head "https://github.com/phpredis/phpredis.git", :branch => "php7"
+
+  def install
+    safe_phpize
+
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    
+    prefix.install "modules/redis.so"
+    
+    write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+
+      ; phpredis can be used to store PHP sessions.
+      ; To do this, uncomment and configure below
+      ;session.save_handler = redis
+      ;session.save_path = "tcp://host1:6379?weight=1, tcp://host2:6379?weight=2&timeout=2.5, tcp://host3:6379?weight=2"
+    EOS
+  end
+end
