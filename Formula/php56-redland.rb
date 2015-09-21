e781904e75529fd20cd4dffea51b0a8358fb3546
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 07:06:31 +0100

    php56-redland: update 1.0.17.1 bottle.

--- Formula/php56-redland.rb
+++ Formula/php56-redland.rb
@@ -8,6 +8,11 @@ class Php56Redland < AbstractPhp56Extension
   sha256 "ff72b587ab55f09daf81799cb3f9d263708fad5df7a5458f0c28566a2563b7f5"
 
   bottle do
+    cellar :any
+    revision 1
+    sha256 "85530f3f07ae5b9872daf144f083657b1c6d4859fb283cf10f28b7c3eb3ab8c0" => :el_capitan
+    sha256 "c18b2564c8f1c1c7d06aec39b72bb89af296294e75e7506767899ce6459db07b" => :yosemite
+    sha256 "b751e2eb1fa57ce6722763ca949122ead5730b059ad3a1823ba6f50da46cb876" => :mavericks
   end
 
   depends_on "pkg-config" => :build
