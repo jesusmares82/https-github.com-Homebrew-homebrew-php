ba81b91de12c17b29ccac8a0bcf242e77c6bd3f6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 08:22:49 +0100

    php55-crypto: add 0.1.1 bottle.

--- Formula/php55-crypto.rb
+++ Formula/php55-crypto.rb
@@ -7,6 +7,13 @@ class Php55Crypto < AbstractPhp55Extension
   sha256 "9844e93078ac165255bdba6944abf6ec196be9964f9807df9f8f7d792a18807b"
   head "https://github.com/bukka/php-crypto.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8fa82e628f4b9df8d3f40a30b28ee72fc108f6863057797a0706842fe73110e7" => :el_capitan
+    sha256 "7e848e3618aad2e5a11cc8095b7eae6366d5f444167f7760d2c33f7c3317b382" => :yosemite
+    sha256 "0c12ed77a1aaa5aa960e6cc5111318bdecb01095f8ba60bf81f6c3d5ccebf1e0" => :mavericks
+  end
+
   def install
     Dir.chdir "crypto-#{version}" unless build.head?
 
