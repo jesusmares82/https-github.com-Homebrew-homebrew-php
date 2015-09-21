4b0c0482edc09492afa004807570e065b4e13d17
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 02:53:28 +0100

    php56-yaz: add 1.1.6 bottle.

--- Formula/php56-yaz.rb
+++ Formula/php56-yaz.rb
@@ -6,6 +6,12 @@ class Php56Yaz < AbstractPhp56Extension
   url 'https://pecl.php.net/get/yaz-1.1.6.tgz'
   sha256 'dd9b919ba3b2a09a9c77c847407f106512fe75979cc0689ebccef08c80719eee'
 
+  bottle do
+    sha256 "7b6cb327d11486cffe95f390decfec202909be4908406c569e1efc92cfdd5762" => :el_capitan
+    sha256 "d1083d2eb020dbba56ece25340bcb572633c0170314fad8796d0eb21b11ff13e" => :yosemite
+    sha256 "379cca773ee52205e5ecd31e312b2e9ad793a0d25ca7b5cebd042db5a20569a7" => :mavericks
+  end
+
   depends_on "yaz"
 
   def install
