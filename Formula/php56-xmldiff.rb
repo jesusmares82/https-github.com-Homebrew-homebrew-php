24844f7f5db94a71ff2c90b7c7e293621a9f4233
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 01:47:29 +0100

    php56-xmldiff: add 0.9.2 bottle.

--- Formula/php56-xmldiff.rb
+++ Formula/php56-xmldiff.rb
@@ -6,6 +6,13 @@ class Php56Xmldiff < AbstractPhp56Extension
   url 'https://pecl.php.net/get/xmldiff-0.9.2.tgz'
   sha256 '60d7d7fde2ebb695ae2cb26803153ad07a6146e0d70c102b3403131c86177550'
 
+  bottle do
+    cellar :any
+    sha256 "374681741f75a3363f7d8cea373be80e01ffe154628d9008d495ec99b43d21db" => :el_capitan
+    sha256 "cc6131f5c71e45c9832dc6178233cbee131e69356dadf7d6857ae4dc13166dba" => :yosemite
+    sha256 "e5d57fa37bc8e5f4db4c37e6bb07b518ac253f7b6a4f3083a50b3089bc60bac4" => :mavericks
+  end
+
   def install
     Dir.chdir "xmldiff-#{version}"
 
