7135dbac75b0fcd106b160f3263d5a218b411bd6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 07:35:38 +0100

    php53-dmtx: update 0.0.3 bottle.

--- Formula/php53-dmtx.rb
+++ Formula/php53-dmtx.rb
@@ -9,6 +9,10 @@ class Php53Dmtx < AbstractPhp53Extension
   head "https://github.com/maZahaca/php-dmtx.git"
 
   bottle do
+    revision 1
+    sha256 "2d94b0e2c675b14f1d1497320dc8d88fe426d327f80008a6b4e58c19482d6efd" => :el_capitan
+    sha256 "fa0f2e6764abbfd30c6a263d115c3bce58920f4e0861b5d31663b8d9cdbade56" => :yosemite
+    sha256 "beef7eaa88d668dc865704193202fe8a8009dfb07b1f67acef8ef3291d33a0e3" => :mavericks
   end
 
   depends_on "libdmtx"
