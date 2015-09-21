d030ed51badaee3a09d4e4717962e766b98e5ee7
Andy Blyler <andy@blyler.cc>
2015-09-21 01:30:31 -0400

    php56-pimple: convert from sha1 to sha256
    Closes #2208.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-pimple.rb
+++ Formula/php56-pimple.rb
@@ -4,7 +4,7 @@ class Php56Pimple < AbstractPhp56Extension
   init
   homepage "http://pimple.sensiolabs.org/"
   url "https://github.com/silexphp/Pimple/archive/v3.0.0.tar.gz"
-  sha1 "4ded64814812e3ec2f955b76cdcfab99831d7318"
+  sha256 "591e706f5cdce06bdd17d306db3fe9df521bee0ef4fcb0ee56524ff258ef66ba"
   head "https://github.com/silexphp/Pimple.git"
 
   def install
