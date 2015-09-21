af3237810117fc6413966d7296f5685f2da4373a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 06:21:06 +0100

    php53-magickwand: add 1.0.9-2 bottle.

--- Formula/php53-magickwand.rb
+++ Formula/php53-magickwand.rb
@@ -7,6 +7,12 @@ class Php53Magickwand < AbstractPhp53Extension
   url "http://www.magickwand.org/download/php/MagickWandForPHP-1.0.9-2.tar.bz2"
   sha256 "05e5fe5bc52ab9169228bbbde38b222208d1ae19db718b66d2c7ac4180847727"
 
+  bottle do
+    sha256 "ae5c8a0f104157ea40a1de9273a82fcebda2a471a183be24f690faacc2512cdd" => :el_capitan
+    sha256 "e2c289ddfd96685f30668118cca8c347132e2fd518b04a95dc5cfd6c8d074650" => :yosemite
+    sha256 "48672a2827013b4ce009448a2dfe48ca02156dc4752debc8fc840b7022994d75" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "imagemagick"
 
