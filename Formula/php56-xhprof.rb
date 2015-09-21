85dea2fcb6a26155700c0d3bd17621711a8b41a8
Andy Blyler <andy@blyler.cc>
2015-09-20 20:02:37 -0400

    php56-xhprof: convert from sha1 to sha256
    Closes #2194.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-xhprof.rb
+++ Formula/php56-xhprof.rb
@@ -4,7 +4,7 @@ class Php56Xhprof < AbstractPhp56Extension
   init
   homepage 'https://github.com/facebook/xhprof'
   url 'https://github.com/facebook/xhprof/archive/254eb24dcfa763c76c57b472093ebc4b81af2b7d.tar.gz'
-  sha1 'bab248d9a44a99515f96eb086bfa9d8932874451'
+  sha256 'c891ce1bf6730c67bd1dae6fb9b428e3adaf9c7b2008791199ba51a8556683ba'
   head 'https://github.com/facebook/xhprof.git'
   version '254eb24'
 
