6f38c207d4be0007347aa92808e0eb40a8367417
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 08:05:41 +0100

    php55-dbase: add 5.1.0 bottle.

--- Formula/php55-dbase.rb
+++ Formula/php55-dbase.rb
@@ -7,6 +7,13 @@ class Php55Dbase < AbstractPhp55Extension
   sha256 "20d6a40fb2efe4a06f503ec53512d02d71ceda87eac1f55208d7b5398f287a97"
   head "https://svn.php.net/repository/pecl/dbase/trunk/"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3b69e559b57216688c7d88df30d111eec4663221e78d94faed1c57dfc78ded41" => :el_capitan
+    sha256 "91ff48015972cebaa05becfee97fb51e52b3d8ed328ede42fb7e0affaae2eea8" => :yosemite
+    sha256 "86ac0cb084263aa172012e36716f6544c182237994904d960111629d3a209eaf" => :mavericks
+  end
+
   def install
     Dir.chdir "dbase-5.1.0"
 
