6739d4cd93eb58e555c61d751d8b43808b122eb4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 02:45:26 +0100

    php56-yp: add 1.0.1 bottle.

--- Formula/php56-yp.rb
+++ Formula/php56-yp.rb
@@ -7,6 +7,13 @@ class Php56Yp < AbstractPhp56Extension
   sha256 '097fc6953c8faaf748acb34bb0c11ca81672f46fc19cd48f8a6c7da6714fa468'
   head 'https://git.php.net:/repository/pecl/networking/yp.git'
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "febf9badc50c6ac4af6e5117fcf6ba3f1da6e72642a5a6904ec0d59a6fc3d425" => :el_capitan
+    sha256 "d0731083f5d69a9b560196eb62d6ac85288d62593fb2cf58bbda7cdbc5ef4e86" => :yosemite
+    sha256 "441d7704b8913c9adb48056f6bd4432f04fa064e7ae01cc7e372e7fd186feb44" => :mavericks
+  end
+
   def install
     Dir.chdir "yp-#{version}" unless build.head?
 
