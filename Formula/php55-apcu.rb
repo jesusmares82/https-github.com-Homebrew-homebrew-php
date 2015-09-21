4bf85d146cbb11d506d287e868dde8278f261b3d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 07:22:20 +0100

    php55-apcu: add 4.0.7 bottle.

--- Formula/php55-apcu.rb
+++ Formula/php55-apcu.rb
@@ -7,6 +7,13 @@ class Php55Apcu < AbstractPhp55Extension
   sha256 "178a731ee3435b451f1144bc4a63c9d70d4909a2c434e17c6e07db554df90bd6"
   head "https://github.com/krakjoe/apcu.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3fff374087db49792c3ea8f03c293dfb4e185da7585fae177393eaf2175100f2" => :el_capitan
+    sha256 "e4c4919f6d67e71713096b48033cebdb5405046a3e3eed8b8387a12f5e83878c" => :yosemite
+    sha256 "269a1a2f312bd069063d05dd227bfa7b8ffd7c77153cd2ed1d015480e49fda1a" => :mavericks
+  end
+
   option "with-apc-bc", "Whether APCu should provide APC full compatibility support"
   depends_on "pcre"
 
