2207bb0fbae31b2e7cb1c6d3d83eae37bdd0b7b5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 07:29:07 +0100

    php55-boxwood: add 888ba12 bottle.

--- Formula/php55-boxwood.rb
+++ Formula/php55-boxwood.rb
@@ -7,6 +7,13 @@ class Php55Boxwood < AbstractPhp55Extension
   sha256 "7e5142706023c4acc1db433998d2350458b00c7bc79eba0cc886683bba6b5343"
   version "888ba12"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bcc78ce1902f94cb4970706ff062caaa1d004833825e2ae75c311570073d8ecf" => :el_capitan
+    sha256 "41fb9c688e4f320bc7faf2c6b8d2966b6353116a62465d25f992c2fa3001a00f" => :yosemite
+    sha256 "d44cae77e6015992ff54bff6a8bb5159c376833bbdd17d40816f5045c488d87f" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
