b5f99363d8f298a09b3140e476d402349688f789
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 00:07:05 +0100

    php55-runkit: add 5e179e9 bottle.

--- Formula/php55-runkit.rb
+++ Formula/php55-runkit.rb
@@ -8,6 +8,13 @@ class Php55Runkit < AbstractPhp55Extension
   head "https://github.com/zenovich/runkit.git"
   version "5e179e9"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b41681ee9295a4dbd2bd1487c191884b2ac5e6cd5d89f4f1988bd573538e5439" => :el_capitan
+    sha256 "107f55e1ecc78478c27f7ab295070600ae65c5bd411d86087e9932ebfae5336f" => :yosemite
+    sha256 "38f85b0f1a602353fb7012ed162df66f7885e6de51117a00d5fdcbe12d83730f" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
