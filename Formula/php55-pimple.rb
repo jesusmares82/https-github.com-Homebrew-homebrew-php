1fed0155b4d39766894f44303c531d6bb32df11b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 16:46:58 +0100

    php55-pimple: add 3.0.0 bottle.

--- Formula/php55-pimple.rb
+++ Formula/php55-pimple.rb
@@ -7,6 +7,13 @@ class Php55Pimple < AbstractPhp55Extension
   sha256 "591e706f5cdce06bdd17d306db3fe9df521bee0ef4fcb0ee56524ff258ef66ba"
   head "https://github.com/silexphp/Pimple.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f3866e8b146fd60504a5fae80b33b654d2e54d692421b19bd7e2bd8166f1eab4" => :el_capitan
+    sha256 "27d3658c91e7084488f173b52343172231270524a8c372d108d07ce134e8f5b1" => :yosemite
+    sha256 "be24f3297d12ac4f86355ea68996507716d18925958530b48f4b86ed56513214" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
