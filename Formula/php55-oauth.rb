50b6e2fae1c0782f111fe6eb8f2c6e2f67e26910
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 14:45:45 +0100

    php55-oauth: add 1.2.3 bottle.

--- Formula/php55-oauth.rb
+++ Formula/php55-oauth.rb
@@ -7,6 +7,13 @@ class Php55Oauth < AbstractPhp55Extension
   sha256 "86bb5ee37afe672d4532ad784c7f711855c79f0dabf0acacafd5344ab6cf0195"
   head "https://svn.php.net/repository/pecl/oauth/trunk"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bbfe73fe3e1f73b807b815b883ec56e3d9a87f879ada0e75b64194c525fbed24" => :el_capitan
+    sha256 "d65d904cca8508e920e15e9055ebef81e263e5ec41cc27021321e795065052f8" => :yosemite
+    sha256 "49d9a31b223eb27c6ee61c1f9bc70cc429b18339e30dc2e8b043448b4d9c7315" => :mavericks
+  end
+
   def install
     Dir.chdir "oauth-#{version}" unless build.head?
 
