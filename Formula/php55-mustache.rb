55fcea5fb9d4403196c88bbc737339153f47719f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 15:00:17 +0100

    php55-mustache: add 0.6.0 bottle.

--- Formula/php55-mustache.rb
+++ Formula/php55-mustache.rb
@@ -2,6 +2,13 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php55Mustache < AbstractPhp55Extension
   desc "Mustache PHP Extension"
+  bottle do
+    cellar :any
+    sha256 "227ba872eec7df813bab7ad85cd4ae8995f778ae6813ee7025f334c02aa1e910" => :el_capitan
+    sha256 "f5b94031e95588ad20c54959adb028bccec0960d11b32dc56dfc84229819f494" => :yosemite
+    sha256 "6a0449574a8fef81faebb152b9a5f5abc2a5d093e0d1291a5a37a16c23dd9c1b" => :mavericks
+  end
+
   init
   homepage "https://github.com/jbboehr/php-mustache#mustache"
   url "https://github.com/jbboehr/php-mustache/archive/v0.6.0.tar.gz"
