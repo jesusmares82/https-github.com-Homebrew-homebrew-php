cb2386b1013fb21f35b79748b2e57be900d5205f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 18:26:36 +0100

    php55-protobuf: add 0.10 bottle.

--- Formula/php55-protobuf.rb
+++ Formula/php55-protobuf.rb
@@ -7,6 +7,13 @@ class Php55Protobuf < AbstractPhp55Extension
   url "https://github.com/allegro/php-protobuf/archive/0.10.tar.gz"
   sha256 "442e83dd47f5052e7e0bd4a46ab190a46115781e3c520df8319779689920a411"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2f26fa247cde57129f0481fcb6e0b705d39b632399555dad312efbf2e39410e2" => :el_capitan
+    sha256 "3bda0a0b31f79739eb484bc20c848aa990a50ebd67dc6094b412590ced5fb288" => :yosemite
+    sha256 "7f753b15c3b392e26efef21f9c4531d53e4d1c58f9c3ee022ccff44300a525cb" => :mavericks
+  end
+
   def install
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
