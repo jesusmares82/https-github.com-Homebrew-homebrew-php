b591cb9e1dfec8556b0ee82fe6c5fa29e869b74a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 16:18:59 +0100

    php55-memcache: add 2.2.7 bottle.

--- Formula/php55-memcache.rb
+++ Formula/php55-memcache.rb
@@ -7,6 +7,13 @@ class Php55Memcache < AbstractPhp55Extension
   sha256 "73006c02194a5a7c196c6488d449e5f8c75573a73568fe1a94b15157c147305d"
   head "https://svn.php.net/repository/pecl/memcache/trunk/"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "159e887ed087a69bdac4a997cd0e226b1079c2de15baf022837f25073ede4fef" => :el_capitan
+    sha256 "5f367b2e2b796d7f5d7888e4a5ac84e5cfdd3e9ac726c1d4852ee573f14d3b6b" => :yosemite
+    sha256 "746f10296ba211557a068b64c348a429a1613fb6c5ba565a87e55832ab995a29" => :mavericks
+  end
+
   devel do
     url "https://pecl.php.net/get/memcache-3.0.8.tgz"
     sha1 "2cae5b423ffbfd33a259829849f6000d4db018debe3e29ecf3056f06642e8311"
