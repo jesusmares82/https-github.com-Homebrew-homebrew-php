516477864008d3d7fdc1c1f1da53f57de0cd136a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 16:38:20 +0100

    php55-pdflib: add 3.0.4 bottle.

--- Formula/php55-pdflib.rb
+++ Formula/php55-pdflib.rb
@@ -6,6 +6,13 @@ class Php55Pdflib < AbstractPhp55Extension
   url "https://pecl.php.net/get/pdflib-3.0.4.tgz"
   sha256 "18de7bf00983a5b0fbbd4f7e993ecf948217072ae6ebff9fbb0eef88b8984b7e"
 
+  bottle do
+    cellar :any
+    sha256 "4d96d421a54c429705c94e2dee462b09024ef16d22a93d7e378a8cf21023b81c" => :el_capitan
+    sha256 "8565359a80d4932d513ecbfc4c5ae2eb88f79e1a5c0ac84ba0ef4b1f19cb1217" => :yosemite
+    sha256 "91b18fdea140e682137748f0e4bb3fe250f79c091728f92c2457f239f0db5911" => :mavericks
+  end
+
   depends_on "pdflib-lite"
 
   def install
