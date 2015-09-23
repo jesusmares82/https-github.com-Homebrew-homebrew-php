597428bef0d5a8bca6819caf4242e56178099099
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 15:51:06 +0100

    php55-mosquitto: add 0.2.2 bottle.

--- Formula/php55-mosquitto.rb
+++ Formula/php55-mosquitto.rb
@@ -7,6 +7,13 @@ class Php55Mosquitto < AbstractPhp55Extension
   sha256 "e9baa3af1d9a62f8fa1b76ffffbd13fffe7b65e0122130fb389915269543915e"
   head "https://github.com/mgdm/Mosquitto-PHP.git"
 
+  bottle do
+    cellar :any
+    sha256 "ad23a1cb0f716d386b5cb68892e0ce65911ce39b350a139b2e88dda606103d83" => :el_capitan
+    sha256 "866427f86c538afbe5d7ef47f38ec7bf180269e4aa81babd5578b1f1465f2ae7" => :yosemite
+    sha256 "5af0611da121646d289cfdfa02c92618e30b1d449e1a99e1ebcee3a0bc4d7479" => :mavericks
+  end
+
   depends_on "mosquitto"
 
   def install
