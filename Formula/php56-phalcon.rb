3c72d0577506ea7b64cef907846ca705d089506b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-26 00:23:06 +0100

    php56-phalcon: update 2.0.8 bottle.

--- Formula/php56-phalcon.rb
+++ Formula/php56-phalcon.rb
@@ -9,6 +9,10 @@ class Php56Phalcon < AbstractPhp56Extension
   head "https://github.com/phalcon/cphalcon.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "3584943dde99e34f270b12965e73e101bcf60b32cf9ee0027383ae543c71b68f" => :el_capitan
+    sha256 "f0f333e665ce28f2b54e40880e17be040b9e7e078dc818f84f86922cb0735637" => :yosemite
+    sha256 "35491943dfae1e0f93b1e23fdfaeea366df0a02b7f4641f923c9fd7565d3b693" => :mavericks
   end
 
   depends_on "pcre"
