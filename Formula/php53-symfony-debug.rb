6fc0485de7907ff29c11b1826450a0cbe1724c6b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 00:08:31 +0100

    php53-symfony-debug: add 2.7.5 bottle.

--- Formula/php53-symfony-debug.rb
+++ Formula/php53-symfony-debug.rb
@@ -8,6 +8,13 @@ class Php53SymfonyDebug < AbstractPhp53Extension
   sha256 "60e9e5e84d01a80c3028bde183eeaf1adb85cdd0761a23076dc3852e84afa52e"
   head "https://github.com/symfony/Debug.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7f0061ae68a0c30b8fa8a50d88b6bb75414e3473dc648f541c9512549d1d294f" => :el_capitan
+    sha256 "4e1ad4c1c9d0593395c48c0ef5587603ba0d7c6a2f2829b191a7cda1f5986462" => :yosemite
+    sha256 "88133c58628d6102211b0b872daaa49a97acb57126218f759d3c5837c5a80480" => :mavericks
+  end
+
   def extension
     "symfony_debug"
   end
