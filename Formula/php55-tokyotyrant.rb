84b670b17ce0f3d36b2628b3b82e111d93bd6075
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 00:18:44 +0100

    php55-tokyotyrant: add 0.7.0 bottle.

--- Formula/php55-tokyotyrant.rb
+++ Formula/php55-tokyotyrant.rb
@@ -7,6 +7,12 @@ class Php55Tokyotyrant < AbstractPhp55Extension
   sha256 "38559ac381670b61600d4736803a016f9e75aee9ae608d3f6b46e79103022d28"
   head "https://github.com/mkoppanen/php-tokyo_tyrant.git"
 
+  bottle do
+    sha256 "bf743c1da3bed26e8e410cddfcd39ec9f2a129750e99c8f1fb8969600aa8f27a" => :el_capitan
+    sha256 "905d6b800a083c548ed181e7bc71224585e403efee6056d0a80668bb7e44b59b" => :yosemite
+    sha256 "62b53cef163c3896bec765778916078307f43f09e6fd1172a5400a2d07d13a37" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "tokyo-tyrant"
 
