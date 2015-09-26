1dce8de692eaa81ed6eb2f53923e9b76e21ea5be
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-26 04:11:25 +0100

    php55-stats: add 1.0.3 bottle.

--- Formula/php55-stats.rb
+++ Formula/php55-stats.rb
@@ -7,6 +7,13 @@ class Php55Stats < AbstractPhp55Extension
   sha256 "e032e02052acf2013f0578da823d60b0b2a89eb5e1dd1379cf0a65c090dffdfc"
   head "https://svn.php.net/repository/pecl/stats/trunk/"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "476fde582d69af590139392d262dc01426893a82765fec6a3ff8669f2eec7d22" => :el_capitan
+    sha256 "b272a90144dee6f6174fa6e5d41de376b1ca48964a9edca36833d4d9eec8f696" => :yosemite
+    sha256 "5ae966beec346099d16f557fd30fcddf290f941f5fec1720811fdc7b3e96ebe6" => :mavericks
+  end
+
   def install
     Dir.chdir "stats-#{version}" unless build.head?
 
