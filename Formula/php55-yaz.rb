184b55aae1dbe6d98a267edf24a03a167dade049
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 14:44:58 +0100

    php55-yaz: add 1.1.6 bottle.

--- Formula/php55-yaz.rb
+++ Formula/php55-yaz.rb
@@ -6,6 +6,12 @@ class Php55Yaz < AbstractPhp55Extension
   url "https://pecl.php.net/get/yaz-1.1.6.tgz"
   sha256 "dd9b919ba3b2a09a9c77c847407f106512fe75979cc0689ebccef08c80719eee"
 
+  bottle do
+    sha256 "9c08b7228b976f4b3433d5f7abd6f676fcd6c93be074333d1995a1d12e2e0c0c" => :el_capitan
+    sha256 "181705173bddc5a65419a6b4098f63a1d0db469eb12a124b01c49a455f6fb950" => :yosemite
+    sha256 "2073312556427088bf4f5a9ab77fe5f261221f8743b00142bc4948e216e6ac1c" => :mavericks
+  end
+
   depends_on "yaz"
 
   def install
