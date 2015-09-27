8e6369caa08c7b783d2d41fabfc10e06c972015a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 22:21:18 +0100

    php54-binpack: add 1.0.1 bottle.

--- Formula/php54-binpack.rb
+++ Formula/php54-binpack.rb
@@ -7,6 +7,13 @@ class Php54Binpack < AbstractPhp54Extension
   sha256 "70617a721df4f8c52b5b1b214c11e464f8c36915cc19a850f24fed500c61b297"
   head "http://binpack.liaohuqiu.net"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ecca2f9d45062e1d9d834f7beedc5eab816e897456dacb9c5e42147b14c53298" => :el_capitan
+    sha256 "dab731ec233b747dc6cc379f9370fad48c75c47d39f9a8975e4df90d4005834e" => :yosemite
+    sha256 "a8be32101cebb5a6bdd94127e7f983bba19221cf91dfac53651559a2369e5be8" => :mavericks
+  end
+
   def install
     Dir.chdir "binpack-#{version}" unless build.head?
 
