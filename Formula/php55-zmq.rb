ece6210a734b25fbccc226b521997b4cc49c50e2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 14:53:11 +0100

    php55-zmq: add 1.1.2 bottle.

--- Formula/php55-zmq.rb
+++ Formula/php55-zmq.rb
@@ -7,6 +7,12 @@ class Php55Zmq < AbstractPhp55Extension
   sha256 "2ae77e90e0ed8112b11e838d6303940bbcae39e8d37683632a299db881bdb217"
   head "https://github.com/mkoppanen/php-zmq.git"
 
+  bottle do
+    sha256 "1dd8485ad3be607372e835544ccbc6e7814b11ad82b710da4409400dccdd5e77" => :el_capitan
+    sha256 "e14cbb249de1d7f798231f661f96dc202905797d11c2b2c1672210f13f57e4b4" => :yosemite
+    sha256 "29e712369fe62313eb773e99849e892549d3ffe8ca4fe4393adbbbb9c7f1961d" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "zeromq"
 
