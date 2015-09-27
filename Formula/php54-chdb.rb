9c2e8df1673072c940a7cdb863a174df755e1f8c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 22:47:57 +0100

    php54-chdb: add 1.0.2 bottle.

--- Formula/php54-chdb.rb
+++ Formula/php54-chdb.rb
@@ -5,6 +5,13 @@ class Php54Chdb < AbstractPhp54Extension
   homepage "https://pecl.php.net/package/chdb"
   url "https://pecl.php.net/get/chdb-1.0.2.tgz"
   sha256 "ac6360fd786fbbbe8b14c7e1943f2f64c1f9a86dd5a4c38ff4d5d65740e99e0b"
+  bottle do
+    cellar :any
+    sha256 "3eb62cf9d79087080998b62112d03ee315e0541b47b247aeb2679486f186509d" => :el_capitan
+    sha256 "f399ee2c505fcbf2e11fc8011f2944ff7e125f1d4f2f2890dcfb2fa0e4d26dce" => :yosemite
+    sha256 "ecfaa7c408ea9e2f67570afeca3701541635d18605e9ba343987e8dbc802b25c" => :mavericks
+  end
+
   head "https://github.com/lcastelli/chdb", :using => :git
 
   depends_on "libcmph"
