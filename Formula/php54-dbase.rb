bc33ab14482deec4a8d701de626ca422692b7f77
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 23:39:22 +0100

    php54-dbase: add 5.1.0 bottle.

--- Formula/php54-dbase.rb
+++ Formula/php54-dbase.rb
@@ -7,6 +7,13 @@ class Php54Dbase < AbstractPhp54Extension
   sha256 "20d6a40fb2efe4a06f503ec53512d02d71ceda87eac1f55208d7b5398f287a97"
   head "https://svn.php.net/repository/pecl/dbase/trunk/"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7e1c5ed22fec861422fb5c9cd849d1dccbc70ff360bcb774f88442b8ed57fb6a" => :el_capitan
+    sha256 "96b742a23ec2e804ecccf38843797e7cc80ac7c64962c7333bc3ed40b4cbdaf9" => :yosemite
+    sha256 "2bb92d6face108044305261b2d0732c770996ae4ac1c323802843e52483ed25c" => :mavericks
+  end
+
   def install
     Dir.chdir "dbase-5.1.0"
 
