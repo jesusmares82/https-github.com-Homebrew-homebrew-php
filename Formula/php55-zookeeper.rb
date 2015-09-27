b8f0ac9f21c721bd1aef2e2ffb50d93f66e576ad
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 14:55:56 +0100

    php55-zookeeper: add 0.2.2 bottle.

--- Formula/php55-zookeeper.rb
+++ Formula/php55-zookeeper.rb
@@ -7,6 +7,12 @@ class Php55Zookeeper < AbstractPhp55Extension
   sha256 "ce657910472b0880e2f9dd0c73558a94a15c2cfc0208ba305dcc02e27cb34f78"
   head "https://github.com/andreiz/php-zookeeper.git"
 
+  bottle do
+    sha256 "64d937669ce9db6f27dab763b6fae85948797777b80102b1b016fae4fadeb92d" => :el_capitan
+    sha256 "f8859ebcd7b37ce3f36f611cd680af4ca7e2eed926c96e7545fce1d6daf0939e" => :yosemite
+    sha256 "245072877a9f96f6f49a1e1bff8db431d81e8262b4095bb4780a66a7de6c1559" => :mavericks
+  end
+
   option "disable-session", "Disable zookeeper session handler support"
   depends_on "zookeeper"
 
