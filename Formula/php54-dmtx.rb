817cab06051e57ac7e26b20a0117e5fe2c28937a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 00:18:24 +0100

    php54-dmtx: add 0.0.3 bottle.

--- Formula/php54-dmtx.rb
+++ Formula/php54-dmtx.rb
@@ -8,6 +8,12 @@ class Php54Dmtx < AbstractPhp54Extension
   sha256 "864a2c9a39812e89d4aa3379cdf543a10a88b227cbe6d57ac94f2fd388b35e26"
   head "https://github.com/maZahaca/php-dmtx.git"
 
+  bottle do
+    sha256 "b0a134cd10af8f927145258dbd564fb932c0a24d39abf780cf1023040149c126" => :el_capitan
+    sha256 "d379b09675084abd4017053df33239ee68b68940d7e735a520974722c0e95bc7" => :yosemite
+    sha256 "325ec0411035b67cc6de52f55dea9f9850d8bc22ab5abf379110b99d5b898daf" => :mavericks
+  end
+
   depends_on "libdmtx"
   depends_on "php54-imagick"
   depends_on "pkg-config" => :build
