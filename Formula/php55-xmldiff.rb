49942bbb3c5424ae76dee333664b538ffc3623ca
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 14:34:41 +0100

    php55-xmldiff: add 0.9.2 bottle.

--- Formula/php55-xmldiff.rb
+++ Formula/php55-xmldiff.rb
@@ -6,6 +6,13 @@ class Php55Xmldiff < AbstractPhp55Extension
   url "https://pecl.php.net/get/xmldiff-0.9.2.tgz"
   sha256 "60d7d7fde2ebb695ae2cb26803153ad07a6146e0d70c102b3403131c86177550"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e90e1ec91dbfd8b2e7eebf7b7c7710dbddd345cada93bb4ad2d793d00999cb01" => :el_capitan
+    sha256 "f68a60ea41527962cb98ce9676a4d5b0ae5d906773acd294ef69f616362b6d6c" => :yosemite
+    sha256 "0060404a3f9f5b785ab90c3e073b84b4c80d029c3e91573466019ebfec41400d" => :mavericks
+  end
+
   def install
     Dir.chdir "xmldiff-#{version}"
 
