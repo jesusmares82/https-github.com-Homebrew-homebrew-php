04f900748e0b268c65aa1ea9ddd4d3f9487e6105
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 06:29:03 +0100

    php53-libevent: add 0.0.5 bottle.

--- Formula/php53-libevent.rb
+++ Formula/php53-libevent.rb
@@ -7,6 +7,12 @@ class Php53Libevent < AbstractPhp53Extension
   sha256 "04c6ebba72a70694a68141a897e347a7f23e57117bffb80ac21e524529b6af78"
   head "http://svn.php.net/repository/pecl/libevent/trunk/"
 
+  bottle do
+    sha256 "bf02921c8ebe55c322626967d7f63d054b39813f635bfc58d5a2b499b483ebc5" => :el_capitan
+    sha256 "6323ea35ab2cf4a4037c449bfce1a326a6f939ef9f86c30b59e40b4cba6708b6" => :yosemite
+    sha256 "a91bcebe9e0b9ec13c3badc36397c1b63bed688d94945b7a52c02e4d8bdeddc9" => :mavericks
+  end
+
   depends_on "libevent"
 
   def install
