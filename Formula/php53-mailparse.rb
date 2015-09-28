d765fc91aa3ed21c26d6e76d0335d30fcc116fe7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 06:31:51 +0100

    php53-mailparse: add 2.1.6 bottle.

--- Formula/php53-mailparse.rb
+++ Formula/php53-mailparse.rb
@@ -7,6 +7,13 @@ class Php53Mailparse < AbstractPhp53Extension
   sha256 "73705197d2b2ee782efa5477eb2a21432f592c2cb05a72c3a037bbe39e02b5cc"
   head "https://svn.php.net/repository/pecl/mailparse/trunk"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "455516e206a7f8c02ee00a8e1dff6e8b4c5c5fca4b580776536a740d3aee668a" => :el_capitan
+    sha256 "5457cf22695df5f4fd3dec5d955e91620c9f62ed7265d3fe75dd5fb17f61ed97" => :yosemite
+    sha256 "6872e84faa6103f668b207e52fc7eb2528ff70e56431d90f029bc861a2fc92c3" => :mavericks
+  end
+
   depends_on "pcre"
 
   def install
