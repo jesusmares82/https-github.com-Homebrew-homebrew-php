142274d33e5e482c78b44e19d2d8af3f3d2df7db
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 02:11:48 +0100

    phpcpd: add 2.0.0 bottle.

--- Formula/phpcpd.rb
+++ Formula/phpcpd.rb
@@ -7,6 +7,13 @@ class Phpcpd < AbstractPhpPhar
   url "https://phar.phpunit.de/phpcpd-2.0.0.phar"
   sha256 "ea4856166beb0dace104124deece3f6d43b9af6dbfe44e0bb41f45aba1d85b26"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "201ca00a23c810187c5b348979ac24e32f384ac86995f648ebf170884443a252" => :el_capitan
+    sha256 "981f0a1a34c144f4ff5f7c3693b990bdc8b55bea35dcad135db6f2331050b304" => :yosemite
+    sha256 "087aae9a8aa00fdee9da8379c3d99cbeb9a6b17215064f37e8515d02549275ad" => :mavericks
+  end
+
   def phar_file
     "phpcpd-#{version}.phar"
   end
