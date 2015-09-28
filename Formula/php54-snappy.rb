6d7b241f89fb2d8c35e0a318e587bfd10d9e9059
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 14:48:54 +0100

    php54-snappy: add 0.1.0 bottle.

--- Formula/php54-snappy.rb
+++ Formula/php54-snappy.rb
@@ -7,6 +7,13 @@ class Php54Snappy < AbstractPhp54Extension
   sha256 "4d1acaad08eacaa108618c6c765fe2d8da82c2f09f8996036e8bccb5438ebd61"
   version "0.1.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "72bce2b968a281e125e81f2243dd91fc9c49e323a454db1e2e87ce2766141db1" => :el_capitan
+    sha256 "9680b67b3b696327299d47af4cdf9cc242d9f256b9da90d3dbd047ca9ccd7432" => :yosemite
+    sha256 "9ab0456a87076606df7126b6d9b4fbef0edbf4182c1cc2a6f4480510b333f54e" => :mavericks
+  end
+
   depends_on "snappy"
 
   def install
