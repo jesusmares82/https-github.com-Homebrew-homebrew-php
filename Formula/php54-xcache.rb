e67c09fddb91b221e67e51ee6d7b9c82dd7baa19
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 15:57:50 +0100

    php54-xcache: add 3.2.0 bottle.

--- Formula/php54-xcache.rb
+++ Formula/php54-xcache.rb
@@ -6,6 +6,13 @@ class Php54Xcache < AbstractPhp54Extension
   url "http://xcache.lighttpd.net/pub/Releases/3.2.0/xcache-3.2.0.tar.bz2"
   sha256 "7d12a3cbd5f729b70f9226ac19406d11219f1c5f5e82b3bbb43ea9460cbf6cbd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "101337b58127e5f3772342b46089a42d2327b0bc82c6c6d6ab0f09b5c4eb1138" => :el_capitan
+    sha256 "49428079815e85fce22b18f0ed10d3442f61dd31c2856dae029199c4423af4bd" => :yosemite
+    sha256 "37522cf39cf53ae4dadc3678aaab3c261d164a4592a0252a7668668c086866d4" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
