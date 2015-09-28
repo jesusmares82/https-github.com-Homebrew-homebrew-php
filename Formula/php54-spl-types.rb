1cff49bfc693991f30a3971d7f06309e73a2a19d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 14:56:16 +0100

    php54-spl-types: add 0.4.0 bottle.

--- Formula/php54-spl-types.rb
+++ Formula/php54-spl-types.rb
@@ -6,6 +6,13 @@ class Php54SplTypes < AbstractPhp54Extension
   url "https://pecl.php.net/get/SPL_Types-0.4.0.tgz"
   sha256 "b44101401b2664822fd17e6f491d912203496108ff9d0b86b043bff67c5f724f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6d1baad8ddcaf4ea2f5ede1460174dcc29e1f6b518478f2f0020abdef68c19d8" => :el_capitan
+    sha256 "d202ec249b3f8ae2cb9810a97c6aeeb6798d369c6a48882b043a13331f45cf4a" => :yosemite
+    sha256 "be66a4d148f9855fba42e215417187383b515af25fd9478e127c2dcab3796fb1" => :mavericks
+  end
+
   def install
     Dir.chdir "SPL_Types-#{version}"
 
