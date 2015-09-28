d329f04bfb94c3d8ac85fadecd4e7cedceabc701
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 02:11:48 +0100

    php-cs-fixer: add 1.10 bottle.

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -7,6 +7,13 @@ class PhpCsFixer < Formula
   sha256 "344a50a46563350916ed2ec037f8f4d56eb8f4fbf8fc2299c9020875846ded01"
   head "https://github.com/FriendsOfPHP/PHP-CS-Fixer.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ade14a00772c49a3dd9b14365f6153f9e0f7300cf8abc721449545c5bf7141ff" => :el_capitan
+    sha256 "dcd30ac28e6c6a9c09611b64659de733315c5cbbd9e72cde56933bddee4f1a09" => :yosemite
+    sha256 "3d1ab0d0ab7241bb50589c8dae68bc5c6ce049f4fbfa3abe27a481a37b17d5ba" => :mavericks
+  end
+
   depends_on PhpMetaRequirement
   depends_on "composer"
 
