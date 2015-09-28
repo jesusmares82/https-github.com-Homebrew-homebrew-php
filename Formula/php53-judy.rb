a0b9e8d07edae7e1751b6b95b76bf25ea108e495
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 06:23:42 +0100

    php53-judy: add 1.0.2 bottle.

--- Formula/php53-judy.rb
+++ Formula/php53-judy.rb
@@ -7,6 +7,13 @@ class Php53Judy < AbstractPhp53Extension
   sha256 "6b913af2c44ac84a2224fecf8a81ddcb0c753252cafd153cb0e9d7c33a333118"
   head "https://github.com/orieg/php-judy.git"
 
+  bottle do
+    cellar :any
+    sha256 "2a8c6d20b113c80c54b01ec614e3883729e93e375c9b4f34454833ad8a10aef0" => :el_capitan
+    sha256 "4a526968a9648110969d46bd9ef196a8fd982ac1f15bee4330e007d28d975182" => :yosemite
+    sha256 "19493775e2e1bb9b85dc26c46abb1f120a437102dd7fcd22c34ea80557c034a4" => :mavericks
+  end
+
   depends_on "judy"
 
   def install
