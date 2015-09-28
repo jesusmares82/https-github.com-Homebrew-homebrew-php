d6a216c2da0344ef45de0b7a843ee71478d9b928
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 03:10:56 +0100

    php54-apc: add 3.1.13 bottle.

--- Formula/php54-apc.rb
+++ Formula/php54-apc.rb
@@ -7,6 +7,13 @@ class Php54Apc < AbstractPhp54Extension
   sha256 "5ef8ba07729e72946e95951672a5378bed98cb5a294e79bf0f0a97ac62829abd"
   head "https://svn.php.net/repository/pecl/apc/trunk/"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f070f2f4f3bc56dc974bd9313f401261461405cff1fbb08d5aa9d7e0d07c363e" => :el_capitan
+    sha256 "a21970664f88153a215ace196e7bfaf858b4dd22532c47eb1e5633f28bb59c27" => :yosemite
+    sha256 "649fc60bf8a037dd9040e158465e2b884a1cd085ad13c5c6a52afb2582c8b56c" => :mavericks
+  end
+
   depends_on "pcre"
 
   def install
