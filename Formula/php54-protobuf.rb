965afc568ed60b294726bfb8b3017a708fd3e8d7
Andy Blyler <andy@blyler.cc>
2015-09-28 08:06:45 -0400

    php54-protobuf: convert from sha1 to sha256
    Closes #2354.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php54-protobuf.rb
+++ Formula/php54-protobuf.rb
@@ -5,7 +5,7 @@ class Php54Protobuf < AbstractPhp54Extension
   homepage "https://github.com/allegro/php-protobuf"
   head "https://github.com/allegro/php-protobuf.git"
   url "https://github.com/allegro/php-protobuf/archive/0.10.tar.gz"
-  sha1 "a8553d4090f4da4882a16f51c50319eba5704be2"
+  sha256 "442e83dd47f5052e7e0bd4a46ab190a46115781e3c520df8319779689920a411"
 
   def install
     safe_phpize
