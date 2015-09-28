0c59b1a0bb0f112c02a0a1a321b0d517d4966375
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-28 02:11:49 +0100

    phpmd: add 2.2.3 bottle.

--- Formula/phpmd.rb
+++ Formula/phpmd.rb
@@ -8,6 +8,13 @@ class Phpmd < AbstractPhpPhar
   sha256 "5cd9d4754a0d2c41a403afe6b7b75e6994d90f8e8df3600a200398dd4271a31b"
   head "https://github.com/phpmd/phpmd.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "35e7188f625ba25bdae97569bf89225146c142fb831838b45020b1b288400216" => :el_capitan
+    sha256 "8431a91345f2b4ee6b23235ddac64b9ee955737caf5dee7d796ff33093cba13f" => :yosemite
+    sha256 "8431a91345f2b4ee6b23235ddac64b9ee955737caf5dee7d796ff33093cba13f" => :mavericks
+  end
+
   test do
     system "phpmd", "--version"
   end
