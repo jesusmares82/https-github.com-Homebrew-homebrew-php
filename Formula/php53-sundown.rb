3eee523b631a19e5ee6023ee637086d40dd4939f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-29 16:13:03 +0100

    php53-sundown: add 0.3.11 bottle.

--- Formula/php53-sundown.rb
+++ Formula/php53-sundown.rb
@@ -6,6 +6,13 @@ class Php53Sundown < AbstractPhp53Extension
   url "https://pecl.php.net/get/sundown-0.3.11.tgz"
   sha256 "0a143a268f43f5f2a07988116acfa62671fad4636b84c0750327042f9cb8004f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c8ea6901e7aa031b10c83e1d48c80c5d7d0af0aafa1d2926e78ae67f295dfbbe" => :el_capitan
+    sha256 "fd816980b9539739b113f1cb92e481bf1cff2e7d6485aeffbc1667bdd97feeff" => :yosemite
+    sha256 "fae2b14bc9ca0a04e29147faa3378db1d694f7430010400c5f5ae0b0ac028ddd" => :mavericks
+  end
+
   def install
     Dir.chdir "sundown-#{version}"
 
