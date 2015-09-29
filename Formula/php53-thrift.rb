704186ecec6ae88d5b34f9b248588814e03f352a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-29 16:18:57 +0100

    php53-thrift: add 0.9.2 bottle.

--- Formula/php53-thrift.rb
+++ Formula/php53-thrift.rb
@@ -6,6 +6,13 @@ class Php53Thrift < AbstractPhp53Extension
   url "https://github.com/apache/thrift/archive/0.9.2.tar.gz"
   sha256 "1eacc3d65b910fadf7969326285170d33cbe9fe30cc7bf421916f753edb3dabb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5e30d11177a51c61a20d1ac736f594401f4239dc250297f0e9904180efc43c9e" => :el_capitan
+    sha256 "79beb63a9c3debde7b793610c1ab4ef35a6036dfa02b5a503b1b0d28d9202740" => :yosemite
+    sha256 "92117c2063c78420e38f7245ec4aed3b4fed95c1a2d9f40d030fc060d9b2616b" => :mavericks
+  end
+
   def module_path
     prefix / "thrift_protocol.so"
   end
