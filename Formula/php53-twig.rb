64f60eef964ea385172e6161524932a1e2afb9e3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-29 18:33:45 +0100

    php53-twig: add 1.16.2 bottle.

--- Formula/php53-twig.rb
+++ Formula/php53-twig.rb
@@ -7,6 +7,13 @@ class Php53Twig < AbstractPhp53Extension
   sha256 "891e0e8f8eeee10f67c84556c28258dc5a96f8ef83e9fee1fefa64cf6aee27ef"
   head "https://github.com/twigphp/Twig.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ed521cd7697893537b0fc8cc3a62c6edef1611370bb121db65457ea4d82ef49e" => :el_capitan
+    sha256 "2a7b5877b9cd9e9097d0cc7ee1e55ecabec6e4a05d3141323f650ceaaaf770f8" => :yosemite
+    sha256 "a4da130bbb7460350b44153eccf2aab69a8cd75fa74f23bc3fdf84a646b88921" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
