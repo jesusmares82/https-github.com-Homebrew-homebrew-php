3c896ab6a3ae4e86f86c1c1a2fd389e2a54e4054
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-29 03:57:05 +0100

    php54-pdflib: update 3.0.4 bottle.

--- Formula/php54-pdflib.rb
+++ Formula/php54-pdflib.rb
@@ -8,10 +8,10 @@ class Php54Pdflib < AbstractPhp54Extension
   sha256 "18de7bf00983a5b0fbbd4f7e993ecf948217072ae6ebff9fbb0eef88b8984b7e"
 
   bottle do
-    cellar :any
-    sha256 "0b4a2cad5822974db5aa768f050fe71d1bc0b1fd5c06e39095f2166d1dbf0a6c" => :el_capitan
-    sha256 "345dee0b210bb7572c3d975a982eb7a2e9ece97d310fcafddf32984df54bd1b0" => :yosemite
-    sha256 "2847489162a872c9852846ecdbdc2fe00b81f9791bcfa03e7496ad6eca263e5f" => :mavericks
+    revision 1
+    sha256 "400c076180f2bb9875b9a36a8fb8d1dabeb3d674f39dcc4c2a05662a0315b6b4" => :el_capitan
+    sha256 "71092c44164f8342542532b2f77aab4058d41736c1128ee85d257737a6e82840" => :yosemite
+    sha256 "c3f54d44627396f0456c98f8266518acd7168ea619da694803546a2d11b7a8b1" => :mavericks
   end
 
   depends_on "pdflib-lite"
