88202292a6d4b4fc969a0938497a6834700a0db6
Andy Blyler <andy@blyler.cc>
2015-09-28 22:59:41 -0400

    php53-sundown: convert from sha1 to sha256
    Closes #2406.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php53-sundown.rb
+++ Formula/php53-sundown.rb
@@ -4,7 +4,7 @@ class Php53Sundown < AbstractPhp53Extension
   init
   homepage "https://pecl.php.net/package/sundown"
   url "https://pecl.php.net/get/sundown-0.3.11.tgz"
-  sha1 "e905cf2cd3540480af598e971a3c37c769775351"
+  sha256 "0a143a268f43f5f2a07988116acfa62671fad4636b84c0750327042f9cb8004f"
 
   def install
     Dir.chdir "sundown-#{version}"
