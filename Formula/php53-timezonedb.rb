16ebd2441f0b13095e2bc6eab7ca2fda99b1bd65
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-29 16:27:37 +0100

    php53-timezonedb: add 2014.7 bottle.

--- Formula/php53-timezonedb.rb
+++ Formula/php53-timezonedb.rb
@@ -7,6 +7,13 @@ class Php53Timezonedb < AbstractPhp53Extension
   sha256 "76e1fba9ea263621810a220ffe280c8ca227a12fd497c9ce430537fbd13357a7"
   head "https://svn.php.net/repository/pecl/timezonedb/trunk/"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bfef1a882dcb239af54f732ed222b03bde93b604e18c66bf2ab65511225c6175" => :el_capitan
+    sha256 "940a91a621c42be5c3a4eab1d56a7a54a6c27d1f5a18762c51413457bcea9423" => :yosemite
+    sha256 "f3f317b41bd608998a48f6d5875ff33fc023643e11587300374503ad425dd66b" => :mavericks
+  end
+
   def install
     Dir.chdir "timezonedb-#{version}" unless build.head?
 
