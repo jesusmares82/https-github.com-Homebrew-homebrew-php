cfc9f57e5a3e8345183a48108c2547ec44112bb5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-29 16:06:51 +0100

    php53-stemmer: add 0f32673 bottle.

--- Formula/php53-stemmer.rb
+++ Formula/php53-stemmer.rb
@@ -7,6 +7,12 @@ class Php53Stemmer < AbstractPhp53Extension
   sha256 "a3e2769afc0473d85f6d097596e3897a48ee294f071edc8d197fa04c6fc30b9f"
   version "0f32673"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "aa183135d748e3f27fab65fac59eb90531a53649d429b1420c16c0867917c5c2" => :el_capitan
+    sha256 "586a4010d7696b32377bc06fe28d6b87cb2b59339b0962525d0f72b7cab255ff" => :yosemite
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
