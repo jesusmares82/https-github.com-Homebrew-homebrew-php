879665c5a9e8dcf732c884ab51570d27b4bb17dc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-29 03:11:38 +0100

    php53-pthreads: add 2.0.10 bottle.

--- Formula/php53-pthreads.rb
+++ Formula/php53-pthreads.rb
@@ -7,6 +7,13 @@ class Php53Pthreads < AbstractPhp53Extension
   sha256 "8bdf8d8918680421ca0ced1e62292eeb626f800a808d0a3b6812841756588cf6"
   head "https://github.com/krakjoe/pthreads.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "25bf089fca730e123ad15e31783977ba59f169d5cb920316ec439b31ed843640" => :el_capitan
+    sha256 "e1b0a3f42609edde2aef6060a6448f7bea8219a76716cbb1bbf872e96d2e472f" => :yosemite
+    sha256 "5e15282b3a7efeeb713156752a00bdd9ce515136c58b98edd0aedf53d4be9a55" => :mavericks
+  end
+
   def install
     Dir.chdir "pthreads-#{version}" unless build.head?
 
