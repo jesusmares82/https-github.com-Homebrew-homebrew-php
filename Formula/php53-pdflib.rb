83dfaf93ead85cb81ae4af533f7f684d167e1e49
Andy Blyler <andy@blyler.cc>
2015-09-28 21:36:10 -0400

    php5*-pdflib: fix up extension name to fix tests
    Closes #2391.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php53-pdflib.rb
+++ Formula/php53-pdflib.rb
@@ -2,11 +2,16 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php53Pdflib < AbstractPhp53Extension
   init
+  desc "Creating PDF on the fly with the PDFlib library"
   homepage "http://www.pdflib.com"
   url "https://pecl.php.net/get/pdflib-3.0.4.tgz"
   sha256 "18de7bf00983a5b0fbbd4f7e993ecf948217072ae6ebff9fbb0eef88b8984b7e"
 
   depends_on "pdflib-lite"
+
+  def extension
+    "pdf"
+  end
 
   def install
     Dir.chdir "pdflib-#{version}"
@@ -14,7 +19,9 @@ class Php53Pdflib < AbstractPhp53Extension
     ENV.universal_binary if build.universal?
 
     safe_phpize
-    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-pdflib=#{Formula["pdflib-lite"].opt_prefix}"
     system "make"
     prefix.install "modules/pdf.so"
     write_config_file if build.with? "config-file"
--- Formula/php54-pdflib.rb
+++ Formula/php54-pdflib.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php54Pdflib < AbstractPhp54Extension
   init
+  desc "Creating PDF on the fly with the PDFlib library"
   homepage "http://www.pdflib.com"
   url "https://pecl.php.net/get/pdflib-3.0.4.tgz"
   sha256 "18de7bf00983a5b0fbbd4f7e993ecf948217072ae6ebff9fbb0eef88b8984b7e"
@@ -15,13 +16,19 @@ class Php54Pdflib < AbstractPhp54Extension
 
   depends_on "pdflib-lite"
 
+  def extension
+    "pdf"
+  end
+
   def install
     Dir.chdir "pdflib-#{version}"
 
     ENV.universal_binary if build.universal?
 
     safe_phpize
-    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-pdflib=#{Formula["pdflib-lite"].opt_prefix}"
     system "make"
     prefix.install "modules/pdf.so"
     write_config_file if build.with? "config-file"
