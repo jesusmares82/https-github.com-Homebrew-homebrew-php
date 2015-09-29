aee384bca275e348aa52bbc7a5c77a0c32da265b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-29 02:56:38 +0100

    php53-protobuf: add 0.10 bottle.

--- Formula/php53-protobuf.rb
+++ Formula/php53-protobuf.rb
@@ -7,6 +7,13 @@ class Php53Protobuf < AbstractPhp53Extension
   url "https://github.com/allegro/php-protobuf/archive/0.10.tar.gz"
   sha256 "442e83dd47f5052e7e0bd4a46ab190a46115781e3c520df8319779689920a411"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "14b87dc0012d0ae5b9a7483e0eff2cb515901578ed2f9b0d828c85cbec72a579" => :el_capitan
+    sha256 "6f5afd80c0eef7d3d222ca0d5ca328448fdc6a06fc3aaeb2f909e485821db0d4" => :yosemite
+    sha256 "2dae3a56f824c1824a63516d1352a0ef766b9ec8ca01d4715506293977496290" => :mavericks
+  end
+
   def install
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
