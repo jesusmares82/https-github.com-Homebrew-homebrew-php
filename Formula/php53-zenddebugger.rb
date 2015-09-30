5e1bdd6f6ed1a4d6580a839bc1f9aedff0eeca40
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-30 03:08:06 +0100

    php53-zenddebugger: add 20100729 bottle.

--- Formula/php53-zenddebugger.rb
+++ Formula/php53-zenddebugger.rb
@@ -3,6 +3,13 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php53Zenddebugger < AbstractPhp53Extension
   init
   homepage "http://www.zend.com/community/pdt/downloads"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cdace9f17337778d998e4ecfff4171f81f9856153c235d9a403fbaedf3fe8f22" => :el_capitan
+    sha256 "b2a14e99774393e8889f1366f535f0d73feba9bf84baf3621da4c6d3a5ffa38c" => :yosemite
+    sha256 "c18af0f8a34dfd3ca92a030c01b87f03182ea3107d720d3439ee0a3a1d94d3b4" => :mavericks
+  end
+
   if MacOS.prefer_64_bit?
     url "http://downloads.zend.com/studio_debugger/20100729/ZendDebugger-20100729-darwin9.5-x86_64.tar.gz"
     sha256 "87a7526738e1de1b20e055f06b7cfc46292e96e79a063893911e3ec42efa6213"
