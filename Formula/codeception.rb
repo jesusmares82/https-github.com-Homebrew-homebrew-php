ab8621676fbc7dd4a40561823858ce955cad0515
Alexander Turiak <alex@hexbrain.com>
2015-10-02 13:15:01 +0300

    Codeception 2.1.3
    Closes #2431.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -4,15 +4,10 @@ class Codeception < AbstractPhpPhar
   init
   desc "Testing Framework designed to work just out of the box"
   homepage "http://codeception.com/quickstart"
-  url "http://codeception.com/releases/2.1.2/codecept.phar"
-  sha256 "8a8fe5c4ba2582bcce3c4435f7d3e92eb08f51094f12a97017646d9c8ea3cdf4"
+  url "http://codeception.com/releases/2.1.3/codecept.phar"
+  sha256 "1d18bc051c96b70b0ee113c85809ae3531e13b4355492422217b7e9ab5daa7ac"
 
   bottle do
-    cellar :any_skip_relocation
-    revision 1
-    sha256 "ebe97f91b11423e8cb7267449b08807a9fa435a5c4cf1cf7146b3c2e9d83edfa" => :el_capitan
-    sha256 "e63b6fef18434fdbcc06d1b82cedce967ccb906f7fdde9634a7e3143d176c43a" => :yosemite
-    sha256 "7de37c178928d376e1ae7e0f575a125da94ac6c9820727e81b562e0f30f56bd9" => :mavericks
   end
 
   def phar_file
