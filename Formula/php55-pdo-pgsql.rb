6225074d0c317d92253473cd6afd2b664bf0a07f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-04 08:14:47 +0100

    php55-pdo-pgsql: update 5.5.30 bottle.

--- Formula/php55-pdo-pgsql.rb
+++ Formula/php55-pdo-pgsql.rb
@@ -8,6 +8,10 @@ class Php55PdoPgsql < AbstractPhp55Extension
   version PHP_VERSION
 
   bottle do
+    revision 2
+    sha256 "f94a6866456c1d75e233e2d8a8096cd081a460cc47409061c3ed7ae23a580c15" => :el_capitan
+    sha256 "3ff06193103d25cc93a8bcbd04bbf27ad945e914817531cbbdccec54ef0fc3c0" => :yosemite
+    sha256 "6a214826a608d55cdb3bb7132eed6fe3a5979f75ff6ebf507ac69a77c7f33d42" => :mavericks
   end
 
   depends_on "postgresql"
