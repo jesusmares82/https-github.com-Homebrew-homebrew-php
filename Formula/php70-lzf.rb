3a82fc8a64a647c23c88042c3fbc86419c312f2e
Tom Moitié <tom@tmoitie.co.uk>
2015-10-05 10:23:31 +0100

    Replace bottles that can stay
    Closes #2433.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php70-lzf.rb
+++ Formula/php70-lzf.rb
@@ -8,6 +8,13 @@ class Php70Lzf < AbstractPhp70Extension
   sha256 "42ec458ea10128a801e8d39736b519ba81fa75d2a617d2777b7d6b3276601a5d"
   head "http://svn.php.net/repository/pecl/lzf/trunk/"
 
+  bottle do
+    cellar :any
+    sha256 "e9913ea66d85cb6f90306e268d2220c5dcfa87e6522fe780de585103fe26472d" => :yosemite
+    sha256 "c8621911a5361c77d6aeee001d956eccd5803944d2ada674d828506ca8b004c3" => :mavericks
+    sha256 "a79956a38739720cdd012545e76a2302eddf85fae37186f310506234f1312b8c" => :mountain_lion
+  end
+
   def install
     Dir.chdir "LZF-#{version}" unless build.head?
 
