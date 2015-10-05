2136b4caaf2a81aada05b86e1173191079d32976
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-05 12:20:43 +0100

    php70-snmp: add 7.0.0-rc.4 bottle.

--- Formula/php70-snmp.rb
+++ Formula/php70-snmp.rb
@@ -4,6 +4,13 @@ class Php70Snmp < AbstractPhp70Extension
   init
   desc "SNMP core php extension"
   homepage "http://php.net/manual/en/book.snmp.php"
+  bottle do
+    revision 3
+    sha256 "220048287a083dc802aa081209e42bf1d60883ea583a57e467c0ad82b20b6ad6" => :el_capitan
+    sha256 "1b2fa138bd80788d636ee983c69c6b96b969e045457639b89519684071754e0a" => :yosemite
+    sha256 "9115838c7e0df6f9d2a0b349a4c890aa16f920225cd8802cf9632ba1f8a9c236" => :mavericks
+  end
+
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
   version PHP_VERSION
