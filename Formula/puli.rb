ed74091177df2af7ac4b10a45badb6b0615955c0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-12 09:47:24 +0100

    puli: add 1.0.0-beta9 bottle.

--- Formula/puli.rb
+++ Formula/puli.rb
@@ -8,6 +8,13 @@ class Puli < AbstractPhpPhar
   version "1.0.0-beta9"
   sha256 "6092944496f4cce2c0523ee58e8ad645394f8da15e2476f9c0eaa8c1f8264ef1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0961bda55a77b816c31f8501d91ff78e5071dde621ef1682144f0b8398a3d6fd" => :el_capitan
+    sha256 "05660a605bb3b90541a301170d6beab45583683e792f0f82d92946fb85c3af8b" => :yosemite
+    sha256 "3adb5471eea219968b5a7ee17752a1be620a1025cb6115144ad53a7516923117" => :mavericks
+  end
+
   test do
     system "puli", "--version"
   end
