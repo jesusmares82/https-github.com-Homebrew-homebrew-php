113588af7f99cea480ff0932fc8b5575f8500137
Andy Blyler <andy@blyler.cc>
2015-10-13 11:15:25 -0400

    Fix php meta requirement to allow for non php56 installs

--- Requirements/php-meta-requirement.rb
+++ Requirements/php-meta-requirement.rb
@@ -1,7 +1,11 @@
 require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
 
 class PhpMetaRequirement < HomebrewPhpRequirement
-  default_formula "php56"
+  default_formula "php53" if Formula["php53"].linked_keg.exist?
+  default_formula "php54" if Formula["php54"].linked_keg.exist?
+  default_formula "php55" if Formula["php55"].linked_keg.exist?
+  default_formula "php56" if Formula["php56"].linked_keg.exist?
+  default_formula "php70" if Formula["php70"].linked_keg.exist?
 
   def satisfied?
     %w{php53 php54 php55 php56 php70}.any? do |php|
