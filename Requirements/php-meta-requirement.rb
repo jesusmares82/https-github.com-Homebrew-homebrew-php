c1ec2ba1b726dcd0e9444be00353c391e386b6a3
Andy Blyler <andy@blyler.cc>
2015-10-19 13:26:02 -0400

    Update PHP Requirement to handle when no version is installed

--- Requirements/php-meta-requirement.rb
+++ Requirements/php-meta-requirement.rb
@@ -1,20 +1,32 @@
 require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
 
 class PhpMetaRequirement < HomebrewPhpRequirement
-  default_formula "php53" if Formula["php53"].linked_keg.exist?
-  default_formula "php54" if Formula["php54"].linked_keg.exist?
-  default_formula "php55" if Formula["php55"].linked_keg.exist?
-  default_formula "php56" if Formula["php56"].linked_keg.exist?
-  default_formula "php70" if Formula["php70"].linked_keg.exist?
+  if Formula["php53"].linked_keg.exist?
+    default_formula "php53"
+  elsif Formula["php54"].linked_keg.exist?
+    default_formula "php54"
+  elsif Formula["php55"].linked_keg.exist?
+    default_formula "php55"
+  elsif Formula["php56"].linked_keg.exist?
+    default_formula "php56"
+  elsif Formula["php70"].linked_keg.exist?
+    default_formula "php70"
+  elsif !Formula["php56"].installed? 
+    default_formula "php56"
+  end
 
   def satisfied?
     %w{php53 php54 php55 php56 php70}.any? do |php|
         f = Formula[php]
-        f.linked_keg.exist?
+        if f.linked_keg.exist?
+          return true
+        end
     end
+
+    return false
   end
 
   def message
-    "Missing a core php install from homebrew-php. Please run: brew install php56"
+    "Missing a core php install from homebrew-php. Please run: brew link php56"
   end
 end
