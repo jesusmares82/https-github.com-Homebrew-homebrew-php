9fd6bc08dfa8d5fb15d50d932b957ad76b71a58f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-22 18:01:01 +0100

    psysh: add 0.5.2 bottle.

--- Formula/psysh.rb
+++ Formula/psysh.rb
@@ -7,6 +7,13 @@ class Psysh < Formula
   sha256 "66cc4ccb4ae864fca6a7e6bb4be7da60bb9d9227da8c9770cd7b4137acc92822"
   head "https://github.com/bobthecow/psysh.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6e73a73ed4044c73dc69cb3726735c43ae50cf6316d22c428cd7332c2d5eef9e" => :el_capitan
+    sha256 "ed7506d750201c29d165227e07c9e95aaaaddad52255178f8f0ba0c5c8e98943" => :yosemite
+    sha256 "a8a3864a11801a8a47664dd5e68e51dfe7af7404941c71c1cbeb5622ebae28b1" => :mavericks
+  end
+
   depends_on PhpMetaRequirement
   depends_on "composer" => :build
   depends_on "php53" if Formula["php53"].linked_keg.exist?
