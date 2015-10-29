6f583a5c21e180dbfc5e1dcf08f493b5a558dcfe
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-29 21:12:03 +0000

    php70-snmp: update 7.0.0-rc.6 bottle.

--- Formula/php70-snmp.rb
+++ Formula/php70-snmp.rb
@@ -5,6 +5,10 @@ class Php70Snmp < AbstractPhp70Extension
   desc "SNMP core php extension"
   homepage "http://php.net/manual/en/book.snmp.php"
   bottle do
+    revision 5
+    sha256 "6f9afb5d2b3f354cfeb90d2bdf8a6eeedcf94f43ac93fe0b83d2a5faee59eb75" => :el_capitan
+    sha256 "666846174bbaf229ed7f86b936e8a21559f10eae44dcf18e9a121e86e9be32db" => :yosemite
+    sha256 "e30b6a71068992f67882f1eb17bbcd44340c1f795bfd369a2997594849e375c0" => :mavericks
   end
 
   url PHP_SRC_TARBALL
