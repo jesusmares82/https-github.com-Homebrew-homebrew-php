b45fdf8d71143084ca3300bbd022fa68d4f29eee
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-30 23:14:28 +0000

    php-plantumlwriter: add 1.6.0 bottle.

--- Formula/php-plantumlwriter.rb
+++ Formula/php-plantumlwriter.rb
@@ -4,6 +4,13 @@ class PhpPlantumlwriter < Formula
   url "https://github.com/davidfuhr/php-plantumlwriter/archive/1.6.0.tar.gz"
   sha256 "e0ee6a22877b506edfdaf174b7bac94f5fd5b113c4c7a2fc0ec9afd20fdc0568"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5124040d7593dc423a8c50eeb3e3961d47f66d017d76b7805c122b4edf74361a" => :el_capitan
+    sha256 "a2ea4a2c54d13207042be78ed52afd7c782306638b05d4572773fec947bfdb13" => :yosemite
+    sha256 "b4625f6b67bc7cdfa097041c58142a9a8dc69008089bd66f1bfd46c59af5b847" => :mavericks
+  end
+
   depends_on "plantuml"
   depends_on "composer" => :build
 
