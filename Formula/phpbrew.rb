cba0b441a51414ef8fd19748defaa97c7b8952f7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-31 20:21:03 +0000

    phpbrew: add 1.20.1 bottle.

--- Formula/phpbrew.rb
+++ Formula/phpbrew.rb
@@ -8,6 +8,13 @@ class Phpbrew < AbstractPhpPhar
   version "1.20.1"
   sha256 "150f81ecad2b2169d1d357f98d5233f0e4b33e654f8ce9d318d7ea13b376c34f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b411227e40458949dc4939d8f800164fb8cc1331bce47b86422bbe0f196c788b" => :el_capitan
+    sha256 "d37f3d3b5645c520c7fa12afe3264a3ea8695d3f8f3502ecd75268e663ad6e26" => :yosemite
+    sha256 "2d6e85115f902e5c7bdaa2759e053767fe5d29ffab5173f261e6160ad3cedc4b" => :mavericks
+  end
+
   depends_on "curl"
   depends_on "gettext"
   depends_on "gmp"
