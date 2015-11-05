f18b318f7faa0345a1c2f34fdeaed9e81e88c7d9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-05 00:38:14 +0000

    wp-cli: add 0.21.0 bottle.

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -5,6 +5,13 @@ class WpCli < Formula
   sha256 "de451fba0112d5cb9b895d2d6b87adb115aa6fc147c15a9594479f042f2745c4"
   head "https://github.com/wp-cli/wp-cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "64b686ce1dd71d68fdbb5c650ac82a4dc6da8749fa0802783c339cc676cc18a9" => :el_capitan
+    sha256 "72c5b3a41ad90fbca69e8be9b7f56cf9af32109eba75ad5830b4ca6754f8e8ff" => :yosemite
+    sha256 "f99eb3110ce787631b6e1f2888f4c8ecb97346058d81d27c8597a13a83373a65" => :mavericks
+  end
+
   option "without-bash-completion", "Don't install bash completion"
   option "without-package-index", "Don't add package index repository (http://wp-cli.org/package-index)"
 
