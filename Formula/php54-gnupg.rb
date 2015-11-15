1caef717791d892b4643665f2ecbd3bd4cb6145c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-15 16:01:37 +0000

    php54-gnupg: add 1.3.6 bottle.

--- Formula/php54-gnupg.rb
+++ Formula/php54-gnupg.rb
@@ -7,6 +7,13 @@ class Php54Gnupg < AbstractPhp54Extension
   url "https://pecl.php.net/get/gnupg-1.3.6.tgz"
   sha256 "50065cb81f1ac3ec5fcd796e58c8433071ff24cc14900e6077682717f5239307"
 
+  bottle do
+    cellar :any
+    sha256 "392e3a93f964644ae21a5ef8695262ec5c15ed8fe81158c299a2a9b8a903d984" => :el_capitan
+    sha256 "dcb54817436592c18ba1bb3c7dce7998b5eabcfe57b2b40ae6398a4f5ad76489" => :yosemite
+    sha256 "2848b982d56c98b0588cbb2abf552dd1394bedb7dcd9a6e0e43bb89b505aa537" => :mavericks
+  end
+
   depends_on "gpgme"
 
   def install
