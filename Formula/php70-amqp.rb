4ab04a40d1823d1cfaa767a8d8b1af3d0949894a
Jiří Pudil <me@jiripudil.cz>
2015-11-17 14:30:01 +0100

    php70-amqp: add amqp for PHP7
    Closes #2530.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php70-amqp.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Amqp < AbstractPhp70Extension
+  init
+  desc "Communicates with any AMQP 0-9-1 compatible server."
+  homepage "https://pecl.php.net/package/amqp"
+  url "https://pecl.php.net/get/amqp-1.7.0alpha1.tgz"
+  version "1.7.0alpha1"
+  sha256 "9c123bd4aae1a6b89e2400c7b9e252496ab1bc0dc3db2d6b6dd3051c03b3a43b"
+  head "https://github.com/pdezwart/php-amqp.git"
+
+  depends_on "rabbitmq-c"
+
+  def install
+    Dir.chdir "amqp-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/amqp.so"
+    write_config_file if build.with? "config-file"
+  end
+end
