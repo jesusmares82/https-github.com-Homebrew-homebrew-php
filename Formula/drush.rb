e6c215fb7aa6d6dc44c6bacad43f568ba6a97473
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-20 10:17:53 +0000

    drush: add 8.0.0 bottle.

--- Formula/drush.rb
+++ Formula/drush.rb
@@ -6,6 +6,13 @@ class Drush < Formula
   url "https://github.com/drush-ops/drush/archive/8.0.0.tar.gz"
   sha256 "843cf1bff04cf86aff50ebd4ed2cf4b6713270d4e4aa2f60d575de85949121b0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f9a536347662f032cdd974eb3cdde34f827fe4cfd58f659c20c09cdf139e0640" => :el_capitan
+    sha256 "a1e2a683cf437d97227d612c00f06fb550cd921120a016a951208053254d0234" => :yosemite
+    sha256 "2487e03d2c3f064fbb1996c95400f50c0bcc5691b3fe660d78394a003145534c" => :mavericks
+  end
+
   head do
     url "https://github.com/drush-ops/drush.git"
   end
