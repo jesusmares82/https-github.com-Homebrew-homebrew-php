f80a6c12eb22fde4524a36b20a15bb35730baa6b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-21 14:13:26 +0000

    php70-amqp: add 1.7.0alpha1 bottle.

--- Formula/php70-amqp.rb
+++ Formula/php70-amqp.rb
@@ -9,6 +9,13 @@ class Php70Amqp < AbstractPhp70Extension
   sha256 "9c123bd4aae1a6b89e2400c7b9e252496ab1bc0dc3db2d6b6dd3051c03b3a43b"
   head "https://github.com/pdezwart/php-amqp.git"
 
+  bottle do
+    cellar :any
+    sha256 "8b40be19311dab03f4b4b7d17f332ab5b1490ca98258936953ffcc3dd7559936" => :el_capitan
+    sha256 "855d75b0eea9b227c3366e87785bb248c8ea34f251d17ece00b182b5481d255f" => :yosemite
+    sha256 "ec4fe1b8cafaba4284193adeee8cfef844cc7ebd7aa2bdc6dd5ead7166afc313" => :mavericks
+  end
+
   depends_on "rabbitmq-c"
 
   def install
