70a29d0a9f53cbe73ebe218c52b52fd7b8a037f3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-27 18:59:08 +0000

    php56-gmp: update 5.6.16 bottle.

--- Formula/php56-gmp.rb
+++ Formula/php56-gmp.rb
@@ -9,6 +9,10 @@ class Php56Gmp < AbstractPhp56Extension
   version PHP_VERSION
 
   bottle do
+    revision 4
+    sha256 "c3be73ef7a73873961e2353bcc0e1ccff8959fb60fa0ddebe52bbc3f4e807052" => :el_capitan
+    sha256 "693c22055d6280dac7bc024a3cd435fd59077ef90697efc3c15a2d9beae41a34" => :yosemite
+    sha256 "1848a06b3349b1dedc528a2928cb918a1c4afea5bf3e5376d64afcc7bb622c67" => :mavericks
   end
 
   depends_on "gmp"
