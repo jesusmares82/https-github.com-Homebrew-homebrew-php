c1f9e05fa3de77da6a55ce3aadf562745f89cb40
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-27 18:59:08 +0000

    php56-pcntl: update 5.6.16 bottle.

--- Formula/php56-pcntl.rb
+++ Formula/php56-pcntl.rb
@@ -8,6 +8,11 @@ class Php56Pcntl < AbstractPhp56Extension
   version PHP_VERSION
 
   bottle do
+    cellar :any_skip_relocation
+    revision 4
+    sha256 "cec71aedaa463c58986b1828f0987486448b017b79700ea9a5ebb3b5570900f4" => :el_capitan
+    sha256 "881112b647d1ef4b6c4d08e9b48871d0540e57473e270f6692e220dd7b1cf3e7" => :yosemite
+    sha256 "af78d4795cc867f501385ebed9d499f68e4c1c794e110416e7fd6921031a7e26" => :mavericks
   end
 
   def install
