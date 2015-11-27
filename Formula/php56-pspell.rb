d5caf88fb28c1e325c9cd7d2ff00c3d0b1e7c325
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-27 18:59:08 +0000

    php56-pspell: update 5.6.16 bottle.

--- Formula/php56-pspell.rb
+++ Formula/php56-pspell.rb
@@ -8,6 +8,10 @@ class Php56Pspell < AbstractPhp56Extension
   version PHP_VERSION
 
   bottle do
+    revision 4
+    sha256 "c810b8c4d523e841aa0e0f91ca79826d8eba387f4845f637e1f67c93a4a4ed3f" => :el_capitan
+    sha256 "4a7d8e640827bd1368c7b33299d84aed4a5b41816c2d71fd0e889e1eb8844506" => :yosemite
+    sha256 "d07674088ef1c26b45f0355f3a010ed147772b184a5ad5f6d778e307b3568d01" => :mavericks
   end
 
   depends_on "aspell"
