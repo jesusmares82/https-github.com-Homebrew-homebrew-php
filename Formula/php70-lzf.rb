2c133b6df0a7a9171e5bc8bdfa5ac43b4afcd61d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-27 18:12:13 +0000

    php70-lzf: update 1.6.3 bottle.

--- Formula/php70-lzf.rb
+++ Formula/php70-lzf.rb
@@ -9,6 +9,11 @@ class Php70Lzf < AbstractPhp70Extension
   head "http://svn.php.net/repository/pecl/lzf/trunk/"
 
   bottle do
+    cellar :any_skip_relocation
+    revision 2
+    sha256 "f8fec0b909bc04c45efa7400c62bce8442e6cd492b209c5bdaaf4a1f1c145c07" => :el_capitan
+    sha256 "2d5add525c3125045e2208efae566273a324d3e37fc29069f152f926192a5e6e" => :yosemite
+    sha256 "0ea8a7ef4d33c537c4a57aef41bf0ad78c41a2526128c995359ddf3c885e068e" => :mavericks
   end
 
   def install
