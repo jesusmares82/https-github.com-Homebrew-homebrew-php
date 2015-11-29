1d3145ee241ffc1b544286f245b0fb4202cfd458
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-29 03:58:07 +0000

    php56-mongodb: add 1.0.0 bottle.

--- Formula/php56-mongodb.rb
+++ Formula/php56-mongodb.rb
@@ -8,6 +8,12 @@ class Php56Mongodb < AbstractPhp56Extension
   sha256 "a1f0ac6835190ce119b3b0646b6918713132e6942003f38dd7a6579e8f255aff"
   head "https://github.com/mongodb/mongo-php-driver.git"
 
+  bottle do
+    sha256 "e3b71ef138862450d08a33b83a9e62170e904fc204d4345795e985aad01f57e7" => :el_capitan
+    sha256 "90039dd2a38f42e87ebb99c5ccf3aaf6aa169b23059de9cef493cb1d8ea97216" => :yosemite
+    sha256 "f5f1b464c092c9611bf4740e3f2ebe304061387e672352409633e47514dd78b6" => :mavericks
+  end
+
   depends_on "openssl"
 
   def install
