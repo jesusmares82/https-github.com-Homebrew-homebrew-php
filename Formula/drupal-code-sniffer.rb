f4a1d928b8621911f77893fe8a738237241b749a
Arne Jørgensen <arne@arnested.dk>
2015-12-01 10:19:12 +0100

    drupal-code-sniffer 8.x-2.5
    Closes #2551.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/drupal-code-sniffer.rb
+++ Formula/drupal-code-sniffer.rb
@@ -3,9 +3,9 @@ require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
 class DrupalCodeSniffer < Formula
   desc "Checks Drupal code against coding standards"
   homepage "https://drupal.org/project/coder"
-  url "http://ftp.drupal.org/files/projects/coder-8.x-2.4.tar.gz"
-  version "8.x-2.4"
-  sha256 "7c966d6bcc03ddbe133f2b7cc3cac7625d9bf529ad4ff97e36949d4252063af7"
+  url "http://ftp.drupal.org/files/projects/coder-8.x-2.5.tar.gz"
+  version "8.x-2.5"
+  sha256 "995df5807251c506a58966f41e57b1842e61dd0a60800d8c45e985600d29ce28"
   head "http://git.drupal.org/project/coder.git", :branch => "8.x-2.x"
 
   option "without-drush-command", "Don't install drush command"
