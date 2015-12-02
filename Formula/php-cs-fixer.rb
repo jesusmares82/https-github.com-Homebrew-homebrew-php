90d9a1e0eaf79be64e7725c8bd0c1babe909eea2
Vincent Klaiber <vincentklaiber@gmail.com>
2015-12-02 13:00:39 +0100

    Update PHP CS Fixer to version 1.11
    Closes #2619.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
 class PhpCsFixer < Formula
   desc "Tries to fix coding standards issues"
   homepage "http://cs.sensiolabs.org"
-  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/archive/v1.10.tar.gz"
-  sha256 "344a50a46563350916ed2ec037f8f4d56eb8f4fbf8fc2299c9020875846ded01"
+  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/archive/v1.11.tar.gz"
+  sha256 "913d9561257b29350d2b9a7ba82fa6f1e205ef9833232086ca928a70bb29663b"
   head "https://github.com/FriendsOfPHP/PHP-CS-Fixer.git"
 
   bottle do
