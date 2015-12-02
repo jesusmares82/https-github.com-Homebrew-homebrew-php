3925e8d519e9fbdb1b7e407f5602fc77036315a8
Andreas Braun <git@alcaeus.org>
2015-12-02 07:50:19 +0100

    Add description for oauth formula
    Closes #2590.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php54-oauth.rb
+++ Formula/php54-oauth.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php54Oauth < AbstractPhp54Extension
   init
+  desc "OAuth 1.0 consumer and provider"
   homepage "https://pecl.php.net/package/oauth"
   url "https://pecl.php.net/get/oauth-1.2.3.tgz"
   sha256 "86bb5ee37afe672d4532ad784c7f711855c79f0dabf0acacafd5344ab6cf0195"
