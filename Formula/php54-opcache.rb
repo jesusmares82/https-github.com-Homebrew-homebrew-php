4a2e7059e4e7961210f9896a284f5bc274af6e9c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 11:37:29 +0000

    php54-opcache: update 7.0.5 bottle.

--- Formula/php54-opcache.rb
+++ Formula/php54-opcache.rb
@@ -10,9 +10,11 @@ class Php54Opcache < AbstractPhp54Extension
   head "https://github.com/zendtech/ZendOptimizerPlus.git"
 
   bottle do
-    sha256 "5c9c610aa86e26f0524125148648a5fa567e30597a14582a552f698969b6023b" => :yosemite
-    sha256 "6afb1733127de95490ac467d015ba621286f1c604e0e824f2fa9478ddaa9f868" => :mavericks
-    sha256 "238b778d9e1446f105bdaeaf4e900030b2f536662131fd4c74b9b28b788b2347" => :mountain_lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "4b9cc3a0729952ff83063e8300232f0df97e046f0bf887d72de8b9345bb5ccb1" => :el_capitan
+    sha256 "24ba09157df24050a723c3888c32009ae76634e1f3eb0b622626de23bcbb6e68" => :yosemite
+    sha256 "4cc160ea8cd1f1f3a7b11411e22ca946121310b65cb3cbf734b4208dbc46a43a" => :mavericks
   end
 
   depends_on "pcre"
