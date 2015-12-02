51ec8480cd8ad4e86366981b0368ac847a36174d
Andreas Braun <git@alcaeus.org>
2015-12-02 07:46:11 +0100

    Add description for boxwood formula
    Closes #2562.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-boxwood.rb
+++ Formula/php56-boxwood.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Boxwood < AbstractPhp56Extension
   init
+  desc "PHP extension for fast replacement of multiple words in a piece of text"
   homepage "https://github.com/ning/boxwood"
   url "https://github.com/ning/boxwood/archive/888ba12635d7c50cb1bbf1cbef513b0ef9238af3.tar.gz"
   sha256 "7e5142706023c4acc1db433998d2350458b00c7bc79eba0cc886683bba6b5343"
