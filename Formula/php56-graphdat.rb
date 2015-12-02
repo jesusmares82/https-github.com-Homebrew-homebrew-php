416cbf74a215a5551bf03bfb13b3c8279b56f213
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 09:12:56 +0000

    php56-graphdat: update 1.0.3 bottle.

--- Formula/php56-graphdat.rb
+++ Formula/php56-graphdat.rb
@@ -10,9 +10,10 @@ class Php56Graphdat < AbstractPhp56Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ddca7ce11952ebf814387d36c991e7e981220f7da6cd72c72810ee1b4b04e8d0" => :el_capitan
-    sha256 "2cdfc6c2f737089b82acbe85936b62316c538eeaf063fd02cdd909660b1275b0" => :yosemite
-    sha256 "a21614d2557246c43e977e8a40b9cfc49f112f024dc3842025be055a1dc465a9" => :mavericks
+    revision 1
+    sha256 "c7f32d9d39a5be060b3c1241214382405c767ef9f07bb95ed49fecb5fc9ab15a" => :el_capitan
+    sha256 "718f727a191f36cc43ca3a10a1e2306431364dc37b9ab8eeb0e26e6ecedf19a5" => :yosemite
+    sha256 "13281e3f392b4c26dfc96b5f241fe3ddd130f975cc94d7ef5cd6dc49d9cddbec" => :mavericks
   end
 
   def install
