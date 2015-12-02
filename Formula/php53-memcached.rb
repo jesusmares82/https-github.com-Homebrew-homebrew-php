2d15cb823abb91e1a09433ddc7017c4ce83a0f2c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 11:03:54 +0000

    php53-memcached: update 2.2.0 bottle.

--- Formula/php53-memcached.rb
+++ Formula/php53-memcached.rb
@@ -9,9 +9,8 @@ class Php53Memcached < AbstractPhp53Extension
   head "https://github.com/php-memcached-dev/php-memcached.git"
 
   bottle do
-    sha256 "7aee805b1e7efb1cabfcc001a9de7b67e6ec6cdf5b183a9be36672dea55bcc0a" => :yosemite
-    sha256 "5e1f4a6af6c16d9f4be2821c3e2ac7f6f919dffe94c8373366d9cfaa1ad67349" => :mavericks
-    sha256 "0bd1e3817c80baeb7fc70d30d40f47b15aac6347309b78d80805832d1e00a859" => :mountain_lion
+    revision 1
+    sha256 "9a2e2393fbc8e846709c705300c067ce5b285c8d9837c6de880743e474ad6d76" => :el_capitan
   end
 
   option "with-sasl", "Build with sasl support"
