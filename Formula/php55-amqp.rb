2dff5af222eda1149c9341157af2b47987c89b5e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 07:29:51 +0000

    php55-amqp: add 1.6.0beta3 bottle.

--- Formula/php55-amqp.rb
+++ Formula/php55-amqp.rb
@@ -9,6 +9,13 @@ class Php55Amqp < AbstractPhp55Extension
   version "1.6.0beta3"
   head "https://github.com/pdezwart/php-amqp.git"
 
+  bottle do
+    cellar :any
+    sha256 "3f510c2b7bc41c8d30f6eb7801dceab550901141bdae48364cf5f147e743d0be" => :el_capitan
+    sha256 "e2ebc878b52017c6c90666089230f8248edba8f8850267c526bc313596272f33" => :yosemite
+    sha256 "5fe6a170e3ca01e8b2dc215728f6bab77ab00d2975529bf970243546d2eea542" => :mavericks
+  end
+
   depends_on "rabbitmq-c"
 
   def install
