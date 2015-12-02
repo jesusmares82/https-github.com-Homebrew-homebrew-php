97afcfb43d267e74265bb3c680ffea0ed3341780
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 03:39:18 +0000

    climb: update 0.7.0 bottle.

--- Formula/climb.rb
+++ Formula/climb.rb
@@ -8,6 +8,10 @@ class Climb < AbstractPhpPhar
   sha256 "50c62a80f487abda75cc2eebb15b7ce7921eefc6014e5c3e4fc82eb5be2074c4"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "b63779bd7955e39cacbbf241505626cb715b20a2ae2714b442aabf37666863c3" => :el_capitan
+    sha256 "5e917f407c10d86e7952522ee89bf77837fb0bf3b6602ccb9177223b30675cb3" => :yosemite
+    sha256 "9299ac3cf99c2478d789673ac08262de95d280740ee402c7064f3551d6357dcd" => :mavericks
   end
 
   test do
