35089ef7bd9775a4faf3c455a0a40e41d389f0f0
Andreas Braun <git@alcaeus.org>
2015-12-02 08:00:51 +0100

    Add description for sphinx formula
    Closes #2610.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-sphinx.rb
+++ Formula/php56-sphinx.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Sphinx < AbstractPhp56Extension
   init
+  desc "Client extension for Sphinx - opensource SQL full-text search engine"
   homepage "https://pecl.php.net/package/sphinx"
   url "https://pecl.php.net/get/sphinx-1.3.2.tgz"
   sha256 "0c3ada36833a44a8147d2dd1b907548010ae53de0a05041a77e68dee036130e6"
