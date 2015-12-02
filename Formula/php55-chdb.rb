2693cd0392edeac9dcb6c919c6f99c77f1cd7cc5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 08:16:17 +0000

    php55-chdb: update 1.0.2 bottle.

--- Formula/php55-chdb.rb
+++ Formula/php55-chdb.rb
@@ -8,9 +8,10 @@ class Php55Chdb < AbstractPhp55Extension
   sha256 "ac6360fd786fbbbe8b14c7e1943f2f64c1f9a86dd5a4c38ff4d5d65740e99e0b"
   bottle do
     cellar :any
-    sha256 "c29e62f9b5d67ae902df6d8b78baad58612ea04be5be5b0317138c2a0e735338" => :el_capitan
-    sha256 "70987c24a1fa9ece79823b63d21a594e4f0251e6799c5595fd9794ff63631c30" => :yosemite
-    sha256 "f666a9fd5d16c4a3fb0f3535214d6b704cc946374b71fda61b22df445ecaee9a" => :mavericks
+    revision 1
+    sha256 "d842d184ec4918c6b0b1601e78e8702bf10c1af9bcc856ccc90afe045f6fe99d" => :el_capitan
+    sha256 "5873c57ce014984f72f75561f9f2113bd2c2f0d18d917dc3097b783426e17bda" => :yosemite
+    sha256 "b826583683207d71db9342259614ec848a8774108e8de1e2caccb11f2508f171" => :mavericks
   end
 
   head "https://github.com/lcastelli/chdb", :using => :git
