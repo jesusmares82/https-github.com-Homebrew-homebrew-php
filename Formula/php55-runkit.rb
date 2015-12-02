ad1aaf393834c9b99f73c370153b954b358c4f35
Andreas Braun <git@alcaeus.org>
2015-12-02 07:51:25 +0100

    Add description for runkit formula
    Closes #2605.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php55-runkit.rb
+++ Formula/php55-runkit.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php55Runkit < AbstractPhp55Extension
   init
+  desc "Extension to modify consts/functions/classes"
   homepage "http://php.net/manual/en/book.runkit.php"
   url "https://github.com/zenovich/runkit/archive/27f33f55eae4459448fc39fac49daba26bb6b575.tar.gz"
   sha256 "4b19ca9d4003eac6af51dd3b6b8e4365a74351a4fd248f2fe5c546b656a16a99"
