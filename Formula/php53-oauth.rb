05e1e8312a96285d721099595944ed2e86024877
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 11:30:53 +0000

    php53-oauth: update 1.2.3 bottle.

--- Formula/php53-oauth.rb
+++ Formula/php53-oauth.rb
@@ -10,9 +10,10 @@ class Php53Oauth < AbstractPhp53Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "884be397c28e254fc86e2959f24f027dce3fcdca42fe672c915d2c9d288fbfc6" => :el_capitan
-    sha256 "73cba6b88bdeba6a778be52f416a4480fd5dbf8a1d96fe4cd5d86d3a75aa807b" => :yosemite
-    sha256 "b8b7a3937070e4188f7fb03064eba16adb6587d7df4a9a1b24c4f6096fc20bf7" => :mavericks
+    revision 1
+    sha256 "7417fde959d04540a902b06be46a39c180e8aed87b400a20f1eae6a0dad1495b" => :el_capitan
+    sha256 "00756eb2418bb99e2812eae70f4f3ffa985d8c3a37674ceea1a9ee0947f0ebcd" => :yosemite
+    sha256 "e2d67cca3733ae0b36d4e5c37dae3a895b96f2bed65d1336d8a8552a558ef464" => :mavericks
   end
 
   def install
