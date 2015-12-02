c6c1f5736e612c12a664f8200d9225f365f560c4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 09:07:01 +0000

    php55-gmagick: update 1.1.7RC2 bottle.

--- Formula/php55-gmagick.rb
+++ Formula/php55-gmagick.rb
@@ -8,9 +8,10 @@ class Php55Gmagick < AbstractPhp55Extension
   sha256 "8e51c8343d6e6d556d7b17417ce338c6ed2b0893869f1494410dfe6ba5105475"
 
   bottle do
-    sha256 "1419c7b3d6a8ff8d072765eabca6cebcd22f8316602bb6705ce146aa10c61e67" => :el_capitan
-    sha256 "e241ff365bc38a158114fa88663d718aac733e3e3ca029defa47c8796ebd44c5" => :yosemite
-    sha256 "a81b82591f1877b37019f56fc0e8b42e4a60a9804a18389d85f89ec17caf5e45" => :mavericks
+    revision 1
+    sha256 "d257844dd7df408384820f20684ccbeb103ac14f94556091e935c208f054568a" => :el_capitan
+    sha256 "66833fbebb1755acdcea3dfc30c69187dbbb2f762cf1e5229b95d8357e083780" => :yosemite
+    sha256 "410d902752fe549f7b863a46ffec55a59fb5a481780d2b2f49a38d7f1b803465" => :mavericks
   end
 
   depends_on "graphicsmagick"
