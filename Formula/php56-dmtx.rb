2807885ebde0d1c985bb6678a3ea8a3b535278b8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 08:41:50 +0000

    php56-dmtx: update 0.0.3 bottle.

--- Formula/php56-dmtx.rb
+++ Formula/php56-dmtx.rb
@@ -10,10 +10,10 @@ class Php56Dmtx < AbstractPhp56Extension
   head "https://github.com/maZahaca/php-dmtx.git"
 
   bottle do
-    revision 1
-    sha256 "1b2f05953a0eecb0f622bc268d4ea111cb6d659d6062b58a5828e1beccc6323f" => :el_capitan
-    sha256 "cff6d0ead77e5b6f3089663f5b2bdb783a09ed404eb4af94a5e8cd8da9ccf9cc" => :yosemite
-    sha256 "f56f8aab243fb6f01d614b0b680e57295a9b35122e3746c6aa6ebb4c35173dd0" => :mavericks
+    revision 2
+    sha256 "2d2e86830015eab368a2660c11e153e5c233ed0999eb8854411cd6d8dddd5433" => :el_capitan
+    sha256 "9dd71cfbd5c36ef613ba364811fc4822318f1e171e78e73eacb3dea8a13ad78a" => :yosemite
+    sha256 "9923a287f831b167c7cd28912e634654bebd15b6baadac3dd66f0a7c4fb1ecdb" => :mavericks
   end
 
   depends_on "libdmtx"
