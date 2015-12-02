f5e4ff5b026ea7c9971baa8890a52a91e49c7232
Andreas Braun <git@alcaeus.org>
2015-12-02 07:46:08 +0100

    Add description for binpack formula
    Closes #2561.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php53-binpack.rb
+++ Formula/php53-binpack.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php53Binpack < AbstractPhp53Extension
   init
+  desc "The php implementation for BINPACK"
   homepage "https://pecl.php.net/package/binpack"
   url "https://pecl.php.net/get/binpack-1.0.1.tgz"
   sha256 "70617a721df4f8c52b5b1b214c11e464f8c36915cc19a850f24fed500c61b297"
