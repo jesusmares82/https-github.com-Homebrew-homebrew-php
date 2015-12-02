719fccb76c100c41f643ca83977bba2f16023fd8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 11:10:46 +0000

    php53-memcache: update 2.2.7 bottle.

--- Formula/php53-memcache.rb
+++ Formula/php53-memcache.rb
@@ -10,9 +10,10 @@ class Php53Memcache < AbstractPhp53Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9cae8ae4341f72794a0be67c5f74d12d5bb8fdb1107bcfa27f92e24794d7ad88" => :el_capitan
-    sha256 "ffe67ac36bbbb17b1411700d423dbbdf11fefba9d4020b064deca87063d9a009" => :yosemite
-    sha256 "02949e591a1a466e6d62f4972fc8d86b28dfbb23050888a652265f713ebf004a" => :mavericks
+    revision 1
+    sha256 "ee0275709afede9ec43bdd7bb7940d7ee0895aeda56e09048d6c5b4332ff576d" => :el_capitan
+    sha256 "26ed890171b49b8eeb7f07762f423b7f37da73bdc025561574a00b1e68e05ee8" => :yosemite
+    sha256 "735c46df7104f9e7b0baebeba879092c0aa70e1829a8ad704400ba6da42ecf7e" => :mavericks
   end
 
   devel do
