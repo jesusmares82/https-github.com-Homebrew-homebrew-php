00a5610ba87c8783c22872451007710a021dfaab
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 08:54:37 +0000

    php54-gearman: update 1.1.2 bottle.

--- Formula/php54-gearman.rb
+++ Formula/php54-gearman.rb
@@ -9,9 +9,10 @@ class Php54Gearman < AbstractPhp54Extension
   head "https://svn.php.net/repository/pecl/gearman/trunk/"
 
   bottle do
-    sha256 "ae66a7bcafaa90f08b95c0cf0a4e8b16b52d6c945c87c3560eec335290b0a07b" => :el_capitan
-    sha256 "4320907d8b8ef19ee8b246315a80648895f4d5d4ba12f307c4bc852efe03a9b4" => :yosemite
-    sha256 "bf05a990d9c0f218e31d771f25b711dc84ef19dde2d302d0f4245c74048e989b" => :mavericks
+    revision 1
+    sha256 "6034ba46dd864cf87e6ee135258d2e9b41f798015001d3f48257412d0e045c34" => :el_capitan
+    sha256 "900d7fe22517c3e594ede4d00ab57ca1608a4f4c3b3cee7cdb66a4a76b1c2ec9" => :yosemite
+    sha256 "70b58b076fecedca9249363d3e48e7f303e3eb6e8f778a3bc8d37347afc7ebd5" => :mavericks
   end
 
   depends_on "gearman"
