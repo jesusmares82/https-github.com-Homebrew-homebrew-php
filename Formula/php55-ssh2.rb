5458d8769924b13e80720f5c07fca6b9d730f042
Andreas Braun <git@alcaeus.org>
2015-12-02 08:00:57 +0100

    Add description for ssh2 formula
    Closes #2612.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php55-ssh2.rb
+++ Formula/php55-ssh2.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php55Ssh2 < AbstractPhp55Extension
   init
+  desc "Provides bindings to the functions of libssh2 which implements the SSH2 protocol."
   homepage "https://pecl.php.net/package/ssh2"
   url "https://pecl.php.net/get/ssh2-0.12.tgz"
   sha256 "600c82d2393acf3642f19914f06a7afea57ee05cb8c10e8a5510b32188b97f99"
