7caa2daefe1ba30b9b6d2aade1e903c41ccbaa54
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 07:29:51 +0000

    php56-amqp: add 1.6.0beta3 bottle.

--- Formula/php56-amqp.rb
+++ Formula/php56-amqp.rb
@@ -9,6 +9,13 @@ class Php56Amqp < AbstractPhp56Extension
   version "1.6.0beta3"
   head "https://github.com/pdezwart/php-amqp.git"
 
+  bottle do
+    cellar :any
+    sha256 "7448af4c667c3f8c5a8cf06c047618158382dba7e9d7247ec065fc33fc7230dc" => :el_capitan
+    sha256 "058cfb0c8e156d9b3fa57226192d2a47784ebde641bdb7559e95cee673eff03e" => :yosemite
+    sha256 "4bc673ad818e510322270f6cb85f8880878634614c56f8da6e6428df48520750" => :mavericks
+  end
+
   depends_on "rabbitmq-c"
 
   def install
