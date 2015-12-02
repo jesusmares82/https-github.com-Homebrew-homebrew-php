b6a2eff7072fbe66e78dbb9573b7df19d7223615
Andreas Braun <git@alcaeus.org>
2015-12-02 07:49:19 +0100

    Add description for lz4 formula
    Closes #2582.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php53-lz4.rb
+++ Formula/php53-lz4.rb
@@ -2,7 +2,8 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php53Lz4 < AbstractPhp53Extension
   init
-  homepage "https://code.google.com/p/lz4/"
+  desc "Extremely Fast Compression algorithm."
+  homepage "https://cyan4973.github.io/lz4/"
   url "https://github.com/kjdev/php-ext-lz4/archive/0.2.2.tar.gz"
   sha256 "9e37b1ca39013dacd392e31a0f037f9adf2b6f710a733166b0d0168f23f99c3a"
 
