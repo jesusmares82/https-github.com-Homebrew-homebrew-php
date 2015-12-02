21679ffadf8ec566b26fb212aade4168c5f0a369
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 08:28:02 +0000

    php53-dbase: update 5.1.0 bottle.

--- Formula/php53-dbase.rb
+++ Formula/php53-dbase.rb
@@ -10,9 +10,10 @@ class Php53Dbase < AbstractPhp53Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ed205bcc1df9594e244adcf0c680f989af341f849efb9a2d156715e97d22b1b8" => :el_capitan
-    sha256 "d5511e20aa859fdd1033ac0ec1dd1329d07558429b6278b70aff74a83beecec0" => :yosemite
-    sha256 "c79897e83521e2f869f1ed42dfd2e938031d231f48ba9030ba80711a71c6bcc8" => :mavericks
+    revision 1
+    sha256 "c1820be47e469d8683c1fa4ad2f61f2399ec53c5b7e51d4ca7ec21faecc6ebfd" => :el_capitan
+    sha256 "ba763e39af1dbf3a25842d857f9f399cbd88889cb6cb22a50edf4b37dd198e2b" => :yosemite
+    sha256 "97f7f6642e6fca647fffa989ca14967b096d6b87783a6dd88a0459bd2633c666" => :mavericks
   end
 
   def install
