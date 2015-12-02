e81e5ffb90477231606a52bdcab9b2c7976779b3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 10:40:58 +0000

    php53-mcrypt: add 5.3.29 bottle.

--- Formula/php53-mcrypt.rb
+++ Formula/php53-mcrypt.rb
@@ -4,6 +4,12 @@ class Php53Mcrypt < AbstractPhp53Extension
   init
   desc "An interface to the mcrypt library"
   homepage "http://php.net/manual/en/book.mcrypt.php"
+  bottle do
+    sha256 "f9e5929632284e8e9111468c00420cce5dde3d087a06b436ac8fe399c00aca36" => :el_capitan
+    sha256 "0deb354896fc635a90f8291e6d8e975bfa25a2a5d67d74ba0e858d96067cacb4" => :yosemite
+    sha256 "a4d9eb7f48f167a388754f2cd38d18b497051017c098c85bfebdd7ac8ce53797" => :mavericks
+  end
+
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
   version PHP_VERSION
