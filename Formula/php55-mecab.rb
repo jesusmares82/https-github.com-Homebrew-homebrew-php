ee50e9178e407984d8668d015f0789dfed860892
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 10:46:52 +0000

    php55-mecab: update 0.5.0 bottle.

--- Formula/php55-mecab.rb
+++ Formula/php55-mecab.rb
@@ -9,9 +9,10 @@ class Php55Mecab < AbstractPhp55Extension
   head "https://github.com/rsky/php-mecab.git"
 
   bottle do
-    sha256 "c74261246fe506e07474962951520a2109f25ce5e91821399b58f9922ff18a07" => :el_capitan
-    sha256 "8560ff34263f8d3ab4cad0b7795c09ec0404514de453c7ef2b77119b9ed7a305" => :yosemite
-    sha256 "de9a8ca52e544891286e132f11efec686ea4deb9194d25b206053ed2702607f3" => :mavericks
+    revision 1
+    sha256 "3706f72fb90857a2d74cba1980b5098adf60283f30b13b6cfcd772397cacd756" => :el_capitan
+    sha256 "7448e3eaa6a6a543192106bc411f6ecaf8a0e39b1d9955a8369f9927d6388836" => :yosemite
+    sha256 "b67d78447f01056948824f2e489b160d9dc64d35feab01f0288c478673a00897" => :mavericks
   end
 
   depends_on "mecab"
