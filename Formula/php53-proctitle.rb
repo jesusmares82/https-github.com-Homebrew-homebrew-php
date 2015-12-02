3d56601535351639afd87be8f4fe071c85e8e83a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 12:13:28 +0000

    php53-proctitle: update 0.1.2 bottle.

--- Formula/php53-proctitle.rb
+++ Formula/php53-proctitle.rb
@@ -10,9 +10,10 @@ class Php53Proctitle < AbstractPhp53Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "db99f49172ec7a61324cc5c7f3c79404701508b243150011e567b35d96d5e20a" => :el_capitan
-    sha256 "196a39c3310209fce1d594a74a9c760336119a006e6265af4faa9dd8544aae0d" => :yosemite
-    sha256 "9348175965961a0269294585682d210feb01e5095f2d59f6d32b865fa9091a74" => :mavericks
+    revision 1
+    sha256 "626e491889b57d14a696f3417f3b54ee4b7b7883d3f7fd945fb7cf1a02949805" => :el_capitan
+    sha256 "2d88ea8743b6c39b37eeeff18290f52a4c37dbe805e22b58d5d3200005197970" => :yosemite
+    sha256 "2454f2fdab1486ed58bec4f8fbf6f2ec5d66a3e87afe3454548204c8b66f0a58" => :mavericks
   end
 
   def install
