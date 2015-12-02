357d8064362659c964d13ef8581d98c711eebd9d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 12:32:26 +0000

    php53-pspell: add 5.3.29 bottle.

--- Formula/php53-pspell.rb
+++ Formula/php53-pspell.rb
@@ -4,6 +4,12 @@ class Php53Pspell < AbstractPhp53Extension
   init
   desc "Extension to check the spelling"
   homepage "http://php.net/manual/en/book.pspell.php"
+  bottle do
+    sha256 "5b34ccda27538ad3ff37c662b59f7cd245f4d288199421553fc094ad7dc7a207" => :el_capitan
+    sha256 "473ac749f0dc97ce2710dca2349cbbf8e2ff16969e1897216e628d8a0ddc544c" => :yosemite
+    sha256 "a09f194f50e8aa10734ebea75fccca84f76328689098a47002254dc21a0d4d9b" => :mavericks
+  end
+
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
   version PHP_VERSION
