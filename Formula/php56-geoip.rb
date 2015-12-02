df01964dce161688d93bb7e7f4f12d710e5dc4b6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-02 09:00:40 +0000

    php56-geoip: update 1.1.0 bottle.

--- Formula/php56-geoip.rb
+++ Formula/php56-geoip.rb
@@ -9,10 +9,10 @@ class Php56Geoip < AbstractPhp56Extension
   head "https://svn.php.net/repository/pecl/geoip/trunk/"
 
   bottle do
-    revision 1
-    sha256 "bf1d6f7ef9fe26c1598e44c24d9bf771d3b5ffe5f8ab22f7dbaeb6c7f4e8dd48" => :el_capitan
-    sha256 "f940c414c46cb8ce9ec1f0f2f0f83c001719e37bd7490742df2edb6201d5c35e" => :yosemite
-    sha256 "838f7ef86e987f79d0a16c2b1b851e8bab567310263669723ba2dcc5c86ec131" => :mavericks
+    revision 2
+    sha256 "35d973a2d4c3708f146d87494956cce5d009da76a3b86ec8d644525edb7933d3" => :el_capitan
+    sha256 "685ae7510fbe6a0fd98d75fd1c42f59d3cceeedf86eca4853cdbdb640b18f3c2" => :yosemite
+    sha256 "649e3c1ae8a69ed266eafd3dae740ef60f915986a1c74d77ff4aaac847d2d153" => :mavericks
   end
 
   depends_on "geoip"
