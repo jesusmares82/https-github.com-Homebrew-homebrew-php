898538a74b219f4e2c44a4860ddbcbf2ed0e9063
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-04 02:03:54 +0000

    php53-solr: update 2.3.0 bottle.

--- Formula/php53-solr.rb
+++ Formula/php53-solr.rb
@@ -9,6 +9,10 @@ class Php53Solr < AbstractPhp53Extension
   head "https://svn.apache.org/repos/asf/lucene/dev/trunk/"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "391ac946647d34b5837cb1ab8772394dd34d2e1d562e7e65bcef9cf68f342c1d" => :el_capitan
+    sha256 "21fe87bcf754be09f8aa8729159d8f61ec2d3b5c1be4f6fd9d37b7b0cbd70971" => :yosemite
+    sha256 "10d3b51043eeb221d420ef42048d924c8a6c672cd3c663317e1d651ec4ea0e0c" => :mavericks
   end
 
   def install
