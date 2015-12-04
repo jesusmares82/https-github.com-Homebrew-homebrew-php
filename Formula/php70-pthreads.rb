6718ef16f04953a830a298f1e3acaeb30dfaac46
Andy Blyler <andy@blyler.cc>
2015-12-03 20:29:35 -0500

    php70-pthreads 3.1.4
    Closes #2626.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php70-pthreads.rb
+++ Formula/php70-pthreads.rb
@@ -4,16 +4,11 @@ class Php70Pthreads < AbstractPhp70Extension
   init ["with-thread-safety"]
   desc "Threading API"
   homepage "https://pecl.php.net/package/pthreads"
-  url "https://pecl.php.net/get/pthreads-3.0.8.tgz"
-  sha256 "44989aa3d070969f0e1660a7cd609b2cb6c17942a6d9b5f680935df2c772df1b"
+  url "https://pecl.php.net/get/pthreads-3.1.4.tgz"
+  sha256 "2d13304bad6829a5e43d91bad8d234b03cd5f9acbbc1134c5a4c04e827b5076c"
   head "https://github.com/krakjoe/pthreads.git"
 
   bottle do
-    cellar :any_skip_relocation
-    revision 2
-    sha256 "9057583b9ba0838c80b7802f4fdb3b902fc263a1eb86e459fe9da68acfde9025" => :el_capitan
-    sha256 "a82c8b45bb60bda76080daaa5e6d3927540536ae94dcfc10581f5fe2efc499cb" => :yosemite
-    sha256 "0860a47a006b5cc299967da42928eb18bb162321ca4444a2dc85324e1a1f2050" => :mavericks
   end
 
   def install
