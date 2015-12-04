3ef6743456f0150b54e0cfe6d781787578d9a887
Andy Blyler <andy@blyler.cc>
2015-12-03 20:18:49 -0500

    php70-apcu 5.1.0
    Closes #2624.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php70-apcu.rb
@@ -0,0 +1,45 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Apcu < AbstractPhp70Extension
+  init
+  desc "APC User Cache"
+  homepage "https://pecl.php.net/package/apcu"
+  url "https://pecl.php.net/get/apcu-5.1.0.tgz"
+  sha256 "51c285a29c544e50cce6ceec83ba9482e46650ac9e87df7e24704c5c6484449a"
+  head "https://github.com/krakjoe/apcu.git"
+
+  bottle do
+  end
+
+  option "with-apc-bc", "Whether APCu should provide APC full compatibility support"
+  depends_on "pcre"
+
+  def install
+    Dir.chdir "apcu-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    args = []
+    args << "--enable-apcu"
+    args << "--enable-apc-bc" if build.with? "apc-bc"
+
+    safe_phpize
+
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          *args
+    system "make"
+    prefix.install "modules/apcu.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+      apc.enabled=1
+      apc.shm_size=64M
+      apc.ttl=7200
+      apc.mmap_file_mask=/tmp/apc.XXXXXX
+      apc.enable_cli=1
+    EOS
+  end
+end
