070b1ab29f51bd0f50836130399e93f4e3e04fd5
Andreas Braun <git@alcaeus.org>
2015-12-06 17:19:07 +0100

    Add description for yp formula
    Closes #2650.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-yp.rb
+++ Formula/php56-yp.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php56Yp < AbstractPhp56Extension
   init
+  desc "YP/NIS functions"
   homepage "https://pecl.php.net/package/yp"
   url "https://pecl.php.net/get/yp-1.0.1.tgz"
   sha256 "097fc6953c8faaf748acb34bb0c11ca81672f46fc19cd48f8a6c7da6714fa468"
