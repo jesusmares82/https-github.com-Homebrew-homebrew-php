d469d2da1406b7b5a74b377998ff55338af287eb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-06 20:04:28 +0000

    php70-pthreads: update 3.1.5 bottle.

--- Formula/php70-pthreads.rb
+++ Formula/php70-pthreads.rb
@@ -10,9 +10,9 @@ class Php70Pthreads < AbstractPhp70Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a0ff43376fcd94d2b45a978ef9d9d32c1277763393d7f9afc8179d424e5c867c" => :el_capitan
-    sha256 "f2d037cc45ff604335b401da01c6a15d5816d8e45863e8b5e5945ea50a30b29a" => :yosemite
-    sha256 "ccb2ebaa4244dc877511030547398f3682be964e3081be1142134b9ec418a94a" => :mavericks
+    sha256 "c300759ad03dc7f4bfe7ba02f72e03a0b46703c4cb649838678c79be4d48fb81" => :el_capitan
+    sha256 "045079bb15949903de35067b9395986977f86ece0511615fa4b79e4cd9ba6aec" => :yosemite
+    sha256 "5c707ef6404e0ca02667fb61ebb4a0f1e33d6943bdc5949e5c31bd72a797476f" => :mavericks
   end
 
   def install
