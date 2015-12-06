a081f62b7ee901d8015863de01ae6df2bd04aba8
Andreas Braun <git@alcaeus.org>
2015-12-06 17:18:52 +0100

    Add description for yac formula
    Closes #2646.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php55-yac.rb
+++ Formula/php55-yac.rb
@@ -2,6 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php55Yac < AbstractPhp55Extension
   init
+  desc "A fast shared memory user data cache for PHP"
   homepage "https://github.com/laruence/yac"
   url "https://github.com/laruence/yac.git", :branch => "master"
   head "https://github.com/laruence/yac.git"
