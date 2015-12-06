17417ebf1e90e765f34f1a98f097ed3e70b955a7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-06 20:07:27 +0000

    php70-v8js: update 1.0.0 bottle.

--- Formula/php70-v8js.rb
+++ Formula/php70-v8js.rb
@@ -8,6 +8,10 @@ class Php70V8js < AbstractPhp70Extension
   sha256 "f44424e1bd9aaf11caee1ec1baf4bd46e71afd832a0b3411d2f3dfaf1bef5377"
 
   bottle do
+    cellar :any
+    sha256 "befbefc9422b89715fb9e684cc2c6201003106fa96534514123fc896fac29264" => :el_capitan
+    sha256 "1d81c7c93b77569d3b7efffdc48353b4e79a1f5de82e4b7b8c7e442c52bffc00" => :yosemite
+    sha256 "ae03a50b337ad7b9486426bbc22bc24a4b3b07272fb6d5a2a2bf27ca2b98c51c" => :mavericks
   end
 
   depends_on "v8"
