33c3c4e5471aca17b92982eb23eb64c972b81a9b
Andy Blyler <andy@blyler.cc>
2015-12-06 12:46:38 -0500

    Add php70-imagick
    Closes #2655.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php70-imagick.rb
@@ -0,0 +1,30 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Imagick < AbstractPhp70Extension
+  init
+  desc "Provides a wrapper to the ImageMagick library."
+  homepage "https://pecl.php.net/package/imagick"
+  url "https://pecl.php.net/get/imagick-3.4.0RC2.tgz"
+  sha256 "5c9391d673f97b11c42bc8b05c4a096d659f1d7d38890d43fe7447fcf5f629d2"
+  head "https://github.com/mkoppanen/imagick.git"
+
+  bottle do
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "imagemagick"
+
+  def install
+    Dir.chdir "imagick-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-imagick=#{Formula["imagemagick"].opt_prefix}"
+    system "make"
+    prefix.install "modules/imagick.so"
+    write_config_file if build.with? "config-file"
+  end
+end
