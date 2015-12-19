27cf5fa0b6da48d1c5f7cf40334fa2c109831bd0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-19 06:04:29 +0000

    php70-pspell: update 7.0.1 bottle.

--- Formula/php70-pspell.rb
+++ Formula/php70-pspell.rb
@@ -5,6 +5,10 @@ class Php70Pspell < AbstractPhp70Extension
   desc "Extension to check the spelling"
   homepage "http://php.net/manual/en/book.pspell.php"
   bottle do
+    revision 9
+    sha256 "eef72d9a8806056a1bfae8f20ff5de5567d1779f5fd70ff3c8c6db84a72e6b81" => :el_capitan
+    sha256 "102b64c420b985889944bcef681991dc55effc428a3d0ace2f059987ba85c789" => :yosemite
+    sha256 "805681724430951a62b0df74a622a6aa5457de12692c554f9a5633778076a96f" => :mavericks
   end
 
   url PHP_SRC_TARBALL
