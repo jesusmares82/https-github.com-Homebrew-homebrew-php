0a32318601224cc715318a462c3c2fdad6b14f5b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-19 05:10:01 +0000

    php70-pcntl: update 7.0.1 bottle.

--- Formula/php70-pcntl.rb
+++ Formula/php70-pcntl.rb
@@ -5,6 +5,11 @@ class Php70Pcntl < AbstractPhp70Extension
   desc "Process Control support"
   homepage "http://php.net/manual/en/book.pcntl.php"
   bottle do
+    cellar :any_skip_relocation
+    revision 9
+    sha256 "cdb203af7c12cad63398f00cac309bad49792705f930a933994b971d5f962a31" => :el_capitan
+    sha256 "2e5a919bf765d6eec0d4d458187d0766ce49d7878f90bf6f6d58b9c5b21be570" => :yosemite
+    sha256 "66a44d080e311bf146871a0a8a089dc4af02119db04cfdd766007f36b0ab69ca" => :mavericks
   end
 
   url PHP_SRC_TARBALL
