9923f880103179fec4973638129b19494f0d0dab
n0ts <me@n0ts.org>
2015-12-31 11:52:31 +0900

    Add php70-mecab
    Closes #2713.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php70-mecab.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Mecab < AbstractPhp70Extension
+  init
+  desc "MeCab binding for PHP"
+  homepage "https://github.com/rsky/php-mecab"
+  url "https://github.com/rsky/php-mecab/archive/v0.6.0.tar.gz"
+  sha256 "8ec57164dd208f700ca16853f07fb62f111e16b025ca1ab5d4100aaaa9aa7c58"
+  head "https://github.com/rsky/php-mecab.git"
+
+  depends_on "mecab"
+
+  def install
+    Dir.chdir "mecab"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-mecab=#{HOMEBREW_PREFIX}/opt/mecab",
+                          phpconfig
+    system "make"
+    prefix.install "modules/mecab.so"
+    write_config_file if build.with? "config-file"
+  end
+end
