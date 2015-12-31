1bad19fc7b0e51a2df4ce2742063d8098adcb937
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-31 05:09:44 +0000

    php70-mecab: add 0.6.0 bottle.

--- Formula/php70-mecab.rb
+++ Formula/php70-mecab.rb
@@ -8,6 +8,12 @@ class Php70Mecab < AbstractPhp70Extension
   sha256 "8ec57164dd208f700ca16853f07fb62f111e16b025ca1ab5d4100aaaa9aa7c58"
   head "https://github.com/rsky/php-mecab.git"
 
+  bottle do
+    sha256 "45b9fa2ae008caa06eae9ad14ad9bc339ed675473d9cfc4fbe7900bc8783ca36" => :el_capitan
+    sha256 "1fa51ae37de43777994ae8cf5d1c7f83790567e5e0fb57088b7820366121c698" => :yosemite
+    sha256 "0bf0217e03c7b58f94b78a7f081945537469cd35129c0bd8560b2d3eab837a51" => :mavericks
+  end
+
   depends_on "mecab"
 
   def install
