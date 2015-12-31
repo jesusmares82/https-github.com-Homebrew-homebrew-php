fd90d4832b6e2dd307ff450af8de710409d2de77
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-31 23:50:44 +0000

    php55-libsodium: update 0.1.3 bottle.

--- Formula/php55-libsodium.rb
+++ Formula/php55-libsodium.rb
@@ -9,6 +9,11 @@ class Php55Libsodium < AbstractPhp55Extension
   head "https://github.com/jedisct1/libsodium-php.git"
 
   bottle do
+    cellar :any
+    revision 2
+    sha256 "e67e176388d5c4b0a1a45967cdbf5d870ca739d18bd881e5ae8df2b49082f9a0" => :el_capitan
+    sha256 "f0c92ee7a2be32958913b1ce2c330f378401095e56cf4bba2c3011721c10b064" => :yosemite
+    sha256 "238835a87685ade190cacbdac0e6d6b870dc066963598ff6e5c0d38ee09f9df3" => :mavericks
   end
 
   depends_on "libsodium"
