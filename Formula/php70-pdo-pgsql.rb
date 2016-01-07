b2201e0fd2b72ad28b9a628cb6f5c0598eb89ee1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-07 21:12:26 +0000

    php70-pdo-pgsql: update 7.0.2 bottle.

--- Formula/php70-pdo-pgsql.rb
+++ Formula/php70-pdo-pgsql.rb
@@ -5,6 +5,11 @@ class Php70PdoPgsql < AbstractPhp70Extension
   desc "A unified PostgreSQL driver for PDO"
   homepage "https://github.com/php/php-src/tree/master/ext/pdo_pgsql"
   bottle do
+    cellar :any
+    revision 10
+    sha256 "b8d4c35cbcdd04643e2c03e772de40ea80e7fd549e915d559e5749f7671a5615" => :el_capitan
+    sha256 "6ff8f9d9bfba3dcdee92e3fddae08785e0fa156f2b3158f4b41fa59b944b5547" => :yosemite
+    sha256 "1795ae1a79fd4fd48f9690ac64ffdad8d214ab7b843ac1264cf6a0bf6dc199e1" => :mavericks
   end
 
   url PHP_SRC_TARBALL
