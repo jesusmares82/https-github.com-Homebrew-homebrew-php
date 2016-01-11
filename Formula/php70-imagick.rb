7412b3cb2050933bc5ca354c4eb497f136a00958
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-11 23:54:13 +0000

    php70-imagick: update 3.4.0RC4 bottle.

--- Formula/php70-imagick.rb
+++ Formula/php70-imagick.rb
@@ -9,6 +9,9 @@ class Php70Imagick < AbstractPhp70Extension
   head "https://github.com/mkoppanen/imagick.git"
 
   bottle do
+    sha256 "df85dd2754d5bd6b7d43efc332ec0a8c1d0cdd1893753ff00fcf39b5881e5be4" => :el_capitan
+    sha256 "c758aaa4ffc1b82fe4eafdff08cee973bba869b40545de38494db2edc01511b4" => :yosemite
+    sha256 "8f8fedb0f8397dcde66659821f533e7043ef509405c997f3e8a2185a1ba42f02" => :mavericks
   end
 
   depends_on "pkg-config" => :build
