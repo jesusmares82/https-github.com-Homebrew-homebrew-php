f79d2d3997ed653f8bd69656c1ca350de922015b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-23 13:13:55 +0000

    boris: add 1.0.10 bottle.

--- Formula/boris.rb
+++ Formula/boris.rb
@@ -7,6 +7,13 @@ class Boris < Formula
   sha256 "06eb9e8efe5ceac00f49ba32731c65047435ab09ff57bbbec56c8c728861a2ee"
   head "https://github.com/borisrepl/boris.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0d80a4137e4a5924d1bea6549d5b20f90e08c15b3e33bf2c57af8fa19efc6a53" => :el_capitan
+    sha256 "689ea4dd37b6a5019bbdb50f87e0ac9806d6a54aa318248aafa9949c45034b63" => :yosemite
+    sha256 "e1ebb95b4a0107582b5460a9f1a4d9043e82578461db46ffbced3302caa2d721" => :mavericks
+  end
+
   depends_on PhpMetaRequirement
   depends_on "composer" => :build
 
