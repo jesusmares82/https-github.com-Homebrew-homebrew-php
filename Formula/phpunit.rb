fa964c41d40c39634e37f0c06a3b79eae26d3f42
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-01-24 15:29:51 +0100

    Cleanup formula
    Closes #2811.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
-require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
-require File.expand_path("../../Requirements/phar-requirement", __FILE__)
+require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 
-class Phpunit < Formula
-  desc "PHPUnit is a programmer-oriented testing framework for PHP."
+class Phpunit < AbstractPhpPhar
+  init
+  desc "Programmer-oriented testing framework for PHP"
   homepage "http://phpunit.de"
   url "https://phar.phpunit.de/phpunit-5.1.0.phar"
   sha256 "83bf6154f7435f78495d6cbdce62db333dc86aa8032b3ed8370ed51fb351314d"
@@ -14,12 +14,8 @@ class Phpunit < Formula
     sha256 "5a911444dcee51d7e45fdc8483814cf785e9da05e19f4ca4e179f69f9f765a05" => :mavericks
   end
 
-  def install
-    libexec.install "phpunit-#{version}.phar"
-    sh = libexec + "phpunit"
-    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpunit-#{version}.phar $*")
-    chmod 0755, sh
-    bin.install_symlink sh
+  def phar_file
+    "phpunit-#{version}.phar"
   end
 
   test do
