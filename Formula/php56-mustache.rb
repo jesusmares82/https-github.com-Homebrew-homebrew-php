e814c5771b7e88d865d0a9f203385568c23b51e4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-29 21:09:01 +0000

    php56-mustache: update 0.7.0 bottle.

--- Formula/php56-mustache.rb
+++ Formula/php56-mustache.rb
@@ -9,6 +9,10 @@ class Php56Mustache < AbstractPhp56Extension
   head "https://github.com/jbboehr/php-mustache.git"
 
   bottle do
+    cellar :any
+    sha256 "c641699cfa7b17040410c867774f715b654e50b3876c0740219a1216b07b6330" => :el_capitan
+    sha256 "dec39fc5ac9c0421395e5fd3b7d5f618dc8d1def98fd8fb94a6d8fc4ad5099e1" => :yosemite
+    sha256 "954094a08eb7e808fa4f1d93de455327c720a50999202c75b0e8efd69ee5b154" => :mavericks
   end
 
   depends_on "libmustache"
