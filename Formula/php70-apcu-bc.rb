5c837a38758cc33bb077812d192f6e44a6b14e55
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-29 16:52:25 +0000

    php70-apcu-bc: add 1.0.1 bottle.

--- Formula/php70-apcu-bc.rb
+++ Formula/php70-apcu-bc.rb
@@ -8,6 +8,13 @@ class Php70ApcuBc < AbstractPhp70Extension
   sha256 "512674d891104d6da91811dbb89d28ab3faa356ee0ab4cbeae9bba9cf3e971cb"
   head "https://github.com/krakjoe/apcu-bc.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "90e7b9b4090376d7677438e2c094c09717e554e2f480837614a7b4e79b5ad1a0" => :el_capitan
+    sha256 "b54b4f1acc267d44a865dcc7d672f7593599c76b3888122bd383c079e7043c7a" => :yosemite
+    sha256 "7e2b62455334fcfb60f3750464709f8b0f68181764a65c2fac5bdaf92c839bba" => :mavericks
+  end
+
   depends_on "php70-apcu"
 
   def install
