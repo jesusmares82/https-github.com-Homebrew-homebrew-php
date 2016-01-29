0518bf9b15e34e1890595df2c6ab9ddfbe398077
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-29 08:52:08 +0000

    php70-hprose: add 1.6.4 bottle.

--- Formula/php70-hprose.rb
+++ Formula/php70-hprose.rb
@@ -8,6 +8,13 @@ class Php70Hprose < AbstractPhp70Extension
   sha256 "e3d96cc51cf442629e7298af153c55e4bd85d832de43f22eb5f0fabe49abacca"
   head "https://github.com/hprose/hprose-pecl.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4dc36f5ca3d6210f41f97029b960b12a4d998ccc6da97563955cf40e94848c07" => :el_capitan
+    sha256 "f271ee58bd5a90fa614d8da93762e664e28a7ef998d5961f07a5a2edd08294d7" => :yosemite
+    sha256 "334c63be492ab06201fa4160420de4de2694ed7c4bb5940810dd3505eb25f8bd" => :mavericks
+  end
+
   def install
     Dir.chdir "hprose-#{version}" unless build.head?
 
