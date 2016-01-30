aca38e476f81a01260844b423df237c42fae097f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-30 12:56:37 +0000

    php56-rdkafka: add 0.9.1 bottle.

--- Formula/php56-rdkafka.rb
+++ Formula/php56-rdkafka.rb
@@ -8,6 +8,12 @@ class Php56Rdkafka < AbstractPhp56Extension
   sha256 "59e87fffe14c80d60d8adee12b2887ca3815efd5ae1885da274d8882b4f6404d"
   head "https://github.com/arnaud-lb/php-rdkafka.git"
 
+  bottle do
+    cellar :any
+    sha256 "76d0695bc35be1563edf5713d01d0b5058c95b6009b940499a0a3a9797c31d29" => :el_capitan
+    sha256 "d5fabb1e8bc958a7d2fca524c0c80be5d3398e61b39ab9a2d6c7aebe47a90a35" => :yosemite
+  end
+
   depends_on "librdkafka"
 
   def install
