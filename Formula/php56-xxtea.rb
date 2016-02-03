a8eb146354a73a837a77925b6f3a013570c580cf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-03 10:31:00 +0000

    php56-xxtea: add 1.0.11 bottle.

--- Formula/php56-xxtea.rb
+++ Formula/php56-xxtea.rb
@@ -8,6 +8,12 @@ class Php56Xxtea < AbstractPhp56Extension
   sha256 "5b1e318d3e70b27ad017d125d09ba3cf7bb3859e11be864a7bc3ddba421108af"
   head "https://github.com/xxtea/xxtea-pecl.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8f5c940ff7cccbccc8241ceca9254b7f225ecc8c746064b11366402e704a34f9" => :el_capitan
+    sha256 "f19c2abfba757354e764271d5b43bf06c5f085dcb0a238cb219137b89c2cc6ad" => :yosemite
+  end
+
   def install
     Dir.chdir "xxtea-#{version}" unless build.head?
 
