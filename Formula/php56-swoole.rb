506e821633cf8c1f73da1dec7db9004370e3ca76
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-04 10:00:06 +0000

    php56-swoole: add 1.8.1 bottle.

--- Formula/php56-swoole.rb
+++ Formula/php56-swoole.rb
@@ -8,6 +8,12 @@ class Php56Swoole < AbstractPhp56Extension
   sha256 "d63c5b9339a86df43b0e3d82efd194ac6f7b25f601580a7abf9305e3a6afeb20"
   head "https://github.com/swoole/swoole-src.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4259801e506eea282778bb24d75b3478cdc184744501db4e0bd66ccc4388275b" => :el_capitan
+    sha256 "39b2c75f336c1479b884dbd025230cd8d45865ab482d0e843067b5d1244a1eff" => :yosemite
+  end
+
   def install
     Dir.chdir "swoole-#{version}" unless build.head?
 
