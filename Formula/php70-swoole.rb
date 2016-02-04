8e36f1185d741e1a3dcc0689a923478fa23f866e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-04 10:00:06 +0000

    php70-swoole: add 1.8.1 bottle.

--- Formula/php70-swoole.rb
+++ Formula/php70-swoole.rb
@@ -8,6 +8,13 @@ class Php70Swoole < AbstractPhp70Extension
   sha256 "d63c5b9339a86df43b0e3d82efd194ac6f7b25f601580a7abf9305e3a6afeb20"
   head "https://github.com/swoole/swoole-src.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1abd9e8969c1e7d6f7d041b64a6309a2fc48c46c4890b8d88a6e62967c3d9a16" => :el_capitan
+    sha256 "de09c35007ee1ab1794e1f406999ba7f5daee7fe30de6525367f913d5e3419ec" => :yosemite
+    sha256 "448655a86aa48dfd5c2154e5ae3554ecec30bc6cfc664389981cd11c875658de" => :mavericks
+  end
+
   def install
     Dir.chdir "swoole-#{version}" unless build.head?
 
