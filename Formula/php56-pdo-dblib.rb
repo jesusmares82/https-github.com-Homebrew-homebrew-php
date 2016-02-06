5aa42e36a755c65f41f7bd80807b68d9e0d99af8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-06 10:22:36 +0000

    php56-pdo-dblib: update 5.6.18 bottle.

--- Formula/php56-pdo-dblib.rb
+++ Formula/php56-pdo-dblib.rb
@@ -9,6 +9,10 @@ class Php56PdoDblib < AbstractPhp56Extension
   version PHP_VERSION
 
   bottle do
+    revision 6
+    sha256 "975e302a686476e7b4d3539a0c405a958525683c918f3f5d2b73b933453ba98e" => :el_capitan
+    sha256 "5ebe8399685c25beaf418664be1c45975e089ec6b447d7f26c4bafd1632c7bd9" => :yosemite
+    sha256 "767cb7e52d6e109a11ace10b2b957b634fa49155083fb2f0ff4eab43d6dfb9de" => :mavericks
   end
 
   depends_on "freetds"
