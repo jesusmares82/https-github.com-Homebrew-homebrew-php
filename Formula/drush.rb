35971c04a79dd2568a443f42d5c446382022b87d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-06 00:37:47 +0000

    drush: update 8.0.3 bottle.

--- Formula/drush.rb
+++ Formula/drush.rb
@@ -8,9 +8,8 @@ class Drush < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c3174d1b288e8f45e5e6c8be07191a3be36702d942a2876eef6b72bb260d29c3" => :el_capitan
-    sha256 "34f4cf2aa08363e3785bcacc33fb69b860da8f7d229b7e103263d3a07c7b22c0" => :yosemite
-    sha256 "5b96d66d0e8d03fe534b2d3616ccda12562271c35bcf9c6e8d9f5fb80aebb8e3" => :mavericks
+    sha256 "fa5aa7a2962a13ed1be70d3d343668129901abe83bc495c77a88ffc96062b985" => :el_capitan
+    sha256 "729b139674ae7ba2614ac4f78003a8eecbf8af79271304254935bf1c7f6e5691" => :yosemite
   end
 
   head do
