a1d2d41400efff0d9985830eaec35009fc01e343
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-06 01:56:01 +0000

    php56-snmp: update 5.6.18 bottle.

--- Formula/php56-snmp.rb
+++ Formula/php56-snmp.rb
@@ -9,6 +9,10 @@ class Php56Snmp < AbstractPhp56Extension
   version PHP_VERSION
 
   bottle do
+    revision 6
+    sha256 "758b4c4693671f50c0b27b3e37aa6b0ff23a8f935609e623f2816ef1c8076b4a" => :el_capitan
+    sha256 "adb29caacda60c6d3d7e0619eb85e496eff1a1b39cf28c5e691c3b559add608b" => :yosemite
+    sha256 "33d5aedb7b021a86f9efaca5d76f8f7df98a20a28ad9317c278a72db49ced732" => :mavericks
   end
 
   depends_on "net-snmp"
