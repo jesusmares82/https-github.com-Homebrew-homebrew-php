fe011da9e16bf423f7de4e1b42bf27add095a84a
Andy Blyler <andy@blyler.cc>
2016-02-16 12:05:59 -0500

    drupalconsole 0.10.11
    Closes #2879.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/drupalconsole.rb
+++ Formula/drupalconsole.rb
@@ -4,8 +4,8 @@ class Drupalconsole < AbstractPhpPhar
   init
   desc "CLI tool for Drupal 8"
   homepage "https://drupalconsole.com/"
-  url "https://github.com/hechoendrupal/DrupalConsole/releases/download/0.10.5/drupal.phar"
-  sha256 "3c9269acedf4064f7016859860bb5fa0394c96dd4e6a82339034d030d0806f81"
+  url "https://github.com/hechoendrupal/DrupalConsole/releases/download/0.10.11/drupal.phar"
+  sha256 "1ee7ddc61aae5122f63405599f50e442621d2a27817d5701024b25877bd2d552"
 
   bottle do
     cellar :any_skip_relocation
