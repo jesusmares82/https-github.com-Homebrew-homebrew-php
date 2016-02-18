cd97df1bd738dc43f69a553ff37cd14c94ad88ed
Andy Blyler <andy@blyler.cc>
2016-02-18 15:42:57 -0500

    phan: remove bottles

--- Formula/phan.rb
+++ Formula/phan.rb
@@ -6,10 +6,6 @@ class Phan < Formula
   head "https://github.com/etsy/phan.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "7aa2af6fec5dfcc403dba3dd94f385491281781addfc40d4500cc21c35d4a92d" => :el_capitan
-    sha256 "6cc25f87be942737994df42d1c68070e50ec7779d100e22e2090bfe13866ba13" => :yosemite
-    sha256 "f538e9d9a64cddd8b2ac8d86daf9fcad5f7620c964c97b3bf6e507da264a3b61" => :mavericks
   end
 
   depends_on "composer"
