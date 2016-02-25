f21508926f29600bcc167b3fb9a4a663522fb6d0
Andy Blyler <andy@blyler.cc>
2016-02-25 13:17:04 -0500

    php5*-ssh2: update head to point to php5 branch

--- Formula/php55-ssh2.rb
+++ Formula/php55-ssh2.rb
@@ -6,7 +6,7 @@ class Php55Ssh2 < AbstractPhp55Extension
   homepage "https://pecl.php.net/package/ssh2"
   url "https://pecl.php.net/get/ssh2-0.12.tgz"
   sha256 "600c82d2393acf3642f19914f06a7afea57ee05cb8c10e8a5510b32188b97f99"
-  head "https://svn.php.net/repository/pecl/ssh2/trunk/"
+  head "https://github.com/php/pecl-networking-ssh2.git", :branch => "php5"
 
   bottle do
     cellar :any
@@ -24,7 +24,8 @@ class Php55Ssh2 < AbstractPhp55Extension
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}",
-                          phpconfig
+                          phpconfig,
+                          "--with-ssh2=#{Formula["libssh2"].opt_prefix}"
     system "make"
     prefix.install "modules/ssh2.so"
     write_config_file if build.with? "config-file"
