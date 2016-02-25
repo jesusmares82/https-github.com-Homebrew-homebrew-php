d2b962c8a75947457b0d5b4ad61cf10bda4f6763
Andy Blyler <andy@blyler.cc>
2016-02-25 12:07:25 -0500

    php70-ssh2 HEAD (new formula)
    Closes #2898.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php70-ssh2.rb
@@ -0,0 +1,27 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Ssh2 < AbstractPhp70Extension
+  init
+  desc "Provides bindings to the functions of libssh2"
+  homepage "https://pecl.php.net/package/ssh2"
+  head "https://github.com/php/pecl-networking-ssh2.git"
+
+  bottle do
+  end
+
+  depends_on "libssh2"
+
+  def install
+    Dir.chdir "ssh2-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-ssh2=#{Formula["libssh2"].opt_prefix}"
+    system "make"
+    prefix.install "modules/ssh2.so"
+    write_config_file if build.with? "config-file"
+  end
+end
