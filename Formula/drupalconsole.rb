1335431f94336c94179dfa5edc7670399032f648
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-02-25 01:52:55 +0100

    drupalconsole 0.10.12
    Closes #2897.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/drupalconsole.rb
+++ Formula/drupalconsole.rb
@@ -4,8 +4,8 @@ class Drupalconsole < AbstractPhpPhar
   init
   desc "CLI tool for Drupal 8"
   homepage "https://drupalconsole.com/"
-  url "https://github.com/hechoendrupal/DrupalConsole/releases/download/0.10.11/drupal.phar"
-  sha256 "1ee7ddc61aae5122f63405599f50e442621d2a27817d5701024b25877bd2d552"
+  url "https://github.com/hechoendrupal/DrupalConsole/releases/download/0.10.12/drupal.phar"
+  sha256 "ac99afd5bcdc8f7dfe7c2e4fcd36f1f0809aa505490777ec5a05c930d8c2f3d5"
 
   bottle do
     cellar :any_skip_relocation
