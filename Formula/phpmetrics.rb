1f8999dd37ead643bb509162992df85cd4522c22
Denis Denisov <denji0k@gmail.com>
2016-03-01 01:05:56 +0200

    PhpMetrics 1.8.3
    Closes #2903.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/phpmetrics.rb
+++ Formula/phpmetrics.rb
@@ -3,10 +3,10 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 class Phpmetrics < AbstractPhpPhar
   init
   desc "Static analysis tool for PHP"
-  homepage "http://www.phpmetrics.org/"
-  url "https://github.com/Halleck45/PhpMetrics/raw/34cf0888e7225d743fc01b467fbbd8a1d54226a6/build/phpmetrics.phar"
-  version "1.4.1"
-  sha256 "77fd4f29f76bf3f8c623e415ee834692077e9b49bd340a0c896f58a9697fe9a3"
+  homepage "http://www.phpmetrics.org"
+  url "https://github.com/Halleck45/PhpMetrics/raw/v1.8.3/build/phpmetrics.phar"
+  version "1.8.3"
+  sha256 "454a4d1806bb44cdb2e8535d1a1db051389fd7c140daebce5a8ab2cd78a5fa7f"
 
   bottle do
     cellar :any_skip_relocation
