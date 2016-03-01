ffae29c0897427cc97438f59eaa5d5ef952e22ce
Walther Lalk <waltherlalk@gmail.com>
2016-03-01 10:01:06 +0200

    Add php7 event formula

---
+++ Formula/php70-event.rb
@@ -0,0 +1,33 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Event < AbstractPhp70Extension
+  init
+  desc "Provides interface to libevent library"
+  homepage "https://pecl.php.net/package/event"
+  url "https://pecl.php.net/get/event-2.0.0.tgz"
+  version "2.0.0"
+  sha256 "3f785022b60d8e8687825c89a314ef94b1dbbbcb9438c3e5b3b9d840d5119a1b"
+
+  depends_on "libevent"
+  depends_on "openssl"
+
+  def install
+    Dir.chdir "event-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    args = []
+    args << "--with-event-core"
+    args << "--with-event-extra"
+    args << "--enable-event-debug"
+    args << "--with-event-libevent-dir=#{Formula["libevent"].opt_prefix}"
+    args << "--with-event-openssl"
+    args << "--with-openssl-dir=#{Formula["openssl"].opt_prefix}"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/event.so"
+    write_config_file if build.with? "config-file"
+  end
+end
