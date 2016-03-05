e47f5fa891f6357f2c26d32a551491d46dabfaa5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-05 22:08:48 +0000

    php56-xdebug: add 2.4.0 bottle.

--- Formula/php56-xdebug.rb
+++ Formula/php56-xdebug.rb
@@ -8,6 +8,13 @@ class Php56Xdebug < AbstractPhp56Extension
   sha256 "3c4dcb2709d1653534e7cfaa546307041afd298ac48a3670183a12cfdb5eee05"
   head "https://github.com/xdebug/xdebug.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8cda23facc8d761daa07f2807c621fe4ca41c1ab7e326898e0aa90e841e34bb9" => :el_capitan
+    sha256 "d0fc25aa293255425cdd5580d962907d78e9c7bc6c74dd57f40798199192ecc6" => :yosemite
+    sha256 "8c7f3d45b2416cc07fe3becc2a72499c369dc8cb0832b9440336ef456fb979f3" => :mavericks
+  end
+
   def extension_type
     "zend_extension"
   end
