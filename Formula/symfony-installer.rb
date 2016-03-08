4a6e633cff3d47a2cf85d5c749cea33b61d06647
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-08 23:10:19 +0000

    symfony-installer: add 1.5.0 bottle.

--- Formula/symfony-installer.rb
+++ Formula/symfony-installer.rb
@@ -11,6 +11,13 @@ class SymfonyInstaller < Formula
     :revision => "74311cd7ffacba6a1246d5cd5fc1a6d90037fe61"
   head "https://github.com/symfony/symfony-installer.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c03be845c4c3d8e66cffdc370bc2f2ef151e74a7a0e04a785f87aed02e3ee6d1" => :el_capitan
+    sha256 "bf2e37808fe95eded99f12dbd1b0989dc2ba71abb73d314d7e1003a7f342b7a0" => :yosemite
+    sha256 "58f4f940a4110994e109430cc1c5cdaaba59a1a714f8df1d2f2c5599fc7699b0" => :mavericks
+  end
+
   depends_on PhpMetaRequirement
   depends_on "box" => :build
   depends_on "composer" => :build
