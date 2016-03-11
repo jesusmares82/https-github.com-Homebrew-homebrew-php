940c6902aee3b0650246b639c73846bc496d96e2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-11 13:14:50 +0000

    pickle: add 0.4.0 bottle.

--- Formula/pickle.rb
+++ Formula/pickle.rb
@@ -8,6 +8,13 @@ class Pickle < AbstractPhpPhar
   version "0.4.0"
   sha256 "d666ca81fde5ecb3881e603edd8f656fbd967cf98c82c2fd9c16fc64e0d4f9a5"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "180239d0570178a678c1a87f0129cfbedb8eec4f5ab06ef0438a3b2508940107" => :el_capitan
+    sha256 "e636aec9f97d5a6ec0dff9825eb0887244a622a96b281c0f133e6942783527aa" => :yosemite
+    sha256 "2cde74cec0aa6e0a8b27995f89e41b7c30bc6df09d9a3ed965db065afc62c3b3" => :mavericks
+  end
+
   test do
     system "pickle", "--version"
   end
