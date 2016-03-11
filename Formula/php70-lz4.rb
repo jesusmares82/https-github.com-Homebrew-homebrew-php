f3cf6a32a2f855745cbaf809c458f4dff29bbfcf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-11 05:08:47 +0000

    php70-lz4: add 0.2.3 bottle.

--- Formula/php70-lz4.rb
+++ Formula/php70-lz4.rb
@@ -8,6 +8,13 @@ class Php70Lz4 < AbstractPhp70Extension
   sha256 "f484c8229b3c5af1385ce11e4b37999702757bad9bdb2eab8bfe48fa3f159904"
   head "https://github.com/kjdev/php-ext-lz4.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f335c9f58c5621848c7d2c3f5729364fb2045204507ccdb50b82413249ad869d" => :el_capitan
+    sha256 "f1930b33464d83b322b1f48b10cd80376f2ae9109a1337189ed184d165bf67fe" => :yosemite
+    sha256 "f7cde47d11347815ab81d93e04ee357a80a76ede0e2335306f7cb7a7cd4088e7" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
