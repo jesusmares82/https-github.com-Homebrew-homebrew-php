01c5273c71b7d7a8e8b666c38e66bba075b58df6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-11 07:21:52 +0000

    php70-mongodb: update 1.1.4 bottle.

--- Formula/php70-mongodb.rb
+++ Formula/php70-mongodb.rb
@@ -9,9 +9,9 @@ class Php70Mongodb < AbstractPhp70Extension
   head "https://github.com/mongodb/mongo-php-driver.git"
 
   bottle do
-    sha256 "e7d7b1a25760aa1dcc6b81beea82646e09b8bd76874b8b2f7cb7500f0427a04e" => :el_capitan
-    sha256 "6b7025a2cc5833e36d6abb79d0da400acd49a4c9a7a88b68a3626f2827ff36b5" => :yosemite
-    sha256 "f5f7a4e94e9935c3c6c67124a2cf9fd396f552e1cf6bfff088b3ddca2cfa45df" => :mavericks
+    sha256 "150f0c63d5fd34c230eb49cd97e9a65e0d171c436f5406af3bc20b5fd1f3c6dd" => :el_capitan
+    sha256 "ae10e2bf3347f77869203d5c28a554b32d67c83a05a58c307d18fcedbab434ba" => :yosemite
+    sha256 "6ad3c5d659daf380a8d47a6cf1afb03024ebe814a6dcabb230ab6063c40afc7a" => :mavericks
   end
 
   depends_on "openssl"
