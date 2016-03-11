7841ac68c5bfa7b5bb13d1971594ca0d4923492a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-11 01:22:05 +0000

    box: update 2.7.1 bottle.

--- Formula/box.rb
+++ Formula/box.rb
@@ -9,9 +9,9 @@ class Box < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d5ce90623297ea68f87a3fd14fccd38a0b09ebf0c27a12e6f9e8177ea6016429" => :el_capitan
-    sha256 "35a5452f6c4479e6a6369f1da78e7513180ea6a7362d8e06583fc3652d3842a9" => :yosemite
-    sha256 "055fdb534cb834dc46e9b8dc311c0e22068d7791a71102fd669e95f7b02e310e" => :mavericks
+    sha256 "74922ea848058e797842b0f0ae52cbeaa91a81fb88d1369d75e1c953fef5cc70" => :el_capitan
+    sha256 "3185c19d31b296eeeba1aa1399f2da462f9e8706d607cb0c9580ab8e28d8c5e8" => :yosemite
+    sha256 "a8f13a129e5939af81d3d75a6de1ebfecd50466d00b60bbe26fea69585edf99a" => :mavericks
   end
 
   def phar_file
