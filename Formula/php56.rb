dfc97e9676a25a1cfecbde25e45ae30de3468b7b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-14 19:53:19 +0000

    php56: update 5.6.19 bottle.

--- Formula/php56.rb
+++ Formula/php56.rb
@@ -12,6 +12,10 @@ class Php56 < AbstractPhp
   head PHP_GITHUB_URL, :branch => PHP_BRANCH
 
   bottle do
+    revision 7
+    sha256 "1e7aecf50c257a57cd93937d19aef728f5f6a8cfb803afaf77c8b785bfc93229" => :el_capitan
+    sha256 "0b7236c6aff651e19b45ee23b0b72f4cb5d3c09e526bffb982331aaf9c5cdd25" => :yosemite
+    sha256 "c27a8b46d4d185099d1cb11ce60ce4fad29cca88aeb71d977f4b168b34e66919" => :mavericks
   end
 
   def install_args
