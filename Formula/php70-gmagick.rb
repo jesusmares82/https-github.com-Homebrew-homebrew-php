44baa967df18b1163985dffdb85c22c775560c87
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-14 20:53:46 +0000

    php70-gmagick: add 2.0.2RC2 bottle.

--- Formula/php70-gmagick.rb
+++ Formula/php70-gmagick.rb
@@ -7,6 +7,12 @@ class Php70Gmagick < AbstractPhp70Extension
   url "https://pecl.php.net/get/gmagick-2.0.2RC2.tgz"
   sha256 "7656bd9042c2aae1325b1cb49e55d9e8ec31b911decec33b6f176195ee1d515a"
 
+  bottle do
+    sha256 "3c109297742e0beb662ee0cb6252262cf570218229b3350ecb54325c205bda60" => :el_capitan
+    sha256 "2c49ce07ee9eda516b08975653bbfa44fcea4dac4ea9b059c94210a46be8b8cb" => :yosemite
+    sha256 "afef9524b1cd47e563bf5b4181da4d5590e9a12d262fa7842fc6094eff481e72" => :mavericks
+  end
+
   depends_on "graphicsmagick"
 
   def install
