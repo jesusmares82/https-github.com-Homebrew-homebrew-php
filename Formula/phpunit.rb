f1f242a105c2c8fa021a0aa69d4ddb1c3e0ee17a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-17 21:23:09 +0000

    phpunit: update 5.2.12 bottle.

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -9,9 +9,9 @@ class Phpunit < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "603e29d80781968be7b19bbfda0f2b80234b9b2b768708cb576965038fffa950" => :el_capitan
-    sha256 "14a199d0b0aad5038cdee4d19c088f91f9f5ef0a16346f52d15ee5bebcd925cc" => :yosemite
-    sha256 "54b94e68092b634bad8472a17fd78f8949c4938b432553b2ded5fc4d0316802d" => :mavericks
+    sha256 "b0a441f06ff78a5b1e67fb32157be5bfecb84df1c345d4d604c6a7663b4e7208" => :el_capitan
+    sha256 "64ebd210b2e0634063cd84e38c22f6c6ebf67eb4b8b7880a6cec1fa94cb00f26" => :yosemite
+    sha256 "fbd5a6b33777bfc0d8df3d39ee5aa29ea3b69a4ac4da70a930c7cd70ac852472" => :mavericks
   end
 
   def phar_file
