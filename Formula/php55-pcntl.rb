2957d18e738de73d7937459feee8ff484c108f73
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-01 06:09:04 +0100

    php55-pcntl: update 5.5.34 bottle.

--- Formula/php55-pcntl.rb
+++ Formula/php55-pcntl.rb
@@ -9,6 +9,10 @@ class Php55Pcntl < AbstractPhp55Extension
   version PHP_VERSION
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "17ee9987d4b3d12fc5693c28c06c639533e29fbf795a13f7c68a4c052dad9803" => :el_capitan
+    sha256 "e94eaeb9e639724442806ef2595ffb30e7b40c706b5c570a14d7a2c21f7b19c1" => :yosemite
+    sha256 "68086270a12684abe30152ff1a870fe7cb87cb352cd6aae666ac8ff36cd753eb" => :mavericks
   end
 
   def install
