49e322a60fee5466ecc573296c19e0f4b248cbb2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-01 08:12:13 +0100

    php55-intl: update 5.5.34 bottle.

--- Formula/php55-intl.rb
+++ Formula/php55-intl.rb
@@ -9,6 +9,9 @@ class Php55Intl < AbstractPhp55Extension
   version PHP_VERSION
 
   bottle do
+    sha256 "7a0d3ecedf27ab80fd5b5f62b7de6bb9b8397adb76f0ae173b7af3fe492759d9" => :el_capitan
+    sha256 "35bbdf90aa2b19ba1740a302e9af4f09109805b91b6bb05ad8999418fd73ab4d" => :yosemite
+    sha256 "7fb398d4c3afcbf4978ac29177770a1202c3a59790f4c22e700af8e60052a6f4" => :mavericks
   end
 
   depends_on "icu4c"
