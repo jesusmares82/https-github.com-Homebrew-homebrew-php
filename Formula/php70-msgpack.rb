19f83b2762192f8f29d4fb2b37dedc7b5d7dd4ff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-05 13:20:16 +0100

    php70-msgpack: add 2.0.1 bottle.

--- Formula/php70-msgpack.rb
+++ Formula/php70-msgpack.rb
@@ -8,6 +8,13 @@ class Php70Msgpack < AbstractPhp70Extension
   sha256 "d32aeef9af3be6135a06f29e28ec9f386cde9d90ad346a396d9ba8018a7044c6"
   head "https://github.com/msgpack/msgpack-php.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b203da6797526dba510146eed907dd650d0d0846b30845f2283f9a884e8a2565" => :el_capitan
+    sha256 "97a0872b19badfaf85120de4e6a65737311a25822c9779bcd104a99f82e96c66" => :yosemite
+    sha256 "d31728038e6c07783926f34ebfe48640bf1dd9ffc9d848c741b614e796c706a4" => :mavericks
+  end
+
   def install
     Dir.chdir "msgpack-#{version}" unless build.head?
 
