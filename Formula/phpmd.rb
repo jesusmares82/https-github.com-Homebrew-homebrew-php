c42ffed192608e9b073f2688e6221c0e24c932c3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 14:03:31 +0100

    phpmd: update 2.4.2 bottle.

--- Formula/phpmd.rb
+++ Formula/phpmd.rb
@@ -10,9 +10,9 @@ class Phpmd < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c9126a56f86d4650246698f14e4c36217cab86d131fbd948dcee40c30d3cbb55" => :el_capitan
-    sha256 "96d453bb29f2256c7b79fded8f995be30379d188cae05bdf1c904cdac1b03df1" => :yosemite
-    sha256 "01d1670856b0d400e2fbce0374b3ce97cd39fe30cd6643b731beda4b522abc2c" => :mavericks
+    sha256 "a8361d3d28fc1112a69a15ced34e84c9265d9c797f65aec1774c648ba1712138" => :el_capitan
+    sha256 "ed0d335b5091175f9ac30b5abc4f4421d4c4e52ce1356a775c27df7968945d1b" => :yosemite
+    sha256 "1cff56129e811173c2a502df3e638fb632bb3239c435306724f8763c597b4783" => :mavericks
   end
 
   test do
