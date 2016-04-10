47f3f7f0441734f6997e9440fa4972698569f5e7
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-04-10 14:00:16 +0200

    puli 1.0 Beta 10
    Closes #3083.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/puli.rb
+++ Formula/puli.rb
@@ -2,11 +2,11 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 
 class Puli < AbstractPhpPhar
   init
-  desc "Puli is a universal package system for PHP"
+  desc "Universal package system for PHP"
   homepage "http://puli.io"
-  url "https://github.com/puli/cli/releases/download/1.0.0-beta9/puli.phar"
-  version "1.0.0-beta9"
-  sha256 "6092944496f4cce2c0523ee58e8ad645394f8da15e2476f9c0eaa8c1f8264ef1"
+  url "https://github.com/puli/cli/releases/download/1.0.0-beta10/puli.phar"
+  version "1.0.0-beta10"
+  sha256 "9aa39070480e5faaf61fb8cb92530cf7fc92ed29e3873a0f79448d6812caef3b"
 
   bottle do
     cellar :any_skip_relocation
