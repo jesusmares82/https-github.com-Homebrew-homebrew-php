10c4eba705152b94609a42921b1621c5dc997a64
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-11 04:59:18 +0100

    php54-yaz: update 1.1.9_1 bottle.

--- Formula/php54-yaz.rb
+++ Formula/php54-yaz.rb
@@ -9,9 +9,9 @@ class Php54Yaz < AbstractPhp54Extension
   revision 1
 
   bottle do
-    sha256 "829b0b0e1ff54e92faf67b28646f63c199579fe8021051a925eeb9637aec5b6d" => :el_capitan
-    sha256 "f6cfd1fbda40ab321e79021eee0d4238bd1fe02a375879b34ab9e5bd176a1419" => :yosemite
-    sha256 "ce7e58bcee7e566c7e7de95c0d574c68b549fbaab476938531c5a6cc398a0739" => :mavericks
+    sha256 "59704b19c95435e6de9f9c51ace3253b423e3be6809f1f18dbca1e8c31bf6bd1" => :el_capitan
+    sha256 "062e28bd4475279d48db155c90f60171c665284f08373310e618fdc63aeeb05b" => :yosemite
+    sha256 "efb41289705af662e5d73b48eaf0c63d7ec2ec6a6e8f31c2e12cbc71ecaec571" => :mavericks
   end
 
   depends_on "yaz"
