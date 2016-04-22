e0f637074290bb953f8cd8d1b23ce5dfaf9154e4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-22 17:29:12 +0100

    php56-v8js: update 0.4.0_2 bottle.

--- Formula/php56-v8js.rb
+++ Formula/php56-v8js.rb
@@ -9,6 +9,10 @@ class Php56V8js < AbstractPhp56Extension
   revision 2
 
   bottle do
+    cellar :any
+    sha256 "fa08487bf6ac45f4bcf3ed06d40f98304aa0d77561125a5faf59f11f2a7a30cb" => :el_capitan
+    sha256 "88f2b94c8a591a1e077d8ffad93d1592df251d09ce35624acd02bfaa201909ce" => :yosemite
+    sha256 "88121f0bb0bbc8422df55751d018ea7750aa132b761f9b21a0959b1d62c8c25d" => :mavericks
   end
 
   depends_on "v8"
