b01bc1ea41825775b99be1325f2f1813be130924
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-22 17:29:12 +0100

    php54-v8js: update 0.4.0_2 bottle.

--- Formula/php54-v8js.rb
+++ Formula/php54-v8js.rb
@@ -9,6 +9,10 @@ class Php54V8js < AbstractPhp54Extension
   revision 2
 
   bottle do
+    cellar :any
+    sha256 "a704d12ce910f61d42b7ba893ce63b4bb796e928f116a8f3a3302e4ed86fd59d" => :el_capitan
+    sha256 "bdc283c1e6988766470847e9cc54d361b1119ba177047769bd81aa1dc049775a" => :yosemite
+    sha256 "200fd3a4b7ad96c71049e7b9d1ca1462df9b2bfa40efd22ffb35e1faf2f828ef" => :mavericks
   end
 
   depends_on "v8"
