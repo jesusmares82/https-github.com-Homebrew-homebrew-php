4430809e46ee85502d6d4e508b5a503ed7e49773
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-04-26 05:02:16 +0200

    Update php56 bottle
    Closes #3114.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56.rb
+++ Formula/php56.rb
@@ -12,6 +12,7 @@ class Php56 < AbstractPhp
   head PHP_GITHUB_URL, :branch => PHP_BRANCH
 
   bottle do
+    revision 2
     sha256 "45c09f65f96bd2e459fe5037e102a7bedb4bec617b664f89d1afb6010953338d" => :el_capitan
     sha256 "6075e1f9d1c9da3ffa52da2a6316134c90982b75f3e44303353544bde25aa399" => :yosemite
     sha256 "7ff18b4253b7c4e9467033bcec416902d74cfe3a9d741bd767b8d9a21a3bbc9c" => :mavericks
@@ -43,7 +44,3 @@ class Php56 < AbstractPhp
     "56"
   end
 end
-
-
-
-
