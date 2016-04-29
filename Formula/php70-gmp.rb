b2c872dd32c7b290b95f0e2a00d3cf35c4795000
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-29 20:01:50 +0100

    php70-gmp: update 7.0.6 bottle.

--- Formula/php70-gmp.rb
+++ Formula/php70-gmp.rb
@@ -5,6 +5,10 @@ class Php70Gmp < AbstractPhp70Extension
   desc "GMP core php extension"
   homepage "http://php.net/manual/en/book.gmp.php"
   bottle do
+    revision 13
+    sha256 "9eea3296da300eaae81b28121271324674082b524098c9495df0a93693257f93" => :el_capitan
+    sha256 "6eed2f3952033da80e7bdd43a2978b528e0f89eda238ceb323e9bcd2a0efbd40" => :yosemite
+    sha256 "d23d0a1aba2d77323b423db083aa9257f0d9ea79bad6fd9072e632105cf42bca" => :mavericks
   end
 
   url PHP_SRC_TARBALL
