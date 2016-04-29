6d64db417335e250af6b5e296a8ff7dd0a594a2f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-29 19:31:00 +0100

    php70-mcrypt: update 7.0.6 bottle.

--- Formula/php70-mcrypt.rb
+++ Formula/php70-mcrypt.rb
@@ -5,6 +5,10 @@ class Php70Mcrypt < AbstractPhp70Extension
   desc "Interface to the mcrypt library"
   homepage "http://php.net/manual/en/book.mcrypt.php"
   bottle do
+    revision 13
+    sha256 "6a41486ed3aedb622ccc4df5bd3afb0abbe8295b0bcfa4df7802fc5cf518aa4c" => :el_capitan
+    sha256 "dd8c8c003d2b211cf376d75d8aa44089aa4d010e3ea7bd99b6257c85c2cb2a9b" => :yosemite
+    sha256 "492212e01b09ce5dcdca0f475d305632311865d3d34b51b08f74fcd7b255db92" => :mavericks
   end
 
   url PHP_SRC_TARBALL
