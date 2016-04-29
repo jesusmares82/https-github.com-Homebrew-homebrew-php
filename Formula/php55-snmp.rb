50541af577e28ac8b28e84bd4ebdf919c71d7897
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-29 16:25:23 +0100

    php55-snmp: update 5.5.35 bottle.

--- Formula/php55-snmp.rb
+++ Formula/php55-snmp.rb
@@ -9,6 +9,10 @@ class Php55Snmp < AbstractPhp55Extension
   version PHP_VERSION
 
   bottle do
+    revision 6
+    sha256 "5d8ab06e702e366df56b52a6f9192275f2054083fd68049fb3da72802f90f6f9" => :el_capitan
+    sha256 "178888b63fb566b678daca54ef7fcfe2a51460efcb982f71edbd815246902b3e" => :yosemite
+    sha256 "c5d879a0166f195e04fe5e3a4b78fe404c2906a1e06b0c3bf13195b08ed370b6" => :mavericks
   end
 
   depends_on "net-snmp"
