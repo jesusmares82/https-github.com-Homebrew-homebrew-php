03e3b7293034fa74f524d865d649702e886f0d17
Tom Kay <oridan82@gmail.com>
2016-05-16 17:40:26 +0100

    Modify composer phar wrapper to disable xdebug
    Stops composer from running with XDebug enabled, improving performance.
    
    Closes #3174.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -8,6 +8,20 @@ class Composer < AbstractPhpPhar
   sha256 "7f26efee06de5a1a061b6b1e330f5acc9ee69976d1551118c45b21f358cbc332"
   head "https://getcomposer.org/composer.phar"
 
+  def phar_wrapper
+    <<-EOS.undent
+      #!/usr/bin/env bash
+
+      TEMP_PATH="$( mktemp -t php-no-debug )"
+      FILES="$( /usr/bin/env php -r 'echo php_ini_loaded_file() . "\n" . str_replace(",","", php_ini_scanned_files());' )"
+      cat $FILES | sed '/xdebug/d' > "$TEMP_PATH"
+
+      /usr/bin/env php -n -c "$TEMP_PATH" -d detect_unicode=Off #{libexec}/#{@real_phar_file} "$@"
+
+      rm -f "$TEMP_PATH"
+    EOS
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "dd4a75805b9d1ca2dfa6c19983886f4468da39c18f21f315e588719c53f2216e" => :el_capitan
