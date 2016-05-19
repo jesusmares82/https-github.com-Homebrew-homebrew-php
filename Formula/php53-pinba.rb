5b5c4b418db7df88cbc0f3fd23b73189eb959b44
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-20 00:16:16 +0100

    php53-pinba: update 1.1.0-dev.7e7cd25 bottle.

--- Formula/php53-pinba.rb
+++ Formula/php53-pinba.rb
@@ -11,9 +11,9 @@ class Php53Pinba < AbstractPhp53Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dd5a7df7ab2a79b75eb368bf8ff46e55f394b2eac8523a3af05eb11ee286748f" => :yosemite
-    sha256 "bfd692ac1374c80f04de63c843b79dd9fb50d2ff444da6c6dfd27bd8bb4c6b25" => :mavericks
-    sha256 "6cabe56cc31318fce81c9a35b69438d4cb186d5a25a727bffcdcad72f3f6bb8c" => :mountain_lion
+    sha256 "f29fb1842a3deb57617b19454e955c657301bfd88037b23fb9e7e4791bfee9c9" => :el_capitan
+    sha256 "129104122e418e48cca47347c0204ba3c71412e186fb13def72a93ffeece2414" => :yosemite
+    sha256 "2b08fdeef7198ba639301ce90fdbecfcdac0a4709d8fd0fad207655f8710a507" => :mavericks
   end
 
   def install
