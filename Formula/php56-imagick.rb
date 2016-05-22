4a936570099b68b0203a0a09765918f0b4a3045f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-22 19:23:07 +0100

    php56-imagick: update 3.3.0_2 bottle.

--- Formula/php56-imagick.rb
+++ Formula/php56-imagick.rb
@@ -10,9 +10,9 @@ class Php56Imagick < AbstractPhp56Extension
   revision 2
 
   bottle do
-    sha256 "15862ffcc65fa4c2e92b132c847878993c906a1a7b15b76aedb97e2e932f8a4b" => :el_capitan
-    sha256 "2d0a4b6b82ba77daedd2b95d411cc5b0719f35b7ce169ec8de9053009945b44b" => :yosemite
-    sha256 "157c64129c3ef78e72f4a2de83ae1353b12008e5d6b8042cc90df332eb8a7a4c" => :mavericks
+    sha256 "fac9d4f4f4b2f86664b72f33dac9e33e25e3332a7512c41a3f2d1698808b9074" => :el_capitan
+    sha256 "0acf2fb4aab5bd0dc80f6319ccc563d258555dd49171a746f871804bc31d27ce" => :yosemite
+    sha256 "e9287d2da19ffb2744178c104dc7e598ddecca11bf614fad97744f7d32dd74d4" => :mavericks
   end
 
   depends_on "pkg-config" => :build
