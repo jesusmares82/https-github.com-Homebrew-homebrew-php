f42d40ef86c9f97b6e8a441a667a8277dd2ca612
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-26 17:49:57 +0100

    composer: update 1.1.1_1 bottle.

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -24,6 +24,10 @@ class Composer < AbstractPhpPhar
   end
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "f2708d441e0b40ba4de79850e4d530aee1a41f30a2343ce722393d03deb108ba" => :el_capitan
+    sha256 "f0cef14d3d5a729b089874e077eb22f888774e9701639bce8eebdabf250c0acb" => :yosemite
+    sha256 "459ee080a94ef4fa35442d6f0aebd7b248c7d41e39b78f2fc088bb75d2eaed49" => :mavericks
   end
 
   test do
