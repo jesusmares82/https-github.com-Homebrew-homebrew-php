b575782ddbff32f3035c0a57c99d0e1fb3badbd1
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-05-30 22:01:41 +0200

    Revert "Modify composer phar wrapper to disable xdebug"
    This reverts commit 03e3b7293034fa74f524d865d649702e886f0d17.
    This caused problems for existing workflows and changed the
    default behaviour of composer. Awaiting a more non-intrusive
    solution this commit is reverted.
    
    Closes #3231.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -6,22 +6,8 @@ class Composer < AbstractPhpPhar
   homepage "http://getcomposer.org"
   url "https://getcomposer.org/download/1.1.1/composer.phar"
   sha256 "7f26efee06de5a1a061b6b1e330f5acc9ee69976d1551118c45b21f358cbc332"
-  revision 1
+  revision 2
   head "https://getcomposer.org/composer.phar"
-
-  def phar_wrapper
-    <<-EOS.undent
-      #!/usr/bin/env bash
-
-      TEMP_PATH="$( mktemp -t php-no-debug )"
-      FILES="$( /usr/bin/env php -r 'echo php_ini_loaded_file() . "\n" . str_replace(",","", php_ini_scanned_files());' )"
-      cat $FILES | sed '/xdebug/d' > "$TEMP_PATH"
-
-      /usr/bin/env php -n -c "$TEMP_PATH" -d detect_unicode=Off #{libexec}/#{@real_phar_file} "$@"
-
-      rm -f "$TEMP_PATH"
-    EOS
-  end
 
   bottle do
     cellar :any_skip_relocation
