738ff6097cb270d8ffdbedd935e7aaf6fe87edcf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-01 17:42:19 +0100

    homebrew/php/php53-timecop: add 1.1.2 bottle.

--- Formula/php53-timecop.rb
+++ Formula/php53-timecop.rb
@@ -8,6 +8,13 @@ class Php53Timecop < AbstractPhp53Extension
   sha256 "b25399067862b1f1ff1e52e36a915dad25832f44628c09fa09bad4a7606fb0ed"
   head "https://github.com/hnw/php-timecop.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4765db24b90f63ab4a68751aa1f802e21a35afddc9e75934ff6dfdd79951ab2c" => :el_capitan
+    sha256 "b47f87582e5fea2f3b869e85534b0246815f9d6f023591bfb1c0e746d90e22ec" => :yosemite
+    sha256 "84d48040fd892e8f93ef3cf488560d66b52bbf720885bd1d61e29afbb369b378" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
