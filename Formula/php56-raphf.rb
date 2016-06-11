06684287c165e4433647a75bde945fbe296a6be2
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-06-12 01:02:24 +0200

    Updated installed include files
    * Updated revision to regen bottle
    
    Closes #3264.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php56-raphf.rb
+++ Formula/php56-raphf.rb
@@ -6,6 +6,7 @@ class Php56Raphf < AbstractPhp56Extension
   homepage "https://pecl.php.net/package/raphf"
   url "https://github.com/m6w6/ext-raphf/archive/release-1.1.2.tar.gz"
   sha256 "b5386eff888e2c06951b4fa65a3d0b536775e35a8afc2db323a3129685f7c2bf"
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -21,7 +22,7 @@ class Php56Raphf < AbstractPhp56Extension
     system "./configure", "--prefix=#{prefix}",
                           phpconfig
     system "make"
-    include.install "php_raphf.h"
+    include.install %w[php_raphf.h src/php_raphf_api.h]
     prefix.install "modules/raphf.so"
     write_config_file if build.with? "config-file"
   end
