e8b9bb3a5fc31212aed61cdfdb77622664c0be89
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-06-14 23:33:37 +0200

    php53-propro: add installed header file
    Closes #3269.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php53-propro.rb
+++ Formula/php53-propro.rb
@@ -6,6 +6,7 @@ class Php53Propro < AbstractPhp53Extension
   homepage "https://pecl.php.net/package/propro"
   url "https://github.com/m6w6/ext-propro/archive/release-1.0.2.tar.gz"
   sha256 "35b1d0881927049b3c7a14dc28306e2788f9b2bc937b1ef18e533a3bef8befce"
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -20,7 +21,7 @@ class Php53Propro < AbstractPhp53Extension
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
     system "make"
-    include.install %w[php_propro.h]
+    include.install %w[php_propro.h src/php_propro_api.h]
     prefix.install "modules/propro.so"
     write_config_file if build.with? "config-file"
   end
