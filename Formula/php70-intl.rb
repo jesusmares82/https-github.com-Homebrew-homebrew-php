33465710fb679736dcc91b1b1e9117ab1e008d25
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-23 20:39:23 +0100

    php70-intl: update 7.0.8_1 bottle.

--- Formula/php70-intl.rb
+++ Formula/php70-intl.rb
@@ -5,6 +5,10 @@ class Php70Intl < AbstractPhp70Extension
   desc "Wrapper for the ICU library"
   homepage "http://php.net/manual/en/book.intl.php"
   bottle do
+    revision 3
+    sha256 "47a8cc5870c554d5b92ad92e1ba0a62facb60ca4eda970c86206eb25039ed198" => :el_capitan
+    sha256 "0208b9c71f9ad66613bf57c2464d0859afa5d3311bf1b34a3fe8111a81cd06b3" => :yosemite
+    sha256 "33b7a89a9f0ebe703956f1b9052dde13c06b8b0626c37e48ad4ddcbf7fa0393f" => :mavericks
   end
 
   url PHP_SRC_TARBALL
