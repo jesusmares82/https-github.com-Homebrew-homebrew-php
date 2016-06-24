690f3a1a0bc7ed3c16a24194a3a65fa663521863
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-24 21:02:07 +0100

    php55: update 5.5.37 bottle.

--- Formula/php55.rb
+++ Formula/php55.rb
@@ -13,6 +13,10 @@ class Php55 < AbstractPhp
   head PHP_GITHUB_URL, :branch => PHP_BRANCH
 
   bottle do
+    revision 8
+    sha256 "816181372341d16edd19a9f011ce559192345c827633aa9110355dc28133a824" => :el_capitan
+    sha256 "5d4f1b07d98da0ccd37a03adf52172dba5a55f4019928a4a79a12a6e8449aa4b" => :yosemite
+    sha256 "7f02a1fd67f63eab1ce3f1a4ca8eab0a78f2aca2a1594feb44a5244979b9c51f" => :mavericks
   end
 
   if build.with? "phpdbg"
