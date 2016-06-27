28d7e9ef866b72cf853c161de40d3bfee56f60ff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-27 10:56:56 +0100

    php55-meminfo: add 0.3.0 bottle.

--- Formula/php55-meminfo.rb
+++ Formula/php55-meminfo.rb
@@ -9,6 +9,13 @@ class Php55Meminfo < AbstractPhp55Extension
     :revision => "a9e0eefe0b9c88abd7215f688ec0b04d0d20f386"
   head "https://github.com/BitOne/php-meminfo.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "08b477a3cdfeda51adb2226222b32b49fdcb57b41d3f1414ad12e1cd3481745d" => :el_capitan
+    sha256 "6ee138403fc41544e11b73125f4a284217f6fa9d4fdfe7a4f3bef3e98f71f381" => :yosemite
+    sha256 "a4e15ea275fc24477b92ea5b0e1690fe9afd52f01004909e975d46f4b1cfc6dd" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
