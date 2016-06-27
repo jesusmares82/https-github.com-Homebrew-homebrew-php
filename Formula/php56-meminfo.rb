816415f025844029698a2917a84f4d8915bcf2a4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-27 10:56:56 +0100

    php56-meminfo: add 0.3.0 bottle.

--- Formula/php56-meminfo.rb
+++ Formula/php56-meminfo.rb
@@ -9,6 +9,13 @@ class Php56Meminfo < AbstractPhp56Extension
     :revision => "a9e0eefe0b9c88abd7215f688ec0b04d0d20f386"
   head "https://github.com/BitOne/php-meminfo.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bd45347175ba1d3569373415a641f1a47d6fdb3f325dd8777d316925f32508c4" => :el_capitan
+    sha256 "b3d03e2b345879197d03fc27c96ef4f1e9597b5e0d474416fcabdaae4fa11b85" => :yosemite
+    sha256 "71b6f58b88f8554f595c330d076aad3e60ebb3bf912817be4b6990e424d1716b" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
