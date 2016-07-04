bc726f20f8c7195755d00d0a0c2225d45867a821
Carlo <admin@maou.me>
2016-07-04 14:20:04 +0800

    Alter php-yaf desc

--- Formula/php70-yaf.rb
+++ Formula/php70-yaf.rb
@@ -2,7 +2,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php70Yaf < AbstractPhp70Extension
   init
-  desc "Yaf is a PHP framework similar to zend framework, which is written in c and built as PHP extension"
+  desc "Yaf is a fast php framework written in c, built in php-ext"
   homepage "https://pecl.php.net/package/yaf"
   url "https://pecl.php.net/get/yaf-3.0.3.tgz"
   sha256 "431a7ad4d4cdd51877cc57b1382c4e27395628699fd065bd5140d2e6f70615e3"
