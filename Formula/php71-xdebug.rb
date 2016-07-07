f2dc7de5f011bfc81964643ea7d8241d6d10597c
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2016-07-07 14:09:06 +0200

    php71-xdebug (new formula)
    Closes #3345.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php71-xdebug.rb
@@ -0,0 +1,33 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Xdebug < AbstractPhp71Extension
+  init
+  desc "Provides debugging and profiling capabilities."
+  homepage "http://xdebug.org"
+  url "https://pecl.php.net/get/xdebug-2.4.0.tgz"
+  sha256 "3c4dcb2709d1653534e7cfaa546307041afd298ac48a3670183a12cfdb5eee05"
+  head "https://github.com/xdebug/xdebug.git"
+
+  bottle do
+  end
+
+  def extension_type
+    "zend_extension"
+  end
+
+  def install
+    Dir.chdir "xdebug-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-xdebug"
+    system "make"
+    prefix.install "modules/xdebug.so"
+    write_config_file if build.with? "config-file"
+  end
+end
