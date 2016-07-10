8d118c1c75c2af87e71a030a90d8bd0cf70a0152
Ma Bingyao <mabingyao@gmail.com>
2016-07-10 22:47:38 +0800

    change the url back to https
    Closes #3343.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

--- Formula/php56-swoole.rb
+++ Formula/php56-swoole.rb
@@ -4,7 +4,7 @@ class Php56Swoole < AbstractPhp56Extension
   init
   desc "Asynchronous and concurrent and distributed networking	framework for PHP."
   homepage "https://pecl.php.net/package/swoole"
-  url "http://pecl.php.net/get/swoole-1.8.7.tgz"
+  url "https://pecl.php.net/get/swoole-1.8.7.tgz"
   sha256 "c2b6fc556b1d4508aacd237f7843b17c3313b72202e3aba2f6ae57eb88061e9a"
   head "https://github.com/swoole/swoole-src.git"
 
