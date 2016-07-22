8eb906c8018c2a54a8a0f166f21919133ad43bef
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-22 03:17:19 +0100

    php56-mcrypt: update 5.6.24 bottle.

--- Formula/php56-mcrypt.rb
+++ Formula/php56-mcrypt.rb
@@ -9,6 +9,10 @@ class Php56Mcrypt < AbstractPhp56Extension
   version PHP_VERSION
 
   bottle do
+    revision 12
+    sha256 "e2e4d2de418d20f187c35077e77074384ebe123c07f0504bae205a6a52aefa11" => :el_capitan
+    sha256 "bc31aeecb045646963961de1744e39cdb06ca13916af0da682b1d7dcaff4376d" => :yosemite
+    sha256 "053e6b1f2edfa4c98b2335a6d0e3146eb5d379d53b70e48644b5f54e8bc516c9" => :mavericks
   end
 
   depends_on "mcrypt"
