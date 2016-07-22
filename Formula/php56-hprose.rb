b896f81f094e416077cb80a5f7d778de42df4fd2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-22 03:41:51 +0100

    php56-hprose: add 1.6.5 bottle.

--- Formula/php56-hprose.rb
+++ Formula/php56-hprose.rb
@@ -8,6 +8,13 @@ class Php56Hprose < AbstractPhp56Extension
   sha256 "a453053719dad6352d54543d708d8d09b62de28df7b29fd47b4b5a9fcf8025a7"
   head "https://github.com/hprose/hprose-pecl.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1e40537205c9ca545bc9d7c06166bfa6c49b12ed01070e21fcfd2c5370447c3e" => :el_capitan
+    sha256 "d9d6b044913cfb5f451605832de12c8a608c4ad42d32c50f2cf7ae5d4f7ef838" => :yosemite
+    sha256 "9183f25c52315d58d52932406d2714aa57a162e1e968be751d268ee460a6f207" => :mavericks
+  end
+
   def install
     Dir.chdir "hprose-#{version}" unless build.head?
 
