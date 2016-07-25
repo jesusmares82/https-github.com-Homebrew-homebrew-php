2ebb7ae1b14dbfbd57eb2195422654b3e02c605e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-25 10:37:08 +0100

    php71-mongodb: add 1.1.8 bottle.

--- Formula/php71-mongodb.rb
+++ Formula/php71-mongodb.rb
@@ -8,6 +8,12 @@ class Php71Mongodb < AbstractPhp71Extension
   sha256 "b93ae7dc9bd270823965f8903c9acf47fe3a0ba7a630d08e8173ff091d593ae0"
   head "https://github.com/mongodb/mongo-php-driver.git"
 
+  bottle do
+    sha256 "ee6569da72eabea909cc43c4659405ee9a6d61d3456844c7915bddd8f1525c3b" => :el_capitan
+    sha256 "c77cde737d06ac6b4e7c9374279a4070cc11d93c2352bba2c45306261f3e9e6e" => :yosemite
+    sha256 "a35e9745530068cb14721f369de59ad9ae46ae15fcb4ed61a1c42c436019c514" => :mavericks
+  end
+
   depends_on "openssl"
 
   def install
