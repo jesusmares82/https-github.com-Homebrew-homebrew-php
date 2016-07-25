6ee29db76c585b521858e6e048247e75ad844d52
Andreas Braun <git@alcaeus.org>
2016-07-25 11:26:37 +0200

    php71-mongodb
    Closes #3385.
    
    Signed-off-by: Andy Blyler <andy@blyler.cc>

---
+++ Formula/php71-mongodb.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Mongodb < AbstractPhp71Extension
+  init
+  desc "MongoDB driver for PHP."
+  homepage "https://pecl.php.net/package/mongodb"
+  url "https://pecl.php.net/get/mongodb-1.1.8.tgz"
+  sha256 "b93ae7dc9bd270823965f8903c9acf47fe3a0ba7a630d08e8173ff091d593ae0"
+  head "https://github.com/mongodb/mongo-php-driver.git"
+
+  depends_on "openssl"
+
+  def install
+    Dir.chdir "mongodb-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-openssl-dir=#{Formula["openssl"].opt_prefix}"
+    system "make"
+    prefix.install "modules/mongodb.so"
+    write_config_file if build.with? "config-file"
+  end
+end
