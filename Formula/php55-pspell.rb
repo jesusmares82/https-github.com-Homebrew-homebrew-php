a68459e97e499c7217e96f8bef3285b528974d42
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-27 12:55:15 +0100

    php55-pspell: update 5.5.38 bottle.

--- Formula/php55-pspell.rb
+++ Formula/php55-pspell.rb
@@ -9,6 +9,10 @@ class Php55Pspell < AbstractPhp55Extension
   version PHP_VERSION
 
   bottle do
+    revision 10
+    sha256 "f9031d69810dacc2df6fa20b0a4f1531b4455901826ea9cf9a143f12392802bf" => :el_capitan
+    sha256 "df4243bab38e0320ee6d8f4df22329958020e79d802065f9b0afa52503cfc872" => :yosemite
+    sha256 "ab077d01af1381f33221dcde496211e598d265e96028adbd02f5e54ec398d2fa" => :mavericks
   end
 
   depends_on "aspell"
