466059f18c45380c3885dd61c8e6019a92b07679
Bogdan Padalko <pinepain@gmail.com>
2016-07-28 22:43:49 +0300

    Add php71-pdo-pgsql formula
    Closes #3415.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-pdo-pgsql.rb
@@ -0,0 +1,31 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71PdoPgsql < AbstractPhp71Extension
+  init
+  desc "Unified PostgreSQL driver for PDO"
+  homepage "https://github.com/php/php-src/tree/master/ext/pdo_pgsql"
+  bottle do
+  end
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on "postgresql"
+
+  def extension
+    "pdo_pgsql"
+  end
+
+  def install
+    Dir.chdir "ext/pdo_pgsql"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", "--with-pdo-pgsql=#{Formula["postgresql"].prefix}", phpconfig
+    system "make"
+    prefix.install "modules/pdo_pgsql.so"
+    write_config_file if build.with? "config-file"
+  end
+end
