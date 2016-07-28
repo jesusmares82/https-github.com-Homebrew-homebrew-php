564792bac4930a9cc53dd32bbdf342df9935559c
Bogdan Padalko <pinepain@gmail.com>
2016-07-28 20:10:14 +0300

    Add php71-mcrypt formula
    Closes #3404.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-mcrypt.rb
@@ -0,0 +1,31 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Mcrypt < AbstractPhp71Extension
+  init
+  desc "Interface to the mcrypt library"
+  homepage "http://php.net/manual/en/book.mcrypt.php"
+  bottle do
+  end
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on "mcrypt"
+  depends_on "libtool" => :build
+
+  def install
+    Dir.chdir "ext/mcrypt"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking",
+                          "--with-mcrypt=#{Formula["mcrypt"].opt_prefix}"
+    system "make"
+    prefix.install "modules/mcrypt.so"
+    write_config_file if build.with? "config-file"
+  end
+end
