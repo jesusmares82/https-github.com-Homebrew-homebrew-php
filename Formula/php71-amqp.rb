eeaf3290b7c029c04c78f5196aa800160b482159
Bogdan Padalko <pinepain@gmail.com>
2016-07-28 20:02:22 +0300

    php71-amqp 1.7.1 (new formula)
    Closes #3403.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-amqp.rb
@@ -0,0 +1,28 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Amqp < AbstractPhp71Extension
+  init
+  desc "Communicates with any AMQP 0-9-1 compatible server."
+  homepage "https://pecl.php.net/package/amqp"
+  url "https://pecl.php.net/get/amqp-1.7.1.tgz"
+  sha256 "38a92eaef1aa863404170bcd15605f7f78140eeec1ff5be6af65c5b8081e2599"
+  head "https://github.com/pdezwart/php-amqp.git"
+
+  bottle do
+  end
+
+  depends_on "rabbitmq-c"
+
+  def install
+    Dir.chdir "amqp-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/amqp.so"
+    write_config_file if build.with? "config-file"
+  end
+end
