56cfc60cac619ac6cd31c5e88d6f955eb1e5806b
Bogdan Padalko <pinepain@gmail.com>
2016-07-28 20:14:01 +0300

    php71-event 2.1.0 (new formula)
    Closes #3405.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-event.rb
@@ -0,0 +1,35 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Event < AbstractPhp71Extension
+  init
+  desc "Provides interface to libevent library"
+  homepage "https://pecl.php.net/package/event"
+  url "https://pecl.php.net/get/event-2.1.0.tgz"
+  sha256 "26bfcf7e29679b8923430f1a8fc9e9b344d52415bc77601a97ea7b71fd24f2cf"
+
+  bottle do
+  end
+
+  depends_on "libevent"
+  depends_on "openssl"
+
+  def install
+    Dir.chdir "event-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    args = []
+    args << "--with-event-core"
+    args << "--with-event-extra"
+    args << "--enable-event-debug"
+    args << "--with-event-libevent-dir=#{Formula["libevent"].opt_prefix}"
+    args << "--with-event-openssl"
+    args << "--with-openssl-dir=#{Formula["openssl"].opt_prefix}"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/event.so"
+    write_config_file if build.with? "config-file"
+  end
+end
