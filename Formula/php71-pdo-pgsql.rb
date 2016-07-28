632aadf57f9e3fbfbc29d4e077ed20742994998c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-28 22:18:38 +0100

    php71-pdo-pgsql: update 7.1.0-beta.1 bottle.

--- Formula/php71-pdo-pgsql.rb
+++ Formula/php71-pdo-pgsql.rb
@@ -5,6 +5,10 @@ class Php71PdoPgsql < AbstractPhp71Extension
   desc "Unified PostgreSQL driver for PDO"
   homepage "https://github.com/php/php-src/tree/master/ext/pdo_pgsql"
   bottle do
+    cellar :any
+    sha256 "c462bdcf62f6789d618d10802da574f5d1cc46c06a6f79b92ea40eeda49ccd60" => :el_capitan
+    sha256 "b898e40345438d7c87a098368ea956a01280dad94b914d05872465560d227518" => :yosemite
+    sha256 "a945dc3ed0ce5d1225f61f594f66ff0ec6bd77d993c30a08131527bef51e2962" => :mavericks
   end
 
   url PHP_SRC_TARBALL
