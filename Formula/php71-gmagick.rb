e566fb88a9e6eb8760789811343521c1a903eae6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-28 21:26:06 +0100

    php71-gmagick: update 2.0.4RC1 bottle.

--- Formula/php71-gmagick.rb
+++ Formula/php71-gmagick.rb
@@ -8,6 +8,9 @@ class Php71Gmagick < AbstractPhp71Extension
   sha256 "4ead19640bebebfa68be41dc0097a93a7bf9beb56ea82b27343dba8ea4c5ecfa"
 
   bottle do
+    sha256 "8b4e2d1a006e3a8cc90430a92443fa0866e0002eacb5ac4912811b24c12991f4" => :el_capitan
+    sha256 "a9a14c61ebd7396020c6b0805f311c53d68901cf311380107d9808ce8d3ea642" => :yosemite
+    sha256 "70908cfb1bdab974c96ff5ee2c39b9a3ea753ff44c2a006a2aeabeb853681d74" => :mavericks
   end
 
   depends_on "graphicsmagick"
