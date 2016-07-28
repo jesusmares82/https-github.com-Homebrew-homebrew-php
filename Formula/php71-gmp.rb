b7ba42fe91b52979107116fa9178fc2f6fd816da
Bogdan Padalko <pinepain@gmail.com>
2016-07-28 20:20:43 +0300

    Add php71-gmp formula
    Closes #3407.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-gmp.rb
@@ -0,0 +1,30 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Gmp < AbstractPhp71Extension
+  init
+  desc "GMP core php extension"
+  homepage "http://php.net/manual/en/book.gmp.php"
+  bottle do
+  end
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on "gmp"
+
+  def install
+    Dir.chdir "ext/gmp"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking",
+                           "--with-gmp=#{Formula["gmp"].opt_prefix}"
+    system "make"
+    prefix.install "modules/gmp.so"
+    write_config_file if build.with? "config-file"
+  end
+end
