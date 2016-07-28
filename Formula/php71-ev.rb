8cb73d65ba4a783cefb69ea35472fd38098e9ad2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-28 18:36:31 +0100

    php71-ev: update 1.0.0 bottle.

--- Formula/php71-ev.rb
+++ Formula/php71-ev.rb
@@ -9,6 +9,10 @@ class Php71Ev < AbstractPhp71Extension
   head "https://bitbucket.org/osmanov/pecl-ev.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "13bf162680736e0a350b704b2f72103c045d9b98331b1b42a726b641fd0b92da" => :el_capitan
+    sha256 "d4eebd7515d09b3cf3583f4d1e584c841526fb8e30f5a555aa8d6bccb05a3d85" => :yosemite
+    sha256 "b6cb605da3ea96f36354b96ad6439954da97e152762c021152075ae5d9d6a3b1" => :mavericks
   end
 
   depends_on "libev"
