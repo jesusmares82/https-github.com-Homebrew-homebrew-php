661c4fc215e432a5c1c9c4bc3af4aec661f48e7d
Bogdan Padalko <pinepain@gmail.com>
2016-07-28 22:46:10 +0300

    Add php71-pdo-dblib formula
    Closes #3416.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-pdo-dblib.rb
@@ -0,0 +1,31 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71PdoDblib < AbstractPhp71Extension
+  init
+  desc "Unified Sybase-DB style driver for PDO"
+  homepage "https://github.com/php/php-src/tree/master/ext/pdo_dblib"
+  bottle do
+  end
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on "freetds"
+
+  def extension
+    "pdo_dblib"
+  end
+
+  def install
+    Dir.chdir "ext/pdo_dblib" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", "--with-pdo-dblib=#{Formula["freetds"].opt_prefix}", phpconfig
+    system "make"
+    prefix.install "modules/pdo_dblib.so"
+    write_config_file if build.with? "config-file"
+  end
+end
