9ccc33a749a80c35306819592ae5ac57e8dbc06a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-29 07:15:00 +0100

    php54-couchbase: add 2.2.0 bottle.

--- Formula/php54-couchbase.rb
+++ Formula/php54-couchbase.rb
@@ -8,6 +8,13 @@ class Php54Couchbase < AbstractPhp54Extension
   sha256 "6ce11f344bd01df7e8b96894ea3db44e72a273633d67f80dbc130147cc161acc"
   head "https://github.com/couchbaselabs/php-couchbase.git"
 
+  bottle do
+    cellar :any
+    sha256 "206539c939487b50ccd009332408ab0e1fec341d735d3d204c20feda6bb2baef" => :el_capitan
+    sha256 "b446e4f8ba8df517ec9e371ac29cee3e7650acbdaf067e5b132406e9bdab8656" => :yosemite
+    sha256 "f1c7a413529d9372bfc73d01ee9d218836727480f99fc2f15b407b0cdaa3c4c2" => :mavericks
+  end
+
   option "with-igbinary", "Build with igbinary support"
 
   depends_on "libcouchbase"
