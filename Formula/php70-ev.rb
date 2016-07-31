e7a8796acd3daa69aa99e15f1b85989e04d3864e
Bogdan Padalko <pinepain@gmail.com>
2016-07-31 14:22:26 +0300

    php70-ev 1.0.3
    Closes #3428.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php70-ev.rb
+++ Formula/php70-ev.rb
@@ -4,8 +4,8 @@ class Php70Ev < AbstractPhp70Extension
   init
   desc "interface to libev library"
   homepage "https://pecl.php.net/package/ev"
-  url "https://pecl.php.net/get/ev-1.0.0RC2.tgz"
-  sha256 "df833194c2f87d9e7d8809863b907c778dcb9323d821fd527978186bfa156e84"
+  url "https://pecl.php.net/get/ev-1.0.3.tgz"
+  sha256 "3c03fde9e72745e6ce6c32d680218389e0f4310908187f1529b7f227b295aeee"
   head "https://bitbucket.org/osmanov/pecl-ev.git"
 
   bottle do
