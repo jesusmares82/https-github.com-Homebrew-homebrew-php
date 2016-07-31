e3a7884738ceb013198474bf9778cf4ac8f35c85
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-31 13:36:24 +0100

    php71-mecab: update 0.6.0 bottle.

--- Formula/php71-mecab.rb
+++ Formula/php71-mecab.rb
@@ -9,6 +9,9 @@ class Php71Mecab < AbstractPhp71Extension
   head "https://github.com/rsky/php-mecab.git"
 
   bottle do
+    sha256 "46680cd8e6bf128a2639e2a10f03c7649f3b695d722e8271694d58baec33479a" => :el_capitan
+    sha256 "299005d816e5a87857bb286683019b5a870a1ecbd32d8a5bead3dcda351117a3" => :yosemite
+    sha256 "d228d16c12235840d1972eba0bf9ddb6eedd8f27f78f270130eb7ba4e97bd533" => :mavericks
   end
 
   depends_on "mecab"
