60d92ca2473d3522589c3d46156bc216d157a271
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-31 13:08:26 +0100

    php71-libsodium: update 1.0.6 bottle.

--- Formula/php71-libsodium.rb
+++ Formula/php71-libsodium.rb
@@ -9,6 +9,10 @@ class Php71Libsodium < AbstractPhp71Extension
   head "https://github.com/jedisct1/libsodium-php.git"
 
   bottle do
+    cellar :any
+    sha256 "a29ed699108b4c2385a0503e04efccdf1dfab11d2b415a76744540f606e03c56" => :el_capitan
+    sha256 "4b352ca5a03e426612923b92cc906daf996a115c07453a7086788e309390873b" => :yosemite
+    sha256 "44dc6ea867f8cc7e77d390fae41fa9593cedca3e779792482bc5dd8d517846c7" => :mavericks
   end
 
   depends_on "libsodium"
