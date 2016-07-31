585f71a9b9495a24cd93eba96418349f62d6e029
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-31 13:16:58 +0100

    php71-lz4: update 0.2.3 bottle.

--- Formula/php71-lz4.rb
+++ Formula/php71-lz4.rb
@@ -9,6 +9,10 @@ class Php71Lz4 < AbstractPhp71Extension
   head "https://github.com/kjdev/php-ext-lz4.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "c82532bc7bd0ce89963e67cf514e742d1b6c91a02d6163c75e665f7085d04d26" => :el_capitan
+    sha256 "62a9b6e5590077735993f8a524807c7acfebc2844f7f0198d8ddd4af714f026c" => :yosemite
+    sha256 "8078789ad18fdda51bcd637b82de0647b53bd4a453b1142ba8bc97ab187cf709" => :mavericks
   end
 
   def install
