c605bf1acf68fdb5db97d94fde5dcb89d70b3ca7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-01 16:08:19 +0100

    php70-scrypt: add 1.4.2 bottle.

--- Formula/php70-scrypt.rb
+++ Formula/php70-scrypt.rb
@@ -8,6 +8,13 @@ class Php70Scrypt < AbstractPhp70Extension
   sha256 "f187076caa8eeb0f5d9992fbc2045b5dff11652f5ce93ccdf3a792ac98b43622"
   head "https://github.com/DomBlack/php-scrypt.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "43067f9037e57e1bcd8ef776d418c899db596e552512c124cba0bb56a0ec151b" => :el_capitan
+    sha256 "dc4864e1aa900f311e7adfe6aa7643a049d3eeee3c8e8a268b0fe2868bc1a7f7" => :yosemite
+    sha256 "caf4b279d9fb64930208f4bd5007a639a87e48d0a1e788e4fff9d5b6e74cd609" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
     ENV["CFLAGS"] = "-arch i386 -arch x86_64"
