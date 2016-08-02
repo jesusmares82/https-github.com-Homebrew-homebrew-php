4e3f0f0208f48f5cef4d0e8ccfb1a9a9885c9fde
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-02 14:15:35 +0100

    php70-ds: add 1.0.4 bottle.

--- Formula/php70-ds.rb
+++ Formula/php70-ds.rb
@@ -8,6 +8,13 @@ class Php70Ds < AbstractPhp70Extension
   sha256 "4cdad2bba1cae18f1196346913ac9a54a23f2e5874fd8678200f88730b2788c6"
   head "https://github.com/php-ds/extension.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5b47e178c446b9663a03911e449fe4eb62a6dc86554d4f4dceff40cbf200ac97" => :el_capitan
+    sha256 "92ec724d8be0130f88be81258ecd014765dd9324d56331c857a595422b7ace82" => :yosemite
+    sha256 "957a3546f6826401babd464ca9b6a63fbadf5f53f65c11f9f82de49ce274eba6" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
