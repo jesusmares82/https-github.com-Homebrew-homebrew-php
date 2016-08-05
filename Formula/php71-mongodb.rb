334c830d62e03349c732657d99fdea9cde606e85
Andreas Braun <git@alcaeus.org>
2016-08-05 10:05:14 +0200

    php71-mongodb 1.2.0alpha1 (devel)
    Closes #3455.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php71-mongodb.rb
+++ Formula/php71-mongodb.rb
@@ -14,6 +14,12 @@ class Php71Mongodb < AbstractPhp71Extension
     sha256 "a35e9745530068cb14721f369de59ad9ae46ae15fcb4ed61a1c42c436019c514" => :mavericks
   end
 
+  devel do
+    url "https://pecl.php.net/get/mongodb-1.2.0alpha1.tgz"
+    sha256 "03ac8a762949699aef0467ccdfbb8dfe70b57b0b57b740fe5041a4aff28685d7"
+    version "1.2.0alpha1"
+  end
+
   depends_on "openssl"
 
   def install
