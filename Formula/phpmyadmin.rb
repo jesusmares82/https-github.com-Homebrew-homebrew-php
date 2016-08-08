60b05ac41e74251207448f285e4d2bc4f6eb5b45
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-08 20:53:33 +0100

    phpmyadmin: add 4.6.3 bottle.

--- Formula/phpmyadmin.rb
+++ Formula/phpmyadmin.rb
@@ -7,6 +7,13 @@ class Phpmyadmin < Formula
   sha256 "f205884516aa54c8ecce23537d1d2da7ce8d1bce9bad8b2e75ed4485d00690aa"
   head "https://github.com/phpmyadmin/phpmyadmin.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b6d4b1581936610dcad0fce2bff7fe16e40201306bb1d1da83f3909f390bf3db" => :el_capitan
+    sha256 "d71de17b44d5bed82f80651288f475253a9343e14480ed79f337978c407f3516" => :yosemite
+    sha256 "4f57b03aecf16bfe3b276a084122f896e002e1db9b375fdd7990e0c6568887a4" => :mavericks
+  end
+
   depends_on PhpMetaRequirement
   depends_on "php53-mcrypt" if Formula["php53"].linked_keg.exist?
   depends_on "php54-mcrypt" if Formula["php54"].linked_keg.exist?
