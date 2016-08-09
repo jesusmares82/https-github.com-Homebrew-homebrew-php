213f3768fc0bb134917538aa910cb1cdd66ec416
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-09 15:41:04 +0100

    wp-cli: update 0.24.1 bottle.

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -11,9 +11,9 @@ class WpCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0c781d66b2247bdd3abc92211e63bacf57f31787c04b8f3719d3a9b4093a5e92" => :el_capitan
-    sha256 "96c0f7ac8164e9668a96914b97934a5bb501670d5b7d44fd5444c0dc6896ada0" => :yosemite
-    sha256 "9b684a473120d822e10a58fd0a65af01369b5a4ef0a23453b55488511fc672e7" => :mavericks
+    sha256 "518178abc782ca4bf4509334fcc4f2305aef76027263b41b5a5c60fa238c6399" => :el_capitan
+    sha256 "48728b492cbc54b363d5bb6287a967a43565810eea7c0cae1906206fa270d3ff" => :yosemite
+    sha256 "a537a3d0ffef87552e1f9f980c852370479a65035a9e596000ea84b8dfda8eb3" => :mavericks
   end
 
   option "without-bash-completion", "Don't install bash completion"
