3c12b7e23ac741ef61372da11c729d88e9003413
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-08-12 05:37:55 +0200

    php70-yaf (new formula)
    Fixes #3328
    
    Closes #3471.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php70-yaf.rb
+++ Formula/php70-yaf.rb
@@ -2,24 +2,22 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php70Yaf < AbstractPhp70Extension
   init
-  desc "Yaf is a fast php framework written in c, built in php-ext"
+  desc "PHP framework similar to zend framework built as PHP extension"
   homepage "https://pecl.php.net/package/yaf"
-  url "https://pecl.php.net/get/yaf-3.0.3.tgz"
-  sha256 "431a7ad4d4cdd51877cc57b1382c4e27395628699fd065bd5140d2e6f70615e3"
+  url "https://github.com/laruence/yaf/archive/yaf-3.0.3.tar.gz"
+  sha256 "6761e636d055ec6756759185e91cf9fd42ca3f59e36172d7773b8052a1fb4887"
   head "https://github.com/laruence/yaf.git"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f362f2731fe4736da0394132ace5f750bb0573f78e113d4efb1ad6380d3a87f1" => :el_capitan
-    sha256 "aa2c61a908313b2dce9ad43d088a89b4bef4f66cf03252e6a52b77f4f8e0746f" => :yosemite
-    sha256 "0bcfd62d0d898dc3b128d72c38291f0c354e62ef45ae2787683ab0e27aa4f166" => :mavericks
+    sha256 "5634fb8c7478c0a152887344134f6fcc6da759fe0cdaefd9016aa134c8d7b5da" => :el_capitan
+    sha256 "768821370e69deb296f787529f88deaf53caf868d8f46fed768b38a98b1fa62b" => :yosemite
+    sha256 "6f53e8a1bcfe463635f99356b25efa6f9951b413abc3e6eb5b6f6390a2e2eb55" => :mavericks
   end
 
   depends_on "pcre"
 
   def install
-    Dir.chdir "yaf-#{version}" unless build.head?
-
     ENV.universal_binary if build.universal?
 
     safe_phpize
