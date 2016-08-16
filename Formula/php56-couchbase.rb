fb58d41be1264e079ed567efcf6bc9eb05a0d8a6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-16 17:00:48 +0100

    php56-couchbase: add 2.2.1 bottle.

--- Formula/php56-couchbase.rb
+++ Formula/php56-couchbase.rb
@@ -8,6 +8,13 @@ class Php56Couchbase < AbstractPhp56Extension
   sha256 "d67c0fd19fdcaa72720d4910e29db12ccd72c30c4f441e5f1d9ef204fd7bc3d8"
   head "https://github.com/couchbaselabs/php-couchbase.git"
 
+  bottle do
+    cellar :any
+    sha256 "47c8563fef4106de65c7b72257004f3d32ad9ccd303ace04f85d8ab8eb57d75b" => :el_capitan
+    sha256 "2822416df97f9cb3bf967348d97670684c1d99b45c5079e756fead5c62fb8745" => :yosemite
+    sha256 "ac0bd96ff446ebdeb8391017f98cce5cba3b3e834d5ea2476a643b2bc4ffb77f" => :mavericks
+  end
+
   option "with-igbinary", "Build with igbinary support"
 
   depends_on "libcouchbase"
