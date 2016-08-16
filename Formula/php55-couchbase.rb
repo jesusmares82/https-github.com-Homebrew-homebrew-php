339174879ca11de5581aa1d83e5208513c2ccc88
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-16 17:00:48 +0100

    php55-couchbase: update 2.2.1 bottle.

--- Formula/php55-couchbase.rb
+++ Formula/php55-couchbase.rb
@@ -10,9 +10,9 @@ class Php55Couchbase < AbstractPhp55Extension
 
   bottle do
     cellar :any
-    sha256 "56e609af00ae84446e128099852e6c58dbd3559fc51fe748250d4e5a192851b8" => :el_capitan
-    sha256 "252ed44c0308ec065802cf9c069648fb39ccadbf1356a219aed44d90243c6f44" => :yosemite
-    sha256 "1c02e1f741ac6bf53039fadc6ad7208d1ecd5d43c6138479f5110e21cc54558a" => :mavericks
+    sha256 "7ffeabb0c7715fdcf276002ee89beeedc88f596d0adb109957ab26c4602b07cc" => :el_capitan
+    sha256 "7ea96ad6359f46e0f5cb271bc2c0ce28efc86a46d38bc0b7a682aa80536636bd" => :yosemite
+    sha256 "4680ad0523d36174ced2282ab0ff6419e2d619042c60074e650a96f671674e7f" => :mavericks
   end
 
   option "with-igbinary", "Build with igbinary support"
