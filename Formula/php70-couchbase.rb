7990d506d6df695f531c46b363248cd36e99ca68
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-16 17:00:48 +0100

    php70-couchbase: add 2.2.1 bottle.

--- Formula/php70-couchbase.rb
+++ Formula/php70-couchbase.rb
@@ -8,6 +8,13 @@ class Php70Couchbase < AbstractPhp70Extension
   sha256 "d67c0fd19fdcaa72720d4910e29db12ccd72c30c4f441e5f1d9ef204fd7bc3d8"
   head "https://github.com/couchbaselabs/php-couchbase.git"
 
+  bottle do
+    cellar :any
+    sha256 "3da4e229a180618ada94da2208089a4822bdc3a1ed2860473fff3fe08c160677" => :el_capitan
+    sha256 "f70073d4bc7396f1cb4f21ad44572739f3ea74d12ac49dd8f6f3b228794d72bc" => :yosemite
+    sha256 "d7da90d9b51e7defe2916a970b30d24ea31bafb758fede5b0dfb05215e369eff" => :mavericks
+  end
+
   option "with-igbinary", "Build with igbinary support"
 
   depends_on "libcouchbase"
