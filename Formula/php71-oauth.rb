46f37a30dc7f0e954268456c3e39ffee1312f4df
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-17 20:51:11 +0100

    php71-oauth: update 2.0.2 bottle.

--- Formula/php71-oauth.rb
+++ Formula/php71-oauth.rb
@@ -9,6 +9,10 @@ class Php71Oauth < AbstractPhp71Extension
   head "https://github.com/php/pecl-web_services-oauth.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "7938f1e1d5fa8023e23f518c1d9fb5b767253b74a962f3a87a4c6ab04713b0cd" => :el_capitan
+    sha256 "429fd54dfdeb181e043e27203cc15404940642e690c23f8a37dda40d11a7d614" => :yosemite
+    sha256 "e35f011fa224a45d0f4c2835cc27b0443219351a9ea3911a4a88f9347db78f80" => :mavericks
   end
 
   depends_on "pcre"
