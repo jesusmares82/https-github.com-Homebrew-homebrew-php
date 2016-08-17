51df7e318b8d355b09b9de95268779286684ba05
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-17 16:27:17 +0100

    php71-ref: update 0.4.1 bottle.

--- Formula/php71-ref.rb
+++ Formula/php71-ref.rb
@@ -9,6 +9,10 @@ class Php71Ref < AbstractPhp71Extension
   head "https://github.com/pinepain/php-ref.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "cca27e2dc10a14f2a09759a50aaa0df010ec7517f95c9ef52bcd4ddbe675e4d2" => :el_capitan
+    sha256 "ee01f8855875df67f9d641d855a2f301c1ef221481870eaa7369150a59e78c3b" => :yosemite
+    sha256 "29ce63f43594b6e341f7faff2fbd5739245d1f2f6c49cdf4aa016b08b0c6e47c" => :mavericks
   end
 
   def install
