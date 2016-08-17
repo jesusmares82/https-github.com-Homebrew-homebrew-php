9986521d70d3dda020995cdf83561063a04afc4a
Bogdan Padalko <pinepain@gmail.com>
2016-08-17 21:12:29 +0300

    php71-snmp 7.1.0-beta.1 (new formula)
    php71-snmp (new formula)
    
    Closes #3490.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-snmp.rb
@@ -0,0 +1,30 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Snmp < AbstractPhp71Extension
+  init
+  desc "SNMP core php extension"
+  homepage "http://php.net/manual/en/book.snmp.php"
+  bottle do
+  end
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on "net-snmp"
+
+  def install
+    Dir.chdir "ext/snmp"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking",
+                          "--with-snmp=#{Formula["net-snmp"].opt_prefix}"
+    system "make"
+    prefix.install "modules/snmp.so"
+    write_config_file if build.with? "config-file"
+  end
+end
