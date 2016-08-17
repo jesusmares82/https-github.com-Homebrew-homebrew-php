9eb4138d789c4f10519c7e1a1c55b3bb53d267b4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-17 19:13:14 +0100

    php71-xxtea: update 1.0.11 bottle.

--- Formula/php71-xxtea.rb
+++ Formula/php71-xxtea.rb
@@ -9,6 +9,10 @@ class Php71Xxtea < AbstractPhp71Extension
   head "https://github.com/xxtea/xxtea-pecl.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "735c9516ea414d40aa7aecee4f49455271c3f9d7f5af63035a1fe274a18d557a" => :el_capitan
+    sha256 "f55a7702b71a8af7e6af67f85d3e530794fea49d832c83a4ed1b1b1027ef5236" => :yosemite
+    sha256 "2d6e711bbc9f3d3bb555bac157653cfb592f90e6ef58fe9c9c7256b5bcb33e7f" => :mavericks
   end
 
   def install
