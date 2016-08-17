8b33ec2e119882375adc99bfa449fe099d650e31
Bogdan Padalko <pinepain@gmail.com>
2016-08-17 20:48:24 +0300

    php71-pcntl 7.1.0-beta.1 (new formula)
    php71-pcntl (new formula)
    
    Closes #3486.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-pcntl.rb
@@ -0,0 +1,27 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Pcntl < AbstractPhp71Extension
+  init
+  desc "Process Control support"
+  homepage "http://php.net/manual/en/book.pcntl.php"
+  bottle do
+  end
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  def install
+    Dir.chdir "ext/pcntl"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking"
+    system "make"
+    prefix.install "modules/pcntl.so"
+    write_config_file if build.with? "config-file"
+  end
+end
