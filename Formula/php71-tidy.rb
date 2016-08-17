3e5cf31404b1421e605ce00c79080f4363ae71b0
Bogdan Padalko <pinepain@gmail.com>
2016-08-17 20:54:12 +0300

    php71-tidy (new formula)
    Closes #3487.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-tidy.rb
@@ -0,0 +1,30 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Tidy < AbstractPhp71Extension
+  init
+  desc "Tidy HTML clean and repair utility"
+  homepage "http://php.net/manual/en/book.tidy.php"
+  bottle do
+  end
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on "tidy-html5"
+
+  def install
+    Dir.chdir "ext/tidy"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking",
+                          "--with-tidy=#{Formula["tidy-html5"].opt_prefix}"
+    system "make"
+    prefix.install "modules/tidy.so"
+    write_config_file if build.with? "config-file"
+  end
+end
