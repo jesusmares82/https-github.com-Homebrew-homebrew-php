1759d117634e8b575b2aad50787d7ed8927e924d
Bogdan Padalko <pinepain@gmail.com>
2016-08-17 21:20:40 +0300

    php71-swoole 1.8.8 (new formula)
    Closes #3492.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-swoole.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Swoole < AbstractPhp71Extension
+  init
+  desc "Event-driven asynchronous & concurrent networking framework"
+
+  homepage "https://pecl.php.net/package/swoole"
+  url "https://pecl.php.net/get/swoole-1.8.8.tgz"
+  sha256 "e464d669c3573336cd59a96662d43a0e13538dfb335a67c6afc0ecaba92c8c4d"
+  head "https://github.com/swoole/swoole-src.git"
+
+  bottle do
+  end
+
+  def install
+    Dir.chdir "swoole-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/swoole.so"
+    write_config_file if build.with? "config-file"
+  end
+end
