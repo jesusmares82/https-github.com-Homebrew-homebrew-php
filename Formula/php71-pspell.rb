df6b486b7f8cc7bc6bb7bc97cd027c14fc03cc7d
Bogdan Padalko <pinepain@gmail.com>
2016-08-17 21:24:11 +0300

    php71-pspell 7.1.0-beta.1 (new formula)
    php71-pspell (new formula)
    
    Closes #3493.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-pspell.rb
@@ -0,0 +1,30 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Pspell < AbstractPhp71Extension
+  init
+  desc "Extension to check the spelling"
+  homepage "http://php.net/manual/en/book.pspell.php"
+  bottle do
+  end
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on "aspell"
+
+  def install
+    Dir.chdir "ext/pspell"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-debug",
+                          "--with-pspell=#{Formula["aspell"].opt_prefix}"
+    system "make"
+    prefix.install "modules/pspell.so"
+    write_config_file if build.with? "config-file"
+  end
+end
