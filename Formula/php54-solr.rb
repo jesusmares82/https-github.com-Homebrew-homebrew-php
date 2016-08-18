1dbef04da7cf7785c6c4c48114792d908e648878
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-18 21:37:14 +0100

    php54-solr: update 2.4.0 bottle.

--- Formula/php54-solr.rb
+++ Formula/php54-solr.rb
@@ -10,9 +10,9 @@ class Php54Solr < AbstractPhp54Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c47e61bd9932fabc26ff765e541f291f332e9486daeb81f1aad974a766b3fe51" => :el_capitan
-    sha256 "7de0c5da36f0fbc140cb7ee698b8736aea2a7a46d55a55e9e3b02331d51a03ce" => :yosemite
-    sha256 "0646597120de5ffdd4e90b10f1b3bfe2887db13dd7235645352793824eec0a4b" => :mavericks
+    sha256 "f08bc7989f09a88003e9728eb7af25c42da101d403edd7844dd27c28e1b8cf89" => :el_capitan
+    sha256 "e3c44d009c5637ee1e3f8ecfa792079359d714cbfe7d51b43e6c8ecfcb7f70f8" => :yosemite
+    sha256 "0a7a50619db6553a7ecb8925dd916cececcae9cb7323dd85d48661164dbfb10e" => :mavericks
   end
 
   def install
