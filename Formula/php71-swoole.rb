86ceaaf4aa6353d60a53a5929b9025bd2465a578
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-18 07:28:25 +0100

    php71-swoole: update 1.8.8 bottle.

--- Formula/php71-swoole.rb
+++ Formula/php71-swoole.rb
@@ -10,6 +10,10 @@ class Php71Swoole < AbstractPhp71Extension
   head "https://github.com/swoole/swoole-src.git"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "18cfb60c83c38ccec5f7b2d12efe6bfd4e143b4c373d8ee70503bc8bf046e593" => :el_capitan
+    sha256 "3039983e0c15d039517018a08a81a1bfd46c2168fb15dd8c3f7bb251af650d00" => :yosemite
+    sha256 "ab39f39e26abc1e1ffe8fd92ea09be1b90d984eba0abccc31e2ac6beb128a253" => :mavericks
   end
 
   def install
