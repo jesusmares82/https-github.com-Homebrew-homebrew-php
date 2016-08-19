180e5081a181e1ea055a58d0bd7e5f16c51cdec2
Maurus Cuelenaere <mcuelenaere@gmail.com>
2016-08-19 11:57:17 +0200

    php-geos v1.0.0rc1 (new formula)
    Closes #3507.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php55-geos.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php55Geos < AbstractPhp55Extension
+  init
+  desc "PHP bindings for GEOS"
+  homepage "https://git.osgeo.org/gogs/geos/php-geos"
+  url "https://git.osgeo.org/gogs/geos/php-geos/archive/1.0.0rc1.tar.gz"
+  version "1.0.0rc1"
+  sha256 "63721d9dc2051aa03034b343c0b334e9cb11643e7009ea9be2e69f51f7b90f71"
+  head "https://git.osgeo.org/gogs/geos/php-geos.git"
+
+  depends_on "geos"
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-geos=#{Formula["geos"].opt_prefix}"
+    system "make"
+    prefix.install "modules/geos.so"
+    write_config_file if build.with? "config-file"
+  end
+end
