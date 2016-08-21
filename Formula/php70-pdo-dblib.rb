7533f722167e3816f732388b6d04770912dc7f86
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-22 00:36:46 +0100

    php70-pdo-dblib: update 7.0.10_1 bottle.

--- Formula/php70-pdo-dblib.rb
+++ Formula/php70-pdo-dblib.rb
@@ -5,6 +5,9 @@ class Php70PdoDblib < AbstractPhp70Extension
   desc "A unified Sybase-DB style driver for PDO"
   homepage "https://github.com/php/php-src/tree/master/ext/pdo_dblib"
   bottle do
+    sha256 "b0fb13b1243914c411880ca3ad93f9cdec54b4ca6cbe24135b0487b0ec1e499f" => :el_capitan
+    sha256 "ef11289346ad17827d7eae5203ad25b1f787b9be5a485c209c19b9e171794481" => :yosemite
+    sha256 "d0dfdf7d9093e68fbddce88be2e850e74aa1950468dc12246678b669b713b303" => :mavericks
   end
 
   url PHP_SRC_TARBALL
