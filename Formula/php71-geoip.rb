38d2561d38b472672311b61a148d57ea909adf1a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-27 10:29:43 +0100

    php71-geoip: add 1.1.1 bottle.

--- Formula/php71-geoip.rb
+++ Formula/php71-geoip.rb
@@ -8,6 +8,12 @@ class Php71Geoip < AbstractPhp71Extension
   sha256 "b2d05c03019d46135c249b5a7fa0dbd43ca5ee98aea8ed807bc7aa90ac8c0f06"
   head "https://svn.php.net/repository/pecl/geoip/trunk/"
 
+  bottle do
+    sha256 "cf4750fed2c134c794646d7b54d64f7622aa11254c9bf31a2cc5a336fc847eaa" => :el_capitan
+    sha256 "f59f96780bd10b9d68e30f284ca258a16c730c223cc3fb3c1928e820329e6742" => :yosemite
+    sha256 "78cd82bfe85b353b7f6fcd67bc485bc60e2e990d735a95dadbbacb6b349691b8" => :mavericks
+  end
+
   depends_on "geoip"
 
   def install
