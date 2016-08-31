666df4c4d56c9e104a0c19b34e617c1923d0d685
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-31 20:57:16 +0100

    php71-geos: add 1.0.0rc1 bottle.

--- Formula/php71-geos.rb
+++ Formula/php71-geos.rb
@@ -9,6 +9,13 @@ class Php71Geos < AbstractPhp71Extension
   sha256 "63721d9dc2051aa03034b343c0b334e9cb11643e7009ea9be2e69f51f7b90f71"
   head "https://git.osgeo.org/gogs/geos/php-geos.git"
 
+  bottle do
+    cellar :any
+    sha256 "b8cfa302799f6fcf264a03f1af55621d47d4b5a45da18fea322bbd623337ea1d" => :el_capitan
+    sha256 "a97ec63be5e253cb04a9603713b1534d677d24cdb8e94d6e191c6db329882e61" => :yosemite
+    sha256 "e8e6039d13c45a3c17cef05ff65189441a644d75559fac15195299e1076e781c" => :mavericks
+  end
+
   depends_on "geos"
 
   def install
