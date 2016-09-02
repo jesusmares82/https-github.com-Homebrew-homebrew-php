35d6324cb6a5651916dddf09668d5b69e2f215c5
Mikulas <rullaf@gmail.com>
2016-09-02 11:08:43 +0200

    php70-ds 1.1.5

--- Formula/php70-ds.rb
+++ Formula/php70-ds.rb
@@ -4,16 +4,9 @@ class Php70Ds < AbstractPhp70Extension
   init
   desc "Data Structures for PHP"
   homepage "https://github.com/php-ds/extension"
-  url "https://github.com/php-ds/extension/archive/v1.0.4.tar.gz"
-  sha256 "4cdad2bba1cae18f1196346913ac9a54a23f2e5874fd8678200f88730b2788c6"
+  url "https://github.com/php-ds/extension/archive/v1.1.5.tar.gz"
+  sha256 "0da5444d07cd283b369c98e177656a96508291b921c3e844b9be6c8269e08372"
   head "https://github.com/php-ds/extension.git"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "5b47e178c446b9663a03911e449fe4eb62a6dc86554d4f4dceff40cbf200ac97" => :el_capitan
-    sha256 "92ec724d8be0130f88be81258ecd014765dd9324d56331c857a595422b7ace82" => :yosemite
-    sha256 "957a3546f6826401babd464ca9b6a63fbadf5f53f65c11f9f82de49ce274eba6" => :mavericks
-  end
 
   def install
     ENV.universal_binary if build.universal?
