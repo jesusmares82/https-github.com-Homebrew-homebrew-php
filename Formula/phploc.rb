64bf7631c813446ba2a1bd94ff978e84a653e9cd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-03 09:27:15 +0100

    phploc: update 3.0.1 bottle.

--- Formula/phploc.rb
+++ Formula/phploc.rb
@@ -9,9 +9,9 @@ class Phploc < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "31e704690f7f364e2db91e276dd163e906100721db7bff020a62903242e8394a" => :el_capitan
-    sha256 "31d7a0a48a7b38c9bae423371225127620a411657c2bfeca40e294f84a12ad9f" => :yosemite
-    sha256 "8cb7901655ec30a4de41fa95a9480194f59915eeef6eb935882003b9245047ca" => :mavericks
+    sha256 "5bed4234b52f850227466925c4a5152792c2ddec3d8b6cbf5378c10799aa8ff9" => :el_capitan
+    sha256 "b770b3f23a8faee174aeb95af5d9df46b43bbddf9df2668603be6af947d9419a" => :yosemite
+    sha256 "b770b3f23a8faee174aeb95af5d9df46b43bbddf9df2668603be6af947d9419a" => :mavericks
   end
 
   def phar_file
