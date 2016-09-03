ac19a69517126c07ec0d73cbc86b534e6ef94a74
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-03 12:32:45 +0100

    php71-tidy: update 7.1.0-rc.1_2 bottle.

--- Formula/php71-tidy.rb
+++ Formula/php71-tidy.rb
@@ -5,9 +5,9 @@ class Php71Tidy < AbstractPhp71Extension
   desc "Tidy HTML clean and repair utility"
   homepage "http://php.net/manual/en/book.tidy.php"
   bottle do
-    sha256 "a45e9534d94abe3a4ac81ce89ad489df31bc7e084b72fee4305a5dc60e634dcf" => :el_capitan
-    sha256 "433d27736c087f58bb827afea9d0a26dfa76a03149984ed3416fc807cf465100" => :yosemite
-    sha256 "00a5cd3f978daf5d9f5c0a9ca1ccbe3062797347f445b3c37949af0174d7fdb3" => :mavericks
+    sha256 "d77a1a818f0e1a573079d79972dd05f4ebfa08441011555672fad48c156df607" => :el_capitan
+    sha256 "f2b0dcb6ca7761f11ccdbf8c6f0ad89d73fd2e0e43aabad4226d37c52d14db0f" => :yosemite
+    sha256 "d93b988da233012738f98ead9cdc46f4cfcb6e8ffd42671e434faeac212085ea" => :mavericks
   end
 
   url PHP_SRC_TARBALL
