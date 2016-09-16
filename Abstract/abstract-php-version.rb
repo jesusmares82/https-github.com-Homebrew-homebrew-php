68ea267a9436ec9ba61ec11d07b7332a95ba25c6
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-09-16 14:21:03 +0100

    PHP 7.0.11
    Closes #3577.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Abstract/abstract-php-version.rb
+++ Abstract/abstract-php-version.rb
@@ -51,13 +51,13 @@ class AbstractPhpVersion < Formula
   end
 
   module Php70Defs
-    PHP_SRC_TARBALL = 'https://php.net/get/php-7.0.10.tar.bz2/from/this/mirror'
+    PHP_SRC_TARBALL = 'https://php.net/get/php-7.0.11.tar.bz2/from/this/mirror'
     PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
-    PHP_VERSION     = '7.0.10'
+    PHP_VERSION     = '7.0.11'
     PHP_BRANCH      = 'PHP-7.0'
 
     PHP_CHECKSUM    = {
-                        :sha256 => '8055bbe5a736986931c0c6a08b765d6d778271ec7d2d56c50a1ad259ec09f6de',
+                        :sha256 => 'f99b729dc1149858844b18af1e8c0de6dd1cdfdd52e22fbb4de2aa78bf9bf7f1',
                       }
   end
 
