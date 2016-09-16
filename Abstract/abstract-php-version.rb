a545517a02dd96aa76db9d0817d8749dd18980c8
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-09-16 23:42:00 +0200

    PHP 7.1-RC2
    Closes #3581.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Abstract/abstract-php-version.rb
+++ Abstract/abstract-php-version.rb
@@ -62,13 +62,13 @@ class AbstractPhpVersion < Formula
   end
 
   module Php71Defs
-    PHP_SRC_TARBALL = 'https://downloads.php.net/~davey/php-7.1.0RC1.tar.bz2'
+    PHP_SRC_TARBALL = 'https://downloads.php.net/~davey/php-7.1.0RC2.tar.bz2'
     PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
-    PHP_VERSION     = '7.1.0-rc.1'
+    PHP_VERSION     = '7.1.0-rc.2'
     PHP_BRANCH      = 'PHP-7.1'
 
     PHP_CHECKSUM    = {
-                        :sha256 => '0ab8830dadcad235e4252feda8b33c43db1ba623926db6007aa1c392bb8ee524',
+                        :sha256 => '27f2a8a6268e787a96fafd75591c8934a4ac602f3bbfcfeba493bab6a6efb430',
                       }
   end
 end
