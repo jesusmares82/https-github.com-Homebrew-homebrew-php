915747adf4c907a4e7c80d97d55672a9b64860d0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 08:00:08 +0100

    php71: update 7.1.0-rc.2_5 bottle.
    Closes #3581.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php71.rb
+++ Formula/php71.rb
@@ -4,9 +4,9 @@ class Php71 < AbstractPhp
   init
   desc "PHP Version 7.1"
   bottle do
-    sha256 "f432f9a644ad25b532d96e4c483543ad6cf5bb1e48931bcc2342afbfa57226f0" => :el_capitan
-    sha256 "096deba01c0d6727f16e1e401a5e3b6790af6531fb904fbfb202d2d8ed99243b" => :yosemite
-    sha256 "0da6fef87a50946677c603b77b132324c950f67df394d26e4d2c6aed6a920e70" => :mavericks
+    sha256 "a92ba720107607a533953b9e8e50b4678d7a3814c134823829ab6423ebc49342" => :el_capitan
+    sha256 "05b684059b078d793316c53d66490cd6e42ea4e3dc747fed74a1f99541f0a810" => :yosemite
+    sha256 "b0bb37bb71f02abc6e0890143dbb556c0c2f9995703be3578db218c4d6fb454c" => :mavericks
   end
 
   include AbstractPhpVersion::Php71Defs
