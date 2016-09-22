e77fd5422a21235fff7d4160230de765c76a0c72
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-22 22:28:31 +0100

    behat: update 3.2.0 bottle.

--- Formula/behat.rb
+++ Formula/behat.rb
@@ -10,9 +10,9 @@ class Behat < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1eb06442283f593d321e1e06645d9bdd54439f47a524c2b149ce096ccecc2c8b" => :el_capitan
-    sha256 "bb69a6e6b65f8bfbfeafabfa763c0658d4677c9459eff538b18b4e991f0061ad" => :yosemite
-    sha256 "d05905075b0ece1cf4d92e46961d1ec0401279b172d77226ad6fabed1921a8bf" => :mavericks
+    sha256 "5ce4fa678695a86950658074af39b68e6808bcd85683081986d83aefc2e9430b" => :sierra
+    sha256 "5ce4fa678695a86950658074af39b68e6808bcd85683081986d83aefc2e9430b" => :el_capitan
+    sha256 "5ce4fa678695a86950658074af39b68e6808bcd85683081986d83aefc2e9430b" => :yosemite
   end
 
   test do
