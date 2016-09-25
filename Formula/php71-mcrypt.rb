40cca6b04f5e84c7b89c837610ab2691c28aa624
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 13:14:59 +0100

    php71-mcrypt: update 7.1.0-rc.2_3 bottle.

--- Formula/php71-mcrypt.rb
+++ Formula/php71-mcrypt.rb
@@ -5,9 +5,9 @@ class Php71Mcrypt < AbstractPhp71Extension
   desc "Interface to the mcrypt library"
   homepage "http://php.net/manual/en/book.mcrypt.php"
   bottle do
-    sha256 "36e3677c89638161b434e909b1ca7aba51cb6e410c4bb869e4f168916e713cf4" => :el_capitan
-    sha256 "3788a472631f5218c945cd797448b799df547dc2e8ae2e35baf832b3fec0567f" => :yosemite
-    sha256 "24618411c45dc8e53eae508ec22903e1c4b07b17e34b7cd64090225c8d41fe49" => :mavericks
+    sha256 "b9751018b30c293f536de0d96c6e99649cf2e267ca68c3016ed653bf2e316d2e" => :sierra
+    sha256 "8a07863dd615e89dab303e5f8158698b36a158d62ceb672ea91b4dea2a177e47" => :el_capitan
+    sha256 "227f740dcdcbef6afbc2c056d6ab51fb0b354fb0a1ca2f855fed0fb094bb6a58" => :yosemite
   end
 
   url PHP_SRC_TARBALL
