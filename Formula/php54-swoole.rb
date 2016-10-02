d203482659b44b52c80c9c815f995ed2a6891368
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-02 19:50:59 +0100

    php54-swoole: add 1.8.11-stable bottle.

--- Formula/php54-swoole.rb
+++ Formula/php54-swoole.rb
@@ -9,6 +9,13 @@ class Php54Swoole < AbstractPhp54Extension
   sha256 "cf2d9ba2c85f29c4a9f5e963878db27fdf552be7a05ca1709b6ad6f294e12b8e"
   head "https://github.com/swoole/swoole-src.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b6b577df243c1372e21037d87a29adb797520c546b90f5a197a10924836913c7" => :sierra
+    sha256 "2fa3f94db9285a91b38a9146a343a34366968346181126b046140bf5dd21805a" => :el_capitan
+    sha256 "e571e7056ff54e4eb6250feb7721f3a6425d12551f740dc5c6d84d223ce4ba1c" => :yosemite
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
