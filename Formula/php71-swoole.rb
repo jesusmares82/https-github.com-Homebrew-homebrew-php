32ef9807a7287a0eb42b591a86544b8c0b329a1e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-02 19:51:00 +0100

    php71-swoole: add 1.8.11-stable bottle.

--- Formula/php71-swoole.rb
+++ Formula/php71-swoole.rb
@@ -9,6 +9,13 @@ class Php71Swoole < AbstractPhp71Extension
   sha256 "cf2d9ba2c85f29c4a9f5e963878db27fdf552be7a05ca1709b6ad6f294e12b8e"
   head "https://github.com/swoole/swoole-src.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5d676eed8340a1f921cf7269ff30c301f3e9e496be8d0db68b814eabcaee0bea" => :sierra
+    sha256 "1d485a5186b66f5b4b04032d0b2b08d810045b750509e662c2b0caa055389157" => :el_capitan
+    sha256 "e75f116cb2e2a5a5a71dbf0e66e547c7bb977511ffde0c77e438b218ef2382fe" => :yosemite
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
