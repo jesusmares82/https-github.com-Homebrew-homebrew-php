5a8e940c658e02f3b5024b52ecb5812141610bfd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-02 22:07:33 +0100

    php70-ssh2: add 1.0 bottle.

--- Formula/php70-ssh2.rb
+++ Formula/php70-ssh2.rb
@@ -8,6 +8,12 @@ class Php70Ssh2 < AbstractPhp70Extension
   sha256 "6a93891878b23904a773eb814fec7aea4ea00b4a412ee779c8535ed9c5e46ced"
   head "https://github.com/php/pecl-networking-ssh2.git"
 
+  bottle do
+    sha256 "f70a1064538ef194c183ab19be83bc8b74abaf9b4838b4523b399a15bb4c41c8" => :sierra
+    sha256 "ef216adb7ef7a60839c87c11f87c38f0a6198149441bbadd9cb67d12d4c5879b" => :el_capitan
+    sha256 "b7bdfb56ee24d0a889b6510c3f9834ccd050bf1e81eb323194458f438ba9268d" => :yosemite
+  end
+
   depends_on "libssh2"
 
   def install
