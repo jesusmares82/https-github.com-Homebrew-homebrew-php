8234c6a585ece4966e2e3eb6a3d01cee74d85ba3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-07 15:12:35 +0100

    php71-v8js: add 1.3.3 bottle.

--- Formula/php71-v8js.rb
+++ Formula/php71-v8js.rb
@@ -7,6 +7,13 @@ class Php71V8js < AbstractPhp71Extension
   url "https://pecl.php.net/get/v8js-1.3.3.tgz"
   sha256 "049d04cbdcb901f358f04bfe0f89337c625b3fb0bbca335af2e2d0c4490412f2"
 
+  bottle do
+    cellar :any
+    sha256 "09c722ff0822acd8c45000249e1d9aaa381b23e9f00fe769337825d3cb931e7d" => :sierra
+    sha256 "9ef25eff64edb1a4bcbd5d06e6db18fbf5edaefc7b2b7114fbf6b50e7b2fce53" => :el_capitan
+    sha256 "1a6db9ed43b7ba01a66feb8db7aedb1bb123c19e9ae399b214bbbd9a98002264" => :yosemite
+  end
+
   depends_on "v8"
 
   def install
