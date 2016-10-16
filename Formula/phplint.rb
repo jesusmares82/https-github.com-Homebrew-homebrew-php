5bf40f178289138800ec99e3befba39d279f7601
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 22:44:49 +0100

    phplint: update 3.0-20160307 bottle.

--- Formula/phplint.rb
+++ Formula/phplint.rb
@@ -17,6 +17,7 @@ class Phplint < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "99ac1b93dae5a1faa33715eddf534db4fd89c685a314a41ee48cb2983fd40cce" => :sierra
     sha256 "a0bbcf5029964b90c8c0c92b60d8d38872ddc3eefaf277dbafad14b8fff60bdb" => :el_capitan
     sha256 "caec309e27822f33611334495bc5eb6246ae7d40b2317dd88b79958478b5918e" => :yosemite
     sha256 "73551ae1bd5d72be2d7b78e3edb8578eadcab799fc96a42c198c9eb456bba794" => :mavericks
