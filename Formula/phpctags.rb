b6a0572914088e24b08f06c1fe0cd5038488193b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 17:35:28 +0100

    phpctags: update 0.6.0 bottle.

--- Formula/phpctags.rb
+++ Formula/phpctags.rb
@@ -9,6 +9,7 @@ class Phpctags < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a7b00c376fd9ccf9f273e876bd11798dfb1d6ee0641d5ce9a7d16ff83dbb471b" => :sierra
     sha256 "3f830cb241d00b38569594b4963d3d337ef809e597a0b202bb0dc7ae437f0c7f" => :el_capitan
     sha256 "30aabc214d07d39589ba24bd74c065b5cc3dfd0b44be1417d83eeb62a5faab0f" => :yosemite
     sha256 "2673139ea129e2afca83fe7fa3cc4913def1cd3bfde5f453d9425aeaf0c83f64" => :mavericks
