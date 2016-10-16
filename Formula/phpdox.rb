2ceceed00c9b6e1bcfa7e2593c45833ee06b3088
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 22:44:49 +0100

    phpdox: update 0.8.1.1 bottle.

--- Formula/phpdox.rb
+++ Formula/phpdox.rb
@@ -9,6 +9,7 @@ class Phpdox < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fd01045fdc17fcc9ef8af18465dbd848c5481282f0ccd6df53907351084ae1d7" => :sierra
     sha256 "ecf5998f3f2d2a8e78d654edc5c7b1971e65d280a4742119bbbec11b4f62944b" => :el_capitan
     sha256 "859176b62c12d0998fbe93d83081403bbbeb1b49c07735d509d6889e9cb2a349" => :yosemite
     sha256 "e1071dce283fcf3193a45dddcac7042b4522137118b1ea74e3074ff7039c0203" => :mavericks
