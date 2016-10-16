a3ff5656490e88e442489f133d0a7c14b8118b82
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 14:20:14 +0100

    mondrian: update 1.3.3 bottle.

--- Formula/mondrian.rb
+++ Formula/mondrian.rb
@@ -10,6 +10,7 @@ class Mondrian < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "400b7e1ac40eb6cbf23492dcd711a615fb49a55e292c64d869445957e3019d03" => :sierra
     sha256 "93eb03f757e8b9d7d91f5a2c041901d56287e9174939554875bc2483f8bcce15" => :el_capitan
     sha256 "0a17c6fa9c186fc59d53f3d235a126b30a4d952c1acf49bd7a28df4f22972a10" => :yosemite
     sha256 "6eea47f4970445bac31fb66240b2dc24387277f4ef50a6a41eee400e45f46a86" => :mavericks
