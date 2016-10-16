88954764fe2f80a5bc45b47eb6d12aac7193b1b6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 15:28:46 +0100

    adminer: update 4.2.5 bottle.

--- Formula/adminer.rb
+++ Formula/adminer.rb
@@ -6,6 +6,7 @@ class Adminer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "883921ac55001d75a7e408a78d96aca522ebb8bd612bbf0c9c1af4691690c90a" => :sierra
     sha256 "e6b8ce60929c841c8755028d98a1fbee0d2e67ec7df23cfe5be674885351e71f" => :el_capitan
     sha256 "254c29ee3d70c9567473f4a47c3921ffb20a8fd2f6cd7478549b4bebdcf09ddd" => :yosemite
     sha256 "c989dd0e378e55f8ce2411790bd03337cdd9c2df218904248ea9144b615cd04e" => :mavericks
