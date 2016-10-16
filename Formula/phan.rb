1a922d807ee113a9a99702e6f10671ab5b4854c9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 14:20:14 +0100

    phan: update 0.6 bottle.

--- Formula/phan.rb
+++ Formula/phan.rb
@@ -11,6 +11,7 @@ class Phan < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "272002f72cb0ccd134a534d6cc849e06ccff009d5f794affe8318427662911c9" => :sierra
     sha256 "ce1cac3f8b542a71da05f94d4115bb3b043c68b2c823a28f4a91b5f432fe6000" => :el_capitan
     sha256 "df9a717231d8aa8133a14caccd75b9480d43d44259e8863dec6e6795ab86a17b" => :yosemite
     sha256 "b5e233ba17c17580ea5d671fe4c45a0aa5af9fbce7078191e9fe8d8e0d58dcb6" => :mavericks
