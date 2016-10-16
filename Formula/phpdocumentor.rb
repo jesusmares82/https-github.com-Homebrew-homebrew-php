37a7fe265ef3c9e0ed2c702ab56e05a1df0d770a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 17:35:28 +0100

    phpdocumentor: update 2.9.0 bottle.

--- Formula/phpdocumentor.rb
+++ Formula/phpdocumentor.rb
@@ -10,6 +10,7 @@ class Phpdocumentor < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9acbf5cef29ca455818fc58dd44baa9d724067e45268bcc01d3acef1a6677fd9" => :sierra
     sha256 "e9c30434edf6f8432a7afc0439c0584ecd0559c163dcd4f3d0466a0561fe8eef" => :el_capitan
     sha256 "80c05a819a837da97b3e72c52859864cd637be7ea54610c2a315922bdde5ce50" => :yosemite
     sha256 "7cd8bef75d61785af68117a53073d76d82cd6e4e0bc097ef54178e7b7493b012" => :mavericks
