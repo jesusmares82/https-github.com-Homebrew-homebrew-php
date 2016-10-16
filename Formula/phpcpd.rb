acbe4de98761f0d90f7d0ab1bfc946de4e2ce7b4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 17:35:28 +0100

    phpcpd: update 2.0.0 bottle.

--- Formula/phpcpd.rb
+++ Formula/phpcpd.rb
@@ -9,6 +9,7 @@ class Phpcpd < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e8b3ce1d18fb6002f9869290c97600e5737adeab348f0141d3af4839671e5af6" => :sierra
     sha256 "201ca00a23c810187c5b348979ac24e32f384ac86995f648ebf170884443a252" => :el_capitan
     sha256 "981f0a1a34c144f4ff5f7c3693b990bdc8b55bea35dcad135db6f2331050b304" => :yosemite
     sha256 "087aae9a8aa00fdee9da8379c3d99cbeb9a6b17215064f37e8515d02549275ad" => :mavericks
