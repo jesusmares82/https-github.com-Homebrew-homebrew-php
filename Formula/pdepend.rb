5978f6b543e622ec8e4d5790a9bfb3f7d5cd99ea
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 14:20:14 +0100

    pdepend: update 2.2.3 bottle.

--- Formula/pdepend.rb
+++ Formula/pdepend.rb
@@ -9,6 +9,7 @@ class Pdepend < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2c5bf14d987656c4329b3254db3eadc447b4df2906dac9684b5e24fcb5aea643" => :sierra
     sha256 "33b23b22dd8ea627fbf68bec2cd410dc6dc187223a838945a3ccf2e4e76f9e3d" => :el_capitan
     sha256 "548af8829d08c0407c4c3a3f5cd7bfde7336e38811276126bc4e5ddef011a8c8" => :yosemite
     sha256 "59cc3cd71a9e6d65d15bd27a36c793da2443720eb2a4dcdf24c12a3fbd5ac153" => :mavericks
