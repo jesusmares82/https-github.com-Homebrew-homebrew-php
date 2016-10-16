72e01720f6d49ea8ba2e0889bd0874ac4fabb3be
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-16 17:35:28 +0100

    php-plantumlwriter: update 1.6.0 bottle.

--- Formula/php-plantumlwriter.rb
+++ Formula/php-plantumlwriter.rb
@@ -10,6 +10,7 @@ class PhpPlantumlwriter < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7f82a56639fa67a63ef687771653b50f511be5d15877e86d050631350368f4ed" => :sierra
     sha256 "5124040d7593dc423a8c50eeb3e3961d47f66d017d76b7805c122b4edf74361a" => :el_capitan
     sha256 "a2ea4a2c54d13207042be78ed52afd7c782306638b05d4572773fec947bfdb13" => :yosemite
     sha256 "b4625f6b67bc7cdfa097041c58142a9a8dc69008089bd66f1bfd46c59af5b847" => :mavericks
