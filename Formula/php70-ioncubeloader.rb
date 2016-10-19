cb2d1dc8f9525a2a55e53b811ef896e2e752c7cd
Alexander Turiak <alex@hexbrain.com>
2016-10-19 13:15:04 +0200

    php70-ioncubeloader 6.0.5 (new formula)
    Closes #3719.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php70-ioncubeloader.rb
@@ -0,0 +1,31 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Ioncubeloader < AbstractPhp70Extension
+  init
+  desc "Loader for ionCube Secured Files"
+  homepage "http://www.ioncube.com/loaders.php"
+  if MacOS.prefer_64_bit?
+    url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz"
+    sha256 "e3649def6e14f8d6aad08326e6579a7b7e660e3eca65c240755f759b3920efa8"
+  else
+    url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86.tar.gz"
+    sha256 "96a0e211971352acb843718565c371938881da7d77dd8bca7519466f06dfdda2"
+  end
+  version "6.0.5"
+
+  bottle do
+  end
+
+  def extension_type
+    "zend_extension"
+  end
+
+  def install
+    prefix.install "ioncube_loader_dar_7.0.so" => "ioncubeloader.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  test do
+    shell_output("php -m").include?("ionCube")
+  end
+end
