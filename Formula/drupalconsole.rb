109a072c9a06abd356d5b0c2d1e6a63f7b98fb16
Jordan Ryan <jordan@facetinteractive.com>
2016-10-22 15:28:04 -0700

    Drupalconsole 1.0.0-rc6
    Closes #3722.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/drupalconsole.rb
+++ Formula/drupalconsole.rb
@@ -4,16 +4,8 @@ class Drupalconsole < AbstractPhpPhar
   init
   desc "CLI tool for Drupal 8"
   homepage "https://drupalconsole.com/"
-  url "https://github.com/hechoendrupal/DrupalConsole/releases/download/0.11.3/drupal.phar"
-  sha256 "a74fb7b7656cf854e7683cd32fc5c7f59f2ae3245f0ffebbb56f6160cf150e95"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "b28218cb4b46d93a8f3c6dd73dc87d0e59d55f6cf7f32bdb9eeae2406bda36ec" => :sierra
-    sha256 "5f0f33a6efa98195201fd9ee07dd0e167e91d8ca5a1f075ccd7350615ef3caaf" => :el_capitan
-    sha256 "6e0e1475d51bd51beaf019e48a15eeb87806d968363bc47335ab0b4f512680d5" => :yosemite
-    sha256 "dda2361d56afa627e49b4e6f19cce6ef8cd02b4df70e8bd47de9c87ce2e6bfcd" => :mavericks
-  end
+  url "https://github.com/hechoendrupal/drupal-console-launcher/releases/download/1.0.0-rc6/drupal.phar"
+  sha256 "f6ec69d78a4f72507ea01880f52e898df9af5cc10d703822759ebd1f32c34b36"
 
   def install
     bin.install "drupal.phar" => "drupal"
