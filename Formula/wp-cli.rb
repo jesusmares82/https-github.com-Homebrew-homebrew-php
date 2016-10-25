46ee1fdb1cdbfb072992cf500be19d451c8313cf
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-25 12:58:40 -0700

    wp-cli 0.25.0
    Closes #3735.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -5,8 +5,8 @@ class WpCli < Formula
 
   desc "Command-line tools for managing WordPress installations."
   homepage "http://wp-cli.org/"
-  url "https://github.com/wp-cli/wp-cli/archive/v0.24.1.tar.gz"
-  sha256 "df6dc42349fbe658c8c5a4fb2664b0eefe74a1fa4fba163f7db047fe4200c512"
+  url "https://github.com/wp-cli/wp-cli/archive/v0.25.0.tar.gz"
+  sha256 "98526f880619bf95959ae9812204af7de6949e295dc1dcb4f9007d0355c4f7c7"
   head "https://github.com/wp-cli/wp-cli.git"
 
   bottle do
