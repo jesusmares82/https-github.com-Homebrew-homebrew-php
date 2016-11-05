224c044791ae47f8763b0095608be59269b3fa84
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-05 15:17:57 +0000

    php71-redis: add 3.0.0 bottle.

--- Formula/php71-redis.rb
+++ Formula/php71-redis.rb
@@ -8,6 +8,14 @@ class Php71Redis < AbstractPhp71Extension
   sha256 "030997370bb1906793989c89550d9cafd4fa35dccbad7040b2339301aa961dba"
   head "https://github.com/phpredis/phpredis.git", :branch => "php7"
 
+  bottle do
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "7ef31f81fcdf58c5209b93e029049166aaab19b42fc1c7f575e9f4078cca4e23" => :sierra
+    sha256 "b4d095ce7ae9b6b5e189df099f630557e48f4e6a037a230a139e4e00b164b7c0" => :el_capitan
+    sha256 "11496ebc40b5fcd4fb9969afdaa7ca877ac7bd6c1df492e3c48288a606b2527e" => :yosemite
+  end
+
   def install
     safe_phpize
 
