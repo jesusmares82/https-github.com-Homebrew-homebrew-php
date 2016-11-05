f599be29b9db67b4ea89785f6615ce7a6672f5a1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-05 15:50:01 +0000

    php53-intl: update 5.3.29_2 bottle.

--- Formula/php53-intl.rb
+++ Formula/php53-intl.rb
@@ -10,9 +10,9 @@ class Php53Intl < AbstractPhp53Extension
   revision 2
 
   bottle do
-    sha256 "f1d57c0a55472bcfe6fc1ec2ba5c9582d10d91e2b955c8ddaab723441833a13e" => :el_capitan
-    sha256 "b0b1300d845b3bef8c19e3449dc20d695ee82c450d8052ae5ab76d18e2538439" => :yosemite
-    sha256 "139a0f834ca7a8e30c514ddd60b04af1c57a5f7db2f788b9f0b808ab24197d57" => :mavericks
+    sha256 "26683df2d453e0518b26fa5bae01a45c3ab7255dd45b910653ee1c0d6103383e" => :sierra
+    sha256 "4b38a186b09874a39dc14247859cb128dc141caf191dca0cbfc1e981fb8d9ccd" => :el_capitan
+    sha256 "79f69188864b93e5654c7fc41a14c61142062641eaec71544bb195380c8e49a3" => :yosemite
   end
 
   depends_on "icu4c"
