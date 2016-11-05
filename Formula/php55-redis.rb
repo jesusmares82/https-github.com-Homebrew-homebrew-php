5933d1fb4435b12d769aa6da6cd9f394da9a93bb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-05 15:26:31 +0000

    php55-redis: add 2.2.8 bottle.

--- Formula/php55-redis.rb
+++ Formula/php55-redis.rb
@@ -8,6 +8,13 @@ class Php55Redis < AbstractPhp55Extension
   sha256 "a7c6e2eef70bd8449bab819c8f01d951fcd86ce0417e03675445040236ed4dda"
   head "https://github.com/phpredis/phpredis.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "45ff6a0965fad42273d3700fea941d4ec6eba92acdfef6e3c4c7fd8498f5a6ee" => :sierra
+    sha256 "1463397a357311d6650dc6cddab934f0f48dccc13c488fab16b963061ec47a4a" => :el_capitan
+    sha256 "9d95af58c747c4f183831b07ec1aad667170c724516c4f8e69ca0d68c41447d6" => :yosemite
+  end
+
   depends_on "php55-igbinary"
   depends_on "igbinary" => :build
 
