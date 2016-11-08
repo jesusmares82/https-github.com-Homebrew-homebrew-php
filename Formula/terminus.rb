cf87ec2748be64a740f2391b335a1c18df9e9761
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-08 11:21:44 +0000

    terminus: update 0.13.4 bottle.

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -12,9 +12,9 @@ class Terminus < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fddfe6d48408a1050de0e6018dd9a2d17655f852933fdd742f1ca129b3a77ecf" => :sierra
-    sha256 "fca0be7f546dedd9a3a5f325c0681f98c9b4855b8abcc540d83935740182ad3c" => :el_capitan
-    sha256 "3bb3990b8d99c06790e84200e32edbbb5aeeacd5354bcc4c943f515932141815" => :yosemite
+    sha256 "258772b4096aba8d589aebcf3a1c5341e6badadea4e2b04e1fc5baeff1c2a417" => :sierra
+    sha256 "b9562051b13b2b0a8a64d47dee99c932866306cdd9d1d17946e36ee43f61e2e5" => :el_capitan
+    sha256 "bdfa5baca6ed85c78063e313f12e1b4c8161f73c74eb7888f6a118f09b1b4eff" => :yosemite
   end
 
   depends_on PhpMetaRequirement
