58a17c7d7af5f72c5e7be1783ee18acf8817b845
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-15 23:14:08 +0000

    phing: add 2.15.2 bottle.

--- Formula/phing.rb
+++ Formula/phing.rb
@@ -7,6 +7,13 @@ class Phing < AbstractPhpPhar
   url "https://www.phing.info/get/phing-2.15.2.phar"
   sha256 "842ad05e71597263f795239487e8ed3b3f170ed7b757cc442cec61d9d413f220"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0e3e0bd42733e37b15db41518788939a29783f527c24d96ea508b13aa08e8fc6" => :sierra
+    sha256 "0e3e0bd42733e37b15db41518788939a29783f527c24d96ea508b13aa08e8fc6" => :el_capitan
+    sha256 "0e3e0bd42733e37b15db41518788939a29783f527c24d96ea508b13aa08e8fc6" => :yosemite
+  end
+
   def phar_file
     "phing-#{version}.phar"
   end
