a3c0b57b8f5abdf91fde8a848322926b4faee407
pd4d10 <pd4d10@gmail.com>
2016-11-19 20:24:23 +0800

    php-5{3-6}-yac 0.9.2
    Closes #3778.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php53-yac.rb
+++ Formula/php53-yac.rb
@@ -2,11 +2,12 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
 class Php53Yac < AbstractPhp53Extension
   init
-  desc "A fast shared memory user data cache for PHP"
+  desc "Fast shared memory user data cache for PHP"
   homepage "https://github.com/laruence/yac"
-  url "https://github.com/laruence/yac.git", :branch => "master"
-  head "https://github.com/laruence/yac.git"
-  version "latest"
+  url "https://github.com/laruence/yac/archive/yac-0.9.2.tar.gz"
+  sha256 "d99023db0f73b0d3ba07b5ed1e9386182c5040202b1c2a4891e90df7b040afba"
+  version_scheme 1
+  head "https://github.com/laruence/yac/tree/php5"
 
   def install
     ENV.universal_binary if build.universal?
