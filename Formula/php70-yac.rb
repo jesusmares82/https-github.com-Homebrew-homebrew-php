5d8200bb444fe1b691caa7adfe8cd7c57a7dedb3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-19 12:49:20 +0000

    php70-yac: add 2.0.1 bottle.

--- Formula/php70-yac.rb
+++ Formula/php70-yac.rb
@@ -8,6 +8,13 @@ class Php70Yac < AbstractPhp70Extension
   sha256 "c9357c9779a87370b53f719cfcf86b80258b119376c718fea336988b1d7354a0"
   head "https://github.com/laruence/yac.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a5856bffa00b481024c36c738e4322ed7162d4888f7dcd2de7af2ef87b620d1c" => :sierra
+    sha256 "6f8d9fc3463f01359aab9f3d105dbb32988dddd5b6d147385d7530a4ab9e827f" => :el_capitan
+    sha256 "a726f09d9008b453842a8a6d269d63b57ac078458f2fedb54962aef4e733685b" => :yosemite
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
