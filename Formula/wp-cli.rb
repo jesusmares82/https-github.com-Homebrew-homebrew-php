dcee741c0706805a4005418770806893b96b22d7
Daniel Bachhuber <daniel@handbuilt.co>
2016-11-29 12:47:05 -0800

    wp-cli 1.0.0
    Closes #3790.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -5,8 +5,8 @@ class WpCli < Formula
 
   desc "Command-line tools for managing WordPress installations."
   homepage "http://wp-cli.org/"
-  url "https://github.com/wp-cli/wp-cli/archive/v0.25.0.tar.gz"
-  sha256 "98526f880619bf95959ae9812204af7de6949e295dc1dcb4f9007d0355c4f7c7"
+  url "https://github.com/wp-cli/wp-cli/archive/v1.0.0.tar.gz"
+  sha256 "7fd238e93ff24ee417a40aa5e147545e8859f386a1c106e251ce34ca8dd84b15"
   head "https://github.com/wp-cli/wp-cli.git"
 
   bottle do
