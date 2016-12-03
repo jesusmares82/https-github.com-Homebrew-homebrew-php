b6f8025251206672423a2b515b4adb2a51f2af7a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-03 15:47:18 +0000

    igbinary: update 2.0.1 bottle.

--- Formula/igbinary.rb
+++ Formula/igbinary.rb
@@ -7,11 +7,9 @@ class Igbinary < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "8f477e5210cdd1a79bac520f9aa978492ad0e82cac8c6d208325b24280a8121a" => :sierra
-    sha256 "634d3f7193996f225520d30366567680a50ac51a6ebbef923f194f8ce70a5955" => :el_capitan
-    sha256 "5a38a58cfb7ca2749a478c9efb329e55b5bd9e9157d0874d672eed5a4a369cc4" => :yosemite
-    sha256 "34d8a2aa98964de408be73d2a79eba66114dfe9fc04643e69d3c0cff372bfed3" => :mavericks
+    sha256 "187670cda5b54b642f3d447999cd52155579c5c898bb5ed017ef5ab9a5c4d854" => :sierra
+    sha256 "187670cda5b54b642f3d447999cd52155579c5c898bb5ed017ef5ab9a5c4d854" => :el_capitan
+    sha256 "187670cda5b54b642f3d447999cd52155579c5c898bb5ed017ef5ab9a5c4d854" => :yosemite
   end
 
   def install
