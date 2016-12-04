15e85464cc9c6291fb7bee3ccf21f098f83e3ee4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-04 18:51:21 +0000

    php71-rdkafka: add 2.0.1 bottle.

--- Formula/php71-rdkafka.rb
+++ Formula/php71-rdkafka.rb
@@ -8,6 +8,13 @@ class Php71Rdkafka < AbstractPhp71Extension
   sha256 "43a3704198f7d8ae23f6ed06d2d28ae4d52bae0f93f484aa5d61d5d60f809eee"
   head "https://github.com/arnaud-lb/php-rdkafka.git"
 
+  bottle do
+    cellar :any
+    sha256 "ee5684377bf7c9a902e1d790814f26f0c1bfe9ebde92ea35c73edf0bd737c6f2" => :sierra
+    sha256 "e7d2e68ed4aca305bab9c50cc872b7c503cd7855c43959fd7f65810bfa1284d7" => :el_capitan
+    sha256 "556fbd4afeef2181143e469f761109bb310cb8a6bb87050bfc53bce97cf63f46" => :yosemite
+  end
+
   depends_on "librdkafka"
 
   def install
