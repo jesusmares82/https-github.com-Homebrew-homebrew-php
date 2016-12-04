d0820c0c764869cc63d5a22cdc64718b9b9af49d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-04 18:51:20 +0000

    php55-rdkafka: update 1.0.0 bottle.

--- Formula/php55-rdkafka.rb
+++ Formula/php55-rdkafka.rb
@@ -10,8 +10,9 @@ class Php55Rdkafka < AbstractPhp55Extension
 
   bottle do
     cellar :any
-    sha256 "6dc5cf3e3d0f2eb51900a32245eb4068eb50bd66466acfa433eb75bc732d7ab3" => :el_capitan
-    sha256 "7929180dd724ce7e2c7fae745f458470b120cc3dc60da46cb33a350f2c5641c1" => :yosemite
+    sha256 "c9ac2b06519bccf6190033b97a6bc01f5169e24c7ba7a8b9b445315822d8ee18" => :sierra
+    sha256 "a78a94d2bd1c6c7d0dcad1006b6eb4841a315028e4c2fd41846aee8fa6658d53" => :el_capitan
+    sha256 "f9ebbe2c292ec90c0a6180c6e6d7b23193e7e4d302855b0ce4958093ef1717df" => :yosemite
   end
 
   depends_on "librdkafka"
