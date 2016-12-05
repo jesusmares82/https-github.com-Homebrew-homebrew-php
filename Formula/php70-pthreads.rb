191c791e33af1b4efb95557c3f1540eed0343a56
Martin Sikora <martin.sikora.ahoj@gmail.com>
2016-12-05 10:19:29 +0100

    php70-pthreads 3.1.6
    Closes #3810.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php70-pthreads.rb
+++ Formula/php70-pthreads.rb
@@ -4,8 +4,8 @@ class Php70Pthreads < AbstractPhp70Extension
   init ["with-thread-safety"]
   desc "Threading API"
   homepage "https://pecl.php.net/package/pthreads"
-  url "https://pecl.php.net/get/pthreads-3.1.5.tgz"
-  sha256 "e59a4e02b0eaff08d9522bf8b491f47187ce0dfe96709a243e2cffe12fb82afe"
+  url "https://pecl.php.net/get/pthreads-3.1.6.tgz"
+  sha256 "bb13da909a7a7ae1f9e499166103a2d24628993238ce03a8aae3eaa492c0b736"
   head "https://github.com/krakjoe/pthreads.git"
 
   bottle do
