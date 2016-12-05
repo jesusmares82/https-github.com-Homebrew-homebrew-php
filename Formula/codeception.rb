ccbc9501ea2616c3f40d0562437f698111aed65b
Alexander Turiak <alex@hexbrain.com>
2016-12-05 11:35:15 +0100

    codeception 2.2.7

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -4,8 +4,8 @@ class Codeception < AbstractPhpPhar
   init
   desc "Testing Framework designed to work just out of the box"
   homepage "http://codeception.com/quickstart"
-  url "http://codeception.com/releases/2.2.6/codecept.phar"
-  sha256 "21ebede0380599dcf7d26a364de355e79d93f14f7acbb355c2ded094d5a57bb1"
+  url "http://codeception.com/releases/2.2.7/codecept.phar"
+  sha256 "4e0a61b6e957d970b9d537cfd79a95ca624116f2083ba6cd25318e6ce81868d1"
 
   bottle do
     cellar :any_skip_relocation
@@ -23,6 +23,6 @@ class Codeception < AbstractPhpPhar
   end
 
   test do
-    system "codecept", "--version"
+    system "#{bin}/codecept", "--version"
   end
 end
