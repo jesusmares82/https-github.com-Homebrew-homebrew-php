a0a0922424c977b0cd34e96733a9ba43018453f3
Andreas Möller <am@localheinz.com>
2016-12-06 15:54:29 +0100

    Fix: Update gearman for php70-gearman
    Closes #3814.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php70-gearman.rb
+++ Formula/php70-gearman.rb
@@ -4,14 +4,11 @@ class Php70Gearman < AbstractPhp70Extension
   init
   desc "PHP wrapper to libgearman"
   homepage "https://github.com/wcgallego/pecl-gearman"
-  url "https://github.com/wcgallego/pecl-gearman/archive/gearman-2.0.1.tar.gz"
-  sha256 "af5172d68fbf111e143d0f8e3a583532b236f1a7137696bc2117955034149adf"
+  url "https://github.com/wcgallego/pecl-gearman/archive/gearman-2.0.3.tar.gz"
+  sha256 "f71e8ff218f31e3b9a15534e18846b9f9526319daffcc12e76b545889e44e130"
   head "https://github.com/wcgallego/pecl-gearman.git"
 
   bottle do
-    sha256 "c8e381e4245466a33d12effddc615406e6267ed36e0137754f1aaba5e738842d" => :el_capitan
-    sha256 "0575426337a8092ae1e6dae0610d2b52f88db95c43319ce6b325e2fddbeaa174" => :yosemite
-    sha256 "4a7f939d1701b6eb4276def33127311d7adcede0a8006873074f4cbf6fb3ca53" => :mavericks
   end
 
   depends_on "gearman"
