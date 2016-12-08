cee1dbf4d6ad35f6594e3b71f9c530a173e0b0b1
Boban Acimovic <boban.acimovic@planet-sports.com>
2016-12-08 17:05:24 +0100

    php{70-71}-yaml 2.0.0
    Updated PHP 7.1 yaml formulas to use PECL yaml 2.0.0
    
    Closes #3824.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php70-yaml.rb
+++ Formula/php70-yaml.rb
@@ -4,7 +4,12 @@ class Php70Yaml < AbstractPhp70Extension
   init
   desc "YAML-1.1 parser and emitter"
   homepage "https://pecl.php.net/package/yaml"
+  url "https://pecl.php.net/get/yaml-2.0.0.tgz"
+  sha256 "ef13ff56c184290c025a522bf9ae2e1b3ecc8543c3a5161dd02adec90897a221"
   head "https://github.com/php/pecl-file_formats-yaml.git", :branch => "php7"
+
+  bottle do
+  end
 
   depends_on "libyaml"
 
--- Formula/php71-yaml.rb
+++ Formula/php71-yaml.rb
@@ -4,14 +4,11 @@ class Php71Yaml < AbstractPhp71Extension
   init
   desc "YAML-1.1 parser and emitter"
   homepage "https://pecl.php.net/package/yaml"
-  url "https://pecl.php.net/get/yaml-2.0.0RC8.tgz"
-  sha256 "44cab15d57b2fb8fa916ff9291059a34b0c3fa99298c2b08a5082f334613753a"
+  url "https://pecl.php.net/get/yaml-2.0.0.tgz"
+  sha256 "ef13ff56c184290c025a522bf9ae2e1b3ecc8543c3a5161dd02adec90897a221"
   head "https://github.com/php/pecl-file_formats-yaml.git", :branch => "php7"
 
   bottle do
-    sha256 "951d6fe821938247b87d9fdca1b80a2b7df212483bfdf14c6090b522cdcef85b" => :el_capitan
-    sha256 "186f7dd99eb7c2a688379979a2242a95fdd01868d67e89ebde27eb08dbba65b0" => :yosemite
-    sha256 "793f8e8281fa16883a6f7c30b32990e5339371239984c2bddeb9fc3a9806f7fe" => :mavericks
   end
 
   depends_on "libyaml"
