a63b9890d9f11ce3602f1a1cd4362a308c7da7c9
Jun Watanabe <j.watanabe@oratta.net>
2016-12-08 19:48:18 +0900

    php71-msgpack 2.0.2 (new formula)
    Closes #3822.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-msgpack.rb
@@ -0,0 +1,23 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Msgpack < AbstractPhp71Extension
+  init
+  desc "MessagePack serialization"
+  homepage "https://pecl.php.net/package/msgpack"
+  url "https://pecl.php.net/get/msgpack-2.0.2.tgz"
+  sha256 "b04980df250214419d9c3d9a5cb2761047ddf5effe5bc1481a19fee209041c01"
+  head "https://github.com/msgpack/msgpack-php.git"
+
+  def install
+    Dir.chdir "msgpack-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/msgpack.so"
+    write_config_file if build.with? "config-file"
+  end
+end
