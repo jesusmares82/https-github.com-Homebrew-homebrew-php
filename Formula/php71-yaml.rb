cb1f6fc299b02bc0a9df4a1373cea35e9b5d15fc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-08 18:34:11 +0000

    php71-yaml: update 2.0.0 bottle.

--- Formula/php71-yaml.rb
+++ Formula/php71-yaml.rb
@@ -9,6 +9,9 @@ class Php71Yaml < AbstractPhp71Extension
   head "https://github.com/php/pecl-file_formats-yaml.git", :branch => "php7"
 
   bottle do
+    sha256 "844ce4f439a430accbe015f18208a349a5f8ba44d8033cca0c310ba95b13e7ea" => :sierra
+    sha256 "0812481119e076e37d0e08ae121e1cff29d09a28ac223c08c85286f6477ee4a8" => :el_capitan
+    sha256 "a4e41bd3df6502a7d4a1d7ba6eb97b486c7bc9c5d5a3a244db762a8e1be4e032" => :yosemite
   end
 
   depends_on "libyaml"
