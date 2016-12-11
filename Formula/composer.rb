54da5c7a454b315145eed9e4d8e51bf900f53fe5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-11 20:47:02 +0000

    composer: update 1.2.4 bottle.

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -10,9 +10,10 @@ class Composer < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f5b9d765b8241ac96a7ccb12cda8d81f71e1a9a258a36c54abbdf3192a847eeb" => :sierra
-    sha256 "f5b9d765b8241ac96a7ccb12cda8d81f71e1a9a258a36c54abbdf3192a847eeb" => :el_capitan
-    sha256 "f5b9d765b8241ac96a7ccb12cda8d81f71e1a9a258a36c54abbdf3192a847eeb" => :yosemite
+    rebuild 1
+    sha256 "80a1aa34f374451eb41e191b35324b608782885eda1ea75eec817deed0420a01" => :sierra
+    sha256 "e922f26368316a63bfe286829d748c1ade48bc608f3cfeb30e26866785a41ccd" => :el_capitan
+    sha256 "c8b87211d540a959f8ce09059798d3785c1e5a037535a209278c21534b8572a0" => :yosemite
   end
 
   devel do
