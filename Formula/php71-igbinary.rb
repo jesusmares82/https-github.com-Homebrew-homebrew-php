c167ad57f96c4a8bd8dd707cd6a8dc9612cc3119
Boban Acimovic <boban.acimovic@planet-sports.com>
2016-12-14 17:24:42 +0100

    php71-igbinary 2.0.1 (new formula)
    Introduced php71-igbinary formula
    
    Closes #3853.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php71-igbinary.rb
@@ -0,0 +1,41 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Igbinary < AbstractPhp71Extension
+  init
+  desc "Igbinary is a replacement for the standard php serializer."
+  homepage "https://pecl.php.net/package/igbinary"
+  url "https://github.com/igbinary/igbinary/archive/2.0.1.tar.gz"
+  sha256 "9c66e6bb8225bf559148661d8ef81451e5f67f0a565d975dc0918abd8c35e0ed"
+  head "https://github.com/igbinary/igbinary.git"
+
+  bottle do
+    cellar :any_skip_relocation
+  end
+
+  depends_on "igbinary" => :build
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/igbinary.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+      ; Enable or disable compacting of duplicate strings
+      ; The default is On.
+      ;igbinary.compact_strings=On
+
+      ; Use igbinary as session serializer
+      ;session.serialize_handler=igbinary
+
+      ; Use igbinary as APC serializer
+      ;apc.serializer=igbinary
+    EOS
+  end
+end
