1131c3662f4f8a240b0ee7d59119af28a197cce8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-14 16:53:01 +0000

    php71-igbinary: update 2.0.1 bottle.

--- Formula/php71-igbinary.rb
+++ Formula/php71-igbinary.rb
@@ -10,6 +10,9 @@ class Php71Igbinary < AbstractPhp71Extension
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f4f28ded4c9c9d5da76a6fd5fc31c72644bacf403acf58b1ac655a32b3e75670" => :sierra
+    sha256 "f287afa211862c3ca24c055f2ff6821b5633ec538891eee9f85fbfbea2149f1c" => :el_capitan
+    sha256 "c82492bb40f065583d297ec9ec09452f7f0bcc6f6aa960a61d124e391948f2c1" => :yosemite
   end
 
   depends_on "igbinary" => :build
