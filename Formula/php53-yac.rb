7efe804a3b7a19003591a4e9803dd2de02b5f37e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-15 14:47:18 +0000

    php53-yac: add 0.9.2 bottle.

--- Formula/php53-yac.rb
+++ Formula/php53-yac.rb
@@ -6,6 +6,13 @@ class Php53Yac < AbstractPhp53Extension
   homepage "https://github.com/laruence/yac"
   url "https://github.com/laruence/yac/archive/yac-0.9.2.tar.gz"
   sha256 "d99023db0f73b0d3ba07b5ed1e9386182c5040202b1c2a4891e90df7b040afba"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dac0bc5a77331815a45ebe3fa1150544184d7a6ba90b19d0b1ef8eb07b6fa114" => :sierra
+    sha256 "01189df71f37190d71c205f6cccda4b89e7db58dd451ac6e715ece59de8fe5e1" => :el_capitan
+    sha256 "f13b1f98c9887267244a00ea9ce308985380b3c7ece2e6f78da56a51613939cd" => :yosemite
+  end
+
   version_scheme 1
   head "https://github.com/laruence/yac/tree/php5"
 
