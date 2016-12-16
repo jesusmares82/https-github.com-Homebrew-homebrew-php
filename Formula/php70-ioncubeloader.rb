fe966399a0196e9c14b0a8397b103c2458c4d8f8
Patrício dos Santos <eu@patricio.me>
2016-12-16 16:14:54 +0100

    pho70-ioncubeloader 6.0.8
    Closes #3856.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php70-ioncubeloader.rb
+++ Formula/php70-ioncubeloader.rb
@@ -6,12 +6,12 @@ class Php70Ioncubeloader < AbstractPhp70Extension
   homepage "http://www.ioncube.com/loaders.php"
   if MacOS.prefer_64_bit?
     url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz"
-    sha256 "808b6bf93662f96579ad4ce9b6d4c999d98e6e543f9630b45a2b31c2907127d5"
+    sha256 "267f9c4810eec91420f338e3206c1fce02e4ee85e1c491a7234707ab7fb200f5"
   else
     url "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86.tar.gz"
-    sha256 "0586f13ac48e383309b7d56bb5ae7b4ee9872d012b6b9b78b273f1de780c4123"
+    sha256 "1413fd42c03eabfcae3b37ebbbf1fbe08d89b865c07dc166231ef1fda26fe2aa"
   end
-  version "6.0.6"
+  version "6.0.8"
 
   bottle do
     cellar :any_skip_relocation
