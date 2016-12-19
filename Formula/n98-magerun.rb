3012e51e2a21df31999b10ab9af1d77bbfe6b479
Alexander Martinkevich <a.martinkevich@mageworx.com>
2016-12-19 17:47:37 +0300

    n98-magerun v1.97.27
    Closes #3865.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/n98-magerun.rb
+++ Formula/n98-magerun.rb
@@ -4,8 +4,8 @@ class N98Magerun < AbstractPhpPhar
   init
   desc "Swiss army knife for Magento developers, sysadmins and devops."
   homepage "http://magerun.net/"
-  url "https://files.magerun.net/n98-magerun-1.97.22.phar"
-  sha256 "199dd0d416260c68e691580ffd3f7a448d60f616529aadadcfee584992d6253e"
+  url "https://files.magerun.net/n98-magerun-1.97.27.phar"
+  sha256 "86e5b6963ca19a78e5c8626ec90710a3967963a86ecef3d4852b5a2e617cf5be"
 
   bottle :unneeded
 
