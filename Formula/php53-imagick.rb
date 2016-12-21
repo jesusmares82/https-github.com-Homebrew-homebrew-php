0c6033fbd16f825f886d7b59f277bfd5b039cf51
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-21 23:10:48 +0000

    php53-imagick: update 3.3.0_6 bottle.

--- Formula/php53-imagick.rb
+++ Formula/php53-imagick.rb
@@ -9,9 +9,9 @@ class Php53Imagick < AbstractPhp53Extension
   revision 6
 
   bottle do
-    sha256 "d626cbb8ebf20a789f1f30e783f589ec3b5251bf795765deb14ea239528cd2d5" => :sierra
-    sha256 "1ebbbd3e747616f57065e0da2b44fb2fe4dc9062049e9adebc1497de92fae974" => :el_capitan
-    sha256 "0a03284de0eed1a56bb79c86e76a4a512d5f3b493e7d047bd71dc5c748402015" => :yosemite
+    sha256 "fe30a14a3c8d3582a8f192d3abb6af71abe434cad63dc7405926c61b88b2368a" => :sierra
+    sha256 "c626c396414930b0a41fa51452d38b5e2d2a7ff320ef7d8a40bfa4afa498aee1" => :el_capitan
+    sha256 "fb7d9ef0292faa59d6b8f7c545c2676aa030c1993467ee89ebc80cd61515d24a" => :yosemite
   end
 
   depends_on "pkg-config" => :build
