a14bcb7a74214e38223c3aaed4647c274cb178cb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-25 18:39:32 +0000

    phpdox: update 0.9.0 bottle.

--- Formula/phpdox.rb
+++ Formula/phpdox.rb
@@ -9,10 +9,9 @@ class Phpdox < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fd01045fdc17fcc9ef8af18465dbd848c5481282f0ccd6df53907351084ae1d7" => :sierra
-    sha256 "ecf5998f3f2d2a8e78d654edc5c7b1971e65d280a4742119bbbec11b4f62944b" => :el_capitan
-    sha256 "859176b62c12d0998fbe93d83081403bbbeb1b49c07735d509d6889e9cb2a349" => :yosemite
-    sha256 "e1071dce283fcf3193a45dddcac7042b4522137118b1ea74e3074ff7039c0203" => :mavericks
+    sha256 "7fb217dee5ed31c1f9ac018b7363fc84a6af1261815eb40146ee708d738176f1" => :sierra
+    sha256 "5021b218d542b7524d5a604426f719c3d46cc86a0752c53334b4526024bd31d8" => :el_capitan
+    sha256 "5021b218d542b7524d5a604426f719c3d46cc86a0752c53334b4526024bd31d8" => :yosemite
   end
 
   def phar_file
