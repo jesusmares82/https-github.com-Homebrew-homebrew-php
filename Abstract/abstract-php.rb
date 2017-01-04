0a3c72a86284daae1ecb0839cfdff470f0d8af4c
Jan Viljanen <jan.viljanen@greenpeace.org>
2017-01-04 11:00:34 +0100

    remove --with-mysql compile option for PHP 5.5+
    Support was deprecated in PHP 5.5
    
    Closes #3891.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Abstract/abstract-php.rb
+++ Abstract/abstract-php.rb
@@ -298,12 +298,12 @@ INFO
     if build.with? 'libmysql'
       args << "--with-mysql-sock=/tmp/mysql.sock"
       args << "--with-mysqli=#{HOMEBREW_PREFIX}/bin/mysql_config"
-      args << "--with-mysql=#{HOMEBREW_PREFIX}" unless build.without? 'legacy-mysql' || php_version.start_with?('7')
+      args << "--with-mysql=#{HOMEBREW_PREFIX}" unless build.without? 'legacy-mysql' || php_version.start_with?('5.5', '5.6', '7')
       args << "--with-pdo-mysql=#{HOMEBREW_PREFIX}"
     elsif build.with? 'mysql'
       args << "--with-mysql-sock=/tmp/mysql.sock"
       args << "--with-mysqli=mysqlnd"
-      args << "--with-mysql=mysqlnd" unless build.without? 'legacy-mysql' || php_version.start_with?('7')
+      args << "--with-mysql=mysqlnd" unless build.without? 'legacy-mysql' || php_version.start_with?('5.5', '5.6', '7')
       args << "--with-pdo-mysql=mysqlnd"
     end
 
