ecaf724c7fc7700c2a34f483380b89574f5b2a5d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-05 10:33:06 +0000

    php70-uopz: add 5.0.1 bottle.

--- Formula/php70-uopz.rb
+++ Formula/php70-uopz.rb
@@ -8,6 +8,13 @@ class Php70Uopz < AbstractPhp70Extension
   sha256 "d4dceb4705f8ea99e836864615bff2ca90da150cc95fb3bc68be8501f932d0bb"
   head "https://github.com/krakjoe/uopz.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9c995c1721f79b7eb72c63405e057cb2c1b0b33b6d37165c51c6215c75835dab" => :sierra
+    sha256 "30046f5bc694adf1e863686bbcc3f160fc613ad56d9e67708c12435d1c8f61c8" => :el_capitan
+    sha256 "0f4fdf004d78c45e3048949c22eb160586b1ca9b2b7685d334ffdea4ad1d94e8" => :yosemite
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
