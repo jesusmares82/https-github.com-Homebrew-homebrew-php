ac1baf28c69f0a0d606e1f5b8c6984eb628eeb0a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-12 09:00:06 +0000

    php56-grpc: add 1.0.1 bottle.

--- Formula/php56-grpc.rb
+++ Formula/php56-grpc.rb
@@ -7,6 +7,13 @@ class Php56Grpc < AbstractPhp56Extension
   url "https://pecl.php.net/get/grpc-1.0.1.tgz"
   sha256 "53fc9972d8817ae96c2d3e882bfdea939d1da7faf66e340364be16dde93d8b54"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bdbc0e1de88c7409f9f92ef824f99b4c585a2cd76ab562154fd31e3bd7515880" => :sierra
+    sha256 "4af946a68bc2cc89ebabbe892738033013f3cad42e1cb8d2636e6d1f0fd27139" => :el_capitan
+    sha256 "812cdd72bcb8eb94936332f67b14d43aab79221d5af6c92af070c5e254b845af" => :yosemite
+  end
+
   depends_on "grpc/grpc/grpc"
 
   def install
