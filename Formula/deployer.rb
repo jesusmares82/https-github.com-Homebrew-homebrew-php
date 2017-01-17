501c9d67962d218b44b03acd25ac70c6e1dd1f82
Anton Medvedev <anton@medv.io>
2017-01-17 20:36:36 +0700

    deployer 4.0.2
    Closes #3918.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -3,11 +3,11 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 
 class Deployer < AbstractPhpPhar
   init
-  desc "Deployment tool for PHP projects, much like Capistrano is to Ruby."
-  homepage "https://deployer.org/"
-  url "https://deployer.org/releases/v4.0.0/deployer.phar"
-  version "4.0.0"
-  sha256 "9f43bb1b49a461bfebcbd7e75acf881c7ad3b7aedbeb15e03c0e5587db15502e"
+  desc "A deployment tool written in PHP with support for popular frameworks out of the box."
+  homepage "https://deployer.org"
+  url "https://deployer.org/releases/v4.0.2/deployer.phar"
+  version "4.0.2"
+  sha256 "4d5f75c0209d557cf7efac0f2830b9054a2ab40ce97c3f58ade41d7846594b47"
 
   bottle :unneeded
 
