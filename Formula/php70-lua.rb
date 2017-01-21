ad5671b9858c3768be2f8889b926fde9aad127c7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-21 09:50:26 +0000

    php70-lua: add 2.0.2 bottle.

--- Formula/php70-lua.rb
+++ Formula/php70-lua.rb
@@ -8,6 +8,12 @@ class Php70Lua < AbstractPhp70Extension
   sha256 "eb11c68a8f2259ad02486f0a135785bd13c08166b6ec4a1301c862e72dc8a30d"
   head "https://github.com/laruence/php-lua.git"
 
+  bottle do
+    sha256 "fc7c300cb3393adc3686c0ea83aacb87423f13f29790ab798385aea884a58b5f" => :sierra
+    sha256 "d1d01f910adfe8d71b38c092f398b6b2880356d8f43baed8efd075da13c3bebc" => :el_capitan
+    sha256 "76c9675ef52ce2973c71999f67dcb825b16ac497ccfb08cdcb7226c2cb3a97a3" => :yosemite
+  end
+
   depends_on "lua"
 
   def install
