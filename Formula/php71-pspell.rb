8f6fd5438c99919826dcbc7e16093ba52cead149
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-21 14:05:56 +0000

    php71-pspell: update 7.1.1_8 bottle.

--- Formula/php71-pspell.rb
+++ Formula/php71-pspell.rb
@@ -7,9 +7,9 @@ class Php71Pspell < AbstractPhp71Extension
   revision 8
 
   bottle do
-    sha256 "1e33abe8b1e12d5f84bd3c4fb2128cd236d40d8613e5042d6020dc7a5ebdb8c7" => :sierra
-    sha256 "7c0f115b06a5f2b2cf6c00eb3168c21c3eb8f3d91362218f1ceec4ed5f009013" => :el_capitan
-    sha256 "09cf66b4f64b76d7ae664b2d06506d331b85dae8412ccd016f96d7f9faa5faf9" => :yosemite
+    sha256 "55256f540aac246290a38a64256e96832c9e28a42cae657085da580e6edc8651" => :sierra
+    sha256 "3a3830227df388bce202278e6c40c6f8995b3ff1534160bcd57dcbfed8fa721e" => :el_capitan
+    sha256 "c0c325683ecd7506faf4c9a35112b8437dd874d12b877003739f2731193accca" => :yosemite
   end
 
   url PHP_SRC_TARBALL
