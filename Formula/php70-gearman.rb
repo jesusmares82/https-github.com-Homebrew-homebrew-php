b3e655f85cff6ce4e0056b8b39dc29d44d0dcbb8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-28 17:17:52 +0000

    php70-gearman: update 2.0.3 bottle.

--- Formula/php70-gearman.rb
+++ Formula/php70-gearman.rb
@@ -9,6 +9,9 @@ class Php70Gearman < AbstractPhp70Extension
   head "https://github.com/wcgallego/pecl-gearman.git"
 
   bottle do
+    sha256 "0e4e36c3a76c147de987773e487313da112b06495ab1dc3d577ff4c4f8e4a9e1" => :sierra
+    sha256 "d8cae1a69fb8c91ae6e088060ce996529cc760df2478973ad64065a9c04d3a94" => :el_capitan
+    sha256 "a2ddcff230e2441c8773cfc01c9c7895f7c986487811b2599700de8bc22f4c58" => :yosemite
   end
 
   depends_on "gearman"
