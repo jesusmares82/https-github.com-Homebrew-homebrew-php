ee87ad7451e3e357871775e75e526e28286a2032
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-06 01:40:15 +0000

    php54-trace: add 1.0.0 bottle.

--- Formula/php54-trace.rb
+++ Formula/php54-trace.rb
@@ -8,6 +8,13 @@ class Php54Trace < AbstractPhp54Extension
   sha256 "068a5c168ee3fd4249cb41ea98a84affc1f8023d3b5745c0d8f9a305c2d672b1"
   head "https://github.com/Qihoo360/phptrace.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ac8acdac8d8ad09ccca2843c880e3568e593e855980e3141cd3993d787aed97f" => :sierra
+    sha256 "7fe9d445c3707685390a60759fec91d9bd5fe241cca21117f4597e2f0258039f" => :el_capitan
+    sha256 "df4ee63546c97af0e3f729416d7c6e470a8f7f1e89ad29d25c6cd2076855f99f" => :yosemite
+  end
+
   def install
     Dir.chdir "trace-#{version}/extension" unless build.head?
 
