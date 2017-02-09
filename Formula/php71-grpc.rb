e9a99c5f54f9c91b9218b7bbf27513005520d971
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-09 22:55:31 +0000

    php71-grpc: update 1.1.0 bottle.

--- Formula/php71-grpc.rb
+++ Formula/php71-grpc.rb
@@ -9,6 +9,9 @@ class Php71Grpc < AbstractPhp71Extension
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "88e4f1dc4e5f53bedd6c3b21357fb1548a3f3e9c5cad6ca7c02add0a1d123e05" => :sierra
+    sha256 "c8de19bab7b6c51a8fd62a57bf776f9144817d6992d711b953770ad3df286198" => :el_capitan
+    sha256 "33f78f3410e4bf8f652f035e8e519d575a347995635986a5868a94a9a3e4a13b" => :yosemite
   end
 
   def install
