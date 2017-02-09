e1282d9e73218d9e29eeea8d4f8496e811431721
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-09 22:55:31 +0000

    php70-grpc: update 1.1.0 bottle.

--- Formula/php70-grpc.rb
+++ Formula/php70-grpc.rb
@@ -9,6 +9,9 @@ class Php70Grpc < AbstractPhp70Extension
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f3ae25ac43039cf22e43e7bf4e70d0455592a1695e5517ae96c1afc70829e3a6" => :sierra
+    sha256 "b8b045246df42501f3108dc0e06cab7156572fc05ed684dc39d400469face126" => :el_capitan
+    sha256 "3eeadc408a086863ec639c2d5bc63cd5c253b21cd28de0539ed20b0d5bb5be8b" => :yosemite
   end
 
   def install
