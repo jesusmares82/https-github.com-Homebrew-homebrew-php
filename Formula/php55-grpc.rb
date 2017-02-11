d8ede4a59682f4f2f82c07aa92b30c4f33d992cf
Stanley Cheung <stanleycheung@google.com>
2017-02-10 23:56:40 -0800

    php55-grpc: 1.1.0
    * Remove dependency on grpc formula
    * Add cflag to use bundled boringssl package
    
    Closes #3988.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php55-grpc.rb
+++ Formula/php55-grpc.rb
@@ -4,16 +4,19 @@ class Php55Grpc < AbstractPhp55Extension
   init
   desc "The PHP extension for the gRPC library"
   homepage "http://grpc.io"
-  url "https://pecl.php.net/get/grpc-0.5.1.tgz"
-  sha256 "d42fbe6f1e67b21d2c08832fda4ff2baf4ca689324200da8abaefd042500c5a4"
+  url "https://pecl.php.net/get/grpc-1.1.0.tgz"
+  sha256 "4d7ad5d1994599d098f072688eb143ba73be12c58cf2c7d154189e68427c79ee"
 
-  depends_on "grpc/grpc/grpc"
+  bottle do
+    cellar :any_skip_relocation
+  end
 
   def install
     Dir.chdir "grpc-#{version}"
     safe_phpize
     system "./configure", "--enable-grpc=#{HOMEBREW_PREFIX}",
-           "--prefix=#{prefix}", phpconfig
+           "--prefix=#{prefix}", phpconfig,
+           "CFLAGS=-Ithird_party/boringssl/include"
     system "make"
     prefix.install "modules/grpc.so"
     write_config_file if build.with? "config-file"
