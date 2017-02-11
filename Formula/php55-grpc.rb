972691019f132b56d5ce97b82d509ff8e704de39
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-11 10:06:48 +0000

    php55-grpc: update 1.1.0 bottle.

--- Formula/php55-grpc.rb
+++ Formula/php55-grpc.rb
@@ -9,6 +9,9 @@ class Php55Grpc < AbstractPhp55Extension
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "513d371ccd1050a9d324483047d8f2522df0d92a41ac45ddebf3f31b6b0db9b0" => :sierra
+    sha256 "0ae29746018ae2404787264e11e9573ae7ec6161c5bad5657308d700dfc5c353" => :el_capitan
+    sha256 "12b2bce76c7e9ce925e31e5f47e6edbbac6775f983facf22f79f13f03f04c048" => :yosemite
   end
 
   def install
