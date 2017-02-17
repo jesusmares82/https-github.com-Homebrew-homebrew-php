50d17f1887cec447d1e7488b73e9995dbfe1ec7a
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-17 07:35:08 -0800

    php70-redis 3.1.1
    Closes #3997.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php70-redis.rb
+++ Formula/php70-redis.rb
@@ -4,8 +4,8 @@ class Php70Redis < AbstractPhp70Extension
   init
   desc "PHP extension for Redis"
   homepage "https://github.com/phpredis/phpredis"
-  url "https://github.com/phpredis/phpredis/archive/3.0.0.tar.gz"
-  sha256 "030997370bb1906793989c89550d9cafd4fa35dccbad7040b2339301aa961dba"
+  url "https://github.com/phpredis/phpredis/archive/3.1.1.tar.gz"
+  sha256 "78c577225daba3282c2968d7b29cb346b9626b2ed3dfddda02e26293e0931995"
   head "https://github.com/phpredis/phpredis.git", :branch => "php7"
 
   bottle do
