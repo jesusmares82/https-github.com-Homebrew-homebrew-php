4e61075a442dfedc95b62052bab41534179a437f
Srihari Thalla <thallasrihari@gmail.com>
2017-02-18 15:08:28 +0530

    php-redis 3.1.1
    Closes #3998.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php54-redis.rb
+++ Formula/php54-redis.rb
@@ -4,9 +4,8 @@ class Php54Redis < AbstractPhp54Extension
   init
   desc "PHP extension for Redis"
   homepage "https://github.com/phpredis/phpredis"
-  url "https://github.com/phpredis/phpredis/archive/2.2.8.tar.gz"
-  sha256 "a7c6e2eef70bd8449bab819c8f01d951fcd86ce0417e03675445040236ed4dda"
-  revision 1
+  url "https://github.com/phpredis/phpredis/archive/3.1.1.tar.gz"
+  sha256 "78c577225daba3282c2968d7b29cb346b9626b2ed3dfddda02e26293e0931995"
   head "https://github.com/phpredis/phpredis.git"
 
   bottle do
