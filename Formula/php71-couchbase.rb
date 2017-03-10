f3625550ed56797f864c83f7d0259f48c1d5d30c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-10 23:51:05 +0000

    php71-couchbase: add 2.3.0 bottle.

--- Formula/php71-couchbase.rb
+++ Formula/php71-couchbase.rb
@@ -8,6 +8,13 @@ class Php71Couchbase < AbstractPhp71Extension
   sha256 "1b7d7fe32c648bff20918ed437eca848c9dc2432198a77051b46a7373e8bbc8b"
   head "https://github.com/couchbase/php-couchbase.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a9d484d168f8dc43b4217f748765888bc6b238b94b60886661491b7e0c6c5635" => :sierra
+    sha256 "b6b0f528a15b268adb67f577e1361738642df777b12deb038910af7baca2e4d6" => :el_capitan
+    sha256 "caf6d19bc98cb823c6935a52cd33c43ef02f4e839a5ff3c746cb54947dd04a47" => :yosemite
+  end
+
   depends_on "php71-igbinary"
   depends_on "igbinary" => :build
 
