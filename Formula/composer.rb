6c73d9a66df44188cd596928ffa52fd03893b185
BePsvPT <freedom@csie.io>
2017-03-10 17:00:46 +0800

    composer 1.4.1
    Closes #4034.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -3,9 +3,9 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 class Composer < AbstractPhpPhar
   init
   desc "Dependency Manager for PHP"
-  homepage "http://getcomposer.org"
-  url "https://getcomposer.org/download/1.3.2/composer.phar"
-  sha256 "6a4f761aa34bb69fca86bc411a5e9836ca8246f0fcd29f3804b174fee9fb0569"
+  homepage "https://getcomposer.org"
+  url "https://getcomposer.org/download/1.4.1/composer.phar"
+  sha256 "abd277cc3453be980bb48cbffe9d1f7422ca1ef4bc0b7d035fda87cea4d55cbc"
   head "https://getcomposer.org/composer.phar"
 
   bottle do
