9c7576b2f938a9b7b314937fe7aadc13d04c2cd9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-22 23:12:57 +0000

    php70-memcache: add 3.0.9.20160311 bottle.

--- Formula/php70-memcache.rb
+++ Formula/php70-memcache.rb
@@ -9,6 +9,13 @@ class Php70Memcache < AbstractPhp70Extension
   sha256 "498ae2f3fde66b471eb8662efedfe5cb2f89f5fff725791ffddbcc7d94b7bf21"
   head "https://github.com/websupport-sk/pecl-memcache.git", :branch => "NON_BLOCKING_IO_php7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "05354dc21b5a4fb48edc215020f8e64344be0c25f2422d64a910595acbdf7ea8" => :sierra
+    sha256 "a93d88ffdcf571b57ee33466195d2da3ceb8ea14d786f7c4aa600cdf065489ab" => :el_capitan
+    sha256 "6112c2a8ad25728d47f13dcd0c67c54fd8d1277666026838fb5b8432354d548e" => :yosemite
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
