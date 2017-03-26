b04d79a05be75f0e39d1ae7b4540e9f17ffc3c16
xutl <xtongle@gmail.com>
2017-03-27 01:39:47 +0800

    php70-swoole 2.0.7
    Closes #4067.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php70-swoole.rb
+++ Formula/php70-swoole.rb
@@ -4,23 +4,16 @@ class Php70Swoole < AbstractPhp70Extension
   init
   desc "Event-driven asynchronous & concurrent networking engine for PHP."
   homepage "https://pecl.php.net/package/swoole"
-  url "https://github.com/swoole/swoole-src/archive/1.8.11-stable.tar.gz"
-  version "1.8.11-stable"
-  sha256 "cf2d9ba2c85f29c4a9f5e963878db27fdf552be7a05ca1709b6ad6f294e12b8e"
+  url "https://github.com/swoole/swoole-src/archive/v2.0.7.tar.gz"
+  version "2.0.7-stable"
+  sha256 "d8370a5f959f2d4082f5b2cec2e3a5b294dd3d7f586a5c7a19e3d154b48c699b"
   head "https://github.com/swoole/swoole-src.git"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "37bb6cb2b97c156aff4a3c9d55f785ace78c372d887e6c9c3e0a49c764be04e8" => :sierra
-    sha256 "e03a91d048da2898116ac49825dd736d49eede14fefc5b4306441398f2dab0f5" => :el_capitan
-    sha256 "29b7e4b70163670760d634982b7d890e612acc93c3f4cf15bb026fdfbcc321cb" => :yosemite
-  end
 
   def install
     ENV.universal_binary if build.universal?
 
     safe_phpize
-    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "./configure", "--prefix=#{prefix}", "--enable-coroutine", phpconfig
     system "make"
     prefix.install "modules/swoole.so"
     write_config_file if build.with? "config-file"
