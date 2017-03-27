a1abbabd72a733ea8dae28d6feeccac3bc68b5c3
Bas Kamer <basz@users.noreply.github.com>
2017-03-27 21:38:07 +0200

    deployer 4.3.0
    Closes #4069.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -3,11 +3,11 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 
 class Deployer < AbstractPhpPhar
   init
-  desc "A deployment tool written in PHP with support for popular frameworks out of the box."
+  desc "Deployment tool written in PHP with support for popular frameworks out of the box."
   homepage "https://deployer.org"
-  url "https://deployer.org/releases/v4.2.1/deployer.phar"
-  version "4.2.1"
-  sha256 "4d2caa8f1f66aee77de64ba7cfcf4ab4acb37f3c38d634d3da16c9d935526b6d"
+  url "https://deployer.org/releases/v4.3.0/deployer.phar"
+  version "4.3.0"
+  sha256 "26f41009261466a7cd5e850fe96a6c4adcd03a4a8a18aa6322a6d38c49eb9b5b"
 
   bottle :unneeded
 
