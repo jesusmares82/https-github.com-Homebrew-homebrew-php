6fac37b6ca8da15e3aed27aa36baba3f512fb378
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-01 17:05:45 +0100

    php70-swoole: add 2.0.7-stable bottle.

--- Formula/php70-swoole.rb
+++ Formula/php70-swoole.rb
@@ -9,6 +9,13 @@ class Php70Swoole < AbstractPhp70Extension
   sha256 "d8370a5f959f2d4082f5b2cec2e3a5b294dd3d7f586a5c7a19e3d154b48c699b"
   head "https://github.com/swoole/swoole-src.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "878b729e02e9fc873e715bf1868009af01d6c283deb6bfa869488e4de55b77b0" => :sierra
+    sha256 "4ecea677a69895cd20e0cfe00db309b283e4aeb2b40976e3ce6d83b6f77db8b9" => :el_capitan
+    sha256 "a08f4506ff3025f2827618db594678e198ed15b011fcad6cfa07d3892a8a6bd0" => :yosemite
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
