0275393c0e584e221849ba2e3e3c63dce72146af
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-01 14:28:13 +0100

    php70-raphf: add 2.0.0 bottle.

--- Formula/php70-raphf.rb
+++ Formula/php70-raphf.rb
@@ -8,6 +8,13 @@ class Php70Raphf < AbstractPhp70Extension
   sha256 "eb4356e13769bf76efc27bce4ad54f508701bcdac3c255dd1c8eb1e87fccb9fa"
   head "https://github.com/m6w6/ext-raphf.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d8a093d9f05560b2f3d2b20aec76e8c8fe8b8e56856af2b2899b56c8a4c88fa8" => :sierra
+    sha256 "4966719a7971781b7f50c468b271e657ab5bb14988f0f6494ee182c990cf3815" => :el_capitan
+    sha256 "7041d87ee38380b22557d7d6c87a94f9c666cdde31b09d3864acdd42042ee909" => :yosemite
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
