9e38d3ebef81e61e13cd78787ee193c5f8b6efd4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-14 15:35:32 +0100

    php71-tidy: update 7.1.4_10 bottle.

--- Formula/php71-tidy.rb
+++ Formula/php71-tidy.rb
@@ -7,9 +7,9 @@ class Php71Tidy < AbstractPhp71Extension
   revision 10
 
   bottle do
-    sha256 "722614949b639354e9a883373ef29511a853779f98e621e3edbf2f3cbd4f68ff" => :sierra
-    sha256 "badc3ef234ce69541e66304d347c6c03a27b5f59389479d11214a39ec68626b5" => :el_capitan
-    sha256 "390a29ce755e75199bddc794126c0c51b2aaa94b0fa4e296f9352d68e17655bb" => :yosemite
+    sha256 "3f03f4598b124c443e95191c8731a5a75ca1bcbbbc9771d3fb9b9bbb1ad43b16" => :sierra
+    sha256 "f46752cf65fffdc072750271d7fc8f662338fd044dcc928da13c62b805649932" => :el_capitan
+    sha256 "a55e291828b976041ebe204e43e368cd11727abdf421f9becf8a97c55cfd066d" => :yosemite
   end
 
   url PHP_SRC_TARBALL
