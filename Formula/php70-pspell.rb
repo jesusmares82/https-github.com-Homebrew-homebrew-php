03908479bb11ae8ecd7dd5bcfe5d35c15e4945f4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-16 13:04:04 +0100

    php70-pspell: update 7.0.18_9 bottle.

--- Formula/php70-pspell.rb
+++ Formula/php70-pspell.rb
@@ -7,9 +7,9 @@ class Php70Pspell < AbstractPhp70Extension
   revision 9
 
   bottle do
-    sha256 "e6960326357f914d7e791e49f7b55d1e1a8f8b680099f80f33b6a680b01cad1d" => :sierra
-    sha256 "c845655486c7a7c6011d37fda14df07a3376b9cb28fe2c254c34009159006875" => :el_capitan
-    sha256 "6bfc08e3fd8ab6dfd4daa9fe779303b5937e485f8de028dbdf978b846abe091e" => :yosemite
+    sha256 "1179c7670c1123475a08d6c73d55039c0dfbd8d0dc3e1e54041c981cdd7bd50a" => :sierra
+    sha256 "cbf82c7db5e56b195ce9258b1286f30b0ec36d2efdc23bf499dbffe29bdd275e" => :el_capitan
+    sha256 "5345c5ade074f9c3e0b74bbb988a5d5cb72f4c053e6646b89305326fdb80b398" => :yosemite
   end
 
   url PHP_SRC_TARBALL
