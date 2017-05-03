31288ebee74d33502570f317dc68ec16f72ec0fb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-03 04:52:35 +0000

    php55-couchbase: update 2.3.2 bottle.

--- Formula/php55-couchbase.rb
+++ Formula/php55-couchbase.rb
@@ -10,9 +10,9 @@ class Php55Couchbase < AbstractPhp55Extension
 
   bottle do
     cellar :any
-    sha256 "d8e5e2b89ba2204c17f634efa4b62016eb94a0023508b0bad14713cbbac06bfd" => :sierra
-    sha256 "2c206c1b9f1c48f709f93af164b2d4f791afebdc3505ef7f0d6c45112cb74019" => :el_capitan
-    sha256 "1f6bc3922a3eb799350b7778bddcb15fad5f9902bddd0961296431c9ffc55352" => :yosemite
+    sha256 "000bdf606f45bfd5acc3ee4af0852201d6b6d8eceec9bbe68352b0bd2eb5993e" => :sierra
+    sha256 "fed95b236b07bf55f6f3350b12658870fef387e38f6fe69e8c24022bb77b2467" => :el_capitan
+    sha256 "1b200151e6021590b65b1b1bf21dfec6f74509fe6398566c930c63a0ff326eaf" => :yosemite
   end
 
   depends_on "php55-igbinary"
