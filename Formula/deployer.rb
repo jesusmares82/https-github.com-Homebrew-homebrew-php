8bec7909725a79ae27d39ac5aba3d940f44e7c1c
Anton Medvedev <anton@medv.io>
2017-05-15 11:25:35 +0700

    deployer 5.0.0
    Closes #4159.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -5,9 +5,9 @@ class Deployer < AbstractPhpPhar
   init
   desc "Deployment tool written in PHP with support for popular frameworks out of the box."
   homepage "https://deployer.org"
-  url "https://deployer.org/releases/v4.3.0/deployer.phar"
-  version "4.3.0"
-  sha256 "26f41009261466a7cd5e850fe96a6c4adcd03a4a8a18aa6322a6d38c49eb9b5b"
+  url "https://deployer.org/releases/v5.0.0/deployer.phar"
+  version "5.0.0"
+  sha256 "98f4acc973e8439a209cde8a861dfcd783cebec1ce44271339c28d2ef72c704d"
 
   bottle :unneeded
 
