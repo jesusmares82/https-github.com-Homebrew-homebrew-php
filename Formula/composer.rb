3a68ef1110016ec45fae41c8603205e8b5cfd689
BePsvPT <freedom@csie.io>
2017-05-17 15:00:33 +0800

    composer 1.4.2
    Closes #4163.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 class Composer < AbstractPhpPhar
   desc "Dependency Manager for PHP"
   homepage "https://getcomposer.org"
-  url "https://getcomposer.org/download/1.4.1/composer.phar"
-  sha256 "abd277cc3453be980bb48cbffe9d1f7422ca1ef4bc0b7d035fda87cea4d55cbc"
+  url "https://getcomposer.org/download/1.4.2/composer.phar"
+  sha256 "6b1945c3ee477f12be508a5bb41a5025d57de5510bcf94855ae6a4d59f3d86f4"
   head "https://getcomposer.org/composer.phar"
 
   bottle do
@@ -38,11 +38,11 @@ class Composer < AbstractPhpPhar
   end
 
   def caveats
-    caveats = <<-EOS
-    composer no longer depends on the homebrew php Formulas since the last couple of macOS releases
-    contains a php version compatible with composer. If this has been part of your workflow
-    previously then please make the appropriate changes and `brew install php71` or other appropriate
-    Homebrew PHP version.
+    <<-EOS
+      composer no longer depends on the homebrew php Formulas since the last couple of macOS releases
+      contains a php version compatible with composer. If this has been part of your workflow
+      previously then please make the appropriate changes and `brew install php71` or other appropriate
+      Homebrew PHP version.
     EOS
   end
 end
