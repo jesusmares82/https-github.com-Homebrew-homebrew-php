f93e64ca024486db7898e1f65d0897e586427154
Emir Beganovic <emir@php.net>
2017-05-17 14:18:25 +0200

    Update redis to 3.1.2

--- Formula/php53-redis.rb
+++ Formula/php53-redis.rb
@@ -4,9 +4,9 @@ class Php53Redis < AbstractPhp53Extension
   init
   desc "PHP extension for Redis"
   homepage "https://github.com/phpredis/phpredis"
-  url "https://github.com/phpredis/phpredis/archive/3.1.1.tar.gz"
-  sha256 "78c577225daba3282c2968d7b29cb346b9626b2ed3dfddda02e26293e0931995"
-  revision 2
+  url "https://github.com/phpredis/phpredis/archive/3.1.2.tar.gz"
+  sha256 "a060fcb7784b9323905cf58557d924a394bb539350bea28d02f910df8ddea1f6"
+  revision 3
   head "https://github.com/phpredis/phpredis.git"
 
   bottle do
