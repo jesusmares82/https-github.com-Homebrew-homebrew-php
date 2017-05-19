0ef4250ec6ed669ebb813485e0a7b4dfde4a82bb
BePsvPT <freedom@csie.io>
2017-05-20 00:00:29 +0800

    phive 0.7.0
    Closes #4169.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/phive.rb
+++ Formula/phive.rb
@@ -4,8 +4,8 @@ class Phive < AbstractPhpPhar
   init
   desc "Installation and verification of phar distributed PHP applications"
   homepage "https://phar.io"
-  url "https://github.com/phar-io/phive/releases/download/0.6.3/phive-0.6.3.phar"
-  sha256 "1e7a3f9b2186a8f54a4caa97fc1142d2c724dceb60131e2916ed088d7edee277"
+  url "https://github.com/phar-io/phive/releases/download/0.7.0/phive-0.7.0.phar"
+  sha256 "6ae0a8294f4e0a0bc170b54fbb8e487e14474dc26ad11fe1004f91f69ac142ce"
   head "https://phar.io/releases/phive.phar"
 
   bottle :unneeded
