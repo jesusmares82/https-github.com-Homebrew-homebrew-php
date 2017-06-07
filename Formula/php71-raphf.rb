9b263ce2349b2d44d769414f9bbd56bc239e021c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-07 17:32:57 +0000

    php71-raphf: update 2.0.0 bottle.

--- Formula/php71-raphf.rb
+++ Formula/php71-raphf.rb
@@ -10,9 +10,9 @@ class Php71Raphf < AbstractPhp71Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d8a093d9f05560b2f3d2b20aec76e8c8fe8b8e56856af2b2899b56c8a4c88fa8" => :sierra
-    sha256 "4966719a7971781b7f50c468b271e657ab5bb14988f0f6494ee182c990cf3815" => :el_capitan
-    sha256 "7041d87ee38380b22557d7d6c87a94f9c666cdde31b09d3864acdd42042ee909" => :yosemite
+    sha256 "3cfc388aa195987bba0a8c31be087eb7c1af3e9f4fa451a9ee546d4faa8b7dbc" => :sierra
+    sha256 "0c7e379613d9ace8bd27c048e37e72c1c43b8ab5ce8bab75a4e14f886e96a2c2" => :el_capitan
+    sha256 "872c541820ba85209891c42973c297ef561bc37325b24c757559d4fae1d6e05d" => :yosemite
   end
 
   def install
