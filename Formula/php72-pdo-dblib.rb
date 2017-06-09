55bb4bb52670aee12e052f3d780b09f6254002bc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-09 10:01:45 +0000

    php72-pdo-dblib: add 7.2.0alpha1 bottle.

--- Formula/php72-pdo-dblib.rb
+++ Formula/php72-pdo-dblib.rb
@@ -5,6 +5,12 @@ class Php72PdoDblib < AbstractPhp72Extension
   desc "Unified Sybase-DB style driver for PDO"
   homepage "https://github.com/php/php-src/tree/master/ext/pdo_dblib"
 
+  bottle do
+    sha256 "5af18368871897c47501f29005fc55cd635f4aa126b9194bb0b721d60c4424d5" => :sierra
+    sha256 "6a33f996f2ea33cc0be89f6a6cf631c3f450ac413bbe0f2da850d78351fb61e5" => :el_capitan
+    sha256 "9bd368aa26ea9cbf39043bb33b8ff61a0160c067e1456065ae5e588c5388e4d9" => :yosemite
+  end
+
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
   version PHP_VERSION
