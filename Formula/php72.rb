d2532a77321412e43845c2f6a323a0d5d49ebbe8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-09 10:01:45 +0000

    php72: add 7.2.0alpha1 bottle.

--- Formula/php72.rb
+++ Formula/php72.rb
@@ -4,6 +4,12 @@ class Php72 < AbstractPhp
   init
   desc "PHP Version 7.2"
 
+  bottle do
+    sha256 "26ab0e2ee3a5374b0e4aff2811715ee9325758264d8c9d9ddf4607379690ba91" => :sierra
+    sha256 "e720ad701b9371cd4dbbf7be205e8675e7f4d7d5ebc803d81a9141acf9f8ab52" => :el_capitan
+    sha256 "9ed7a4c2df0ccaeeaf7b86ce8f2ce795955f784bd26405b515a81215674fc2c9" => :yosemite
+  end
+
   include AbstractPhpVersion::Php72Defs
 
   url PHP_SRC_TARBALL
