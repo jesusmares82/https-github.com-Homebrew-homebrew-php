b15af89f480391f4d0e09561ba4c4e783a6e1ee4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-09 10:01:45 +0000

    php72-pcntl: add 7.2.0alpha1 bottle.

--- Formula/php72-pcntl.rb
+++ Formula/php72-pcntl.rb
@@ -5,6 +5,13 @@ class Php72Pcntl < AbstractPhp72Extension
   desc "Process Control support"
   homepage "http://php.net/manual/en/book.pcntl.php"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7e4652275fac735fe1e119b3e454a27fda503bd0cade78f56482e111463ad49a" => :sierra
+    sha256 "e290a1220454e6f281700b1c17fa87f397b4ff53a2e3c49fc079194f22599beb" => :el_capitan
+    sha256 "2fc6650e0f00aa65350f88efd77bd2425d6f759e971ad8e5e5653b9de2e3f069" => :yosemite
+  end
+
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
   version PHP_VERSION
