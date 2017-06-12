4eaa3397a2eab8c8db304615764d6fd1752d3cb5
Emir Beganovic <beganovic.emir@gmail.com>
2017-06-12 16:23:52 +0200

    php-amqp: 1.9.1
    * Fixed audit issues
    
    Closes #4215.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php53-amqp.rb
+++ Formula/php53-amqp.rb
@@ -4,8 +4,8 @@ class Php53Amqp < AbstractPhp53Extension
   init
   desc "Communicates with any AMQP 0-9-1 compatible server."
   homepage "https://pecl.php.net/package/amqp"
-  url "https://pecl.php.net/get/amqp-1.7.1.tgz"
-  sha256 "38a92eaef1aa863404170bcd15605f7f78140eeec1ff5be6af65c5b8081e2599"
+  url "https://pecl.php.net/get/amqp-1.9.1.tgz"
+  sha256 "219cdad0ef26d30c9d820be3d0c06a9b35d3d1cb6b7e049948bc952223721119"
   head "https://github.com/pdezwart/php-amqp.git"
 
   bottle do
@@ -19,8 +19,6 @@ class Php53Amqp < AbstractPhp53Extension
 
   def install
     Dir.chdir "amqp-#{version}" unless build.head?
-
-    ENV.universal_binary if build.universal?
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}",
