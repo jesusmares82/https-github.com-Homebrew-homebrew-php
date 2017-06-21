28329d2d99de71165b3668c5cf11c7e840a4dc5a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-21 21:53:31 +0000

    php70-imagick: update 3.4.3_3 bottle.

--- Formula/php70-imagick.rb
+++ Formula/php70-imagick.rb
@@ -10,9 +10,10 @@ class Php70Imagick < AbstractPhp70Extension
   revision 3
 
   bottle do
-    sha256 "3e1ff5482ca8fb41122441c8d7b5d8279d429dc7557d2ce63d2fec70cdc2152b" => :sierra
-    sha256 "dee4a50a01c864a4fef5db3acd1995ff23e45f9e25f4d9ac46d11e6f5385faf9" => :el_capitan
-    sha256 "fdfacaf444f10b46d074c5757f78ebf1636ee70fd5c1151aeb9b6e8193d6cc26" => :yosemite
+    rebuild 1
+    sha256 "ab471210f8f4f40431faa1e14a0847dc44fdb7291d1c959965b9f2cfaffbe8f3" => :sierra
+    sha256 "46b4f1f346fc6e70c797c002e470463cd73ac577a50901adcf1fcf947ff4e8d9" => :el_capitan
+    sha256 "e8581f461336ceb51a518df75313032fa37bb33996e39fa9cf0d753fddbe8982" => :yosemite
   end
 
   depends_on "pkg-config" => :build
