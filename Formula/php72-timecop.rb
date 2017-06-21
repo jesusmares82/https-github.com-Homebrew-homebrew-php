7247b844d07cc8528727ea4119e3b5c743846a02
Romain Neutron <imprec@gmail.com>
2017-06-21 15:13:12 +0200

    php72-timecop 1.2.4 (new formula)
    Closes #4251.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-timecop.rb
@@ -0,0 +1,18 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Timecop < AbstractPhp72Extension
+  init
+  desc "timecop is providing \"time travel\" capabilities."
+  homepage "https://github.com/hnw/php-timecop"
+  url "https://github.com/hnw/php-timecop/archive/v1.2.4.tar.gz"
+  sha256 "1e0bc0b47097500152aab0cd8fbc7f4b1323816cdbcd1b65df639d95356fbf85"
+  head "https://github.com/hnw/php-timecop.git"
+
+  def install
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/timecop.so"
+    write_config_file if build.with? "config-file"
+  end
+end
