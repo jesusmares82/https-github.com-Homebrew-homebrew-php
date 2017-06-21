f57987d21afa4506bf5dc42b91e26c53585ca785
Romain Neutron <imprec@gmail.com>
2017-06-21 14:58:59 +0200

    php72-hprose 1.6.5 (new formula)
    Closes #4269.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-hprose.rb
@@ -0,0 +1,21 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Hprose < AbstractPhp72Extension
+  init
+  desc "High Performance Remote Object Service Engine"
+  homepage "https://pecl.php.net/package/hprose"
+  url "https://pecl.php.net/get/hprose-1.6.5.tgz"
+  sha256 "a453053719dad6352d54543d708d8d09b62de28df7b29fd47b4b5a9fcf8025a7"
+  head "https://github.com/hprose/hprose-pecl.git"
+
+  def install
+    Dir.chdir "hprose-#{version}" unless build.head?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/hprose.so"
+    write_config_file if build.with? "config-file"
+  end
+end
