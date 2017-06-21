2f683ec55ddb3041591c52026388397097cf2284
Romain Neutron <imprec@gmail.com>
2017-06-21 15:08:42 +0200

    php72-xxtea 1.0.11 (new formula)
    Closes #4245.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-xxtea.rb
@@ -0,0 +1,21 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Xxtea < AbstractPhp72Extension
+  init
+  desc "XXTEA encryption algorithm extension for PHP."
+  homepage "https://pecl.php.net/package/xxtea"
+  url "https://pecl.php.net/get/xxtea-1.0.11.tgz"
+  sha256 "5b1e318d3e70b27ad017d125d09ba3cf7bb3859e11be864a7bc3ddba421108af"
+  head "https://github.com/xxtea/xxtea-pecl.git"
+
+  def install
+    Dir.chdir "xxtea-#{version}" unless build.head?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/xxtea.so"
+    write_config_file if build.with? "config-file"
+  end
+end
