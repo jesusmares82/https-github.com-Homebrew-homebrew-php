942efe3e9674b998e0f584efb047fadacac4a99f
Romain Neutron <imprec@gmail.com>
2017-06-21 15:12:51 +0200

    php72-rdkafka 2.0.1 (new formula)
    Closes #4256.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-rdkafka.rb
@@ -0,0 +1,22 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Rdkafka < AbstractPhp72Extension
+  init
+  desc "PHP extension for Apache Kafka (php-rdkafka)"
+  homepage "https://github.com/arnaud-lb/php-rdkafka"
+  url "https://pecl.php.net/get/rdkafka-2.0.1.tgz"
+  sha256 "43a3704198f7d8ae23f6ed06d2d28ae4d52bae0f93f484aa5d61d5d60f809eee"
+  head "https://github.com/arnaud-lb/php-rdkafka.git"
+
+  depends_on "librdkafka"
+
+  def install
+    Dir.chdir "rdkafka-#{version}" unless build.head?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/rdkafka.so"
+    write_config_file if build.with? "config-file"
+  end
+end
