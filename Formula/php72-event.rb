85742d89c26843298e891f4c73ae7e92a5549834
Romain Neutron <imprec@gmail.com>
2017-06-21 14:58:37 +0200

    php72-event 2.2.1 (new formula)
    Closes #4274.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-event.rb
@@ -0,0 +1,31 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Event < AbstractPhp72Extension
+  init
+  desc "Provides interface to libevent library"
+  homepage "https://pecl.php.net/package/event"
+  url "https://pecl.php.net/get/event-2.2.1.tgz"
+  sha256 "44756686df68d8ef4fcee31359c0c03802b1f55ad88db7ac142169777f3d17ef"
+  head "https://bitbucket.org/osmanov/pecl-event.git"
+
+  depends_on "libevent"
+  depends_on "openssl"
+
+  def install
+    Dir.chdir "event-#{version}" unless build.head?
+
+    args = []
+    args << "--with-event-core"
+    args << "--with-event-extra"
+    args << "--enable-event-debug"
+    args << "--with-event-libevent-dir=#{Formula["libevent"].opt_prefix}"
+    args << "--with-event-openssl"
+    args << "--with-openssl-dir=#{Formula["openssl"].opt_prefix}"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/event.so"
+    write_config_file if build.with? "config-file"
+  end
+end
