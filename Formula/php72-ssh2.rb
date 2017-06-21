93f4b27f43c213d3d5d3713967650a0580df7dc1
Romain Neutron <imprec@gmail.com>
2017-06-21 15:13:00 +0200

    php72-ssh2 1.0 (new formula)
    Closes #4253.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-ssh2.rb
@@ -0,0 +1,24 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Ssh2 < AbstractPhp72Extension
+  init
+  desc "Provides bindings to the functions of libssh2"
+  homepage "https://pecl.php.net/package/ssh2"
+  url "https://pecl.php.net/get/ssh2-1.0.tgz"
+  sha256 "6a93891878b23904a773eb814fec7aea4ea00b4a412ee779c8535ed9c5e46ced"
+  head "https://github.com/php/pecl-networking-ssh2.git"
+
+  depends_on "libssh2"
+
+  def install
+    Dir.chdir "ssh2-#{version}" unless build.head?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-ssh2=#{Formula["libssh2"].opt_prefix}"
+    system "make"
+    prefix.install "modules/ssh2.so"
+    write_config_file if build.with? "config-file"
+  end
+end
