faa5afc80ddcbb414e9e6b2036279296a30fdfb9
Romain Neutron <imprec@gmail.com>
2017-06-21 15:12:38 +0200

    php72-mustache 0.7.2 (new formula)
    Closes #4258.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

---
+++ Formula/php72-mustache.rb
@@ -0,0 +1,20 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Mustache < AbstractPhp72Extension
+  init
+  desc "Mustache PHP Extension"
+  homepage "https://github.com/jbboehr/php-mustache#mustache"
+  url "https://github.com/jbboehr/php-mustache/archive/v0.7.2.tar.gz"
+  sha256 "5eb0a25d42532db98e2e9087e49db060369651b16ac1accd61415424a47561f7"
+  head "https://github.com/jbboehr/php-mustache.git"
+
+  depends_on "libmustache"
+
+  def install
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/mustache.so"
+    write_config_file if build.with? "config-file"
+  end
+end
