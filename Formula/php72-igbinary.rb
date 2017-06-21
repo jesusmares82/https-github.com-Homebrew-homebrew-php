37e5089df52291b2819bc7ff4266b48edac8f877
Romain Neutron <imprec@gmail.com>
2017-06-21 14:59:02 +0200

    php72-igbinary 2.0.4 (new formula)
    Closes #4270.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-igbinary.rb
@@ -0,0 +1,35 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Igbinary < AbstractPhp72Extension
+  init
+  desc "Igbinary is a replacement for the standard php serializer."
+  homepage "https://pecl.php.net/package/igbinary"
+  url "https://github.com/igbinary/igbinary/archive/2.0.4.tar.gz"
+  sha256 "7b71e60aeada2b9729f55f3552da28375e3c5c66194b2c905af15c3756cf34c8"
+  head "https://github.com/igbinary/igbinary.git"
+
+  depends_on "igbinary" => :build
+
+  def install
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/igbinary.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+      ; Enable or disable compacting of duplicate strings
+      ; The default is On.
+      ;igbinary.compact_strings=On
+
+      ; Use igbinary as session serializer
+      ;session.serialize_handler=igbinary
+
+      ; Use igbinary as APC serializer
+      ;apc.serializer=igbinary
+    EOS
+  end
+end
