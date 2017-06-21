539672d0dadd014620cc5e4b0a1584544dbce3ae
Romain Neutron <imprec@gmail.com>
2017-06-21 14:59:05 +0200

    php72-imagick 3.4.3 (new formula)
    Closes #4267.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-imagick.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Imagick < AbstractPhp72Extension
+  init
+  desc "Provides a wrapper to the ImageMagick library."
+  homepage "https://pecl.php.net/package/imagick"
+  url "https://pecl.php.net/get/imagick-3.4.3.tgz"
+  sha256 "1f3c5b5eeaa02800ad22f506cd100e8889a66b2ec937e192eaaa30d74562567c"
+  head "https://github.com/mkoppanen/imagick.git"
+
+  depends_on "pkg-config" => :build
+  depends_on "imagemagick"
+
+  def install
+    Dir.chdir "imagick-#{version}" unless build.head?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-imagick=#{Formula["imagemagick"].opt_prefix}"
+    system "make"
+    prefix.install "modules/imagick.so"
+    write_config_file if build.with? "config-file"
+  end
+end
