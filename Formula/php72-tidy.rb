7aa6ba417a2ce59a3dce9cb6d6f62fb82bf7a3d5
Romain Neutron <imprec@gmail.com>
2017-06-21 15:13:09 +0200

    php72-tidy 7.2.0alpha2 (new formula)
    Closes #4250.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

---
+++ Formula/php72-tidy.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Tidy < AbstractPhp72Extension
+  init
+  desc "Tidy HTML clean and repair utility"
+  homepage "http://php.net/manual/en/book.tidy.php"
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+  version PHP_VERSION
+
+  depends_on "tidy-html5"
+
+  def install
+    Dir.chdir "ext/tidy"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking",
+                          "--with-tidy=#{Formula["tidy-html5"].opt_prefix}"
+    system "make"
+    prefix.install "modules/tidy.so"
+    write_config_file if build.with? "config-file"
+  end
+end
