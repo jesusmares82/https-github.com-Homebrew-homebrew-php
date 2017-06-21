afa42ce169f66ec29d2dab70941312268a91cdf0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-21 15:45:28 +0000

    php72-uv: add 0.1.1 bottle.

--- Formula/php72-uv.rb
+++ Formula/php72-uv.rb
@@ -8,6 +8,12 @@ class Php72Uv < AbstractPhp72Extension
   sha256 "e576df44997a0b656deb4a1c2bfd1879fb3647419b0724bd6e87c7ddf997e2c1"
   head "https://github.com/bwoebi/php-uv.git"
 
+  bottle do
+    sha256 "0c0ed5f12874ffe089e7d1feaad070bb011fe584959ec599347824f7699ba837" => :sierra
+    sha256 "71cf0d69386e6bd0eb203db38e5d4a2002cf2ae919ea89cd0f6f2f303fe3b663" => :el_capitan
+    sha256 "c6b30bd22b009a150a481e0d224422151cf84c6de2b63bd701257988047db20f" => :yosemite
+  end
+
   depends_on "libuv"
 
   def install
