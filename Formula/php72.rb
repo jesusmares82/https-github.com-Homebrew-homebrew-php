b6505f87a6219320694c9f6dc6ea39d04867c902
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-23 06:00:18 +0000

    php72: update 7.2.0alpha2_1 bottle.

--- Formula/php72.rb
+++ Formula/php72.rb
@@ -6,9 +6,9 @@ class Php72 < AbstractPhp
   revision 1
 
   bottle do
-    sha256 "26ab0e2ee3a5374b0e4aff2811715ee9325758264d8c9d9ddf4607379690ba91" => :sierra
-    sha256 "e720ad701b9371cd4dbbf7be205e8675e7f4d7d5ebc803d81a9141acf9f8ab52" => :el_capitan
-    sha256 "9ed7a4c2df0ccaeeaf7b86ce8f2ce795955f784bd26405b515a81215674fc2c9" => :yosemite
+    sha256 "023124efa4776df53e5f2720394961882185d3e7fca5859ba568d56e74c43ab3" => :sierra
+    sha256 "45d6fa7b08897d6fff74ff4c83c2890e532289530d5c5726eb83d1dc89670645" => :el_capitan
+    sha256 "fcca7a764844eeab60aa192be9ee3fcfd6adee3b858fcbca36476362d0508052" => :yosemite
   end
 
   include AbstractPhpVersion::Php72Defs
