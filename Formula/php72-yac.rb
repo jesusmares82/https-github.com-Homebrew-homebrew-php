31bc908bf5d6e017cb1f54ca8738ae17ef64eeca
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 19:11:35 +0000

    php72-yac: add 2.0.1 bottle.

--- Formula/php72-yac.rb
+++ Formula/php72-yac.rb
@@ -8,6 +8,13 @@ class Php72Yac < AbstractPhp72Extension
   sha256 "c9357c9779a87370b53f719cfcf86b80258b119376c718fea336988b1d7354a0"
   head "https://github.com/laruence/yac.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0caed93e4a5e1e824d328be94dff6eea1531140123b32dd0213e905dbef31828" => :sierra
+    sha256 "1e74ff64b4a0656bcec3ea396839c1dd2670323ce272635beef5f08e6be21097" => :el_capitan
+    sha256 "5bd24abfd6ed69e52160f29aa5256cb2c684f526e5f2276fe084b6608a651bfe" => :yosemite
+  end
+
   def install
     safe_phpize
     system "./configure", "--prefix=#{prefix}",
