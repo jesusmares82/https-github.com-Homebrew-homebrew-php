b4acd0af251e2eb96c8b16c431b87e86c1a924cd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 19:33:11 +0000

    php72-pspell: add 7.2.0alpha2 bottle.

--- Formula/php72-pspell.rb
+++ Formula/php72-pspell.rb
@@ -5,6 +5,12 @@ class Php72Pspell < AbstractPhp72Extension
   desc "Extension to check the spelling"
   homepage "http://php.net/manual/en/book.pspell.php"
 
+  bottle do
+    sha256 "a69227d5b69483f5ae44e08322c6c0a91ff775018071a26a5f1b1ca625bda79b" => :sierra
+    sha256 "65ba23f80762d059963c04e1416e8f64f176e99c6c437268b5f35ea18bcde281" => :el_capitan
+    sha256 "07af28260a44e7f78fe27ddf265efde39d20930ab7238bf2710346d71a84f3f5" => :yosemite
+  end
+
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
   version PHP_VERSION
