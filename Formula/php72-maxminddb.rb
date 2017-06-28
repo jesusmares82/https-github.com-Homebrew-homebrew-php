feafe0415699ad18a65406e5fe62e5f065eecaca
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 19:19:25 +0000

    php72-maxminddb: add 1.1.0 bottle.

--- Formula/php72-maxminddb.rb
+++ Formula/php72-maxminddb.rb
@@ -7,6 +7,13 @@ class Php72Maxminddb < AbstractPhp72Extension
   url "https://github.com/maxmind/MaxMind-DB-Reader-php/archive/v1.1.0.tar.gz"
   sha256 "0c80f55bc56dd8f26b04358b5533f78adcb060f721b4347f510cbf43f32848b4"
 
+  bottle do
+    cellar :any
+    sha256 "f3158bfdef093d54cb85148a42b7f39c17c54f6cae329bf772182a45921f6e87" => :sierra
+    sha256 "ef4de086617ad67580fe91053959a1bf2f6a6311662d386c690d2349c1403f0f" => :el_capitan
+    sha256 "99e9b9f106218c78dc928e5609b2f0e1be8550f969271e19ce0af75322032c36" => :yosemite
+  end
+
   depends_on "libmaxminddb"
 
   def install
