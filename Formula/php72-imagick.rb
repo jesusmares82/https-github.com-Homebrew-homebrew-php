f6c7920b8f397a40c125ef0246651db10b8b9425
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 18:23:07 +0000

    php72-imagick: add 3.4.3 bottle.

--- Formula/php72-imagick.rb
+++ Formula/php72-imagick.rb
@@ -8,6 +8,12 @@ class Php72Imagick < AbstractPhp72Extension
   sha256 "1f3c5b5eeaa02800ad22f506cd100e8889a66b2ec937e192eaaa30d74562567c"
   head "https://github.com/mkoppanen/imagick.git"
 
+  bottle do
+    sha256 "bcb9c7dfe2c80db04bdf95c4b4e4f4d8f22d1fc97be7ed9fbbfbc64250e71ae0" => :sierra
+    sha256 "7f010b06855883127c75c73abfa7d47a11c55081754c355eb6e080f13d2414e3" => :el_capitan
+    sha256 "fe0a8dc9b3f87caffa831ad4d68d84d9df647a1b2a88b4d5d445d3ecc8ac8fa6" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "imagemagick"
 
