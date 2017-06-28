f8fae81161423308905b3bfef10458eb097bfcf9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 20:12:41 +0000

    php72-igbinary: add 2.0.4 bottle.

--- Formula/php72-igbinary.rb
+++ Formula/php72-igbinary.rb
@@ -8,6 +8,13 @@ class Php72Igbinary < AbstractPhp72Extension
   sha256 "7b71e60aeada2b9729f55f3552da28375e3c5c66194b2c905af15c3756cf34c8"
   head "https://github.com/igbinary/igbinary.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "662553810944e7de7882dbdae683f55cdcd001b955e0921435cc46cc1c7bf137" => :sierra
+    sha256 "34171d71fc5a320c83e7fff680e6f9b221ab62d7bb4abfecc1227003697b9150" => :el_capitan
+    sha256 "52a1235c1216d33d32aae59d41ab336a9b87dd28d5f9e3d98af8a6080ec3138e" => :yosemite
+  end
+
   depends_on "igbinary" => :build
 
   def install
