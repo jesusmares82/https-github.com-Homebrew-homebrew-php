8377b2fa52ec7ad2f682953553988a8e74a4ddf6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 18:30:27 +0000

    php72-lz4: add 0.2.3 bottle.

--- Formula/php72-lz4.rb
+++ Formula/php72-lz4.rb
@@ -8,6 +8,13 @@ class Php72Lz4 < AbstractPhp72Extension
   sha256 "f484c8229b3c5af1385ce11e4b37999702757bad9bdb2eab8bfe48fa3f159904"
   head "https://github.com/kjdev/php-ext-lz4.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ca1ece747407e353e65e668d8ed1a426f5d6ed002232a522cb9a26bd3637d343" => :sierra
+    sha256 "d245dad829a58e345c1cdd921a56206180ac3e370c945caf7464f5b8fb329d0e" => :el_capitan
+    sha256 "3ce8074478b68e52ff7298eac05d6797db44d80f638d9e980fe024b70f14cd00" => :yosemite
+  end
+
   def install
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
