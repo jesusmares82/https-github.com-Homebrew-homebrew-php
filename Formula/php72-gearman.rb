49bb9ae664927935df5b671d2a47d77ecc0d7261
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 17:58:30 +0000

    php72-gearman: add 2.0.3 bottle.

--- Formula/php72-gearman.rb
+++ Formula/php72-gearman.rb
@@ -8,6 +8,12 @@ class Php72Gearman < AbstractPhp72Extension
   sha256 "f71e8ff218f31e3b9a15534e18846b9f9526319daffcc12e76b545889e44e130"
   head "https://github.com/wcgallego/pecl-gearman.git"
 
+  bottle do
+    sha256 "6ba75c0f79091c89dcc08ced2f727683de005216fa84cd74c1faa055f34a4d5c" => :sierra
+    sha256 "9569b4894b3d00f163d81a0579b6e728c67686c107efb090af145869f8898901" => :el_capitan
+    sha256 "d3bd19442e0cdc220199e36a2c38cc38e5fbde86c055cdca8c650ca6e3f48f28" => :yosemite
+  end
+
   depends_on "gearman"
 
   def install
