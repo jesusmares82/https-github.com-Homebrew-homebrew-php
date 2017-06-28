9b06bfbcb709f273f2fc5abd43371063677e85b8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 18:56:21 +0000

    php72-timecop: add 1.2.4 bottle.

--- Formula/php72-timecop.rb
+++ Formula/php72-timecop.rb
@@ -8,6 +8,13 @@ class Php72Timecop < AbstractPhp72Extension
   sha256 "1e0bc0b47097500152aab0cd8fbc7f4b1323816cdbcd1b65df639d95356fbf85"
   head "https://github.com/hnw/php-timecop.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e30c17ea19633a5adaf614bc9beda974ce081c33fff30b7e0d1428306e389963" => :sierra
+    sha256 "150e028a01c39c8197ba0844422e70e8c2bc959d0904d237d5351e93c38f37c8" => :el_capitan
+    sha256 "7e7d1a84ebb18b405bb912f13d16889077c31aa50080b015ecf7c69e150cf4f3" => :yosemite
+  end
+
   def install
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
