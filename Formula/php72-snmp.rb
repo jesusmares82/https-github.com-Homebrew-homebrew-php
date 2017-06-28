d501c56eb61d22ac063d49a099d89f7a010c39cd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 19:37:07 +0000

    php72-snmp: add 7.2.0alpha2 bottle.

--- Formula/php72-snmp.rb
+++ Formula/php72-snmp.rb
@@ -5,6 +5,12 @@ class Php72Snmp < AbstractPhp72Extension
   desc "SNMP core php extension"
   homepage "http://php.net/manual/en/book.snmp.php"
 
+  bottle do
+    sha256 "80ddd877f587f974af3d45c74294c9bdba637c67d39f89511325f619702afa0e" => :sierra
+    sha256 "52c43398bc861425925685902bdc5656c05cddc4811db227099e87e22a14982f" => :el_capitan
+    sha256 "02a5a26b7c63a65c453e5f488b236decca3cbc328e7feca44e17da91bfcfbf80" => :yosemite
+  end
+
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
   version PHP_VERSION
