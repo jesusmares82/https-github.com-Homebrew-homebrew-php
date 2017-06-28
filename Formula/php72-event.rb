866f3fa913ea42ba42f0e74c47cba70dbfbe26ff
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 18:02:29 +0000

    php72-event: add 2.2.1 bottle.

--- Formula/php72-event.rb
+++ Formula/php72-event.rb
@@ -8,6 +8,12 @@ class Php72Event < AbstractPhp72Extension
   sha256 "44756686df68d8ef4fcee31359c0c03802b1f55ad88db7ac142169777f3d17ef"
   head "https://bitbucket.org/osmanov/pecl-event.git"
 
+  bottle do
+    sha256 "11de92c1be6b7c655293d628f9bfbf911d277e794faee4ecbd85b31e67f274be" => :sierra
+    sha256 "30dc893400f6d1055d894919c61a4ded59693f762012156ddbac226deec82001" => :el_capitan
+    sha256 "1d99173dd984c7687a15677ec928094e1ec86d6e62f56d6ba1f937a4f8d4936b" => :yosemite
+  end
+
   depends_on "libevent"
   depends_on "openssl"
 
