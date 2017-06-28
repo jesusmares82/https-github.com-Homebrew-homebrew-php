cb806fb505b4767ca2664e1403ef2d973ad93688
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 08:21:19 +0000

    php70-ds: update 1.1.10 bottle.

--- Formula/php70-ds.rb
+++ Formula/php70-ds.rb
@@ -10,9 +10,9 @@ class Php70Ds < AbstractPhp70Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2b8c00df9e8c76a67964e1833997cc3e77e8e9750d39e60c48731c6cbc38b8b5" => :el_capitan
-    sha256 "14e1a3e4ff5f2beadb6fa86035e582e18f21d6a1099387b53f1c411ad4ee8231" => :yosemite
-    sha256 "081281698a8294baf46d9b9896ff2f34b654a21b656067f414e093e282784da3" => :mavericks
+    sha256 "bef59b0f46c72e6a3e8e136803b73a377c626018c7071f70e648d14bb728c625" => :sierra
+    sha256 "8084469526b4c903bad13c449d83159094c174b7662993804eb6d7ef7b1ab6ba" => :el_capitan
+    sha256 "c7e9b0b0f07c98c94635bd7d23e56152528530d4c558ec15906ff55ac4b6cefb" => :yosemite
   end
 
   def install
