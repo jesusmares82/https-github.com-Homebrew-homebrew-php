fb208656a3d80d12f70671b575de3bbd1f1621b9
Ben Casey <bcasey@tigerstrikemedia.com>
2017-06-29 00:06:58 +0930

    Update phpxx-phalcon to Phalcon 3.2
    Closes #4296.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php71-phalcon.rb
+++ Formula/php71-phalcon.rb
@@ -4,8 +4,8 @@ class Php71Phalcon < AbstractPhp71Extension
   init
   desc "Full-stack PHP framework"
   homepage "https://phalconphp.com/"
-  url "https://github.com/phalcon/cphalcon/archive/v3.1.2.tar.gz"
-  sha256 "da619cab5979b3592b5e880a5e286e8d59841e911000be944fcddb030a7af587"
+  url "https://github.com/phalcon/cphalcon/archive/v3.2.0.tar.gz"
+  sha256 "32f83fc7ab4eeee37a6577e5920ed5d94f455c723cf9499d195fad3be30c104e"
   head "https://github.com/phalcon/cphalcon.git"
 
   bottle do
@@ -18,13 +18,7 @@ class Php71Phalcon < AbstractPhp71Extension
   depends_on "pcre"
 
   def install
-    if MacOS.prefer_64_bit?
-      Dir.chdir "build/php7/64bits"
-    else
-      Dir.chdir "build/php7/32bits"
-    end
-
-    ENV.universal_binary if build.universal?
+    Dir.chdir "build/php7/64bits"
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}",
