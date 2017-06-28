44231b73fdfefb3c91d6100ae0ec4fec064772e0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 18:41:35 +0000

    php72-oauth: add 2.0.2 bottle.

--- Formula/php72-oauth.rb
+++ Formula/php72-oauth.rb
@@ -8,6 +8,13 @@ class Php72Oauth < AbstractPhp72Extension
   sha256 "87ce1a5d585f439f0ead2683f2f7dca76d538df9143da681978356b5e1c6e8e8"
   head "https://github.com/php/pecl-web_services-oauth.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8e800a206c530343f1a380d241e01c4270c6909c8d2be79561ae6ba89e03a626" => :sierra
+    sha256 "550a7f3cc2145782d95a3404d6784bfb8d0cd724316ad0458886aff7b59849b2" => :el_capitan
+    sha256 "5acc56f5b50e75e1d497c19481f836c1994790c62bb2fdb057472272ecffb618" => :yosemite
+  end
+
   depends_on "pcre"
 
   def install
