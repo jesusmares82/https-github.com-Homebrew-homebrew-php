928226fee3e7adde6d0a468cad79a8ffabc5216e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-30 12:27:59 +0000

    php72-phalcon: add 3.2.0 bottle.

--- Formula/php72-phalcon.rb
+++ Formula/php72-phalcon.rb
@@ -8,6 +8,13 @@ class Php72Phalcon < AbstractPhp72Extension
   sha256 "32f83fc7ab4eeee37a6577e5920ed5d94f455c723cf9499d195fad3be30c104e"
   head "https://github.com/phalcon/cphalcon.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "23326e3cbc7cab588492f6b829620df790ead135c2bd6e1331c312c50f94fa94" => :sierra
+    sha256 "cbf85b388aebe108b4e8dc0c2888b71a2593559f2a020cac601c54b9921dd35a" => :el_capitan
+    sha256 "543008a7186665d1e51d810480026d64246a4d3bafca7b179a5cedbd7d990dd8" => :yosemite
+  end
+
   depends_on "pcre"
 
   def install
