bc50ab1e33c2e0c7bfc713400e98ae337ac66fa5
Felipe Rovere <flp.rovere@gmail.com>
2017-07-11 16:18:51 -0400

    deployer 5.1.3
    Closes #4322.
    
    Signed-off-by: Jan Viljanen <janne@martins-macbook-pro.home>

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -1,13 +1,11 @@
-
 require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 
 class Deployer < AbstractPhpPhar
   init
   desc "Deployment tool written in PHP with support for popular frameworks out of the box."
   homepage "https://deployer.org"
-  url "https://deployer.org/releases/v5.0.1/deployer.phar"
-  version "5.0.1"
-  sha256 "a6f2cc4227abe4a069608a81a14f92bfc9428dd0c225af63ddb43cff4ec7a9e5"
+  url "https://deployer.org/releases/v5.1.3/deployer.phar"
+  sha256 "06f7dc0479106e25b582bd9555ca597ddfa28c56d64df2965b6af484dc671686"
 
   bottle :unneeded
 
