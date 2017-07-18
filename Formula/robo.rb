131ad928b611ae70c9734fc1368f381c9c334f06
Michael Utz <michael@theutz.com>
2017-07-18 07:35:03 -0500

    robo 1.1.1

--- Formula/robo.rb
+++ Formula/robo.rb
@@ -4,8 +4,8 @@ class Robo < AbstractPhpPhar
   init
   desc "Modern Task Runner for PHP"
   homepage "http://robo.li/"
-  url "https://github.com/consolidation/Robo/releases/download/1.0.5/robo.phar"
-  sha256 "863b4ac9594e58f6dc822a7b18673e053f8763910c6ef2b29b453d20483e74f7"
+  url "https://github.com/consolidation/Robo/releases/download/1.1.1/robo.phar"
+  sha256 "aee018b00ac2190de6d87b80366acc7c685b9915a17c7bb04029d77865299a49"
 
   bottle :unneeded
 
