7d3b858e89bc8278d16d1bef09133472cd77b012
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-19 14:51:26 +0000

    codeception: update 2.3.4 bottle.

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -9,9 +9,9 @@ class Codeception < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0a8cb8382ffffa5834c8c7eaab2a844b751aadb301689ea39b5cbb432ba05042" => :sierra
-    sha256 "44cffd5643370029b5e4e394a97457f66d244f395c56880d1c3cd56fc77cacfa" => :el_capitan
-    sha256 "44cffd5643370029b5e4e394a97457f66d244f395c56880d1c3cd56fc77cacfa" => :yosemite
+    sha256 "be133a4a3b96900947fed48ebf71fb3f34bc619db41bd83725f2eec96dce6357" => :sierra
+    sha256 "be133a4a3b96900947fed48ebf71fb3f34bc619db41bd83725f2eec96dce6357" => :el_capitan
+    sha256 "be133a4a3b96900947fed48ebf71fb3f34bc619db41bd83725f2eec96dce6357" => :yosemite
   end
 
   def phar_file
