2e887b25d31f663f7beaf59876d85b954e544c6e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-21 12:44:38 +0000

    php72-gmp: update 7.2.0beta1_1 bottle.

--- Formula/php72-gmp.rb
+++ Formula/php72-gmp.rb
@@ -7,9 +7,9 @@ class Php72Gmp < AbstractPhp72Extension
   revision 1
 
   bottle do
-    sha256 "e6a5739ecf8a8b2f8dbef098d17290ecc4a10a386811b882a108ff0872df1d86" => :sierra
-    sha256 "e7e3e6a61b6b1b4b49efc115d773a86e6284460b79f299f1acfe67217d1fa26a" => :el_capitan
-    sha256 "63e93a11bd38ed7e92aed1ea25f5ed8b210708f55c599ca1e745e6a54256eae2" => :yosemite
+    sha256 "afecb543ad4e87d25614ddc092520b20863a5b4a44ecc76ec6114d4cc6b499f2" => :sierra
+    sha256 "89100030fa7c560ce1d90c9f0cc6e7fd525fa2b42c49e5c2f87565331b55dc8b" => :el_capitan
+    sha256 "6668a5949578be7d772277421cc3e7a038299b93a39b420306ed983ca787d83c" => :yosemite
   end
 
   url PHP_SRC_TARBALL
