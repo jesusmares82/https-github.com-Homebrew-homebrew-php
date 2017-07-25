c71aae3860dea27a75b53267e102d28b48010adb
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-07-25 19:55:32 +0100

    php71-trader: 0.4.0 (new formula)

---
+++ Formula/php71-trader.rb
@@ -0,0 +1,22 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Trader < AbstractPhp71Extension
+  init
+  desc "Technical Analysis for traders"
+  homepage "https://pecl.php.net/package/trader"
+  url "https://pecl.php.net/get/trader-0.4.0.tgz"
+  sha256 "64400b2331cd843cd1bb684b5d02145ac6c00118565915fe592c6eee3c108784"
+
+  depends_on "ta-lib"
+  depends_on "libtool" => :run
+
+  def install
+    Dir.chdir "trader-#{version}"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/trader.so"
+    write_config_file if build.with? "config-file"
+  end
+end
