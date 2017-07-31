af11c00e8428b4b34cf3933d7dbaa9b69f1430c5
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:17 -0700

    php54-protobuf: bottle revision to rebuild.

--- Formula/php54-protobuf.rb
+++ Formula/php54-protobuf.rb
@@ -10,7 +10,7 @@ class Php54Protobuf < AbstractPhp54Extension
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
     sha256 "3f828ff5b3e24bbe85b220b4ee731f965c911adcdff15ec3290aa3b49176aea8" => :el_capitan
     sha256 "963330d7bed30528c015a207ae7978dbecadb2a9c34ea9b44160a80e41adc3ea" => :yosemite
     sha256 "84a261c541f40c5ddb9225e52fd839247d5d2db983b1b5593e185e4372f09e06" => :mavericks
