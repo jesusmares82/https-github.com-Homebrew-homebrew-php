79556d8b755dba0eaee5e54ccfa08a454a0208e1
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:13 -0700

    php53-dbus: bottle revision to rebuild.

--- Formula/php53-dbus.rb
+++ Formula/php53-dbus.rb
@@ -9,7 +9,7 @@ class Php53Dbus < AbstractPhp53Extension
   head "http://svn.php.net/repository/pecl/dbus/trunk/"
 
   bottle do
-    revision 1
+    rebuild 1
     sha256 "03aee7d7e19c4c0b509dd59923c427d00120b995d1a645db3189fe6548c4ce40" => :el_capitan
     sha256 "51a89eb5862f603889b9911f25ee0a48f7f6afbcd36ad95bd102669c6c22e436" => :yosemite
     sha256 "84723ab8f3f096df56a920091fc945874afde520a67ced9aa58419b81096ec5f" => :mavericks
