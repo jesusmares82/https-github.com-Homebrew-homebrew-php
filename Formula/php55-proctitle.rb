3f5179791a410da75a7b61086aa3c82da00dd9e8
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:20 -0700

    php55-proctitle: bottle revision to rebuild.

--- Formula/php55-proctitle.rb
+++ Formula/php55-proctitle.rb
@@ -10,7 +10,7 @@ class Php55Proctitle < AbstractPhp55Extension
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
     sha256 "e0112adb7308cf07c61e5dff0febe4abd3d192d351d8a7865aea2a3797860bab" => :el_capitan
     sha256 "745681a9079914a80a7981880b98f5956b0edf6efd7e0a915e1aa5499fd227f8" => :yosemite
     sha256 "6536d504403e783230d6f41ead9097caf2746e6a05697c48d9990823b5f8be3f" => :mavericks
