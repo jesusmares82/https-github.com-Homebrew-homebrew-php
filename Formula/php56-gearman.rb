af4bc2d09a125285ea7fd5a2845f916d16d72cbe
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:21 -0700

    php56-gearman: bottle revision to rebuild.

--- Formula/php56-gearman.rb
+++ Formula/php56-gearman.rb
@@ -9,7 +9,7 @@ class Php56Gearman < AbstractPhp56Extension
   head "https://svn.php.net/repository/pecl/gearman/trunk/"
 
   bottle do
-    revision 1
+    rebuild 1
     sha256 "3b91d773978e6b43031c89a11f3e7f1f0eecb5462582e77d0b16b312e41e79f8" => :el_capitan
     sha256 "b383cc1cf3a6ff793089f6e12fc6388b873590bd5693d87e93e05bb53338bd77" => :yosemite
     sha256 "c56d88eaae19455b00a5a071ea3e3d8622d7f747968d7875a82a71c75bb74de0" => :mavericks
