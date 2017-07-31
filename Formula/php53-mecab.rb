8a1a3ff0613341222ce007f16d6ae5837878c9f6
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:14 -0700

    php53-mecab: bottle revision to rebuild.

--- Formula/php53-mecab.rb
+++ Formula/php53-mecab.rb
@@ -9,7 +9,7 @@ class Php53Mecab < AbstractPhp53Extension
   head "https://github.com/rsky/php-mecab.git"
 
   bottle do
-    revision 1
+    rebuild 1
     sha256 "4c3dcda4684724f018210a990a379db84efeff4d6c0d8b3f766aacd3456d9adb" => :el_capitan
     sha256 "bb5cff0cbca74abd83cc22b20ecd96ef8b0def8045075ffef8da0aaf882dce6c" => :yosemite
     sha256 "8158d871d832acb0ec00752422a6b45f86eca242a3e89175ad15d20b54b3d62a" => :mavericks
