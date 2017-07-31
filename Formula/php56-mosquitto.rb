c775af92c960f5b51055dc7de34ddc5827a4de91
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:22 -0700

    php56-mosquitto: bottle revision to rebuild.

--- Formula/php56-mosquitto.rb
+++ Formula/php56-mosquitto.rb
@@ -10,7 +10,7 @@ class Php56Mosquitto < AbstractPhp56Extension
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
     sha256 "a16fdda4fa23cbd9b8ecf7b029d3fec62e9ec0591d65b7159f172d1a981037cb" => :el_capitan
     sha256 "c29e8a565293f6519a749ee674c3c569ab84c6bba3894da28bb7b420f1f65647" => :yosemite
     sha256 "478d267893a49fba086e3a1dd9f899b95b14e5a639c6280e0ff9f83a703bcb06" => :mavericks
