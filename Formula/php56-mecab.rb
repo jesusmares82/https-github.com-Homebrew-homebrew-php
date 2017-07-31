b7117c7885a363d07c815c63c74fe31ef31391fc
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:22 -0700

    php56-mecab: bottle revision to rebuild.

--- Formula/php56-mecab.rb
+++ Formula/php56-mecab.rb
@@ -9,7 +9,7 @@ class Php56Mecab < AbstractPhp56Extension
   head "https://github.com/rsky/php-mecab.git"
 
   bottle do
-    revision 1
+    rebuild 1
     sha256 "df7be0ed6da1da8f27bf0afa2b08fa4e442debef2ad38a3d528a8d09de3d4f2b" => :el_capitan
     sha256 "8c6d2a7405452713b58adf8c5667bc56578d689e6b25c4b06ed7e04a2b69b8a6" => :yosemite
     sha256 "f82091ab7e3a667b8a4ecff880fb32dd547450af159a5620df5a30981d3381eb" => :mavericks
