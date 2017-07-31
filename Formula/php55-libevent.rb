82a1735ae17e1f309baa3c2f055ae1da0a79cc7c
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:19 -0700

    php55-libevent: bottle revision to rebuild.

--- Formula/php55-libevent.rb
+++ Formula/php55-libevent.rb
@@ -9,7 +9,7 @@ class Php55Libevent < AbstractPhp55Extension
   head "http://svn.php.net/repository/pecl/libevent/trunk/"
 
   bottle do
-    revision 1
+    rebuild 1
     sha256 "fe99f493d79f243a3ccad314e6342f0ee90ec5ef912dc068abf579009967ece3" => :el_capitan
     sha256 "bb341e274cf91f892867ac6c5468cc29bae73ccf709fa1c95d06eb75fc5d221d" => :yosemite
     sha256 "fad5ab08259e3e3af93a56dd7ac958a45b1cf3d7b0ed9199a45bf85df0b8a5ab" => :mavericks
