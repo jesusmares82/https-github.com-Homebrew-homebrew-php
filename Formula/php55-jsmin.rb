b6247e8baca907ee81f157c556b0919942ca9523
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:19 -0700

    php55-jsmin: bottle revision to rebuild.

--- Formula/php55-jsmin.rb
+++ Formula/php55-jsmin.rb
@@ -10,7 +10,7 @@ class Php55Jsmin < AbstractPhp55Extension
 
   bottle do
     cellar :any_skip_relocation
-    revision 2
+    rebuild 2
     sha256 "41328d52f924eb5fd920fb4ca5af852050e8c5653662e03ef88f9850a6d1dc3d" => :el_capitan
     sha256 "ec6fc371111b75ec29f492f915ff42a81cb5ce992bc6f0431d94ecf4e8b7851c" => :yosemite
     sha256 "7248655b324ac3769aa15cafc90d4dada869c3936043ea0e902b106e1319f634" => :mavericks
