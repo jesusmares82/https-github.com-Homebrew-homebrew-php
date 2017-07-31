3a8258016cd8e8e0a89aa0862392f6dd61c1af04
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:22 -0700

    php56-protobuf: bottle revision to rebuild.

--- Formula/php56-protobuf.rb
+++ Formula/php56-protobuf.rb
@@ -11,7 +11,7 @@ class Php56Protobuf < AbstractPhp56Extension
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
     sha256 "043dccfb1da3e3f9cf303511d19e96ee9dfbbfa4cd8c25018f2ef5356073557c" => :el_capitan
     sha256 "633d0171ed34a8e4cd15d7f5a756faf924f5dac0178807d6d5e32c92c582bf29" => :yosemite
     sha256 "fdf187e3c88665af8d92a290ad3ca4682d9a0d5e41d1f52333eb9baff7c570ef" => :mavericks
