a7ca27ea5be69aa908a362e7cde9b70de8f48806
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:17 -0700

    php54-opcache: bottle revision to rebuild.

--- Formula/php54-opcache.rb
+++ Formula/php54-opcache.rb
@@ -11,7 +11,7 @@ class Php54Opcache < AbstractPhp54Extension
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
     sha256 "4b9cc3a0729952ff83063e8300232f0df97e046f0bf887d72de8b9345bb5ccb1" => :el_capitan
     sha256 "24ba09157df24050a723c3888c32009ae76634e1f3eb0b622626de23bcbb6e68" => :yosemite
     sha256 "4cc160ea8cd1f1f3a7b11411e22ca946121310b65cb3cbf734b4208dbc46a43a" => :mavericks
