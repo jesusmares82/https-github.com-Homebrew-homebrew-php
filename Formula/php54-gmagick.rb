763c1628256aea692ce32654b83932422edbe214
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:16 -0700

    php54-gmagick: bottle revision to rebuild.

--- Formula/php54-gmagick.rb
+++ Formula/php54-gmagick.rb
@@ -8,7 +8,7 @@ class Php54Gmagick < AbstractPhp54Extension
   sha256 "8e51c8343d6e6d556d7b17417ce338c6ed2b0893869f1494410dfe6ba5105475"
 
   bottle do
-    revision 1
+    rebuild 1
     sha256 "6889f8c7b0b3e24ab4cdd42d12cde28303ec7fc6aa849326d3bdadbaf3a4fd61" => :el_capitan
     sha256 "631252fe89e421e9f5b55ce2746fbe17078f9e761bed43cc2ce76148642bdaa1" => :yosemite
     sha256 "afa2a334366ce68db8844119b6cf5b943ba971803a95ec0d88bfc3629b7839ab" => :mavericks
