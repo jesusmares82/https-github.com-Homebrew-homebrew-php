1f5e670086fbb054a025742a994421e19d015d80
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:15 -0700

    php54-binpack: bottle revision to rebuild.

--- Formula/php54-binpack.rb
+++ Formula/php54-binpack.rb
@@ -10,7 +10,7 @@ class Php54Binpack < AbstractPhp54Extension
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
     sha256 "bf67b87d114a4b3bc5a2ed06a5020d1bbb9afbdea37f84bb678083e3551c5d1c" => :el_capitan
     sha256 "2bce9f13b36290870a0ed44608a7dd93f7223b8f9fa63f46211f8a071d2d7bcb" => :yosemite
     sha256 "c484eb30fd0b3d949e8a75fad05fc9f3190a129eea02f4ab3bf7165a40dcf462" => :mavericks
