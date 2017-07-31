6c3bee7372bcff2bb68462294a1ac29d29dad122
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:22 -0700

    php56-pimple: bottle revision to rebuild.

--- Formula/php56-pimple.rb
+++ Formula/php56-pimple.rb
@@ -10,7 +10,7 @@ class Php56Pimple < AbstractPhp56Extension
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
     sha256 "8800e7a0c9c696e5ea1de5c324706343d4ae066af01d0ea84a5f419511d04e61" => :el_capitan
     sha256 "60df7292d22c871248f6927ce67f262693a69ddc77d189451498fd77cf51b698" => :yosemite
     sha256 "9815ce167b07fa3da2221b939498946c7a0121bcc7b9c4f890980d74dcffb993" => :mavericks
