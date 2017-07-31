e7473c62c76544ff4404dc552d2c7e01b4525b24
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:22 -0700

    php56-redland: bottle revision to rebuild.

--- Formula/php56-redland.rb
+++ Formula/php56-redland.rb
@@ -9,7 +9,7 @@ class Php56Redland < AbstractPhp56Extension
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
     sha256 "85530f3f07ae5b9872daf144f083657b1c6d4859fb283cf10f28b7c3eb3ab8c0" => :el_capitan
     sha256 "c18b2564c8f1c1c7d06aec39b72bb89af296294e75e7506767899ce6459db07b" => :yosemite
     sha256 "b751e2eb1fa57ce6722763ca949122ead5730b059ad3a1823ba6f50da46cb876" => :mavericks
