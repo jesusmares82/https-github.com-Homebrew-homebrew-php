bdf7a752ce21afbd5060fa55681ed7b782a47ff6
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:05:13 -0700

    php-build: bottle revision to rebuild.

--- Formula/php-build.rb
+++ Formula/php-build.rb
@@ -7,7 +7,7 @@ class PhpBuild < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
     sha256 "4dfd489892f5e846fcaae4995396e19990f2d950afbc6552d432cbe56a590e42" => :el_capitan
     sha256 "b250b4adcd147317bbfce32b0660cf9b64a35cac42a5df859b94f0dd8f322186" => :yosemite
     sha256 "8fb4b8ad763a6292d934a02c35bda05737ed30df5dc1feebc4375f90c75dc2af" => :mavericks
