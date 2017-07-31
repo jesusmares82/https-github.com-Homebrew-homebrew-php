f3111558cece102b9d09e19b3999967089c2dd90
ilovezfs <ilovezfs@icloud.com>
2017-07-31 03:16:40 -0700

    php53-redland: remove deprecated fails_with :llvm.

--- Formula/php53-redland.rb
+++ Formula/php53-redland.rb
@@ -20,10 +20,6 @@ class Php53Redland < AbstractPhp53Extension
   depends_on "raptor"
   depends_on "rasqal"
 
-  fails_with :llvm do
-    build 2334
-  end
-
   def install
     args = %W[
       --disable-dependency-tracking
