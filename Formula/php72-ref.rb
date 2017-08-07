9866c8932d91296087550c0af0968198873d7d22
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-07 17:30:57 +0000

    php72-ref: update 0.4.4 bottle.

--- Formula/php72-ref.rb
+++ Formula/php72-ref.rb
@@ -10,9 +10,9 @@ class Php72Ref < AbstractPhp72Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cca27e2dc10a14f2a09759a50aaa0df010ec7517f95c9ef52bcd4ddbe675e4d2" => :el_capitan
-    sha256 "ee01f8855875df67f9d641d855a2f301c1ef221481870eaa7369150a59e78c3b" => :yosemite
-    sha256 "29ce63f43594b6e341f7faff2fbd5739245d1f2f6c49cdf4aa016b08b0c6e47c" => :mavericks
+    sha256 "cfd6b74a7530c8ec9ef31e781f2ee5c48870a8c0529f1c5b0d2d11c6a4a626b7" => :sierra
+    sha256 "2a960f30cd3ce608d122cd0260ec522c06828dbc45d7a207d609c848bf866f75" => :el_capitan
+    sha256 "661f28161adbe8c2c694b6cd8b7ad64093603690483220884370f23369b8ad02" => :yosemite
   end
 
   def install
