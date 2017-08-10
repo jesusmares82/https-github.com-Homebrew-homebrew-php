4f310ca030c7c09d46fb33be76909d2f6fd8d7b4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-10 14:13:58 +0000

    php72-v8js: add 1.4.1 bottle.

--- Formula/php72-v8js.rb
+++ Formula/php72-v8js.rb
@@ -8,6 +8,13 @@ class Php72V8js < AbstractPhp72Extension
   sha256 "38a16ae4381eb36695cb2f4cce802124242c279cfbd7640d256aff57d260ad4c"
   head "https://github.com/phpv8/v8js.git"
 
+  bottle do
+    cellar :any
+    sha256 "de8701069e35e9b23d8bdfcc7d1626426de777f3d70624a33a1d2fe26ad582bc" => :sierra
+    sha256 "4a29df0dcb00c00475da0038b797930440abbd351382a06c019965b0b27832ee" => :el_capitan
+    sha256 "77b13ce4d6468d36b9c585a30912ffda33db094943608067cbe901dd556e0147" => :yosemite
+  end
+
   depends_on "v8"
 
   def install
