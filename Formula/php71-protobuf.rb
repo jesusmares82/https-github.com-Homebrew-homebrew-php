680f718187b34a29a75676ea658e342fd39dd051
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-15 20:43:27 +0000

    php71-protobuf: add 3.3.2 bottle.

--- Formula/php71-protobuf.rb
+++ Formula/php71-protobuf.rb
@@ -7,6 +7,13 @@ class Php71Protobuf < AbstractPhp71Extension
   url "https://pecl.php.net/get/protobuf-3.3.2.tgz"
   sha256 "01ee52961eb468149e23b36cf1f3ce1d2fab74bef3be988cac6e5c3d4651ff52"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c609ce590a954b233ea45be4888aa4d544bf01a2cd3409bc6ec7376bbeef4ebc" => :sierra
+    sha256 "ef906debd654600db9051979e7a15d7e316d88e6379c23f081a00dc91b6df5ba" => :el_capitan
+    sha256 "b2acb0eeabcf02672e2a06951d79e0349cca184d72013c405b631943b0cbda5a" => :yosemite
+  end
+
   depends_on "protobuf"
 
   def install
