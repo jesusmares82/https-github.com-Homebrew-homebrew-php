9d8b7fb9721d7f7540b8e1f1b74a2658dd388368
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-18 05:36:22 +0000

    phan@0.8: update 0.8.6 bottle.

--- Formula/phan@0.8.rb
+++ Formula/phan@0.8.rb
@@ -11,9 +11,9 @@ class PhanAT08 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7c684f36976ec0fbe0252d824d2274127ffaf84470d4742634c1427583ddd2c6" => :sierra
-    sha256 "9865b3cb0c444223c46a1a6f9586aeef751e8b8716fcf874e236835a396efd33" => :el_capitan
-    sha256 "7e2d11ed186bd8142e9bbc69030d12e1ab62482d5511207862d251528197c7f6" => :yosemite
+    sha256 "18825c0adc215e31adb78ad4913077498e09bfcd0dff1e8df34d49733c03ee99" => :sierra
+    sha256 "ce78c87943311aa2d067fe88c1bff4724dbd38f023a3e5e0c82173c7c22d5183" => :el_capitan
+    sha256 "6bd6bdce03d37db0ae61b8002c69bc9a089ee54cfbcd23bb9165b6841fd937a8" => :yosemite
   end
 
   depends_on "php70-ast"
