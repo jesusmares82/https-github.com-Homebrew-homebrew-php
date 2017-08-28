70a6177a0657da51cbabd603916a01161c1e6d2a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-28 21:00:03 +0000

    php72-lz4: update 0.2.3_1 bottle.

--- Formula/php72-lz4.rb
+++ Formula/php72-lz4.rb
@@ -11,9 +11,9 @@ class Php72Lz4 < AbstractPhp72Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ca1ece747407e353e65e668d8ed1a426f5d6ed002232a522cb9a26bd3637d343" => :sierra
-    sha256 "d245dad829a58e345c1cdd921a56206180ac3e370c945caf7464f5b8fb329d0e" => :el_capitan
-    sha256 "3ce8074478b68e52ff7298eac05d6797db44d80f638d9e980fe024b70f14cd00" => :yosemite
+    sha256 "e0d59e7a80ed74a99d15a033dcce0320cdcadbf1b17046694082ed7488bc8d19" => :sierra
+    sha256 "db7bdfcaa2531d84d23d420876f748696e87bbd9fd487ba8dc9f97c1498ce17a" => :el_capitan
+    sha256 "0c0cc6eaee1878e8544dad66156cd5c259eb3073bd5326aa9c18748c91e5bf72" => :yosemite
   end
 
   def install
