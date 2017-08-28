03269443099c57080ccaf9984009798d5ad182af
BePsvPT <freedom@csie.io>
2017-08-28 08:00:27 +0800

    n98-magerun2 1.6.0
    Closes #4421.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/n98-magerun2.rb
+++ Formula/n98-magerun2.rb
@@ -4,8 +4,8 @@ class N98Magerun2 < AbstractPhpPhar
   init
   desc "Swiss army knife for Magento 2 developers, sysadmins and devops."
   homepage "http://magerun.net/"
-  url "https://files.magerun.net/n98-magerun2-1.5.0.phar"
-  sha256 "d94f4b938f9c43d4a42f0e0ddcd38a051525f1ab0eb0cd84816ca48bbe5d2c20"
+  url "https://files.magerun.net/n98-magerun2-1.6.0.phar"
+  sha256 "3f185a4199d0137bc0961ed6eebe37b1c9363496101df56457ddfb591523ac6a"
 
   bottle :unneeded
 
