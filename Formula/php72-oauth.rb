cee58f846ed79f0a9cd11abf31adcc51fd3d359e
Andreas Braun <git@alcaeus.org>
2017-08-29 07:19:47 +0200

    Bump revision for php72-oauth
    Closes #4424.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/php72-oauth.rb
+++ Formula/php72-oauth.rb
@@ -7,6 +7,7 @@ class Php72Oauth < AbstractPhp72Extension
   url "https://pecl.php.net/get/oauth-2.0.2.tgz"
   sha256 "87ce1a5d585f439f0ead2683f2f7dca76d538df9143da681978356b5e1c6e8e8"
   head "https://github.com/php/pecl-web_services-oauth.git"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
