4383ef7ab12b947d31859fe6441453b547829912
tepsl <tepsl@users.noreply.github.com>
2017-08-30 18:14:04 +0900

    php71-protobuf: update to 3.4.0
    Closes #4438.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/php71-protobuf.rb
+++ Formula/php71-protobuf.rb
@@ -4,8 +4,8 @@ class Php71Protobuf < AbstractPhp71Extension
   init
   desc "Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data."
   homepage "https://pecl.php.net/package/protobuf"
-  url "https://pecl.php.net/get/protobuf-3.3.2.tgz"
-  sha256 "01ee52961eb468149e23b36cf1f3ce1d2fab74bef3be988cac6e5c3d4651ff52"
+  url "https://pecl.php.net/get/protobuf-3.4.0.tgz"
+  sha256 "510d8ea544e3d5fc44fa5d13f6db5dd7af3e292aee66f90409f95f4450414a71"
 
   bottle do
     cellar :any_skip_relocation
