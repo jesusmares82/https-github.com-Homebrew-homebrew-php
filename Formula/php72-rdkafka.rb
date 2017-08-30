a75d21a70e4d78b3e773064563a0f7a35902b6bd
Andreas Braun <git@alcaeus.org>
2017-08-30 07:09:32 +0200

    Bump revision for php72-rdkafka
    Closes #4427.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/php72-rdkafka.rb
+++ Formula/php72-rdkafka.rb
@@ -7,6 +7,7 @@ class Php72Rdkafka < AbstractPhp72Extension
   url "https://pecl.php.net/get/rdkafka-2.0.1.tgz"
   sha256 "43a3704198f7d8ae23f6ed06d2d28ae4d52bae0f93f484aa5d61d5d60f809eee"
   head "https://github.com/arnaud-lb/php-rdkafka.git"
+  revision 1
 
   bottle do
     cellar :any
