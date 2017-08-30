3da470827fb3fe7958689c98da681ad5a596c304
Andreas Braun <git@alcaeus.org>
2017-08-30 09:54:09 +0200

    Bump revision for php72-yac
    Closes #4436.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/php72-yac.rb
+++ Formula/php72-yac.rb
@@ -7,6 +7,7 @@ class Php72Yac < AbstractPhp72Extension
   url "https://github.com/laruence/yac/archive/yac-2.0.1.tar.gz"
   sha256 "c9357c9779a87370b53f719cfcf86b80258b119376c718fea336988b1d7354a0"
   head "https://github.com/laruence/yac.git"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
