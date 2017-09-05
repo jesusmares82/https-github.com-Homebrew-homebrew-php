f96babbee87eb76dea0f70544bf1a299f34a46c9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-05 17:48:49 +0000

    drupalconsole: update 1.0.2 bottle.

--- Formula/drupalconsole.rb
+++ Formula/drupalconsole.rb
@@ -9,9 +9,9 @@ class Drupalconsole < AbstractPhpPhar
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7ed94f13ff6eacfcfc8d1676e4343456d2d402ce576e63951acdbf8685c320be" => :sierra
-    sha256 "7ed94f13ff6eacfcfc8d1676e4343456d2d402ce576e63951acdbf8685c320be" => :el_capitan
-    sha256 "7ed94f13ff6eacfcfc8d1676e4343456d2d402ce576e63951acdbf8685c320be" => :yosemite
+    sha256 "9941992a509368b6afb7519c046d33e953167608352fac58517d7d0f02503825" => :sierra
+    sha256 "9941992a509368b6afb7519c046d33e953167608352fac58517d7d0f02503825" => :el_capitan
+    sha256 "9941992a509368b6afb7519c046d33e953167608352fac58517d7d0f02503825" => :yosemite
   end
 
   def install
