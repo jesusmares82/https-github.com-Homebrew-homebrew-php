b9af3cea9a0821a7d6a9f8fb169868a936f113a2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 14:37:40 +0000

    php55-mosquitto: update 0.4.0 bottle.

--- Formula/php55-mosquitto.rb
+++ Formula/php55-mosquitto.rb
@@ -10,10 +10,8 @@ class Php55Mosquitto < AbstractPhp55Extension
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "a16fdda4fa23cbd9b8ecf7b029d3fec62e9ec0591d65b7159f172d1a981037cb" => :el_capitan
-    sha256 "c29e8a565293f6519a749ee674c3c569ab84c6bba3894da28bb7b420f1f65647" => :yosemite
-    sha256 "478d267893a49fba086e3a1dd9f899b95b14e5a639c6280e0ff9f83a703bcb06" => :mavericks
+    sha256 "5216a87cef11e4c40d2f5209af577a15302050563c1e00dd90989d2ea2321f28" => :sierra
+    sha256 "57788eae72312bd05e35e9ef028cd7334b650b918e89b9ce3f52319111a1f169" => :el_capitan
   end
 
   depends_on "mosquitto"
