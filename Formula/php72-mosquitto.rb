8340e5eda221adccff93bce9612a7b3d414d2908
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 14:37:40 +0000

    php72-mosquitto: update 0.4.0 bottle.

--- Formula/php72-mosquitto.rb
+++ Formula/php72-mosquitto.rb
@@ -10,10 +10,8 @@ class Php72Mosquitto < AbstractPhp72Extension
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "a16fdda4fa23cbd9b8ecf7b029d3fec62e9ec0591d65b7159f172d1a981037cb" => :el_capitan
-    sha256 "c29e8a565293f6519a749ee674c3c569ab84c6bba3894da28bb7b420f1f65647" => :yosemite
-    sha256 "478d267893a49fba086e3a1dd9f899b95b14e5a639c6280e0ff9f83a703bcb06" => :mavericks
+    sha256 "ac8a0840038db30ee21552540b5c5134803580f63bf8aba296c8500b3e0e50a2" => :sierra
+    sha256 "dde5f0ad882de64c6d8baf55ffb5cfb85271b16a9d99ec7a8324b2e4b2ca4af0" => :el_capitan
   end
 
   depends_on "mosquitto"
