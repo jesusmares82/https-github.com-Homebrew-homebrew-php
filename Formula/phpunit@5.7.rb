abf62372feac021d8357e8c60543e57036cabffb
Sean Molenaar <sean@m2mobi.com>
2017-09-24 13:10:08 +0200

    phpunit@5.7 5.7.22
    Closes #4479.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/phpunit@5.7.rb
+++ Formula/phpunit@5.7.rb
@@ -4,8 +4,8 @@ class PhpunitAT57 < AbstractPhpPhar
   init
   desc "(Old stable) Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-5.7.21.phar"
-  sha256 "ea349e6c2643b4b092f2da0d12d7d234c6bb2cdf7dd7903c3882223d0c362b4e"
+  url "https://phar.phpunit.de/phpunit-5.7.22.phar"
+  sha256 "908fa85bbf085c0fce85af36da40cbaa075997c81f085704716e0a68b88c38c7"
 
   bottle do
     cellar :any_skip_relocation
