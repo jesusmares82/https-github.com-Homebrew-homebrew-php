1d52628a92c66ecda97acbce23d045b624eae220
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-25 11:50:45 +0000

    php70-mailparse: add 3.0.2 bottle.

--- Formula/php70-mailparse.rb
+++ Formula/php70-mailparse.rb
@@ -8,6 +8,13 @@ class Php70Mailparse < AbstractPhp70Extension
   sha256 "d81a6f8a48e43fca1c0f0d6c90d666830c24d584a23a60add43062aaf221843b"
   head "https://github.com/php/pecl-mail-mailparse.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "923347391f09977576d7c3aa7d106bdeb7076c413f7d534b3554e9d311d6d27e" => :high_sierra
+    sha256 "0cdef239df0e04b9252b99b9970a198c53fd134a594f452032a6604e84c430e5" => :sierra
+    sha256 "c3ec9f3ddb636a422a9c9b4218b43567e6231bd3cd6823ec7a22401bf0923edf" => :el_capitan
+  end
+
   depends_on "pcre"
 
   def install
