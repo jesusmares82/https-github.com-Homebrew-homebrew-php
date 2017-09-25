3033a79391edc1a77249494ff1d1fcb8f12cc26c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-25 20:28:43 +0000

    php70-ice: add 3.7.0 bottle.

--- Formula/php70-ice.rb
+++ Formula/php70-ice.rb
@@ -7,6 +7,12 @@ class Php70Ice < AbstractPhp70Extension
   url "https://github.com/zeroc-ice/ice/archive/v3.7.0.tar.gz"
   sha256 "809fff14a88a7de1364c846cec771d0d12c72572914e6cc4fb0b2c1861c4a1ee"
 
+  bottle do
+    sha256 "6c114e551f0e06dc726724305fff785caa114bbb48dde44225404fc986d52fa3" => :high_sierra
+    sha256 "6c114e551f0e06dc726724305fff785caa114bbb48dde44225404fc986d52fa3" => :sierra
+    sha256 "0a725470f110501693e96a916e3d5de415066277b5988d3dcc6d96bdfc215492" => :el_capitan
+  end
+
   depends_on "ice"
 
   def config_file
