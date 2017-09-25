cc6b01208d097aa8d3898d4f4815b778f3ded92f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-25 16:04:09 +0000

    php72-timecop: update 1.2.4_1 bottle.

--- Formula/php72-timecop.rb
+++ Formula/php72-timecop.rb
@@ -11,9 +11,9 @@ class Php72Timecop < AbstractPhp72Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e30c17ea19633a5adaf614bc9beda974ce081c33fff30b7e0d1428306e389963" => :sierra
-    sha256 "150e028a01c39c8197ba0844422e70e8c2bc959d0904d237d5351e93c38f37c8" => :el_capitan
-    sha256 "7e7d1a84ebb18b405bb912f13d16889077c31aa50080b015ecf7c69e150cf4f3" => :yosemite
+    sha256 "e32b0860ad3fda83db47db9c4fa6817d89c831acff9d16f0b78c5c3deaf5c7d0" => :high_sierra
+    sha256 "10ceb834a357dd6dec0d8405df1ac2307482c22954df7c95ca9940401d8c7aa1" => :sierra
+    sha256 "26ef4944535d08cf0d23f3db16c8772c00b89a6cc1d209de76be4f6ef56f26cb" => :el_capitan
   end
 
   def install
