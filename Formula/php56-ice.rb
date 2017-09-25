ec13883ec42c7de24c1adbb703cd0cbcec529f99
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-25 20:28:43 +0000

    php56-ice: add 3.7.0 bottle.

--- Formula/php56-ice.rb
+++ Formula/php56-ice.rb
@@ -7,6 +7,12 @@ class Php56Ice < AbstractPhp56Extension
   url "https://github.com/zeroc-ice/ice/archive/v3.7.0.tar.gz"
   sha256 "809fff14a88a7de1364c846cec771d0d12c72572914e6cc4fb0b2c1861c4a1ee"
 
+  bottle do
+    sha256 "1093bd1c095cd1258a293f0d120aefe328ee55d41c57ca796dd16231284d7032" => :high_sierra
+    sha256 "1093bd1c095cd1258a293f0d120aefe328ee55d41c57ca796dd16231284d7032" => :sierra
+    sha256 "6e853b6eb6e6ba05b31ce9d3ae4bb19defb28b20c949aa3bd1acaa7d89152ece" => :el_capitan
+  end
+
   depends_on "ice"
 
   def config_file
