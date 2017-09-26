9ff421608bb55ffab02d655f801026de018f7c0c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-26 00:22:19 +0000

    php71-trader: add 0.4.0 bottle.

--- Formula/php71-trader.rb
+++ Formula/php71-trader.rb
@@ -7,6 +7,13 @@ class Php71Trader < AbstractPhp71Extension
   url "https://pecl.php.net/get/trader-0.4.0.tgz"
   sha256 "64400b2331cd843cd1bb684b5d02145ac6c00118565915fe592c6eee3c108784"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f5caff0e7e51b77c09be949e16681f24142b3eb40200ad9bc8457a5342b10bf8" => :high_sierra
+    sha256 "e901ce8726bc7df98dab90d083db7d250f8969e97023e6ffa25fc9b008e98557" => :sierra
+    sha256 "757759b55d1d9a8c29cdc7b8580e7760f313d4fce27f3240e8114203a8a5525c" => :el_capitan
+  end
+
   depends_on "ta-lib"
   depends_on "libtool" => :run
 
