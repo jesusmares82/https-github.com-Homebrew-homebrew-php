d97deef49b5f76ccdf9852e81a3206f44263d0df
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-26 00:22:19 +0000

    php56-trader: add 0.4.0 bottle.

--- Formula/php56-trader.rb
+++ Formula/php56-trader.rb
@@ -7,6 +7,13 @@ class Php56Trader < AbstractPhp56Extension
   url "https://pecl.php.net/get/trader-0.4.0.tgz"
   sha256 "64400b2331cd843cd1bb684b5d02145ac6c00118565915fe592c6eee3c108784"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8c0a7951352b910cf00b97dbb5d4c071c7206a27b1e7071f845378bed094f256" => :high_sierra
+    sha256 "90697f8b042fd48d7402c7d65445c08ffe425628777a0f8259f69952a95eef97" => :sierra
+    sha256 "25b1bb7b4cdbd508d67fe320cf855fa61be26cb72291885c8b60546661552eaf" => :el_capitan
+  end
+
   depends_on "ta-lib"
   depends_on "libtool" => :run
 
