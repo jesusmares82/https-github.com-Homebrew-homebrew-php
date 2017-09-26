216e00de9a2b72bf1439a5c9bd91eb6443a9bb8c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-26 16:27:20 +0000

    php70-ast: update 0.1.5 bottle.

--- Formula/php70-ast.rb
+++ Formula/php70-ast.rb
@@ -9,10 +9,10 @@ class Php70Ast < AbstractPhp70Extension
   head "https://github.com/nikic/php-ast.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "df5b5723249fa0a9ec8df775836d22d14c79aaa7fcf4914d6c4f126ce4cf0451" => :sierra
-    sha256 "4390b2bda0321a5ae8564739ca0f8a3b167242904690d3add00a646dbcaf5af0" => :el_capitan
-    sha256 "01f16769082ba04368aea5b47d8779d1d3d068950d44b223ae0e0745596e0745" => :yosemite
+    cellar :any
+    sha256 "289f41974d101bebeb8c8c047ceac828c3ba702250c46c113e0480de0385cb90" => :high_sierra
+    sha256 "9a99263ebe2860b92cf8c941caee10e4fc9507ac1bd7cd7541f718b6fb6e5d48" => :sierra
+    sha256 "681e0ca5312fbfb0a5aac3651c3a0ad17a9ccb5d3e7d78c372b30ed0510f0bc2" => :el_capitan
   end
 
   def install
