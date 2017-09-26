52cfacf19031bb8d3c9ee64efbfb7e4affaa1d8f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-26 00:22:19 +0000

    php70-trader: add 0.4.0 bottle.

--- Formula/php70-trader.rb
+++ Formula/php70-trader.rb
@@ -7,6 +7,13 @@ class Php70Trader < AbstractPhp70Extension
   url "https://pecl.php.net/get/trader-0.4.0.tgz"
   sha256 "64400b2331cd843cd1bb684b5d02145ac6c00118565915fe592c6eee3c108784"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "30e914af51fb6d3f132deb6c7cd46e6ba7baa37587929d2858e0f3ba638d5ca5" => :high_sierra
+    sha256 "0d8e67385613fc928d2887fbf8f7d4fa56e01bf3b89f3a93de53f302520e1a1c" => :sierra
+    sha256 "8a1efdfa7dfc5a3979d9d33fa1512d3582bb68ef82dc90145c91cb3369b4dcf2" => :el_capitan
+  end
+
   depends_on "ta-lib"
   depends_on "libtool" => :run
 
