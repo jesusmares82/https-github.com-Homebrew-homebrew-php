c475a408df0d7a907bb2d68e71344b7adee993ab
Jeff King <peff@peff.net>
2017-09-26 12:44:08 -0700

    terminus: update tarball sha256 due to git bugfix
    The hash update is discussed further in:
    
      https://github.com/Homebrew/homebrew-core/issues/18044
    
    This was found by running the same script as was used for
    homebrew-core in:
    
      https://github.com/Homebrew/homebrew-core/pull/18073
    
    Closes #4484.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -7,7 +7,7 @@ class Terminus < Formula
   desc "Command-line interface for the Pantheon Platform"
   homepage "https://github.com/pantheon-systems/terminus"
   url "https://github.com/pantheon-systems/terminus/archive/1.5.0.tar.gz"
-  sha256 "f219cb89f6aca80556c3124f2fe33b8a10c42823328de73d99ecc8643bcbebc9"
+  sha256 "d0ca4b586cb712b0e489d3c5061ee26f9cf9905f12a984a2329c8e74d5e1a7b9"
   head "https://github.com/pantheon-systems/terminus.git"
 
   bottle do
