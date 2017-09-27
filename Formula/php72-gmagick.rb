4d17462d40aa77f8a33160a9012f430e2df7f58a
Sean Molenaar <sean@m2mobi.com>
2017-09-27 19:18:01 +0200

    php72-gmagick: bottle rebuild

--- Formula/php72-gmagick.rb
+++ Formula/php72-gmagick.rb
@@ -8,6 +8,7 @@ class Php72Gmagick < AbstractPhp72Extension
   sha256 "4ead19640bebebfa68be41dc0097a93a7bf9beb56ea82b27343dba8ea4c5ecfa"
 
   bottle do
+    rebuild 1
     sha256 "68ca83d9c1cf2db81416445ad509c887ba8ba89429f37e1d9969757b2b52b086" => :sierra
     sha256 "77250e9b96c360e937847c15d3770b18565aad4f61665da7fdcc501d3a5982e5" => :el_capitan
     sha256 "c25954d583f24a7e977de4c096e16692d4abc95a31a06a58521eb4e7e5b81d5b" => :yosemite
