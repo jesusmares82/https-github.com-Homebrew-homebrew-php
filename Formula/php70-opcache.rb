26f455b26ba6781f4b6f4028c5d0635edd6dc6b3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-05 23:12:23 +0000

    php70-opcache: update 7.0.24_16 bottle.

--- Formula/php70-opcache.rb
+++ Formula/php70-opcache.rb
@@ -8,9 +8,9 @@ class Php70Opcache < AbstractPhp70Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1409c1bef5fffc7a671d44ab80ceac3fa2ef58b15b12b08f70acdc5671efb65d" => :high_sierra
-    sha256 "e8dbd61e548ae4ab434f5b0e18a278b605c497dc62944db895c68fc0082a70b7" => :sierra
-    sha256 "e2f36e401e09d7809a76f8614310981c430d728fd99cbd87dc485474bd8a4eed" => :el_capitan
+    sha256 "f39e1e79c973f7a393990de183fbc938e1885586db4899c7b450b25e1aa86184" => :high_sierra
+    sha256 "c52172fe5dde8d3a56418e442bb8f398e0ead430734a0ce5652b0b41754b2929" => :sierra
+    sha256 "275239c35dfff371a97c93225636d9d15118a88fbe7e6b90269938ff95c2e057" => :el_capitan
   end
 
   url PHP_SRC_TARBALL
