9bc2c6eb3efdebbe4e9c34ff59190f0b42bcd676
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-06 00:00:04 +0000

    php72-pspell: update 7.2.0RC3_4 bottle.

--- Formula/php72-pspell.rb
+++ Formula/php72-pspell.rb
@@ -7,9 +7,9 @@ class Php72Pspell < AbstractPhp72Extension
   revision 4
 
   bottle do
-    sha256 "deae766fe6b5d17fd57c4dc525719ffd3807fa51ee49206752e4fcfcfc9cce0f" => :sierra
-    sha256 "2c79ffdbc7063e659f5635e54d4ab768557e89371bd02d7d37bcb43b968b7c03" => :el_capitan
-    sha256 "c2f40489bd10f0c5c358e0dffee3096a209de8d71ba90121b580391e23afdcb4" => :yosemite
+    sha256 "19a7d121c33d585ced6406a922634437c3b954301e886eeecc5922cc3b4a89d5" => :high_sierra
+    sha256 "286a7310ba261b1b290f3d58ad812fc010d4a0ad291221e883d1d9ca3fad7f06" => :sierra
+    sha256 "7a57c09c888f396c0478b5dd0f72d3eb6f458c137a94e837e1a0d107512e98b9" => :el_capitan
   end
 
   url PHP_SRC_TARBALL
