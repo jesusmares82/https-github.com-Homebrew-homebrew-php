29b06b391d3885cd47e6920767a752b75d6ae17f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-10 10:01:32 +0000

    php55-ioncubeloader: update 10.0.3 bottle.

--- Formula/php55-ioncubeloader.rb
+++ Formula/php55-ioncubeloader.rb
@@ -10,9 +10,9 @@ class Php55Ioncubeloader < AbstractPhp55Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dbe57e3b95d21f3a6ba1aef4110a7ba09f3ef9dfa6b3e8b000f023dec694659e" => :sierra
-    sha256 "06595e8995a94b56c965dbd93d90b1f5d218e511a957ad1b045341e3f785747c" => :el_capitan
-    sha256 "a48aad687f50b131efd230c6f8a3162e42b610c91e20bffb6d7b02edad415b91" => :yosemite
+    sha256 "0d9ad65f1bb4cb74134b80505181e4be134db9977cc96d3087cd75e4d44b0304" => :high_sierra
+    sha256 "0d9ad65f1bb4cb74134b80505181e4be134db9977cc96d3087cd75e4d44b0304" => :sierra
+    sha256 "0d9ad65f1bb4cb74134b80505181e4be134db9977cc96d3087cd75e4d44b0304" => :el_capitan
   end
 
   def extension_type
