ffefb02baea9d55127e205a2388b9684d8ecb310
Sean Molenaar <sean@m2mobi.com>
2017-10-16 13:26:55 +0200

    phpunit@5.7 5.7.23
    Closes #4525.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/phpunit@5.7.rb
+++ Formula/phpunit@5.7.rb
@@ -4,8 +4,8 @@ class PhpunitAT57 < AbstractPhpPhar
   init
   desc "(Old stable) Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-5.7.22.phar"
-  sha256 "908fa85bbf085c0fce85af36da40cbaa075997c81f085704716e0a68b88c38c7"
+  url "https://phar.phpunit.de/phpunit-5.7.23.phar"
+  sha256 "a67bb9589bafa194206c8eb8f5b7a95b4b842ae0b4a2d538018a1761a13c0fbc"
 
   bottle do
     cellar :any_skip_relocation
