35664e04bfabbdc4dc58a823592b04f40e748177
Alexander Turiak <turyak@gmail.com>
2017-10-17 20:51:36 +0200

    n98-magerun2 2.0.0
    Closes #4534.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/n98-magerun2.rb
+++ Formula/n98-magerun2.rb
@@ -4,8 +4,8 @@ class N98Magerun2 < AbstractPhpPhar
   init
   desc "Swiss army knife for Magento 2 developers, sysadmins and devops."
   homepage "http://magerun.net/"
-  url "https://files.magerun.net/n98-magerun2-1.6.0.phar"
-  sha256 "3f185a4199d0137bc0961ed6eebe37b1c9363496101df56457ddfb591523ac6a"
+  url "https://files.magerun.net/n98-magerun2-2.0.0.phar"
+  sha256 "a79444b075f1b9e7ace216ad4299b1551a753826b1c006e7566607a48d552da8"
 
   bottle :unneeded
 
