ad909e5092ca4ad777bff5d987214c3f318c94f6
Florian Weber <fnwbr@users.noreply.github.com>
2017-10-21 23:53:50 +0200

    deployer 6.0.3
    Closes #4545.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -4,8 +4,8 @@ class Deployer < AbstractPhpPhar
   init
   desc "Deployment tool written in PHP with support for popular frameworks out of the box."
   homepage "https://deployer.org"
-  url "https://deployer.org/releases/v5.1.3/deployer.phar"
-  sha256 "06f7dc0479106e25b582bd9555ca597ddfa28c56d64df2965b6af484dc671686"
+  url "https://deployer.org/releases/v6.0.3/deployer.phar"
+  sha256 "8a0054535c922f7a4e10449c62f9d6be79f2cb34c2fe9137f78f9fff89b44970"
 
   bottle :unneeded
 
