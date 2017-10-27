3267b160fd71a02dc3ba98dd597ab0c10d698909
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-27 10:47:15 +0000

    php71-pcntl: update 7.1.11_17 bottle.

--- Formula/php71-pcntl.rb
+++ Formula/php71-pcntl.rb
@@ -8,9 +8,9 @@ class Php71Pcntl < AbstractPhp71Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "db908c37ee0c40ae3293d0ca4a0c95f633dec355ec14bbeb02b916b215b45844" => :high_sierra
-    sha256 "79bccc7e2384b9bcc4a646c4e439c8810354040b7686e1c43f2a031aee697b96" => :sierra
-    sha256 "d7d99205c8cd0ac66454b67108c9ed89db9b44416ed227250c6fa9e0c1e4e9ed" => :el_capitan
+    sha256 "2ac4d476204e940daff8a9a6f6614a32a81f0d34262c039767243caa4fae851b" => :high_sierra
+    sha256 "3ace6ecfde0c306558694e278168c97e28f7e8d1b2594060580475845781dcbe" => :sierra
+    sha256 "215e32b59000ac4ba29973f71edd9b5dc5e391f6a34f910aee7eb2cada420613" => :el_capitan
   end
 
   url PHP_SRC_TARBALL
