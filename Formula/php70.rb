c977c39a9b6d4db7dce733dd101546d9d5235313
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-27 12:41:12 +0000

    php70: update 7.0.25_17 bottle.

--- Formula/php70.rb
+++ Formula/php70.rb
@@ -5,9 +5,9 @@ class Php70 < AbstractPhp
   desc "PHP Version 7.0"
   revision 17
   bottle do
-    sha256 "07b6a904685c88d1c88a35996c831cb8372c5680c5730b0ff46ed9e440d2e887" => :high_sierra
-    sha256 "fd5bdd9b6bf88a6659230d9a85d4cc3f90d9d0144c3da4fb1c97444cbec2c50e" => :sierra
-    sha256 "86751709aef4498fc28c5f94e15386604dbd8f6776c3cf5e2d128fc3bd74d0f8" => :el_capitan
+    sha256 "5957857677dd6fc853586e9a025116de48adccbeb75c67369594effe47260154" => :high_sierra
+    sha256 "ff7768f76e916b25cfecf52217c559d41e423f0f7ad1bbb8482cf4d8ca0a97f7" => :sierra
+    sha256 "2b3567794310d6611e99ee2748ad1f079698b84b72e0b9d2ca5f8ecd239440ed" => :el_capitan
   end
 
   include AbstractPhpVersion::Php70Defs
