5f5f284047fa7965f048c0a9570071ac2f893828
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-05 08:37:03 +0000

    drush-launcher: add 0.4.3 bottle.

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -6,6 +6,13 @@ class DrushLauncher < AbstractPhpPhar
   url "https://github.com/drush-ops/drush-launcher/releases/download/0.4.3/drush.phar"
   sha256 "7da7bf2552da681a1a87788d4c4a34abdc0ace47bc9ddc395d8fc88480e13dde"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d08e7e85b3b70a369efba60d6cb8d101a78a63f2bbe2082c97ecadcb9c62ead3" => :high_sierra
+    sha256 "1e04852795c2dcd5866aafa63dc32018175f674dd6049fdb03975884fcb8137e" => :sierra
+    sha256 "1e04852795c2dcd5866aafa63dc32018175f674dd6049fdb03975884fcb8137e" => :el_capitan
+  end
+
   def install
     bin.install "drush.phar" => "drush"
   end
