1de08e50347a8297db24e5fb267339b241923376
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-12 15:54:11 +0000

    php54-molten: update 0.1.1_1 bottle.

--- Formula/php54-molten.rb
+++ Formula/php54-molten.rb
@@ -11,9 +11,9 @@ class Php54Molten < AbstractPhp54Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8d7b55bb2a72bc7f8142c2a95307b1545aa808aab8b61dc8acb19ee16ff2b376" => :high_sierra
-    sha256 "aa41ef2bb04aeebe521969c5f4b7f53dd36344226c0d3b82376438629b37e8e8" => :sierra
-    sha256 "af3261c834ba4b1092470b30e773f6496e7043ac2912334983e4162c63be0711" => :el_capitan
+    sha256 "bc1f72827f53c2f795f73f388de6f393e22df6cc7ab14b05664b37714ce3b402" => :high_sierra
+    sha256 "7a26c847f986a2cbccb9c4ecaace5408abd37980c3b5b33dc7930360aad5ca39" => :sierra
+    sha256 "4ca9d479177edadead009490db2afd448eb537968ff0ea662e08b56f422ab2ac" => :el_capitan
   end
 
   option "without-zipkin", "Disable zipkin headers"
