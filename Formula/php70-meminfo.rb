149eeeec67f8aa331db8dea33a86b5def5d8b7fe
Jérémy DECOOL <contact@jdecool.fr>
2017-11-17 20:12:47 +0100

    php70-meminfo 1.0.0 (new formula)

---
+++ Formula/php70-meminfo.rb
@@ -0,0 +1,24 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Meminfo < AbstractPhp70Extension
+  init
+  desc "PHP extension to get insight about memory usage"
+  homepage "https://github.com/BitOne/php-meminfo"
+  url "https://github.com/BitOne/php-meminfo.git",
+    :tag => "v1.0.0",
+    :revision => "0e4f884d02b9af4321d9b5121b017194047fb10e"
+  head "https://github.com/BitOne/php-meminfo.git"
+
+  def install
+    Dir.chdir "extension/php7" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install %w[modules/meminfo.so]
+    write_config_file if build.with? "config-file"
+  end
+end
