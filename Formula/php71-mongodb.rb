cc6112b76886ae07e6a55471b834a3418913f3f8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-22 06:42:58 +0000

    php71-mongodb: update 1.3.3 bottle.

--- Formula/php71-mongodb.rb
+++ Formula/php71-mongodb.rb
@@ -9,9 +9,9 @@ class Php71Mongodb < AbstractPhp71Extension
   head "https://github.com/mongodb/mongo-php-driver.git"
 
   bottle do
-    sha256 "7a899edf1e5a59ccd30980ffd507aad402f2f9c25109b2f51f1d952081516775" => :high_sierra
-    sha256 "2703180e362f2702825f6e7b72c885867588b1acd9474101f155b7e3c10d6407" => :sierra
-    sha256 "2b87b0171da93adb684b307ee81b3bee946ae6cd2a91dabf7283beb51716f086" => :el_capitan
+    sha256 "4bf978fa78cfd7e7bade17617d5a34457ae0492c515bb61ff8994bdd86b54067" => :high_sierra
+    sha256 "920a8b35b1eee199b4f7e102f8f33d4249bc183e13f3b4eca1d8c8f09b7af139" => :sierra
+    sha256 "fa881f70d7aa4c4ee258c90816ceccbf34539189b7d5aa530df23a8fb04999b9" => :el_capitan
   end
 
   depends_on "openssl"
