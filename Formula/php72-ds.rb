7729c4c0e476f6b52c2b0fc241db81224487402a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-15 14:56:33 +0000

    php72-ds: add 1.2.4 bottle.

--- Formula/php72-ds.rb
+++ Formula/php72-ds.rb
@@ -8,6 +8,13 @@ class Php72Ds < AbstractPhp72Extension
   sha256 "c080bb08445fe690da2271ff866602cf27cadee529ab1a1edbf4aa7a1d6e104c"
   head "https://github.com/php-ds/extension.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0789774de4f65ecc8ce40ab4abe0916ca2be66aa3bd4c9c718c313da8300a251" => :high_sierra
+    sha256 "35dfe329fe45b255687ca34ca84ed063e67792abff96cbcbc26f095f78711ee6" => :sierra
+    sha256 "1d7f711dcd2eee9e5e078ddef2736196c0a4df2eef7e7f399e170d8dc56e83fb" => :el_capitan
+  end
+
   def install
     safe_phpize
 
