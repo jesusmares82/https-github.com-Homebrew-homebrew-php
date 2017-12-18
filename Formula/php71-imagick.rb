e6bcc0ab94cba9c19f36e0bf367df17729894ec4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-18 08:53:17 +0000

    php71-imagick: update 3.4.3_6 bottle.

--- Formula/php71-imagick.rb
+++ Formula/php71-imagick.rb
@@ -10,9 +10,9 @@ class Php71Imagick < AbstractPhp71Extension
   revision 6
 
   bottle do
-    sha256 "136307c54b7d0056728d776da3b89ae5578bb5f0efe0987c5d4f382d4d7ffd91" => :high_sierra
-    sha256 "f3cbba491accfb067e6ff4febaf5d571db6e4f930be2d8555e70bc5332b06185" => :sierra
-    sha256 "846968a16e0c3c51352bc09f2c5deb96eec6fd5f79a9e52e8d5592d1f2bf8492" => :el_capitan
+    sha256 "79bd418f5c7b3b88a52cfbea6f85919bb566902afa776d971d052feefd4ac599" => :high_sierra
+    sha256 "c164b04b8da8896c67ac2090b83c073fc7adfbc9cce731b9e017a8aa89290487" => :sierra
+    sha256 "f29b7db081837f9ecdae549d80b0b6304a36355ca828de915fefa7951bbd2115" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
