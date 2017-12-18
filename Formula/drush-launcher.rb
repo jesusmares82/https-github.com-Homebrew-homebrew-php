49ab952769f41edc983af9e4ac30a94c54a22eac
Florian Weber <florian.weber@ueberbit.de>
2017-12-18 15:37:53 +0100

    drush-launcher 0.5.0
    Closes #4662.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -3,18 +3,17 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 class DrushLauncher < AbstractPhpPhar
   desc "A small wrapper around Drush for your global $PATH."
   homepage "https://github.com/drush-ops/drush-launcher"
-  url "https://github.com/drush-ops/drush-launcher/releases/download/0.4.3/drush.phar"
-  sha256 "7da7bf2552da681a1a87788d4c4a34abdc0ace47bc9ddc395d8fc88480e13dde"
+  url "https://github.com/drush-ops/drush-launcher/releases/download/0.5.0/drush.phar"
+  sha256 "ef8556bad7162a59a14e688e154e5b1564702c6a341522b06a1339b7d395cd18"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "d08e7e85b3b70a369efba60d6cb8d101a78a63f2bbe2082c97ecadcb9c62ead3" => :high_sierra
-    sha256 "1e04852795c2dcd5866aafa63dc32018175f674dd6049fdb03975884fcb8137e" => :sierra
-    sha256 "1e04852795c2dcd5866aafa63dc32018175f674dd6049fdb03975884fcb8137e" => :el_capitan
+  bottle :unneeded
+
+  def phar_file
+    "drush.phar"
   end
 
-  def install
-    bin.install "drush.phar" => "drush"
+  def phar_bin
+    "drush"
   end
 
   test do
