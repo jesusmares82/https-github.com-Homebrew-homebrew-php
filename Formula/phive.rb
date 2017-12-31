14dc00fd1eca6e81805ea50f495137541cd8a5a8
BePsvPT <freedom@csie.io>
2017-12-31 21:00:46 +0800

    phive 0.9.0
    Closes #4681.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/phive.rb
+++ Formula/phive.rb
@@ -4,8 +4,8 @@ class Phive < AbstractPhpPhar
   init
   desc "Installation and verification of phar distributed PHP applications"
   homepage "https://phar.io"
-  url "https://github.com/phar-io/phive/releases/download/0.8.2/phive-0.8.2.phar"
-  sha256 "de2f6034e78c33477af39da39b4f57cf6fee0d77d6ab69792db720879e2a25e7"
+  url "https://github.com/phar-io/phive/releases/download/0.9.0/phive-0.9.0.phar"
+  sha256 "99e8bb7fc7af21b5d37aabdd253ad4afec16800bc6cbb82d0786cf7ecf7ec1de"
   head "https://phar.io/releases/phive.phar"
 
   bottle :unneeded
