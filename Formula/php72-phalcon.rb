80d2a7c9c4f275e42474285f405b5e167620f742
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-05 15:21:10 +0000

    php72-phalcon: update 3.3.0 bottle.

--- Formula/php72-phalcon.rb
+++ Formula/php72-phalcon.rb
@@ -10,9 +10,9 @@ class Php72Phalcon < AbstractPhp72Extension
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "48e9b70209119372c328a62fd375997f4b1b18b4e6797851709fadad51921ab8" => :sierra
-    sha256 "c6d9fcec5a685b27ef30ed302b79ecb82a556d8fde9b87e96de00b110620e3a9" => :el_capitan
-    sha256 "cb2ba950e518fe5d3c0f3a79d02451a66f15dc512042a2907d8807f2b27e0e8e" => :yosemite
+    sha256 "168d9bf65b4efbecb6346e7cec60318225c3db09f4ec389e31fed2bef709bb46" => :high_sierra
+    sha256 "952104aab811c4b2cc740ed8800a6df158d5515cff4b62a0ecf34651e2a77213" => :sierra
+    sha256 "cf1fc06497db9b3c941e2fd9ba42b0d6a16e5915891bea8b4c5f88fb6cdd7b8c" => :el_capitan
   end
 
   depends_on "pcre"
