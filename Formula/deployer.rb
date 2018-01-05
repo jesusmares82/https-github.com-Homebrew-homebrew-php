bdc53cee224823396a9c8a8a0110feeacf0695a7
Fabio Cagliero <caglierofabio@gmail.com>
2018-01-05 12:11:04 +0100

    deployer 6.0.5
    Signed-off-by: Fabio Cagliero <caglierofabio@gmail.com>
    
    Closes #4695.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -4,8 +4,8 @@ class Deployer < AbstractPhpPhar
   init
   desc "Deployment tool written in PHP with support for popular frameworks out of the box."
   homepage "https://deployer.org"
-  url "https://deployer.org/releases/v6.0.3/deployer.phar"
-  sha256 "8a0054535c922f7a4e10449c62f9d6be79f2cb34c2fe9137f78f9fff89b44970"
+  url "https://deployer.org/releases/v6.0.5/deployer.phar"
+  sha256 "e2b1e61aac73dc4ab2f4abfbbc6065ab9af097041c8f0ac33ba263bfa9c52875"
 
   bottle :unneeded
 
