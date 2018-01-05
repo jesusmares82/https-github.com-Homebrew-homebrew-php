c75bd216a0f1ebab53c46c7d934b0751ec7455c7
Jan Viljanen <527069+javian@users.noreply.github.com>
2018-01-05 12:53:19 +0100

    php7{01}-libsodium@1.0 1.0.7
    Closes #4697.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/php70-libsodium@1.0.rb
+++ Formula/php70-libsodium@1.0.rb
@@ -4,8 +4,8 @@ class Php70LibsodiumAT10 < AbstractPhp70Extension
   init
   desc "Modern and easy-to-use crypto library"
   homepage "https://github.com/jedisct1/libsodium-php"
-  url "https://github.com/jedisct1/libsodium-php/archive/1.0.6.tar.gz"
-  sha256 "537944529e7c591e4bd6c73f37e926e538e8ff1f6384747c301436fb78269b9c"
+  url "https://github.com/jedisct1/libsodium-php/archive/1.0.7.tar.gz"
+  sha256 "b66c795fa39909eccbc4310e6e9700230e5ad9e8e9d7fcf79bb344dbf9d2f905"
   head "https://github.com/jedisct1/libsodium-php.git"
 
   bottle do
