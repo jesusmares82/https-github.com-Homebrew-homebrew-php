86e7976af248ab871b689cf3620569afcf37d43d
Mike McQuaid <mike@mikemcquaid.com>
2018-01-09 18:02:31 +0000

    php-meta-requirement: remove default_formula usage
    This will be deprecated in the near future in Homebrew/brew.
    
    Closes #4703.
    
    Signed-off-by: Jan Viljanen <jan.viljanen@greenpeace.org>

--- Requirements/php-meta-requirement.rb
+++ Requirements/php-meta-requirement.rb
@@ -5,24 +5,6 @@ class PhpMetaRequirement < HomebrewPhpRequirement
     $supported_php_versions = %w{php53 php54 php55 php56 php70 php71 php72}
   end
 
-  if Formula["php53"].linked_keg.exist? && $supported_php_versions.include?('php53')
-    default_formula "php53"
-  elsif Formula["php54"].linked_keg.exist? && $supported_php_versions.include?('php54')
-    default_formula "php54"
-  elsif Formula["php55"].linked_keg.exist? && $supported_php_versions.include?('php55')
-    default_formula "php55"
-  elsif Formula["php56"].linked_keg.exist? && $supported_php_versions.include?('php56')
-    default_formula "php56"
-  elsif Formula["php70"].linked_keg.exist? && $supported_php_versions.include?('php70')
-    default_formula "php70"
-  elsif Formula["php71"].linked_keg.exist? && $supported_php_versions.include?('php71')
-    default_formula "php71"
-  elsif Formula["php72"].linked_keg.exist? && $supported_php_versions.include?('php72')
-    default_formula "php72"
-  else
-    default_formula $supported_php_versions.last
-  end
-
   def satisfied?
     $supported_php_versions.any? do |php|
         f = Formula[php]
@@ -35,6 +17,10 @@ class PhpMetaRequirement < HomebrewPhpRequirement
   end
 
   def message
-    "Missing a core php install from homebrew-php. Please run: brew link php56"
+    <<-EOLTEXT.undent
+    Missing a core php install from homebrew-php. 
+    Please run: brew install php72
+    or install another php version from the homebrew-php tap
+    EOLTEXT
   end
 end
