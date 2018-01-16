297c5e1adf1e136de8d6d652a7dc07de78aa8ebd
Jan Viljanen <527069+javian@users.noreply.github.com>
2018-01-16 18:43:19 +0100

    php: update satisfy method for detecting php
    Closes #4712.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Requirements/php-meta-requirement.rb
+++ Requirements/php-meta-requirement.rb
@@ -1,20 +1,7 @@
 require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
 
 class PhpMetaRequirement < HomebrewPhpRequirement
-  if $supported_php_versions.nil?
-    $supported_php_versions = %w{php53 php54 php55 php56 php70 php71 php72}
-  end
-
-  def satisfied?
-    $supported_php_versions.any? do |php|
-        f = Formula[php]
-        if f.linked_keg.exist?
-          return true
-        end
-    end
-
-    return false
-  end
+  satisfy { which("php") }
 
   def message
     <<-EOLTEXT.undent
