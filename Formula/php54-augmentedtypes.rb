27e821f6838cc9eb8e0ea07b89fb18010b7819a4
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 08:45:30 +0000

    php54-augmentedtypes: use <<~EOS.

--- Formula/php54-augmentedtypes.rb
+++ Formula/php54-augmentedtypes.rb
@@ -38,7 +38,7 @@ class Php54Augmentedtypes < AbstractPhp54Extension
 
   def config_file
     if active_spec.build.with? "default-enforcement"
-      super + <<-EOS.undent
+      super + <<~EOS
         augmented_types.enforce_by_default = 1
       EOS
     else
