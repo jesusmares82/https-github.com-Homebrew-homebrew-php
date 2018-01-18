edc00b8881f7ad964216d516d542e4f199036409
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 08:45:30 +0000

    php54-igbinary: use <<~EOS.

--- Formula/php54-igbinary.rb
+++ Formula/php54-igbinary.rb
@@ -27,7 +27,7 @@ class Php54Igbinary < AbstractPhp54Extension
   end
 
   def config_file
-    super + <<-EOS.undent
+    super + <<~EOS
       ; Enable or disable compacting of duplicate strings
       ; The default is On.
       ;igbinary.compact_strings=On
