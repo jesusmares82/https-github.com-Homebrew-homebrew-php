6417dbe5bb3e855466a22144af51d6d7a5ca5476
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 08:45:29 +0000

    chphp: use <<~EOS.

--- Formula/chphp.rb
+++ Formula/chphp.rb
@@ -18,7 +18,7 @@ class Chphp < Formula
     system "make", "install", "PREFIX=#{prefix}"
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<~EOS
     Add the following to the ~/.bashrc or ~/.zshrc file:
 
       source #{opt_prefix}/share/chphp/chphp.sh
