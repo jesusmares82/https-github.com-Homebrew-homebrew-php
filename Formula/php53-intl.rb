4c48e8a0e9daa6054624b25d93b68125663c75ae
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 08:45:30 +0000

    php53-intl: use <<~EOS.

--- Formula/php53-intl.rb
+++ Formula/php53-intl.rb
@@ -35,7 +35,7 @@ class Php53Intl < AbstractPhp53Extension
   end
 
   def config_file
-    super + <<-EOS.undent
+    super + <<~EOS
 
       ;intl.default_locale =
       ; This directive allows you to produce PHP errors when some error
