4f680db428b6e029213fbf434893ede90f57fbae
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 08:45:31 +0000

    php56-redis: use <<~EOS.

--- Formula/php56-redis.rb
+++ Formula/php56-redis.rb
@@ -36,7 +36,7 @@ class Php56Redis < AbstractPhp56Extension
   end
 
   def config_file
-    super + <<-EOS.undent
+    super + <<~EOS
 
       ; phpredis can be used to store PHP sessions.
       ; To do this, uncomment and configure below
