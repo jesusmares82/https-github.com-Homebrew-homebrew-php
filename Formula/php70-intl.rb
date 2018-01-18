a8c31906b8449f2ca5ff9f3813881d63482c154d
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 08:45:32 +0000

    php70-intl: use <<~EOS.

--- Formula/php70-intl.rb
+++ Formula/php70-intl.rb
@@ -35,7 +35,7 @@ class Php70Intl < AbstractPhp70Extension
   end
 
   def config_file
-    super + <<-EOS.undent
+    super + <<~EOS
 
       ;intl.default_locale =
       ; This directive allows you to produce PHP errors when some error
