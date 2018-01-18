797d995e27798912e11eb08b43ff5866b4102ae2
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 08:45:32 +0000

    php72-memcached: use <<~EOS.

--- Formula/php72-memcached.rb
+++ Formula/php72-memcached.rb
@@ -44,7 +44,7 @@ class Php72Memcached < AbstractPhp72Extension
   def config_file
     # Use upsteam defaults (https://github.com/php-memcached-dev/php-memcached/blob/2.2.0/memcached.ini)
     # with default values pre-filled
-    super + <<-EOS.undent
+    super + <<~EOS
 
       ; Use session locking
       ; valid values: On, Off
