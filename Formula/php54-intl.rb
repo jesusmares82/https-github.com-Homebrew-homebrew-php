02d205657988ce1ca99070b62b441079600d4355
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 08:45:30 +0000

    php54-intl: use <<~EOS.

--- Formula/php54-intl.rb
+++ Formula/php54-intl.rb
@@ -35,7 +35,7 @@ class Php54Intl < AbstractPhp54Extension
   end
 
   def config_file
-    super + <<-EOS.undent
+    super + <<~EOS
 
       ;intl.default_locale =
       ; This directive allows you to produce PHP errors when some error
