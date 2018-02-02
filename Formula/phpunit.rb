f05fcb470194adf44c03abaaa3e054d6bb2c5666
Sean Molenaar <sean@m2mobi.com>
2018-02-02 09:17:00 +0100

    phpunit 7.0.1
    Closes #4747.
    
    Signed-off-by: Sean Molenaar <sean@m2mobi.com>

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -4,15 +4,10 @@ class Phpunit < AbstractPhpPhar
   init
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-6.5.5.phar"
-  sha256 "7b871b0726d40dc4d783c08f4051b400615b0d85f0a5e26c86b9a75be47c8da1"
+  url "https://phar.phpunit.de/phpunit-7.0.1.phar"
+  sha256 "6bd53faeff3fe5290de245863c221a80de3efd58a502988fa167d679f99f0a1d"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "b2e8438ce37c5494cd09067f01624aa2d1528876fdb59243fa0ae5838b26be02" => :high_sierra
-    sha256 "b2e8438ce37c5494cd09067f01624aa2d1528876fdb59243fa0ae5838b26be02" => :sierra
-    sha256 "b2e8438ce37c5494cd09067f01624aa2d1528876fdb59243fa0ae5838b26be02" => :el_capitan
-  end
+  bottle :unneeded
 
   def phar_file
     "phpunit-#{version}.phar"
