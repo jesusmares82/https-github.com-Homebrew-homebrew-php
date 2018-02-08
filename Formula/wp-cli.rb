32d8ec5add725d5d7f3fe4470c4d45110ac7d1a8
Chris Wilcoxson <chris@slushman.com>
2018-02-08 10:41:17 -0600

    wp-cli 1.50
    Closes #4758.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
 class WpCli < AbstractPhpPhar
   desc "Command-line tools for managing WordPress installations."
   homepage "https://wp-cli.org"
-  url "https://github.com/wp-cli/wp-cli/releases/download/v1.4.1/wp-cli-1.4.1.phar"
-  sha256 "325924cf161856f9478f2a154572698ecb5d1054e620843b9c43a7baf4e5ce3b"
+  url "https://github.com/wp-cli/wp-cli/releases/download/v1.5.0/wp-cli-1.5.0.phar"
+  sha256 "f615d57957e66a09f57acc844a1fc5402e9fa581dcb387bbe1affc4d155baf9d"
   head "https://github.com/wp-cli/wp-cli.git"
 
   bottle do
