48b11cfc8804d4734143a5fab9d558fa0a89b628
ilovezfs <ilovezfs@icloud.com>
2018-03-04 23:13:44 -0800

    libmustache 0.4.4
    Closes #4802.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libmustache.rb
+++ Formula/libmustache.rb
@@ -1,9 +1,9 @@
 class Libmustache < Formula
-  desc "C++ implementation of Mustache."
+  desc "C++ implementation of Mustache"
   homepage "https://github.com/jbboehr/libmustache"
   url "https://github.com/jbboehr/libmustache.git",
-      :tag => "v0.4.3",
-      :revision => "081952948f6e50f0eeb116fd652585813f31a2ab"
+      :tag => "v0.4.4",
+      :revision => "f9b9d977a2804a0cf8d849e2fd50e1516289a6e7"
 
   bottle do
     cellar :any
